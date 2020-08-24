73e1820aa872fb6830565cde7086454a638c9b23
Chef Nomi <defichefnomi@gmail.com>
2020-08-24 20:11:23 +0700

    Remove trailing spaces

--- contracts/MockERC20.sol
+++ contracts/MockERC20.sol
@@ -6,8 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
     constructor(
-        string memory name, 
-        string memory symbol, 
+        string memory name,
+        string memory symbol,
         uint256 supply
     ) public ERC20(name, symbol) {
         _mint(msg.sender, supply);
