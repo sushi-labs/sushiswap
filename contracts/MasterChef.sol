4c8445af0c5b77055bcf4190c79f4a9956a8b3df
Chef Nomi <chefnomi@pop-os.localdomain>
2020-08-24 08:47:22 +0700

    Add governance and timelock

--- contracts/MasterChef.sol
+++ contracts/MasterChef.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./SushiToken.sol";
 
 
@@ -12,13 +13,13 @@ interface Migrator {
     // Perform LP token migration from legacy UniswapV2 to SushiSwap.
     // Take ERC20 token address and return the new LP token address.
     // Migrator should have full access to the caller's LP token. 
-    // Return the new LP token address.
+    // Return the new LP token address and new `howmany`.
     //
     // XXX Migrator must have allowance access to UniswapV2 LP tokens.
     // SushiSwap must mint EXACTLY the same amount of SushiSwap LP tokens or
     // else something bad will happen. Traditional UniswapV2 does not
     // do that so be careful!
-    function migrate(IERC20 token) external returns (IERC20);
+    function migrate(IERC20 token) external returns (IERC20, uint256);
 }
 
 // MasterChef is the master of Sushi. He can make Sushi and he is a fair guy.
@@ -53,7 +54,7 @@ contract MasterChef is Ownable {
         // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
         //   1. The pool's `accSushiPerShare` (and `lastRewardBlock`) gets updated.
         //
-        //   accSushiPerShare += howmany(+bonus?) * (block.number - lastRewardBlock) / pool.lpToken.balanceOf(this)
+        //   accSushiPerShare += howmany * (block.number - lastRewardBlock) / pool.lpToken.balanceOf(this)
         //   lastRewardBlock = block.number
         //
         //   2. User receives the pending reward sent to his/her address.
@@ -65,40 +66,49 @@ contract MasterChef is Ownable {
 
     // The SUSHI TOKEN!
     SushiToken public sushi;
-    // List of ERC20 tokens available for cooking.
-    IERC20[] public tokens;
     // Dev share ratio, multiplied by the usual 1e18.
     uint256 public devshare = 0.1 * 1e18;
     // Dev address.
     address public devaddr;
-    // The bonus multiplier before pool migration is complete. 
-    uint256 public bonusMultiplier = 10;
-    // Whether the migration has happened.
-    bool public migrated;
 
     // Info of each user that stakes LP tokens.
     mapping (IERC20 => mapping (address => UserInfo)) public userInfo;
     // Info of each pool.
     mapping (IERC20 => PoolInfo) public poolInfo;
 
-    constructor(
-        SushiToken _sushi,
-        IERC20[] memory _tokens, 
-        uint256[] memory _howmany,
-        IERC20[] memory _lpTokens,
-        uint256 startRewardBlock
-    ) public {
+    constructor(SushiToken _sushi, address _devaddr) public {
         sushi = _sushi;
-        for (uint256 idx = 0; idx < _tokens.length; ++idx) {
-            tokens.push(_tokens[idx]);
-            poolInfo[_tokens[idx]] = PoolInfo({
-                lpToken: _lpTokens[idx],
-                howmany: _howmany[idx],
-                lastRewardBlock: startRewardBlock,
-                accSushiPerShare: 0
-            });
-        }
-        devaddr = msg.sender;
+        devaddr = _devaddr;
+    }
+
+    // Add a new token + lp to the pool. Can only be called by the owner.
+    function add(IERC20 _token, uint256 _howmany, IERC20 _lpToken, uint256 rewardAt) public onlyOwner {
+        require(poolInfo[_token].lpToken == IERC20(address(0)), "add: already there");
+        poolInfo[_token] = PoolInfo({
+            lpToken: _lpToken,
+            howmany: _howmany,
+            lastRewardBlock: rewardAt,
+            accSushiPerShare: 0
+        });
+    }
+
+    // Update the given pool's SUSHI per block. Can only be called by the owner.
+    function set(IERC20 _token, uint256 _howmany) public onlyOwner {
+        require(poolInfo[_token].lpToken != IERC20(address(0)), "set: not there");
+        poolInfo[_token].howmany = _howmany;
+    }
+
+    // Migrate lp token to another lp contract. Can only be called by the owner.
+    function migrate(IERC20 _token, Migrator _migrator) public onlyOwner {
+        PoolInfo storage pool = poolInfo[_token];
+        IERC20 lpToken = pool.lpToken;
+        require(lpToken != IERC20(address(0)), "migrate: wut?");
+        uint256 bal = lpToken.balanceOf(address(this));
+        lpToken.safeApprove(address(_migrator), bal);
+        (IERC20 newLpToken, uint256 newHowmany) = _migrator.migrate(lpToken);
+        require(bal == newLpToken.balanceOf(address(this)), "migrate: bad");
+        pool.lpToken = newLpToken;
+        pool.howmany = newHowmany;
     }
 
     // Update reward variables of the given pool to be up-to-date.
@@ -108,9 +118,6 @@ contract MasterChef is Ownable {
         }
         uint256 blockCount = _pool.lastRewardBlock.sub(block.number);
         uint256 sushiReward = blockCount.mul(_pool.howmany);
-        if (!migrated) {
-            sushiReward = sushiReward.mul(bonusMultiplier);
-        }
         sushi.mint(devaddr, sushiReward.mul(devshare).div(1e18));
         sushi.mint(address(this), sushiReward);
         _pool.accSushiPerShare = _pool.accSushiPerShare.add(
@@ -134,7 +141,7 @@ contract MasterChef is Ownable {
         user.rewardDebt = user.amount.mul(pool.accSushiPerShare).div(1e18);
     }
 
-    // Withdraw LP tokens from MasterChel.
+    // Withdraw LP tokens from MasterChef.
     function withdraw(IERC20 _token, uint256 _amount) public {
         PoolInfo storage pool = poolInfo[_token];
         UserInfo storage user = userInfo[_token][msg.sender];
@@ -143,8 +150,24 @@ contract MasterChef is Ownable {
         updatePool(pool);
         uint256 pending = user.amount.mul(pool.accSushiPerShare).div(1e18).sub(user.rewardDebt);
         sushi.transfer(msg.sender, pending);
-        pool.lpToken.safeTransfer(address(msg.sender), _amount);
         user.amount = user.amount.sub(_amount);
         user.rewardDebt = user.amount.mul(pool.accSushiPerShare).div(1e18);
+        pool.lpToken.safeTransfer(address(msg.sender), _amount);
+    }
+
+    // Withdraw without caring about rewards. EMERGENCY ONLY.
+    function emergencyWithdraw(IERC20 _token) public {
+        PoolInfo storage pool = poolInfo[_token];
+        UserInfo storage user = userInfo[_token][msg.sender];
+        require(pool.lpToken != IERC20(address(0)), "emergencyWithdraw: wut?");
+        user.amount = 0;
+        user.rewardDebt = 0;
+        pool.lpToken.safeTransfer(address(msg.sender), user.amount);
+    }
+
+    // Update dev address by the previous dev.
+    function dev(address _devaddr) public {
+        require(msg.sender == devaddr, "dev: wut?");
+        devaddr = _devaddr;
     }
 }
