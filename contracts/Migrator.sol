f48fd7b0e819db2624e8071ebff2f3a0fc48106d
Chef Nomi <defichefnomi@gmail.com>
2020-08-26 11:28:50 +0700

    First pass SushiMaker contract

--- contracts/Migrator.sol
+++ contracts/Migrator.sol
@@ -1,37 +1,36 @@
 pragma solidity 0.6.12;
 
-import "./uniswapv2/interfaces/IUniswapV2ERC20.sol";
 import "./uniswapv2/interfaces/IUniswapV2Pair.sol";
 import "./uniswapv2/interfaces/IUniswapV2Factory.sol";
 
 
 contract Migrator {
     address public chef;
-    address public factory;
+    IUniswapV2Factory public factory;
     uint256 public notBeforeBlock;
     uint256 public desiredLiquidity = uint256(-1);
 
-    constructor(address _chef, address _factory, uint256 _notBeforeBlock) public {
+    constructor(address _chef, IUniswapV2Factory _factory, uint256 _notBeforeBlock) public {
         chef = _chef;
         factory = _factory;
         notBeforeBlock = _notBeforeBlock;
     }
 
-    function migrate(address orig) public returns (address) {
+    function migrate(IUniswapV2Pair orig) public returns (IUniswapV2Pair) {
         require(msg.sender == chef, "not the chef");
         require(block.number >= notBeforeBlock, "too early to migrate");
-        address token0 = IUniswapV2Pair(orig).token0();
-        address token1 = IUniswapV2Pair(orig).token1();
-        address pair = IUniswapV2Factory(factory).getPair(token0, token1);
-        if (pair == address(0)) {
-            pair = IUniswapV2Factory(factory).createPair(token0, token1);
+        address token0 = orig.token0();
+        address token1 = orig.token1();
+        IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(token0, token1));
+        if (pair == IUniswapV2Pair(address(0))) {
+            pair = IUniswapV2Pair(factory.createPair(token0, token1));
         }
-        uint256 lp = IUniswapV2ERC20(orig).balanceOf(msg.sender);
+        uint256 lp = orig.balanceOf(msg.sender);
         if (lp == 0) return pair;
         desiredLiquidity = lp;
-        IUniswapV2ERC20(orig).transferFrom(msg.sender, orig, lp);
-        IUniswapV2Pair(orig).burn(pair);
-        IUniswapV2Pair(pair).mint(msg.sender);
+        orig.transferFrom(msg.sender, address(orig), lp);
+        orig.burn(address(pair));
+        pair.mint(msg.sender);
         desiredLiquidity = uint256(-1);
         return pair;
     }
---
+++ contracts/SushiMaker.sol
@@ -0,0 +1,64 @@
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "./uniswapv2/interfaces/IUniswapV2ERC20.sol";
+import "./uniswapv2/interfaces/IUniswapV2Pair.sol";
+import "./uniswapv2/interfaces/IUniswapV2Factory.sol";
+
+
+contract SushiMaker {
+    using SafeMath for uint256;
+
+    IUniswapV2Factory public factory;
+    address public bar;
+    address public sushi;
+    address public weth;
+
+    constructor(IUniswapV2Factory _factory, address _bar, address _sushi, address _weth) public {
+        factory = _factory;
+        sushi = _sushi;
+        bar = _bar;
+        weth = _weth;
+    }
+
+    function convert(address token0, address token1) public {
+        // At least we try to make front-running harder to do.
+        require(!Address.isContract(msg.sender), "do not convert from contract");
+        IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(token0, token1));
+        pair.transfer(address(pair), pair.balanceOf(address(this)));
+        pair.burn(address(this));
+        uint256 wethAmount = _toWETH(token0) + _toWETH(token1);
+        _toSUSHI(wethAmount);
+    }
+
+    function _toWETH(address token) internal returns (uint256) {
+        if (token == sushi || token == weth) return 0; // No action needed
+        IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(token, weth));
+        (uint reserve0, uint reserve1,) = pair.getReserves();
+        address token0 = pair.token0();
+        (uint reserveIn, uint reserveOut) = token0 == token ? (reserve0, reserve1) : (reserve1, reserve0);
+        uint amountIn = IERC20(token).balanceOf(address(this));
+        uint amountInWithFee = amountIn.mul(997);
+        uint numerator = amountInWithFee.mul(reserveOut);
+        uint denominator = reserveIn.mul(1000).add(amountInWithFee);
+        uint amountOut = numerator / denominator;
+        (uint amount0Out, uint amount1Out) = token0 == token ? (uint(0), amountOut) : (amountOut, uint(0));
+        pair.swap(amount0Out, amount1Out, factory.getPair(weth, sushi), new bytes(0));
+        return amountOut;
+    }
+
+    function _toSUSHI(uint256 amountIn) internal {
+        IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(weth, sushi));
+        (uint reserve0, uint reserve1,) = pair.getReserves();
+        address token0 = pair.token0();
+        (uint reserveIn, uint reserveOut) = token0 == weth ? (reserve0, reserve1) : (reserve1, reserve0);
+        uint amountInWithFee = amountIn.mul(997);
+        uint numerator = amountInWithFee.mul(reserveOut);
+        uint denominator = reserveIn.mul(1000).add(amountInWithFee);
+        uint amountOut = numerator / denominator;
+        (uint amount0Out, uint amount1Out) = token0 == weth ? (uint(0), amountOut) : (amountOut, uint(0));
+        pair.swap(amount0Out, amount1Out, factory.getPair(weth, sushi), new bytes(0));
+    }
+}
