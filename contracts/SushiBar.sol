5a4fa05ccf9f2f389155837488c05f58a2199d29
Chef Nomi <defichefnomi@gmail.com>
2020-08-26 14:15:28 +0700

    Make SushiBar ERC20 compat and add tests

--- contracts/SushiBar.sol
+++ contracts/SushiBar.sol
@@ -1,15 +1,13 @@
 pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 
-contract SushiBar {
+contract SushiBar is ERC20("SushiBar", "xSUSHI"){
     using SafeMath for uint256;
-
     IERC20 public sushi;
-    mapping (address => uint256) public shares;
-    uint256 public totalShares;
 
     constructor(IERC20 _sushi) public {
         sushi = _sushi;
@@ -18,22 +16,21 @@ contract SushiBar {
     // Enter the bar. Pay some SUSHIs. Earn some shares.
     function enter(uint256 _amount) public {
         uint256 totalSushi = sushi.balanceOf(address(this));
+        uint256 totalShares = totalSupply();
         if (totalShares == 0 || totalSushi == 0) {
-            shares[msg.sender] = _amount;
-            totalShares = _amount;
+            _mint(msg.sender, _amount);
         } else {
             uint256 what = _amount.mul(totalShares).div(totalSushi);
-            shares[msg.sender] = shares[msg.sender].add(what);
-            totalShares = totalShares.add(what);
+            _mint(msg.sender, what);
         }
         sushi.transferFrom(msg.sender, address(this), _amount);
     }
 
     // Leave the bar. Claim back your SUSHIs.
     function leave(uint256 _share) public {
+        uint256 totalShares = totalSupply();
         uint256 what = _share.mul(sushi.balanceOf(address(this))).div(totalShares);
-        shares[msg.sender] = shares[msg.sender].sub(_share);
-        totalShares = totalShares.sub(_share);
+        _burn(msg.sender, _share);
         sushi.transfer(msg.sender, what);
     }
 }
