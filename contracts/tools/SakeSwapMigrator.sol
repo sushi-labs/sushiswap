92ef6b5a3293df5f6ad5ddb0ee0041ee1c94f40d
therealsakeswap <sakeswap@protonmail.com>
2020-09-29 10:03:36 +0800

    add a tool to migrate to sakeswap from uniswap/sushiswap

---
+++ contracts/tools/SakeSwapMigrator.sol
@@ -0,0 +1,151 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import "../sakeswap/interfaces/ISakeSwapERC20.sol";
+
+interface IRouter {
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 amountADesired,
+        uint256 amountBDesired,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountA, uint256 amountB, uint256 liquidity);
+
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 liquidity,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountA, uint256 amountB);
+}
+
+interface IFactory {
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
+}
+
+// Migrate from SUSHISWAP/UNISWAP to SAKESWAP
+contract SakeSwapMigrator {
+    IFactory public uniFactory = IFactory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
+    IRouter public uniRouter = IRouter(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+    IFactory public sushiFactory = IFactory(0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac);
+    IRouter public sushiRouter = IRouter(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F);
+    IRouter public sakeRouter = IRouter(0x9C578b573EdE001b95d51a55A3FAfb45f5608b1f);
+
+    // constructor(
+    //     address _uniFactory, 
+    //     address _uniRouter, 
+    //     address _sushiFactory, 
+    //     address _sushiRouter, 
+    //     address _sakeRouter
+    // ) public {
+    //     uniFactory = IFactory(_uniFactory);
+    //     uniRouter = IRouter(_uniRouter);
+    //     sushiFactory = IFactory(_sushiFactory);
+    //     sushiRouter = IRouter(_sushiRouter);
+    //     sakeRouter = IRouter(_sakeRouter);
+    // }
+
+    function migrateUniswapWithPermit(
+        address token0,
+        address token1,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public {
+        address pair = uniFactory.getPair(token0, token1);
+
+        // Permit
+        ISakeSwapERC20(pair).permit(
+            msg.sender,
+            address(this),
+            value,
+            deadline,
+            v,
+            r,
+            s
+        );
+
+        _migrate(uniRouter, ISakeSwapERC20(pair), token0, token1, value);
+    }
+
+    function migrateSushiSwapWithPermit(
+        address token0,
+        address token1,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public {
+        address pair = sushiFactory.getPair(token0, token1);
+
+        // Permit
+        ISakeSwapERC20(pair).permit(
+            msg.sender,
+            address(this),
+            value,
+            deadline,
+            v,
+            r,
+            s
+        );
+
+        _migrate(sushiRouter, ISakeSwapERC20(pair), token0, token1, value);
+    }
+
+    function migrateUniswap(address token0, address token1, uint256 value) public {
+        address pair = uniFactory.getPair(token0, token1);
+        _migrate(uniRouter, ISakeSwapERC20(pair), token0, token1, value);
+    }
+
+    function migrateSushiSwap(address token0, address token1, uint256 value) public {
+        address pair = sushiFactory.getPair(token0, token1);
+        _migrate(sushiRouter, ISakeSwapERC20(pair), token0, token1, value);
+    }
+
+    function _migrate(IRouter router, ISakeSwapERC20 pair, address token0, address token1, uint256 value) internal {
+        // Removes liquidity
+        pair.transferFrom(msg.sender, address(this), value);
+        pair.approve(address(router), value);
+        router.removeLiquidity(
+            token0,
+            token1,
+            value,
+            0,
+            0,
+            address(this),
+            now + 60
+        );
+
+        // Adds liquidity to SakeSwap
+        uint256 bal0 = ISakeSwapERC20(token0).balanceOf(address(this));
+        uint256 bal1 = ISakeSwapERC20(token1).balanceOf(address(this));
+        ISakeSwapERC20(token0).approve(address(sakeRouter), bal0);
+        ISakeSwapERC20(token1).approve(address(sakeRouter), bal1);
+        sakeRouter.addLiquidity(
+            token0,
+            token1,
+            bal0,
+            bal1,
+            0,
+            0,
+            msg.sender,
+            now + 60
+        );
+
+        // Refund sender any remaining tokens
+        uint256 remainBal0 = ISakeSwapERC20(token0).balanceOf(address(this));
+        uint256 remainBal1 = ISakeSwapERC20(token1).balanceOf(address(this));
+        if (remainBal0 > 0) ISakeSwapERC20(token0).transfer(msg.sender, remainBal0);
+        if (remainBal1 > 1) ISakeSwapERC20(token1).transfer(msg.sender, remainBal1);
+    }
+}
---
+++ test/SakeSwapMigrator.test.js
@@ -0,0 +1,153 @@
+const { expectRevert } = require('@openzeppelin/test-helpers');
+const { web3 } = require('@openzeppelin/test-helpers/src/setup');
+const MockERC20 = artifacts.require('MockERC20');
+const SakeSwapRouter = artifacts.require('SakeSwapRouter');
+const SakeSwapFactory = artifacts.require('SakeSwapFactory');
+const SakeSwapPair = artifacts.require('SakeSwapPair');
+const Weth = artifacts.require('WETH9');
+const SakeSwapMigrator = artifacts.require('SakeSwapMigrator');
+const { keccak256, defaultAbiCoder, solidityPack } = require("ethers/utils");
+const util = require("ethereumjs-util");
+
+contract('SakeSwapMigrator', ([alice, bar, minter]) => {
+    beforeEach(async () => {
+        this.weth = await Weth.new();
+        this.uniFactory = await SakeSwapFactory.new(alice);
+        this.uniRouter = await SakeSwapRouter.new(this.uniFactory.address, this.weth.address)
+        this.sushiFactory = await SakeSwapFactory.new(alice);
+        this.sushiRouter = await SakeSwapRouter.new(this.sushiFactory.address, this.weth.address);
+        this.sakeFactory = await SakeSwapFactory.new(alice);
+        this.sakeRouter = await SakeSwapRouter.new(this.sakeFactory.address, this.weth.address);
+        this.token1 = await MockERC20.new('TOKEN1', 'TOKEN', '100000000');
+        this.token2 = await MockERC20.new('TOKEN2', 'TOKEN2', '100000000');
+        this.uniPair = await SakeSwapPair.at((await this.uniFactory.createPair(this.token1.address, this.token2.address)).logs[0].args.pair);
+        this.sushiPair = await SakeSwapPair.at((await this.sushiFactory.createPair(this.token1.address, this.token2.address)).logs[0].args.pair);
+        this.sakePair = await SakeSwapPair.at((await this.sakeFactory.createPair(this.token1.address, this.token2.address)).logs[0].args.pair);
+        this.sakeSwapMigrator = await SakeSwapMigrator.new(this.uniFactory.address, this.uniRouter.address, this.sushiFactory.address,
+            this.sushiRouter.address, this.sakeRouter.address);
+        this.bob = await web3.eth.accounts.privateKeyToAccount('f26d2c81b56dfa568219bf842a6402f449501ed80719eac17792191cadcd707f');
+    });
+
+    function getApprovalDigest(that, owner, spender, value, nonce, deadline) {
+        return keccak256(
+            solidityPack(
+                ["bytes1", "bytes1", "bytes32", "bytes32"],
+                [
+                    "0x19",
+                    "0x01",
+                    that.DOMAIN_SEPARATOR,
+                    keccak256(
+                        defaultAbiCoder.encode(
+                            ["bytes32", "address", "address", "uint256", "uint256", "uint256"],
+                            [that.PERMIT_TYPEHASH, owner, spender, value, nonce, deadline]
+                        )
+                    ),
+                ]
+            )
+        );
+    }
+
+    it('should migrate from uniswap successfully', async () => {
+        await this.token1.approve(this.uniRouter.address, '10000');
+        await this.token2.approve(this.uniRouter.address, '10000');
+        await this.uniRouter.addLiquidity(this.token1.address, this.token2.address, '10000', '10000', '0', '0', bar, '10000000000000');
+        const uniLpAmount = await this.uniPair.balanceOf(bar);
+        assert.equal(uniLpAmount.valueOf(), '9000');
+
+        await this.token1.approve(this.sakeRouter.address, '10000');
+        await this.token2.approve(this.sakeRouter.address, '10000');
+        await this.sakeRouter.addLiquidity(this.token1.address, this.token2.address, '10000', '10000', '0', '0', bar, '10000000000000');
+        const sakeLpAmount = await this.sakePair.balanceOf(bar);
+        assert.equal(sakeLpAmount.valueOf(), '9000');
+
+        await this.uniPair.approve(this.sakeSwapMigrator.address, '9000', { from: bar });
+        await this.sakeSwapMigrator.migrateUniswap(this.token1.address, this.token2.address, '9000', { from: bar });
+        assert.equal((await this.uniPair.balanceOf(bar)).valueOf(), '0');
+        assert.equal((await this.sakePair.balanceOf(bar)).valueOf(), '18000');
+    });
+
+    it('should migrate from sushiswap successfully', async () => {
+        await this.token1.approve(this.sushiRouter.address, '10000');
+        await this.token2.approve(this.sushiRouter.address, '10000');
+        await this.sushiRouter.addLiquidity(this.token1.address, this.token2.address, '10000', '10000', '0', '0', bar, '10000000000000');
+        const sushiLpAmount = await this.sushiPair.balanceOf(bar);
+        assert.equal(sushiLpAmount.valueOf(), '9000');
+
+        await this.token1.approve(this.sakeRouter.address, '10000');
+        await this.token2.approve(this.sakeRouter.address, '10000');
+        await this.sakeRouter.addLiquidity(this.token1.address, this.token2.address, '10000', '10000', '0', '0', bar, '10000000000000');
+        const sakeLpAmount = await this.sakePair.balanceOf(bar);
+        assert.equal(sakeLpAmount.valueOf(), '9000');
+
+        await this.sushiPair.approve(this.sakeSwapMigrator.address, '9000', { from: bar });
+        await this.sakeSwapMigrator.migrateSushiSwap(this.token1.address, this.token2.address, '9000', { from: bar });
+        assert.equal((await this.sushiPair.balanceOf(bar)).valueOf(), '0');
+        assert.equal((await this.sakePair.balanceOf(bar)).valueOf(), '18000');
+    });
+
+    it('should migrate from uniswap with permit successfully', async () => {
+        await web3.eth.personal.importRawKey(this.bob.privateKey, "");
+        await web3.eth.personal.unlockAccount(this.bob.address, "", 60000);
+        await web3.eth.sendTransaction({
+            from: alice,
+            to: this.bob.address,
+            value: '1000000000000000000'
+        });
+
+        await this.token1.approve(this.uniRouter.address, '10000');
+        await this.token2.approve(this.uniRouter.address, '10000');
+        await this.uniRouter.addLiquidity(this.token1.address, this.token2.address, '10000', '10000', '0', '0', this.bob.address, '10000000000000');
+        const uniLpAmount = await this.uniPair.balanceOf(this.bob.address);
+        assert.equal(uniLpAmount.valueOf(), '9000');
+
+        await this.token1.approve(this.sakeRouter.address, '10000');
+        await this.token2.approve(this.sakeRouter.address, '10000');
+        await this.sakeRouter.addLiquidity(this.token1.address, this.token2.address, '10000', '10000', '0', '0', this.bob.address, '10000000000000');
+        const sakeLpAmount = await this.sakePair.balanceOf(this.bob.address);
+        assert.equal(sakeLpAmount.valueOf(), '9000');
+
+        this.DOMAIN_SEPARATOR = (await this.uniPair.DOMAIN_SEPARATOR()).toString();
+        this.PERMIT_TYPEHASH = (await this.uniPair.PERMIT_TYPEHASH()).toString();
+
+        const digest = getApprovalDigest(this, this.bob.address, this.sakeSwapMigrator.address, 9000, 0, 10000000000000);
+        const priKeyBuf = util.toBuffer(this.bob.privateKey);
+        const { v, r, s } = util.ecsign(Buffer.from(digest.slice(2), "hex"), priKeyBuf);
+        await this.sakeSwapMigrator.migrateUniswapWithPermit(this.token1.address, this.token2.address,
+            '9000', '10000000000000', v, r, s, { from: this.bob.address });
+        assert.equal((await this.uniPair.balanceOf(this.bob.address)).valueOf(), '0');
+        assert.equal((await this.sakePair.balanceOf(this.bob.address)).valueOf(), '18000');
+    });
+
+    it('should migrate from sushiswap with permit successfully', async () => {
+        await web3.eth.personal.importRawKey(this.bob.privateKey, "");
+        await web3.eth.personal.unlockAccount(this.bob.address, "", 60000);
+        await web3.eth.sendTransaction({
+            from: alice,
+            to: this.bob.address,
+            value: '1000000000000000000'
+        });
+
+        await this.token1.approve(this.sushiRouter.address, '10000');
+        await this.token2.approve(this.sushiRouter.address, '10000');
+        await this.sushiRouter.addLiquidity(this.token1.address, this.token2.address, '10000', '10000', '0', '0', this.bob.address, '10000000000000');
+        const sushiLpAmount = await this.sushiPair.balanceOf(this.bob.address);
+        assert.equal(sushiLpAmount.valueOf(), '9000');
+
+        await this.token1.approve(this.sakeRouter.address, '10000');
+        await this.token2.approve(this.sakeRouter.address, '10000');
+        await this.sakeRouter.addLiquidity(this.token1.address, this.token2.address, '10000', '10000', '0', '0', this.bob.address, '10000000000000');
+        const sakeLpAmount = await this.sakePair.balanceOf(this.bob.address);
+        assert.equal(sakeLpAmount.valueOf(), '9000');
+
+        this.DOMAIN_SEPARATOR = (await this.sushiPair.DOMAIN_SEPARATOR()).toString();
+        this.PERMIT_TYPEHASH = (await this.sushiPair.PERMIT_TYPEHASH()).toString();
+
+        const digest = getApprovalDigest(this, this.bob.address, this.sakeSwapMigrator.address, 9000, 0, 10000000000000);
+        const priKeyBuf = util.toBuffer(this.bob.privateKey);
+        const { v, r, s } = util.ecsign(Buffer.from(digest.slice(2), "hex"), priKeyBuf);
+        await this.sakeSwapMigrator.migrateSushiSwapWithPermit(this.token1.address, this.token2.address,
+            '9000', '10000000000000', v, r, s, { from: this.bob.address });
+        assert.equal((await this.sushiPair.balanceOf(this.bob.address)).valueOf(), '0');
+        assert.equal((await this.sakePair.balanceOf(this.bob.address)).valueOf(), '18000');
+    });
+})
