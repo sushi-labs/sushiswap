ee05c789b7c9008bb9fba1a726535ced83bd4043
BoringCrypto <b@rtje.net>
2020-11-18 15:32:03 +0900

    Support for bonuses

--- contracts/SushiBarV2.sol
+++ contracts/SushiBarV2.sol
@@ -8,6 +8,44 @@ library BoringMath {
     function mul(uint a, uint b) internal pure returns (uint c) {require(a == 0 || (c = a * b)/b == a, "BoringMath: Mul Overflow");}
 }
 
+// Source: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+// Edited by BoringCrypto
+// - removed GSN context
+// - removed comments (we all know this contract)
+// - updated solidity version
+// - made _owner public and renamed to owner
+// - simplified code
+// TODO: Consider using the version that requires acceptance from new owner
+
+contract Ownable {
+    address public owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor () internal {
+        owner = msg.sender;
+        emit OwnershipTransferred(address(0), msg.sender);
+    }
+
+    function renounceOwnership() public virtual {
+        require(owner == msg.sender, "Ownable: caller is not the owner");
+        emit OwnershipTransferred(owner, address(0));
+        owner = address(0);
+    }
+
+    function transferOwnership(address newOwner) public virtual {
+        require(owner == msg.sender, "Ownable: caller is not the owner");
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Ownable: caller is not the owner");
+        _;
+    }
+}
+
 interface IERC20 {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
@@ -21,6 +59,7 @@ contract ERC20Data {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping (address => uint256)) allowance;
+    mapping(address => uint256) public nonces;
 }
 
 contract ERC20 is ERC20Data {
@@ -55,9 +94,27 @@ contract ERC20 is ERC20Data {
         emit Approval(msg.sender, spender, amount);
         return true;
     }
+
+    function DOMAIN_SEPARATOR() public view returns (bytes32){
+      uint256 chainId;
+      assembly {chainId := chainid()}
+      return keccak256(abi.encode(keccak256("EIP712Domain(uint256 chainId,address verifyingContract)"), chainId, address(this)));
+    }
+
+    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external {
+        require(block.timestamp < deadline, 'xSUSHI2: Expired');
+        bytes32 digest = keccak256(abi.encodePacked(
+            '\x19\x01', DOMAIN_SEPARATOR(),
+            keccak256(abi.encode(0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9, owner, spender, value, nonces[owner]++, deadline))
+        ));
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        require(recoveredAddress == owner, 'xSUSHI2: Invalid Signature');
+        allowance[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
 }
 
-contract SushiBar is ERC20 {
+contract SushiBar is ERC20, Ownable {
     using BoringMath for uint256;
     
     string public name = "SushiBar V2";
@@ -68,10 +125,17 @@ contract SushiBar is ERC20 {
     
     mapping(address => uint256) public startTime;
     mapping(address => uint256) public claimed;
+    
+    mapping(address => uint256) public bonus;
 
     event Enter(address indexed owner, address indexed to, uint256 amount);
     event Leave(address indexed owner, address indexed to, uint256 amount);
     event Claim(address indexed owner, address indexed to, uint256 amount);
+
+    function setBonus(address sender, uint256 newBonus) public onlyOwner {
+        require(newBonus <= 365 days, "SushiBar: Bonus too large");
+        bonus[sender] = newBonus;
+    }
 
     function pending(address owner) public view returns(uint256) {
         // Reward calculation:
@@ -87,16 +151,22 @@ contract SushiBar is ERC20 {
     }
 
     function enter(uint256 amount, address to) public {
-        uint256 startTimeCurrent = startTime[to];
         uint256 balanceCurrent = balanceOf[to];
         if (balanceCurrent == 0) {
             // If there is no balance, startTime is now
-            startTime[to] = block.timestamp;
+            startTime[to] = block.timestamp.sub(bonus[msg.sender]);
         } else {
             // If there is already a balance:
             // Increase startTime by: amount / (balance + amount) * (now - startTime)
             // If you added 100 SUSHI 3 momnths ago and you add 200 SUSHI now, startTime should become 1 month ago
-            startTime[to] = startTimeCurrent.add(amount.mul(block.timestamp.sub(startTimeCurrent) / (balanceCurrent.add(amount))));
+            uint256 startTimeCurrent = startTime[to];
+            uint256 durationCurrent = block.timestamp.sub(startTimeCurrent);
+            uint256 bonusTime = bonus[msg.sender];
+            if (durationCurrent >= bonusTime) {
+                startTime[to] = startTimeCurrent.add(amount.mul(durationCurrent.sub(bonusTime) / (balanceCurrent.add(amount))));
+            } else {
+                startTime[to] = startTimeCurrent.sub(amount.mul(bonusTime.sub(durationCurrent) / (balanceCurrent.add(amount))));
+            }
         }
         
         totalSupply = totalSupply.add(amount);
@@ -106,7 +176,7 @@ contract SushiBar is ERC20 {
         sushi.transferFrom(msg.sender, address(this), amount);
         emit Enter(msg.sender, to, amount);
     }
-
+    
     function claim(address to) public {
         uint256 claimedCurrent = claimed[msg.sender];
         uint256 amount = pending(msg.sender).sub(claimedCurrent);
