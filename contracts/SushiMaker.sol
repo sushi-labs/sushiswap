bc484dc05882af1100303501a22b8890f6bb61e5
BoringCrypto <b@rtje.net>
2021-01-03 09:30:20 +1100

    Multiple fixes

--- contracts/SushiMaker.sol
+++ contracts/SushiMaker.sol
@@ -15,84 +15,62 @@ contract SushiMaker {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    IUniswapV2Factory public factory;
-    address public bar;
-    address public sushi;
-    address public weth;
-
-    constructor(IUniswapV2Factory _factory, address _bar, address _sushi, address _weth) public {
-        factory = _factory;
-        sushi = _sushi;
-        bar = _bar;
-        weth = _weth;
-    }
+    IUniswapV2Factory public constant factory = IUniswapV2Factory(0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac);
+    address public constant bar = 0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272;
+    address public constant sushi = 0x6B3595068778DD592e39A122f4f5a5cF09C90fE2;
+    address public constant weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     function convert(address token0, address token1) public {
         // At least we try to make front-running harder to do.
         require(msg.sender == tx.origin, "do not convert from contract");
+        
         IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(token0, token1));
         pair.transfer(address(pair), pair.balanceOf(address(this)));
-        pair.burn(address(this));
-        // First we convert everything to WETH
-        uint256 wethAmount = _toWETH(token0) + _toWETH(token1);
-        // Then we convert the WETH to Sushi
+        (uint amount0, uint amount1) = pair.burn(address(this));
+        if (token0 != pair.token0()) {
+            (amount0, amount1) = (amount1, amount0);
+        }
+        uint256 wethAmount = _toWETH(token0, amount0) + _toWETH(token1, amount1);
         _toSUSHI(wethAmount);
     }
 
-    // Converts token passed as an argument to WETH
-    function _toWETH(address token) internal returns (uint256) {
-        // If the passed token is Sushi, don't convert anything
+    function _toWETH(address token, uint amountIn) internal returns (uint256) {
         if (token == sushi) {
-            uint amount = IERC20(token).balanceOf(address(this));
-            _safeTransfer(token, bar, amount);
+            _safeTransfer(token, bar, amountIn);
             return 0;
         }
-        // If the passed token is WETH, don't convert anything
         if (token == weth) {
-            uint amount = IERC20(token).balanceOf(address(this));
-            _safeTransfer(token, factory.getPair(weth, sushi), amount);
-            return amount;
+            _safeTransfer(token, factory.getPair(weth, sushi), amountIn);
+            return amountIn;
         }
-        // If the target pair doesn't exist, don't convert anything
         IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(token, weth));
         if (address(pair) == address(0)) {
             return 0;
         }
-        // Choose the correct reserve to swap from
         (uint reserve0, uint reserve1,) = pair.getReserves();
         address token0 = pair.token0();
         (uint reserveIn, uint reserveOut) = token0 == token ? (reserve0, reserve1) : (reserve1, reserve0);
-        // Calculate information required to swap
-        uint amountIn = IERC20(token).balanceOf(address(this));
         uint amountInWithFee = amountIn.mul(997);
-        uint numerator = amountInWithFee.mul(reserveOut);
-        uint denominator = reserveIn.mul(1000).add(amountInWithFee);
-        uint amountOut = numerator / denominator;
+        uint amountOut = amountInWithFee.mul(reserveOut) / reserveIn.mul(1000).add(amountInWithFee);
         (uint amount0Out, uint amount1Out) = token0 == token ? (uint(0), amountOut) : (amountOut, uint(0));
-        // Swap the token for WETH
         _safeTransfer(token, address(pair), amountIn);
         pair.swap(amount0Out, amount1Out, factory.getPair(weth, sushi), new bytes(0));
         return amountOut;
     }
 
-    // Converts WETH to Sushi
     function _toSUSHI(uint256 amountIn) internal {
         IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(weth, sushi));
-        // Choose WETH as input token
         (uint reserve0, uint reserve1,) = pair.getReserves();
         address token0 = pair.token0();
         (uint reserveIn, uint reserveOut) = token0 == weth ? (reserve0, reserve1) : (reserve1, reserve0);
-        // Calculate information required to swap
         uint amountInWithFee = amountIn.mul(997);
         uint numerator = amountInWithFee.mul(reserveOut);
         uint denominator = reserveIn.mul(1000).add(amountInWithFee);
         uint amountOut = numerator / denominator;
         (uint amount0Out, uint amount1Out) = token0 == weth ? (uint(0), amountOut) : (amountOut, uint(0));
-        // Swap WETH for Sushi
         pair.swap(amount0Out, amount1Out, bar, new bytes(0));
     }
 
-    // Wrapper for safeTransfer
     function _safeTransfer(address token, address to, uint256 amount) internal {
         IERC20(token).safeTransfer(to, amount);
     }
