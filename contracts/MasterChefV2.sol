baad9e1b629f5c630a41470a9c7fe7f1e54b8391
Keno <kenobudde@gmail.com>
2021-02-09 16:08:00 +0100

    - exported interface into new file

--- contracts/MasterChefV2.sol
+++ contracts/MasterChefV2.sol
@@ -8,24 +8,7 @@ import "@boringcrypto/boring-solidity/contracts/BoringBatchable.sol";
 import "@boringcrypto/boring-solidity/contracts/BoringOwnable.sol";
 import "./libraries/SignedSafeMath.sol";
 import "./interfaces/IRewarder.sol";
-
-interface IMasterChef {
-    struct UserInfo {
-        uint256 amount;     // How many LP tokens the user has provided.
-        uint256 rewardDebt; // Reward debt. See explanation below.
-    }
-
-    struct PoolInfo {
-        IERC20 lpToken;           // Address of LP token contract.
-        uint256 allocPoint;       // How many allocation points assigned to this pool. SUSHIs to distribute per block.
-        uint256 lastRewardBlock;  // Last block number that SUSHIs distribution occurs.
-        uint256 accSushiPerShare; // Accumulated SUSHIs per share, times 1e12. See below.
-    }
-
-    function poolInfo(uint256 pid) external view returns (IMasterChef.PoolInfo memory);
-    function totalAllocPoint() external view returns (uint256);
-    function deposit(uint256 _pid, uint256 _amount) external;
-}
+import "./interfaces/IMasterChef.sol";
 
 contract MasterChefV2 is BoringOwnable, BoringBatchable {
     using BoringMath for uint256;
