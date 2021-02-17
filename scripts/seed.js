43e61a83f9575fb947499b08d7f38f8b53bc8996
Matthew Lilley <hello@matthewlilley.com>
2021-02-17 21:59:54 +0000

    Tasks

--- scripts/seed.js
+++
@@ -1,2 +0,0 @@
-// Get compound token list
-// 
---
+++ tasks/accounts.js
@@ -0,0 +1,21 @@
+const { normalizeHardhatNetworkAccountsConfig } = require("hardhat/internal/core/providers/util")
+
+const { BN, bufferToHex, privateToAddress, toBuffer } = require("ethereumjs-util")
+
+module.exports = async function (taskArguments, hre, runSuper) {
+  const networkConfig = hre.config.networks["mainnet"]
+
+  const accounts = normalizeHardhatNetworkAccountsConfig(networkConfig.accounts)
+
+  console.log("Accounts")
+  console.log("========")
+
+  for (const [index, account] of accounts.entries()) {
+    const address = bufferToHex(privateToAddress(toBuffer(account.privateKey)))
+    const privateKey = bufferToHex(toBuffer(account.privateKey))
+    const balance = new BN(account.balance).div(new BN(10).pow(new BN(18))).toString(10)
+    console.log(`Account #${index}: ${address} (${balance} ETH)
+Private Key: ${privateKey}
+`)
+  }
+}
