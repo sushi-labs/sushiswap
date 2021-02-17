bb45ff0d5d29fcb8d4d398d344416ffd8d2244f7
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2021-02-17 16:56:00 +0100

    review

--- contracts/MasterChefV2.sol
+++ contracts/MasterChefV2.sol
@@ -233,9 +233,15 @@ contract MasterChefV2 is BoringOwnable, BoringBatchable {
         // Interactions
         SUSHI.safeTransfer(to, _pendingSushi);
 
-        IRewarder _rewarder = rewarder[pid];
-        if (address(_rewarder) != address(0)){
-            _rewarder.onSushiReward(pid, msg.sender, _pendingSushi);
+
+        address _rewarder = address(rewarder[pid]);
+        if (_rewarder != address(0)){
+            // Note: Do it this way because we we don't want to fail harvest if only the delegate call fails.
+            // Additionally, we forward less gas so that we have enough buffer to complete harvest if the call eats up too much gas.
+            // Forwarding: (63/64 of gasleft by evm convention) minus 5000
+            // solhint-disable-next-line
+            _rewarder.call{ gas: gasleft() - 5000 }(abi.encodeWithSelector(SIG_ON_SUSHI_REWARD, pid, msg.sender, _pendingSushi));
+
         }
 
         emit Harvest(msg.sender, pid, _pendingSushi);
