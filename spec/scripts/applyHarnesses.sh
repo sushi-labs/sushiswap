8ba56b59c394a2ccf756e3b933b520da5c5ed82b
Nurit Dor <nurit@certora.com>
2021-04-08 12:19:04 +0300

    harness for node_modules

--- spec/scripts/applyHarnesses.sh
+++ spec/scripts/applyHarnesses.sh
@@ -13,3 +13,8 @@ perl -0777 -i -pe 's/uint256 totalAllocPoint/uint256 public totalAllocPoint/g' c
 
 # Adding virtual to sushiPerBlock()
 perl -0777 -i -pe 's/function sushiPerBlock\(\) public view returns/function sushiPerBlock\(\) public view virtual returns/g' contracts/MasterChefV2.sol
+
+perl -0777 -i -pe 's/\) external payable returns\(/\) external virtual payable returns\(/g' node_modules/@boringcrypto/boring-solidity/contracts/BoringBatchable.sol
+
+# Add transfer function declaration 
+perl -0777 -i -pe 's/\}/function transfer\(address to, uint256 amount\) external;\n function transferFrom\(address from, address to, uint256 amount\) external;\n\}/g' node_modules/@boringcrypto/boring-solidity/contracts/interfaces/IERC20.sol
