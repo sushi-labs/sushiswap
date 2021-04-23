3f1935cca5c3066af5353da27a512073004b171b
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-04-22 20:47:59 -0400

    Update BoshiFactory.sol
    import erc20 to help test checks on ierc20 refs

--- contracts/bentobox/boshi/BoshiFactory.sol
+++ contracts/bentobox/boshi/BoshiFactory.sol
@@ -2,7 +2,25 @@
 
 pragma solidity 0.6.12;
 
-import './interfaces/IBoshiFactory.sol';
+import './BoshiERC20.sol';
+
+interface IBoshiFactory {
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint256);
+
+    function feeTo() external view returns (address);
+    function feeToSetter() external view returns (address);
+    function migrator() external view returns (address);
+
+    function getPair(IERC20 tokenA, IERC20 tokenB) external view returns (address pair);
+    function allPairs(uint256) external view returns (address pair);
+    function allPairsLength() external view returns (uint256);
+
+    function createPair(IERC20 tokenA, IERC20 tokenB) external returns (address pair);
+
+    function setFeeTo(address) external;
+    function setFeeToSetter(address) external;
+    function setMigrator(address) external;
+}
 
 contract BoshiFactory is IBoshiFactory {
     address public override feeTo;
