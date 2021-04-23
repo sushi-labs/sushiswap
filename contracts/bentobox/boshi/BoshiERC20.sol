0373c55c667cea293ae70ebad1d9d01b7806c02b
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-04-22 20:07:16 -0400

    Create BoshiERC20.sol

---
+++ contracts/bentobox/boshi/BoshiERC20.sol
@@ -0,0 +1,180 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import './libraries/BoshiMath.sol';
+
+// File @boringcrypto/boring-solidity/contracts/Domain.sol@v1.2.0
+// License-Identifier: MIT
+// Based on code and smartness by Ross Campbell and Keno
+// Uses immutable to store the domain separator to reduce gas usage
+// If the chain id changes due to a fork, the forked chain will calculate on the fly.
+
+contract Domain {
+    bytes32 private constant DOMAIN_SEPARATOR_SIGNATURE_HASH = keccak256("EIP712Domain(uint256 chainId,address verifyingContract)");
+    // See https://eips.ethereum.org/EIPS/eip-191
+    string private constant EIP191_PREFIX_FOR_EIP712_STRUCTURED_DATA = "\x19\x01";
+
+    // solhint-disable var-name-mixedcase
+    bytes32 private immutable _DOMAIN_SEPARATOR;
+    uint256 private immutable DOMAIN_SEPARATOR_CHAIN_ID;
+
+    /// @dev Calculate the DOMAIN_SEPARATOR
+    function _calculateDomainSeparator(uint256 chainId) private view returns (bytes32) {
+        return keccak256(abi.encode(DOMAIN_SEPARATOR_SIGNATURE_HASH, chainId, address(this)));
+    }
+
+    constructor() public {
+        uint256 chainId;
+        assembly {
+            chainId := chainid()
+        }
+        _DOMAIN_SEPARATOR = _calculateDomainSeparator(DOMAIN_SEPARATOR_CHAIN_ID = chainId);
+    }
+
+    /// @dev Return the DOMAIN_SEPARATOR
+    // It's named internal to allow making it public from the contract that uses it by creating a simple view function
+    // with the desired public name, such as DOMAIN_SEPARATOR or domainSeparator.
+    // solhint-disable-next-line func-name-mixedcase
+    function DOMAIN_SEPARATOR() public view returns (bytes32) {
+        uint256 chainId;
+        assembly {
+            chainId := chainid()
+        }
+        return chainId == DOMAIN_SEPARATOR_CHAIN_ID ? _DOMAIN_SEPARATOR : _calculateDomainSeparator(chainId);
+    }
+
+    function _getDigest(bytes32 dataHash) internal view returns (bytes32 digest) {
+        digest = keccak256(abi.encodePacked(EIP191_PREFIX_FOR_EIP712_STRUCTURED_DATA, DOMAIN_SEPARATOR(), dataHash));
+    }
+}
+
+// File @boringcrypto/boring-solidity/contracts/ERC20.sol@v1.2.0
+// License-Identifier: MIT
+
+// solhint-disable no-inline-assembly
+// solhint-disable not-rely-on-time
+
+// Data part taken out for building of contracts that receive delegate calls
+contract ERC20Data {
+    /// @notice owner > balance mapping.
+    mapping(address => uint256) public balanceOf;
+    /// @notice owner > spender > allowance mapping.
+    mapping(address => mapping(address => uint256)) public allowance;
+    /// @notice owner > nonce mapping. Used in `permit`.
+    mapping(address => uint256) public nonces;
+}
+
+interface IERC20 {} contract BoshiERC20 is Domain, ERC20Data {
+    using BoshiMath for uint256;
+    
+    string public constant name = 'Boshi LP Token';
+    string public constant symbol = 'bSLP';
+    uint8 public constant decimals = 18;
+    uint256 public totalSupply;
+    
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    
+    function _mint(address to, uint256 amount) internal {
+        totalSupply = totalSupply.add(amount);
+        balanceOf[to] = balanceOf[to].add(amount);
+        emit Transfer(address(0), to, amount);
+    }
+
+    function _burn(address from, uint256 amount) internal {
+        balanceOf[from] = balanceOf[from].sub(amount);
+        totalSupply = totalSupply.sub(amount);
+        emit Transfer(from, address(0), amount);
+    }
+
+    /// @notice Transfers `amount` tokens from `msg.sender` to `to`.
+    /// @param to The address to move the tokens.
+    /// @param amount of the tokens to move.
+    /// @return (bool) Returns True if succeeded.
+    function transfer(address to, uint256 amount) public returns (bool) {
+        // If `amount` is 0, or `msg.sender` is `to` nothing happens
+        if (amount != 0) {
+            uint256 srcBalance = balanceOf[msg.sender];
+            require(srcBalance >= amount, "BoshiERC20: balance too low");
+            if (msg.sender != to) {
+                require(to != address(0), "BoshiERC20: no zero address"); // Moved down so low balance calls safe some gas
+
+                balanceOf[msg.sender] = srcBalance - amount; // Underflow is checked
+                balanceOf[to] += amount; // Can't overflow because totalSupply would be greater than 2^256-1
+            }
+        }
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    /// @notice Transfers `amount` tokens from `from` to `to`. Caller needs approval for `from`.
+    /// @param from Address to draw tokens from.
+    /// @param to The address to move the tokens.
+    /// @param amount The token amount to move.
+    /// @return (bool) Returns True if succeeded.
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public returns (bool) {
+        // If `amount` is 0, or `from` is `to` nothing happens
+        if (amount != 0) {
+            uint256 srcBalance = balanceOf[from];
+            require(srcBalance >= amount, "BoshiERC20: balance too low");
+
+            if (from != to) {
+                uint256 spenderAllowance = allowance[from][msg.sender];
+                // If allowance is infinite, don't decrease it to save on gas (breaks with EIP-20).
+                if (spenderAllowance != type(uint256).max) {
+                    require(spenderAllowance >= amount, "BoshiERC20: allowance too low");
+                    allowance[from][msg.sender] = spenderAllowance - amount; // Underflow is checked
+                }
+                require(to != address(0), "BoshiERC20: no zero address"); // Moved down so other failed calls save some gas
+
+                balanceOf[from] = srcBalance - amount; // Underflow is checked
+                balanceOf[to] += amount; // Can't overflow because totalSupply would be greater than 2^256-1
+            }
+        }
+        emit Transfer(from, to, amount);
+        return true;
+    }
+
+    /// @notice Approves `amount` from sender to be spend by `spender`.
+    /// @param spender Address of the party that can draw from msg.sender's account.
+    /// @param amount The maximum collective amount that `spender` can draw.
+    /// @return (bool) Returns True if approved.
+    function approve(address spender, uint256 amount) public returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    bytes32 private constant PERMIT_SIGNATURE_HASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
+
+    /// @notice Approves `value` from `owner_` to be spend by `spender`.
+    /// @param owner_ Address of the owner.
+    /// @param spender The address of the spender that gets approved to draw from `owner_`.
+    /// @param value The maximum collective amount that `spender` can draw.
+    /// @param deadline This permit must be redeemed before this deadline (UTC timestamp in seconds).
+    function permit(
+        address owner_,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        require(owner_ != address(0), "ERC20: Owner cannot be 0");
+        require(block.timestamp < deadline, "ERC20: Expired");
+        require(
+            ecrecover(_getDigest(keccak256(abi.encode(PERMIT_SIGNATURE_HASH, owner_, spender, value, nonces[owner_]++, deadline))), v, r, s) ==
+                owner_,
+            "ERC20: Invalid Signature"
+        );
+        allowance[owner_][spender] = value;
+        emit Approval(owner_, spender, value);
+    }
+}
