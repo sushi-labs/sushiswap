4f53c82f555da176522bc101926ea832ae644e8e
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-04-22 20:53:35 -0400

    Create IBoshiFactory.sol
    replace IBoshiFactory

---
+++ contracts/bentobox/boshi/interfaces/IBoshiFactory.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+interface IBoshiFactory {
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint256);
+
+    function feeTo() external view returns (address);
+    function feeToSetter() external view returns (address);
+    function migrator() external view returns (address);
+
+    function getPair(IERC20 tokenA, IERC20 tokenB) external view returns (address pair);
+    function allPairs(uint256) external view returns (address pair);
+    function allPairsLength() external view returns (uint256);
+
+    function createPair(IERC20 tokenA, IERC20 tokenB) external returns (address pair);
+
+    function setFeeTo(address) external;
+    function setFeeToSetter(address) external;
+    function setMigrator(address) external;
+}
