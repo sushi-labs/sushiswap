b5063850a719ffd494ca40fd9c07d7caff736889
Roohdad <ramin.roohdad@gmail.com>
2021-05-06 22:37:48 +0300

    s

--- contracts/mocks/ERC20Mock.sol
+++ contracts/mocks/ERC20Mock.sol
@@ -8,10 +8,8 @@ contract ERC20Mock is ERC20 {
     constructor(
         string memory name,
         string memory symbol,
-        uint256 memory decimal,
         uint256 supply
-    ) public ERC20(name, symbol, decimal) {
-        _setupDecimals(decimal);
+    ) public ERC20(name, symbol) {
         _mint(msg.sender, supply);
     }
 }
