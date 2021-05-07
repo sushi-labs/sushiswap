30dd60a17bd4e579032787020889026baa267da5
Moonster <Moonster.bsc@gmail.com>
2021-05-07 12:55:24 -0400

    remove import

--- contracts/interfaces/IMiniChefV2.sol
+++ contracts/interfaces/IMiniChefV2.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
-import "@boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol";
 
 interface IMiniChefV2 {
     struct UserInfo {
