5039917fb6123f22067552f363a6c266516e71ca
Clearwood <Clearwood@users.noreply.github.com>
2021-09-04 21:20:43 +0200

    Reset interface

--- contracts/interfaces/IRewarder.sol
+++ contracts/interfaces/IRewarder.sol
@@ -6,5 +6,4 @@ interface IRewarder {
     using BoringERC20 for IERC20;
     function onSushiReward(uint256 pid, address user, address recipient, uint256 sushiAmount, uint256 newLpAmount) external;
     function pendingTokens(uint256 pid, address user, uint256 sushiAmount) external view returns (IERC20[] memory, uint256[] memory);
-    function emergencyWithdraw(uint256 pid, address user, address recipient) external;
 }
