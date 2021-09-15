b552fab7f5ee0e8c7ff630d5fc801297ade6c6b8
Kasumi <kasumi.null@yandex.com>
2021-09-15 00:39:12 -0500

    chore: uniswap -> benswap sushiroll address and var names

--- deploy/SushiRoll.js
+++ deploy/SushiRoll.js
@@ -1,12 +1,6 @@
-const UNISWAP_ROUTER = new Map()
-UNISWAP_ROUTER.set("1", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
-UNISWAP_ROUTER.set("3", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
-UNISWAP_ROUTER.set("4", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
-UNISWAP_ROUTER.set("5", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
-UNISWAP_ROUTER.set("42", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
-UNISWAP_ROUTER.set("1287", "0x2823caf546C7d09a4832bd1da14f2C6b6E665e05")
-UNISWAP_ROUTER.set("31337", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
-UNISWAP_ROUTER.set("79377087078960", "0x0B72c0193CD598b536210299d358A5b720A262b8")
+const BENSWAP_ROUTER = new Map()
+BENSWAP_ROUTER.set("10000", "0xa194133ED572D86fe27796F2feADBAFc062cB9E0")
+BENSWAP_ROUTER.set("10001", "0x221C2c57B0dAec7aF9A3B03c384c0C99e066b612")
 
 module.exports = async function ({ getNamedAccounts, getChainId, deployments }) {
   const { deploy } = deployments
@@ -15,17 +9,17 @@ module.exports = async function ({ getNamedAccounts, getChainId, deployments }) 
 
   const chainId = await getChainId()
 
-  if (!UNISWAP_ROUTER.has(chainId)) {
+  if (!BENSWAP_ROUTER.has(chainId)) {
     throw Error("No Uniswap Router")
   }
 
-  const uniswapRouterAddress = UNISWAP_ROUTER.get(chainId)
+  const benswapRouterAddress = BENSWAP_ROUTER.get(chainId)
 
   const sushiswapRouterAddress = (await deployments.get("UniswapV2Router02")).address
 
   await deploy("SushiRoll", {
     from: deployer,
-    args: [uniswapRouterAddress, sushiswapRouterAddress],
+    args: [benswapRouterAddress, sushiswapRouterAddress],
     log: true,
     deterministicDeployment: false
   })
