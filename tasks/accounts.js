1d904e99afa82cbc5ee02ea56e6c170129c05d7b
Kasumi <kasumi.null@yandex.com>
2021-09-22 23:51:53 -0500

    fix: fix accounts task

--- tasks/accounts.js
+++ tasks/accounts.js
@@ -3,20 +3,19 @@ const { normalizeHardhatNetworkAccountsConfig } = require("hardhat/internal/core
 const { BN, bufferToHex, privateToAddress, toBuffer } = require("ethereumjs-util")
 
 module.exports = async function (taskArguments, hre, runSuper) {
-  const networkConfig = hre.config.networks["mainnet"]
-
-  console.log(networkConfig.accounts)
+  const networkConfig = hre.config.networks[hre.network.name]
 
   const accounts = normalizeHardhatNetworkAccountsConfig(networkConfig.accounts)
 
   console.log("Accounts")
   console.log("========")
 
-  for (const [index, account] of accounts.entries()) {
-    const address = bufferToHex(privateToAddress(toBuffer(account.privateKey)))
-    const privateKey = bufferToHex(toBuffer(account.privateKey))
-    const balance = new BN(account.balance).div(new BN(10).pow(new BN(18))).toString(10)
-    console.log(`Account #${index}: ${address} (${balance} ETH)
+  for (const [index, privkey] of accounts.entries()) {
+    const address = bufferToHex(privateToAddress(toBuffer(privkey)))
+    const privateKey = bufferToHex(toBuffer(privkey))
+    const balance = new BN(await hre.ethers.provider.getBalance(address).toString())
+      .toString()
+    console.log(`Account #${index}: ${address} (${balance} WEI)
 Private Key: ${privateKey}
 `)
   }
