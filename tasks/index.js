8ef4d0fa5320c87adf289eff84140b75738feb0f
Kasumi <kasumi.null@yandex.com>
2021-09-29 20:32:09 -0500

    feat: additional tasks

--- tasks/index.js
+++ tasks/index.js
@@ -50,18 +50,41 @@ task("erc20:approve", "ERC20 approve")
 });
 
 task("factory:set-fee-to", "Factory set fee to")
-.addParam("feeTo", "Fee To")
-.setAction(async function ({ feeTo }, { ethers: { getNamedSigner } }, runSuper) {
+.addParam("address", "Fee To")
+.setAction(async function ({ address }, { ethers: { getNamedSigner } }, runSuper) {
   const factory = await ethers.getContract("UniswapV2Factory")
-  console.log(`Setting factory feeTo to ${feeTo} address`)
-  await (await factory.connect(await getNamedSigner('dev')).setFeeTo(feeTo)).wait() 
+  console.log(`Setting factory feeTo to ${address} address`)
+  await (await factory.connect(await getNamedSigner('dev')).setFeeTo(address, {
+    gasPrice: 1050000000,
+  })).wait()
 });
 
-task("factory:paircodehash", "Get pair code hash")
+task("factory:get-fee-to", "Factory get fee to")
+.setAction(async function ({ address }, { ethers: { getNamedSigner } }, runSuper) {
+  const factory = await ethers.getContract("UniswapV2Factory")
+  console.log(`factory: ${factory.address} feeTo: ${await (await factory.connect(await getNamedSigner('dev')).feeTo())}`)
+});
+
+task("factory:get-pair", "Factory get pair")
+.addParam("tokenA", "Token A")
+.addParam("tokenB", "Token B")
+.setAction(async function ({ tokenA, tokenB }, { ethers: { getNamedSigner } }, runSuper) {
+  const factory = await ethers.getContract("UniswapV2Factory")
+  if (parseInt(tokenA.substring(2, 8), 16) > parseInt(tokenB.substring(2, 8), 16)) {
+    [tokenA, tokenB] = [tokenB, tokenA]
+  }
+  console.log(`${await (await factory.connect(await getNamedSigner('dev')).getPair(tokenA, tokenB))}`)
+});
+
+task("factory:query", "Get pair code hash")
 .setAction(async function ({ }, { ethers: { getNamedSigner } }, runSuper) {
-  const Factory = await ethers.getContractFactory("UniswapV2Factory")
-  const factory = Factory.attach('0xaa094cA3FBd19dCCcE91C79d1FffA28293B05f28')
-  console.log(await factory.pairCodeHash())
+  const factory = await ethers.getContract("UniswapV2Factory")
+  console.log('address', factory.address)
+  console.log('feeTo', await (await factory.connect(await getNamedSigner('dev')).feeTo()))
+  console.log('feeToSetter', await (await factory.connect(await getNamedSigner('dev')).feeToSetter()))
+  console.log('migrator', await (await factory.connect(await getNamedSigner('dev')).migrator()))
+  console.log('pairCodeHash', await factory.pairCodeHash())
+  console.log('allPairsLength', (await factory.allPairsLength()).toString())
 });
 
 // TODO: Swap?
@@ -102,13 +125,42 @@ task("migrate", "Migrates liquidity from BenSwap to SushiSwap")
   .addOptionalParam("b", "Token B", "0xc778417E063141139Fce010982780140Aa0cD5Ab")
   .setAction(require("./migrate"))
 
+task("erc20:balance", "Look up balance")
+.addParam("address", "Token address")
+.addParam("account", "Account")
+.setAction(async function ({ address, account }, { ethers: { getNamedSigner } }, runSuper) {
+  const erc20 = await ethers.getContractFactory("UniswapV2ERC20")
+
+  const token = erc20.attach(address)
+
+  console.log('balance', (await token.balanceOf(account)).toString())
+});
+
+task("pair:query", "Look up pair")
+.addParam("address", "Token address")
+.setAction(async function ({ address }, { ethers: { getNamedSigner } }, runSuper) {
+  const erc20 = await ethers.getContractFactory("UniswapV2Pair")
+  const pair = erc20.attach(address)
+  const [ reserve0, reserve1, blockTimestampLast ] = await pair.getReserves();
+
+  console.log('factory', await pair.factory());
+  console.log('token0', await pair.token0());
+  console.log('token1', await pair.token1());
+  console.log('reserve0', reserve0.toString());
+  console.log('reserve1', reserve1.toString());
+  console.log('blockTimestampLast', blockTimestampLast);
+  console.log('price0CumulativeLast', (await pair.price0CumulativeLast()).toString());
+  console.log('price1CumulativeLast', (await pair.price1CumulativeLast()).toString());
+  console.log('kLast', (await pair.kLast()).toString());
+});
+
 task("masterchef:add", "Add farm to masterchef")
 .setAction(async function (taskArguments, { ethers: { getNamedSigner } }, runSuper) {
   const masterChef = await ethers.getContract("MasterChef")
 
-  // add bch/tc pool
-  // await (await masterChef.connect(await getNamedSigner("dev")).add(1000, '0x9A520C877c62aB833276F6FD871D61898aFE0896', true, {
-  await (await masterChef.connect(await getNamedSigner("dev")).add(10000, '0xDB70603f600a3eab200bA0F08fbDca467bD5a1D5', true, {
+  // add bch/mist pool
+  // await (await masterChef.connect(await getNamedSigner("dev")).add(10000, '0xa6A957Eb0DB729AB096310A8723139873513b0B3', true, {
+  await (await masterChef.connect(await getNamedSigner("dev")).add(1000, '0x07DE6fc05597E0E4c92C83637A8a0CA411f3a769', true, {
     gasPrice: 1050000000,
     gasLimit: 5198000,
   })).wait()
@@ -198,6 +250,6 @@ task("maker:serve", "SushiBar serve")
 .setAction(async function ({ a, b }, { ethers: { getNamedSigner } }, runSuper) {
   const maker = await ethers.getContract("SushiMaker")
 
-  await (await maker.connect(await getNamedSigner("dev")).convert(a, b, { gasPrice: 1050000000, gasLimit: 5198000 })).wait()
+  console.log(await (await maker.connect(await getNamedSigner("dev")).convert(a, b, { gasPrice: 1050000000, gasLimit: 5198000 })).wait())
 });
 
