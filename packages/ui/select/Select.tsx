188c671f04237a26a6b2232c7a900296fcb7e05f
Chilli Chelli <ramin@sushi.com>
2022-02-07 00:00:53 +0100

    style(Select): remove close animation

--- packages/ui/select/Select.tsx
+++ packages/ui/select/Select.tsx
@@ -39,9 +39,6 @@ const SelectRoot: FC<SelectProps> = ({
               enter="transition ease-out duration-100"
               enterFrom="transform opacity-0 scale-95"
               enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
             >
               {children}
             </Transition>
