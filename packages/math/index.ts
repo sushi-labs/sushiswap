cb8f9fb2909e05e928eed9ff3d889b2f34c9f860
matthewlilley <hello@matthewlilley.com>
2022-02-21 23:46:18 +0000

    feat: math

---
+++ packages/math/index.ts
@@ -0,0 +1,4 @@
+export { default as BigintIsh } from "./BigintIsh";
+export { default as Fraction } from "./Fraction";
+export { default as Percent } from "./Percent";
+export { default as Rounding } from "./Rounding";
