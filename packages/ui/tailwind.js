eaa5aca96240ee6c5ecf060aa9b64778cb6bf416
matthewlilley <hello@matthewlilley.com>
2022-02-21 23:05:20 +0000

    feat: extract tailwind config to ui for reuse

---
+++ packages/ui/tailwind.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // `content` is replaced instead of extended, so this line has to be added in
+  // the `content` of each app' tailwind.config.js
+  content: ["node_modules/ui/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+};
