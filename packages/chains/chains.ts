4a42ebd8f2e12db8e644a843ddb727130a1966a8
matthewlilley <hello@matthewlilley.com>
2022-02-25 22:00:52 +0000

    feat: export CHAIN constant

--- packages/chains/chains.ts
+++
@@ -1,5 +0,0 @@
-import chains from './chains.json'
-
-import { Chain } from './types'
-
-export default chains as Chain[]
--- packages/chains/index.ts
+++ packages/chains/index.ts
@@ -1,3 +1,7 @@
 export * from './types'
-import chains from './chains'
-export default chains
+
+import chains from './chains.json'
+
+export const CHAIN = Object.fromEntries(chains.map((data) => [data.chainId, data]))
+
+export default CHAIN
