718fcb8347825e591e61b12707e5afea29026567
matthewlilley <hello@matthewlilley.com>
2022-03-01 01:22:51 +0000

    fix: wallet connector rpcs

--- packages/wallet-connector/connectors/network.ts
+++ packages/wallet-connector/connectors/network.ts
@@ -1,16 +1,16 @@
 import { initializeConnector } from '@web3-react/core'
 import { Network } from '@web3-react/network'
-import chains from 'chains'
+import chain from 'chain'
 
 const [connector, hooks, store] = initializeConnector<Network>(
   (actions) => {
     return new Network(
       actions,
-      Object.fromEntries(Object.entries(chains).map(([chainId, { rpc }]) => [Number(chainId), String(rpc)])),
+      Object.fromEntries(Object.entries(chain).map(([chainId, { rpc }]) => [Number(chainId), rpc])),
       true,
     )
   },
-  Object.entries(chains).map(([chainId]) => Number(chainId)),
+  Object.entries(chain).map(([chainId]) => Number(chainId)),
 )
 
 export const networkConnector = {
