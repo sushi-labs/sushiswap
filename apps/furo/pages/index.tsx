b5e16b3ebda897cac148fdc3b7e745b188e35d24
OlaStenberg <hello@olastenberg.com>
2022-03-31 10:07:47 +0200

    feat: show streams

--- apps/furo/pages/index.tsx
+++ apps/furo/pages/index.tsx
@@ -1,41 +1,41 @@
-// import { FactoriesQueryQuery, getBuiltGraphSDK } from '../.graphclient'
+import { StreamsQueryQuery, getBuiltGraphSDK } from '../.graphclient'
 import { KPI } from '../components'
 
-export default function Analytics() {
-// export default function Analytics(props: FactoriesQueryQuery) {
-  // const { liquidityUSD, volumeUSD, txCount, pairCount, userCount, tokenCount } = Object.values(props).reduce(
-  //   (previousValue, currentValue) => ({
-  //     liquidityUSD: previousValue.liquidityUSD + Number(currentValue.liquidityUSD),
+export default function Furo(props: StreamsQueryQuery) {
+  // const { id, status, txCount, startedAt, expiresAt, amount, recipient, createdBy, transactionCount, token } = props.streams
+  // Object.values(props).map(
+  //   (stream) => ({
+  //     : previousValue.liquidityUSD + Number(currentValue.liquidityUSD),
   //     volumeUSD: previousValue.volumeUSD + Number(currentValue.volumeUSD),
   //     txCount: previousValue.txCount + Number(currentValue.txCount),
   //     pairCount: previousValue.pairCount + Number(currentValue.pairCount),
   //     userCount: previousValue.userCount + Number(currentValue.userCount),
   //     tokenCount: previousValue.tokenCount + Number(currentValue.tokenCount),
-  //   }),
-  //   { liquidityUSD: 0, volumeUSD: 0, txCount: 0, pairCount: 0, userCount: 0, tokenCount: 0 },
+  //   })
   // )
 
-  // console.log({ props })
+  console.log({ props })
 
   return (
     <div className="px-2 pt-16">
       <h1 className="py-4 text-2xl font-bold">Overview</h1>
+        <h2>Incoming streams</h2>
       <div className="grid grid-cols-2 gap-2">
-        {/* <KPI label="TVL" value={`$${liquidityUSD.toLocaleString(undefined, { maximumFractionDigits: 2 })}`} />
-        <KPI label="VOLUME" value={`$${volumeUSD.toLocaleString(undefined, { maximumFractionDigits: 2 })}`} />
-        <KPI label="TRANSACTIONS" value={txCount.toLocaleString()} />
-        <KPI label="POOLS" value={pairCount.toLocaleString()} />
-        <KPI label="USERS" value={userCount.toLocaleString()} />
-        <KPI label="TOKENS" value={tokenCount.toLocaleString()} /> */}
+      {Object.values(props.streams).map( (stream) => (
+        <div key={stream.id}> {stream.status} {stream.recipient.id} {stream.amount} {new Date(parseInt(stream.startedAt) * 1000).toDateString()} {new Date(parseInt(stream.expiresAt) * 1000).toDateString()}</div>
+      ))}
+
       </div>
+      
+      <h2>Outgoing streams</h2>
     </div>
   )
 }
 
 export async function getStaticProps() {
-  // const sdk = await getBuiltGraphSDK()
+  const sdk = await getBuiltGraphSDK()
   return {
-    // props: await sdk.FactoriesQuery(),
+    props: await sdk.StreamsQuery(),
     revalidate: 60,
   }
 }
