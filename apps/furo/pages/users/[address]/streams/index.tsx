d8e785872987fb8e3a7b7f21fb577462a3da883a
OlaStenberg <hello@olastenberg.com>
2022-04-02 20:45:00 +0200

    feat: create stream init

--- apps/furo/pages/users/[address]/streams/index.tsx
+++ apps/furo/pages/users/[address]/streams/index.tsx
@@ -1,9 +1,12 @@
+import { ContractInterface } from 'ethers'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { FC, useState } from 'react'
 import { Dialog } from 'ui'
 import DialogContent from 'ui/dialog/DialogContent'
+import { erc20ABI, useContract, useContractWrite, useSigner } from 'wagmi'
 import { getBuiltGraphSDK } from '../../../../.graphclient'
+// import FuroStreamABI from '../../../../abis/FuroStream.json'
 
 interface StreamsProps {
   incomingStreams: Stream[]
@@ -38,6 +41,20 @@ const Streams: FC<StreamsProps> = (props) => {
   const address = router.query.address as string
   let { incomingStreams, outgoingStreams } = props
   let [isOpen, setIsOpen] = useState(false)
+  const FuroStreamABI = require("../../../../abis/FuroStream.json")
+  const [{ data, error, loading }, getSigner] = useSigner()
+
+  const contract = useContract({
+    addressOrName: '0x511D5aef6eb2eFDf71b98B4261Bbe68CC0A94Cd4',
+    contractInterface: FuroStreamABI,
+    signerOrProvider: data
+  }
+  )
+  const [token, setToken] = useState<string>("0xb7a4F3E9097C08dA09517b5aB877F7a917224ede")
+  const [amount, setAmount] = useState<number>()
+  const [recipient, setRecipient] = useState<string>()
+  const [startDate, setStartDate] = useState<Date>()
+  const [endDate, setEndDate] = useState<Date>()
 
   function closeModal() {
     setIsOpen(false)
@@ -46,7 +63,24 @@ const Streams: FC<StreamsProps> = (props) => {
   function openModal() {
     setIsOpen(true)
   }
-  console.log({ isOpen })
+
+  function createStream() {
+    if(!token || !amount || !recipient || !startDate || !endDate) {
+      console.log("missing required field")
+    }
+    console.log({token, amount, recipient, startDate, endDate})
+    console.log(contract)
+    contract.createStream(recipient, token, startDate.getTime() / 1000, endDate.getTime() / 1000, amount, false)
+      // write({args: [
+      //   recipient,
+      //   token,
+      //   startDate.getTime(),
+      //   endDate.getTime(),
+      //   amount,
+      //   false
+      // ]})
+  }
+
   return (
     <>
       <div className="px-2 pt-16">
@@ -76,33 +110,41 @@ const Streams: FC<StreamsProps> = (props) => {
           Create stream
         </button>
         <Dialog open={isOpen} onClose={closeModal}>
-          {/* <DialogHeader title={'Create stream'}/> */}
+          
           <DialogContent>
             {/* TODO: replace with Select component from ui package */}
             <div className="text-blue-600">
-                Which asset do you want to stream?
+              Which asset do you want to stream?
               <div>
-                <select value="Token">
-                  <option value="Orange">USDC</option>
+                <select>
+                  <option value="0xb7a4F3E9097C08dA09517b5aB877F7a917224ede">USDC</option>
                 </select>
               </div>
               <div>
                 How much do you want to send?
-                <input type={'number'} defaultValue={500000}></input>
+                <input
+                  type={'number'}
+                  defaultValue={500000}
+                  onChange={(e) => setAmount(parseInt(e.target.value))}
+                ></input>
               </div>
               <div>
                 Who is the recipient?
-              <input type={'text'} defaultValue={'0x23defc2ca207e7fbd84ae43b00048fb5cb4db5b2'}></input>
+                <input
+                  type={'text'}
+                  defaultValue={'0x23defc2ca207e7fbd84ae43b00048fb5cb4db5b2'}
+                  onChange={(e) => setRecipient(e.target.value)}
+                ></input>
               </div>
               <div>
                 When should the stream start?
-                <input type="datetime-local"></input>
+                <input type="datetime-local" onChange={(e) => setStartDate(new Date(e.target.value))}></input>
               </div>
               <div>
                 When should the stream end?
-                <input type="datetime-local"></input>
+                <input type="datetime-local" onChange={(e) => setEndDate(new Date(e.target.value))}></input>
               </div>
-              <button>Create stream</button>
+              <button onClick={createStream}>Create stream</button>
             </div>
           </DialogContent>
         </Dialog>
