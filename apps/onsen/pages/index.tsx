fb21fdff66fb26f6583ff57032391c6ed7e838b1
OlaStenberg <hello@olastenberg.com>
2022-04-09 14:28:34 +0200

    feat(Onsen): farms

--- apps/onsen/pages/index.tsx
+++ apps/onsen/pages/index.tsx
@@ -1,41 +1,57 @@
-import Link from 'next/link'
-import { useAccount, useConnect } from 'wagmi'
 
-export default function Furo() {
-  const [{ data, error }, connect] = useConnect()
-  const [{ data: accountData }, disconnect] = useAccount({
-    fetchEns: true,
-  })
+import { FC } from 'react'
+import { getBuiltGraphSDK } from '../.graphclient'
+
+interface Props {
+  farms: Farm[]
+}
+interface Farm {
+  id: string
+  token: {
+    id: string
+  }
+  rewardToken: {
+    id: string
+  }
+  endTime: string
+  lastRewardTime: string
+  rewardRemaining: string
+  liquidityStaked: string
+}
+
+const Farms: FC<Props> = (props) => {
+  const {farms} = props
   return (
     <div className="px-2 pt-16">
-      {/* <h1 className="py-4 text-2xl font-bold">Overview</h1>
-      {data.connectors.map((connector) => (
-        <button disabled={!connector.ready} key={connector.id} onClick={() => connect(connector)}>
-          {`Connect to: ` + connector.name + ` ${accountData?.address}`}
-        </button>
-      ))}
-      {!error && accountData?.address ? (
-        <>
+       {farms.length ? (
+          Object.values(farms).map((farm) => (
+            <div key={farm.id}>
+              {farm.id} {``}
+              {farm.token.id} {``}
+              {farm.rewardToken.id} {``}
+              {new Date(parseInt(farm.endTime) * 1000).toLocaleString()} {``}
+              {new Date(parseInt(farm.lastRewardTime) * 1000).toLocaleString()} {``}
+              {farm.rewardRemaining} {``}
+              {farm.liquidityStaked} {``}
+            </div>
+          ))
+        ) : (
           <div>
-            <Link href={`/users/${accountData.address.toLowerCase()}/streams/`}>Streams</Link>
+            <i>No farms found..</i>
           </div>
-          <div>
-            <Link href={`/users/${accountData.address.toLowerCase()}/vestings/`}>Vestings</Link>
-          </div>
-        </>
-      ) : (
-        <div>{error?.message ?? 'Failed to connect'}</div>
-      )} */}
+        )}
 
       
     </div>
   )
 }
 
-// export async function getServerSideProps({ query }) {
-//   const sdk = await getBuiltGraphSDK()
-//   const user = (await sdk.UserVestings({ id: query.address })).vestingUser
-//   return {
-//     props: user,
-//   }
-// }
+export default Farms
+
+export async function getServerSideProps() {
+  const sdk = await getBuiltGraphSDK()
+  const farms = (await sdk.Farms()).incentives
+  return {
+    props: {farms: farms},
+  }
+}
