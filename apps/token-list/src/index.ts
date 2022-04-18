967b6d7ade015e2052837e77a596bb27fe803dff
matthewlilley <hello@matthewlilley.com>
2022-04-18 15:39:50 +0100

    fix(token-list): vercel config builds

--- apps/token-list/src/index.ts
+++ apps/token-list/src/index.ts
@@ -4,10 +4,6 @@ import { log } from 'logger'
 const port = process.env.PORT || 5001
 const server = createServer()
 
-if (process.env.NODE_ENV === 'development') {
-  server.listen(port, () => {
-    log(`api running on ${port}`)
-  })
-}
-
-export { server }
+server.listen(port, () => {
+  log(`api running on ${port}`)
+})
