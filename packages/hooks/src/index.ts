636aa4d41b5acee107a62b50c500baff4cdf54ab
OlaStenberg <hello@olastenberg.com>
2022-04-27 11:33:58 +0200

    feat(hooks): add copy clipboard hook

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -3,3 +3,5 @@ export { default as isWindowVisible } from './useIsWindowVisible'
 export { default as useOnClickOutside } from './useOnClickOutside'
 export { default as usePrevious } from './usePrevious'
 export { default as useWindowSize } from './useWindowSize'
+export { default as useCopyClipboard } from './useCopyClipboard'
+
