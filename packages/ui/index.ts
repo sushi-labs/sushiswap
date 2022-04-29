92ec2dd3862c915458f47595f5239246bbb0c479
matthewlilley <hello@matthewlilley.com>
2022-04-29 06:24:06 +0100

    chore: update dependencies

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,7 +1,8 @@
+// Re-export classnames for conviniance
+export { default as classNames } from 'classnames'
+
 export * from './app'
 export * from './dialog'
 export * from './menu'
 export * from './select'
 export * from './types'
-
-export { default as classNames } from 'classnames'
