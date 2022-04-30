e2ce922f9b66e933cf049eb8ef84f048112cc86e
matthewlilley <hello@matthewlilley.com>
2022-04-30 16:49:16 +0100

    extract: hooks

--- packages/hooks/src/useIsMounted.ts
+++ packages/hooks/src/useIsMounted.ts
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
 
-export function useIsMounted() {
+function useIsMounted() {
   const [mounted, setMounted] = useState(false)
 
   useEffect(() => setMounted(true), [])
 
   return mounted
 }
+
+export default useIsMounted
