d0789bb1b9efec04d3225e1e97feb83bb8ac612a
matthewlilley <hello@matthewlilley.com>
2022-04-30 19:35:24 +0100

    feat: start to flesh out cli commands

--- packages/cli/bin/index.js
+++ packages/cli/bin/index.js
@@ -2,12 +2,28 @@
 
 const { program } = require('commander')
 
+program.version('0.0.0').description('Sushi CLI')
+
 program
-  .version('0.0.0')
-  .description('Sushi CLI')
-  .option('-f, --foo', 'enable some foo')
-  .option('-b, --bar', 'enable some bar')
-  .option('-B, --baz', 'enable some baz')
+  .command('bar')
+  .description('print bar data')
+  .action(() => {
+    console.log('Total SUSHI transfered to the bar:')
+    console.log('Total SUSHI entered the bar:')
+    console.log('Total SUSHI exited the bar:')
+    console.log('APY 24h:')
+    console.log('APY 1w:')
+    console.log('APY 1m:')
+    console.log('APY 1y:')
+  })
+
+program
+  .command('chef')
+  .description('print chef data')
+  .option('-v,--version <version', 'version', 1)
+  .action(({ version }) => {
+    console.log(`printing v${version} chef data`)
+  })
 
 program.parse(process.argv)
 
