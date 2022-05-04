810f8026329f94a1723a4386ea38f9f092b61fa1
LufyCZ <lufy@sushi.com>
2022-05-04 09:47:53 +0000

    refactor: extract bar from cli index

---
+++ packages/cli/actions/bar.ts
@@ -0,0 +1,57 @@
+import { subYears, subMonths, getUnixTime } from 'date-fns'
+import numeral from 'numeral'
+
+import log from '@sushiswap/logger'
+
+import { getBuiltGraphSDK } from './../.graphclient'
+
+export async function bar() {
+  const sdk = getBuiltGraphSDK()
+
+  const oneMonthAgo = getUnixTime(subMonths(new Date(), 1))
+  const threeMonthAgo = getUnixTime(subMonths(new Date(), 3))
+  const sixMonthAgo = getUnixTime(subMonths(new Date(), 6))
+  const oneYearAgo = getUnixTime(subYears(new Date(), 1))
+
+  const [
+    {
+      blocks: [oneYearBlock],
+    },
+    {
+      blocks: [oneMonthBlock],
+    },
+    {
+      blocks: [threeMonthBlock],
+    },
+    {
+      blocks: [sixMonthBlock],
+    },
+  ] = await Promise.all([
+    sdk.Blocks({ where: { timestamp_gt: oneYearAgo, timestamp_lt: oneYearAgo + 30000 } }),
+    sdk.Blocks({ where: { timestamp_gt: oneMonthAgo, timestamp_lt: oneMonthAgo + 30000 } }),
+    sdk.Blocks({ where: { timestamp_gt: threeMonthAgo, timestamp_lt: threeMonthAgo + 30000 } }),
+    sdk.Blocks({ where: { timestamp_gt: sixMonthAgo, timestamp_lt: sixMonthAgo + 30000 } }),
+  ])
+
+  const { bar } = await sdk.Bar()
+
+  const [{ bar: oneYearBar }, { bar: oneMonthBar }, { bar: threeMonthBar }, { bar: sixMonthBar }] = await Promise.all([
+    sdk.Bar({ block: { number: Number(oneYearBlock.number) } }),
+    sdk.Bar({ block: { number: Number(oneMonthBlock.number) } }),
+    sdk.Bar({ block: { number: Number(threeMonthBlock.number) } }),
+    sdk.Bar({ block: { number: Number(sixMonthBlock.number) } }),
+  ])
+
+  // Lukas Witpeerd, [02/05/2022 19:23] ((current ratio / ratio 365 days ago) - 1) * 100
+  log('APR 1y:', numeral(bar?.ratio / oneYearBar?.ratio - 1).format('0.00%'))
+  log('APR 6m:', numeral((bar?.ratio / sixMonthBar?.ratio - 1) * 2).format('0.00%'))
+  log('APR 3m:', numeral((bar?.ratio / threeMonthBar?.ratio - 1) * 4).format('0.00%'))
+  log('APR 1m:', numeral((bar?.ratio / oneMonthBar?.ratio - 1) * 12).format('0.00%'))
+
+  // log('Total SUSHI transfered to the bar:')
+  // log('Total SUSHI entered the bar:', numeral(bar?.sushiStaked).format('0.00 a'))
+  // log('Total SUSHI exited the bar:', numeral(bar?.sushiHarvested).format('0.00 a'))
+  // log('Total xSUSHI minted:', numeral(bar?.xSushiMinted).format('0.00 a'))
+  // log('Total xSUSHI burned:', numeral(bar?.xSushiBurned).format('0.00 a'))
+  // log('Total xSUSHI:', numeral(bar?.totalSupply).format('0.00 a'))
+}
