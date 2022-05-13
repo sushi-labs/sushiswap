bc6c1d0bd471bac85bf54ccee6f81bde11eac310
Chilli Chelli <ramin@sushi.com>
2022-05-13 17:12:25 +0200

    fix(Furo): hydration error on landing page

--- apps/furo/pages/index.tsx
+++ apps/furo/pages/index.tsx
@@ -7,9 +7,11 @@ import { useRouter } from 'next/router'
 import ViewAddressModal from 'features/ViewAddressModal'
 import Button from '@sushiswap/ui/button/Button'
 import Link from 'next/link'
+import { useIsMounted } from '@sushiswap/hooks'
 
 export default function Index() {
   const router = useRouter()
+  const isMounted = useIsMounted()
   const { data: account } = useAccount()
   const connect = useConnect({
     onConnect: () => {
@@ -18,54 +20,57 @@ export default function Index() {
   })
 
   const { isConnected, isReconnecting, isConnecting } = connect
-  if (isConnected || isReconnecting || isConnecting) {
+  if (isMounted && (isConnected || isReconnecting || isConnecting)) {
     if (!account?.address) return <></>
   }
 
-  return (
-    <Layout>
-      <div className="flex flex-col h-full gap-8 pt-40">
-        <div className="flex flex-col gap-1">
-          <Typography variant="hero" className="text-slate-100 font-stretch" weight={900}>
-            Welcome to <span className="text-blue">Furo</span>
-          </Typography>
-          <Typography variant="h3" className="text-slate-500">
-            Decentralized asset streaming on steroids.
-          </Typography>
-        </div>
-        <div className="flex flex-col sm:flex-row gap-4">
-          {account?.address ? (
-            <>
-              <div className="z-10 flex items-center border-[3px] border-slate-900 bg-slate-800 rounded-2xl">
-                <div className="px-6">
-                  <Account.Name address={account.address} className="text-base" />
+  if (isMounted)
+    return (
+      <Layout>
+        <div className="flex flex-col h-full gap-8 pt-40">
+          <div className="flex flex-col gap-1">
+            <Typography variant="hero" className="text-slate-100 font-stretch" weight={900}>
+              Welcome to <span className="text-blue">Furo</span>
+            </Typography>
+            <Typography variant="h3" className="text-slate-500">
+              Decentralized asset streaming on steroids.
+            </Typography>
+          </div>
+          <div className="flex flex-col sm:flex-row gap-4">
+            {account?.address ? (
+              <>
+                <div className="z-10 flex items-center border-[3px] border-slate-900 bg-slate-800 rounded-2xl">
+                  <div className="px-6">
+                    <Account.Name address={account.address} className="text-base" />
+                  </div>
+                  <Link passHref={true} href="/dashboard">
+                    <Button className="w-full transition-all hover:ring-4 ring-blue-800 btn btn-blue btn-filled btn-default text-base text-slate-50 px-10 !h-[56px] rounded-2xl">
+                      Go to Dashboard
+                    </Button>
+                  </Link>
                 </div>
-                <Link passHref={true} href="/dashboard">
-                  <Button className="w-full transition-all hover:ring-4 ring-blue-800 btn btn-blue btn-filled btn-default text-base text-slate-50 px-10 !h-[56px] rounded-2xl">
-                    Go to Dashboard
-                  </Button>
-                </Link>
-              </div>
-              <ViewAddressModal />
-            </>
-          ) : (
-            <>
-              <Wallet.Button
-                hack={connect}
-                button={
-                  <Menu.Button
-                    className="transition-all hover:ring-4 ring-blue-800 btn btn-blue btn-filled btn-default w-full text-base text-slate-50 px-10 !h-[56px] rounded-2xl"
-                    as="div"
-                  >
-                    Connect Wallet
-                  </Menu.Button>
-                }
-              />
-              <ViewAddressModal />
-            </>
-          )}
+                <ViewAddressModal />
+              </>
+            ) : (
+              <>
+                <Wallet.Button
+                  hack={connect}
+                  button={
+                    <Menu.Button
+                      className="transition-all hover:ring-4 ring-blue-800 btn btn-blue btn-filled btn-default w-full text-base text-slate-50 px-10 !h-[56px] rounded-2xl"
+                      as="div"
+                    >
+                      Connect Wallet
+                    </Menu.Button>
+                  }
+                />
+                <ViewAddressModal />
+              </>
+            )}
+          </div>
         </div>
-      </div>
-    </Layout>
-  )
+      </Layout>
+    )
+
+  return null
 }
