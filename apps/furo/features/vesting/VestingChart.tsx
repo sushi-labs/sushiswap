48629cbe3014fc16fe9560f9055cc0d4e282517c
Jack Clancy <jack.clancy93@gmail.com>
2022-05-16 01:57:03 -0400

    feat: styled charts

--- apps/furo/features/vesting/VestingChart.tsx
+++ apps/furo/features/vesting/VestingChart.tsx
@@ -1,8 +1,7 @@
 import { curveStep } from '@visx/curve'
-import { TooltipWithBounds } from '@visx/tooltip'
-import { AnimatedAxis, AnimatedGrid, AnimatedLineSeries, XYChart } from '@visx/xychart'
+import { ParentSize } from '@visx/responsive'
+import { AnimatedAxis, AnimatedGrid, AnimatedLineSeries, XYChart, buildChartTheme, Tooltip } from '@visx/xychart'
 import { FC, useEffect, useState } from 'react'
-
 import { ScheduleRepresentation, Vesting } from '../context'
 
 interface Props {
@@ -11,56 +10,118 @@ interface Props {
 }
 
 interface ChartDataTuple {
-  x: string
+  x: Date
   y: string
 }
 
+const customTheme = buildChartTheme({
+  backgroundColor: '#f09ae9',
+  colors: ['#c90a7a', '#eccdfe'],
+  gridColor: 'rgba(255,255,255,0.05)',
+  gridStyles: { strokeWidth: 1, 'stroke-dasharray': '2' },
+})
+
 export const VestingChart: FC<Props> = (props) => {
   const [chartData, setChartData] = useState<ChartDataTuple[]>()
-
+  const [currentData, setCurrentData] = useState<ChartDataTuple[]>()
   let { vesting, schedule } = props
 
+  console.log(schedule)
+
   useEffect(() => {
-    const data = schedule?.periods.map((period) => {
+    const data = schedule.periods.map((period): ChartDataTuple => {
       const date = new Date(parseInt(period.time) * 1000)
       return {
-        x: date.toISOString().slice(0, 10),
+        x: date,
         y: period.amount,
       }
     })
+    const dataBeforeNow = data.filter(({ x }) => Date.now() >= x.getTime())
     setChartData(data)
+    setCurrentData(data.slice(0, 10))
   }, [schedule])
 
   const accessors = {
-    xAccessor: (d: ChartDataTuple) => d.x,
-    yAccessor: (d: ChartDataTuple) => d.y,
+    xAccessor: (d: ChartDataTuple) => {
+      // console.log(d)
+      return d.x
+    },
+    yAccessor: (d: ChartDataTuple) => {
+      // console.log(d)
+      return d.y
+    },
   }
 
   return (
-    <XYChart
-      height={350}
-      width={700}
-      xScale={{ type: 'band' }}
-      yScale={{ type: 'linear', domain: [0, Number(vesting?.amount.numerator.toString())] }}
-    >
-      <AnimatedAxis orientation="left" numTicks={4} />
-      <AnimatedAxis orientation="bottom" />
-      <AnimatedGrid columns={false} numTicks={4} />
-      <AnimatedLineSeries dataKey={''} data={chartData ?? []} {...accessors} curve={curveStep} />
-      <TooltipWithBounds
-        // @ts-ignore
-        snapTooltipToDatumX
-        snapTooltipToDatumY
-        showSeriesGlyphs
-        renderTooltip={({ tooltipData, colorScale }: { tooltipData: any; colorScale: any }) => (
-          <div>
-            <div style={{ color: colorScale(tooltipData.nearestDatum.key) }}>{tooltipData.nearestDatum.key}</div>
-            {accessors.xAccessor(tooltipData.nearestDatum.datum)}
-            {', '}
-            {accessors.yAccessor(tooltipData.nearestDatum.datum)}
-          </div>
-        )}
-      />
-    </XYChart>
+    <ParentSize>
+      {(parent) => (
+        <>
+          <XYChart
+            height={400}
+            width={parent.width}
+            margin={{ left: 100, right: 0, top: 50, bottom: 50 }}
+            captureEvents={true}
+            xScale={{
+              type: 'time',
+              domain: [vesting.startTime, vesting.endTime],
+            }}
+            yScale={{ type: 'linear', domain: [0, Number(vesting.amount.numerator.toString())] }}
+            theme={customTheme}
+          >
+            <AnimatedAxis
+              orientation="left"
+              numTicks={7}
+              hideTicks={true}
+              hideAxisLine={true}
+              tickClassName="text-gray-100"
+              tickComponent={({ formattedValue, ...tickProps }) => (
+                <text {...tickProps} style={{ fill: 'white' }}>
+                  {formattedValue}
+                </text>
+              )}
+            />
+            <AnimatedAxis
+              orientation="bottom"
+              hideTicks={true}
+              hideAxisLine={true}
+              tickComponent={({ formattedValue, ...tickProps }) => (
+                <text {...tickProps} style={{ fill: 'white' }}>
+                  {formattedValue}
+                </text>
+              )}
+            />
+            <AnimatedGrid columns={false} numTicks={7} />
+            <AnimatedLineSeries
+              enableEvents={true}
+              dataKey={''}
+              data={chartData ?? []}
+              {...accessors}
+              curve={curveStep}
+            />
+            <AnimatedLineSeries
+              dataKey={'1'}
+              enableEvents={false}
+              data={currentData ?? []}
+              {...accessors}
+              curve={curveStep}
+            />
+            <Tooltip
+              // @ts-ignore
+              snapTooltipToDatumX
+              snapTooltipToDatumY
+              showSeriesGlyphs
+              renderTooltip={({ tooltipData, colorScale }: { tooltipData: any; colorScale: any }) => (
+                <div>
+                  <div>{tooltipData.nearestDatum.key}</div>
+                  {accessors.xAccessor(tooltipData.nearestDatum.datum).toString()}
+                  {', '}
+                  {accessors.yAccessor(tooltipData.nearestDatum.datum)}
+                </div>
+              )}
+            />
+          </XYChart>
+        </>
+      )}
+    </ParentSize>
   )
 }
