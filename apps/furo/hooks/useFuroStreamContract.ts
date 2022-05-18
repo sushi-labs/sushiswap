8ae842dcfe15c68e4c8a96a24bd2467acffcd095
OlaStenberg <hello@olastenberg.com>
2022-05-18 17:24:02 +0200

    chore(Furo): update kovan stream contract address

--- apps/furo/hooks/useFuroStreamContract.ts
+++ apps/furo/hooks/useFuroStreamContract.ts
@@ -9,7 +9,7 @@ import { useContract, useContractRead, useNetwork, useSigner } from 'wagmi'
 import FUROSTREAM_ABI from '../abis/FuroStream.json'
 
 export const STREAM_ADDRESS: AddressMap = {
-  [ChainId.KOVAN]: '0xd178a02b5a31bc44c3e2cbf4bb252c7d03faf3fe',
+  [ChainId.KOVAN]: '0xe964d26101136ce2873af5377eb56db8ee502d81',
   [ChainId.GÖRLI]: '0x43d824309cd23c064c0d9740f1dc37078b91adda',
 }
 
