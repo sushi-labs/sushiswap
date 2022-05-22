921a98d2c8de788e4378c51d9abfa80b6bfcbbc3
Chilli Chelli <ramin@sushi.com>
2022-05-22 13:19:26 +0200

    feat(Furo): typo in url

--- apps/furo/functions/getTenderly.ts
+++ apps/furo/functions/getTenderly.ts
@@ -6,7 +6,8 @@ type LogTenderly = (params: LogTenderlyParams) => void
 export const logTenderlyUrl: LogTenderly = async ({ chainId, from, to, data }) => {
   const req = await fetch(`/furo/api/tenderly?chainId=${chainId}&from=${from}&to=${to}&data=${data}`)
   const resp = await req.json()
+
   console.log(
-    `Oops transaction failed! Send the following link to the Sushi Samurai: https://dashboard.tenderly.co/Sushi/furo/simulator/${resp.simulation.id}`,
+    `Oops transaction failed! Send the following link to the Sushi Samurai: https://dashboard.tenderly.com/Sushi/furo/simulator/${resp.simulation.id}`,
   )
 }
