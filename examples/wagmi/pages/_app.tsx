1be95ce1fa78373566c5ccea1bd9383e3992d72d
matthewlilley <hello@matthewlilley.com>
2022-05-26 07:41:32 +0100

    chore: bump deps and fix a few compilation errors

--- examples/wagmi/pages/_app.tsx
+++ examples/wagmi/pages/_app.tsx
@@ -1,19 +1,18 @@
 import '@sushiswap/ui/index.css'
 
-import { client } from '@sushiswap/wagmi'
+import { client, WagmiConfig } from '@sushiswap/wagmi'
 import type { AppProps } from 'next/app'
 import NextHead from 'next/head'
 import { FC } from 'react'
-import { Provider } from 'wagmi'
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <Provider client={client}>
+    <WagmiConfig client={client}>
       <NextHead>
         <title>wagmi</title>
       </NextHead>
       <Component {...pageProps} />
-    </Provider>
+    </WagmiConfig>
   )
 }
 
--- examples/wagmi/pages/index.tsx
+++ examples/wagmi/pages/index.tsx
@@ -1,13 +1,13 @@
 // import dynamic from 'next/dynamic'
 // const Connect = dynamic(() => import('@sushiswap/wagmi/components/Connect'), { ssr: false })
-import { Connect } from '@sushiswap/wagmi'
+import { Wallet } from '@sushiswap/wagmi'
 import React, { FC } from 'react'
 
 const Index: FC = () => {
   return (
     <div className="flex flex-col gap-10">
       <h1 className="text-3xl font-bold underline">Wallet example</h1>
-      <Connect />
+      <Wallet.Button />
     </div>
   )
 }
