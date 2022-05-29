6e0cff958ba516a05f99b90af8e0fb3a42ab34af
OlaStenberg <hello@olastenberg.com>
2022-05-29 16:02:24 +0200

    feat(Onsen): add Incentive table

---
+++ apps/onsen/components/IncentiveTable.tsx
@@ -0,0 +1,120 @@
+import { shortenAddress } from '@sushiswap/format'
+import { Table, Typography } from '@sushiswap/ui'
+import { createTable, getCoreRowModel, useTableInstance } from '@tanstack/react-table'
+import { Incentive, IncentiveRepresentation } from 'features'
+import { useRouter } from 'next/router'
+import React, { FC, useEffect, useMemo, useState } from 'react'
+import { useNetwork } from 'wagmi'
+
+interface IncentiveTableProps {
+  chainId: number | undefined
+  incentives: IncentiveRepresentation[] | undefined
+  placeholder: string
+  loading: boolean
+}
+
+const table = createTable().setRowType<Incentive>()
+
+const defaultColumns = (tableProps: IncentiveTableProps) => [
+  table.createDisplayColumn({
+    id: 'Token',
+    header: () => <div className="w-full text-left"> Token </div>,
+    cell: (props) => shortenAddress(props.row.original?.liquidityStaked.currency.address as string),
+  }),
+  table.createDataColumn('liquidityStaked', {
+    header: () => <div className="w-full text-right">TVL</div>,
+    cell: (props) => {
+      return (
+        <div className="flex flex-col w-full">
+          <Typography variant="sm" weight={700} className="text-right text-slate-200">
+            {props.getValue().greaterThan('0') ? props.getValue().toSignificant(6) : '< 0.01'}
+          </Typography>
+          <Typography variant="xs" weight={500} className="text-right text-slate-500">
+            {props.getValue().currency.symbol}
+          </Typography>
+        </div>
+      )
+    },
+  }),
+  table.createDisplayColumn({
+    id: 'rewards_24h',
+    header: () => <div className="w-full text-left"> Rewards per 24h </div>,
+    cell: () => '',
+  }),
+  table.createDisplayColumn({
+    id: 'APR',
+    header: () => <div className="w-full text-left"> APR </div>,
+    cell: () => '',
+  }),
+]
+
+export const IncentiveTable: FC<IncentiveTableProps> = (props) => {
+  const { incentives, placeholder, loading } = props
+  const [initialized, setInitialized] = useState(!loading)
+
+  useEffect(() => {
+    if (!loading) setInitialized(true)
+  }, [loading])
+
+  const router = useRouter()
+  const { activeChain } = useNetwork()
+  const data = useMemo(() => {
+    if (!incentives) return []
+    return incentives.map((incentive) => new Incentive({ incentive }))
+  }, [incentives])
+
+  const [columns] = React.useState<typeof defaultColumns>(() => [
+    ...defaultColumns({ ...props, chainId: activeChain?.id }),
+  ])
+
+  const instance = useTableInstance(table, {
+    data,
+    // @ts-ignore
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
+  return (
+    <Table.container>
+      <Table.table>
+        <Table.thead>
+          {instance.getHeaderGroups().map((headerGroup) => (
+            <Table.thr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <Table.th key={header.id} colSpan={header.colSpan}>
+                  {header.renderHeader()}
+                </Table.th>
+              ))}
+            </Table.thr>
+          ))}
+        </Table.thead>
+        <Table.tbody>
+          {instance.getRowModel().rows.length === 0 && (
+            <Table.tr>
+              {!initialized ? (
+                <td colSpan={columns.length}>
+                  <div className="w-full h-12 animate-pulse bg-slate-800/30" />
+                </td>
+              ) : (
+                <Table.td colSpan={columns.length} className="!text-xs italic text-center text-slate-500">
+                  {placeholder}
+                </Table.td>
+              )}
+            </Table.tr>
+          )}
+          {instance.getRowModel().rows.map((row) => {
+            return (
+              <Table.tr key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  return <Table.td key={cell.id}>{cell.renderCell()}</Table.td>
+                })}
+              </Table.tr>
+            )
+          })}
+        </Table.tbody>
+      </Table.table>
+    </Table.container>
+  )
+}
+
+export default IncentiveTable
--- apps/onsen/pages/farms/index.tsx
+++ apps/onsen/pages/farms/index.tsx
@@ -1,11 +1,13 @@
 import { Button } from '@sushiswap/ui'
+import IncentiveTable from 'components/IncentiveTable'
+import Layout from 'components/Layout'
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { FC, useMemo } from 'react'
+import { FC } from 'react'
 import useSWR, { SWRConfig } from 'swr'
+import { useAccount, useConnect, useNetwork } from 'wagmi'
 
-import { Incentive } from '../../features/onsen/context/Incentive'
 import { IncentiveRepresentation } from '../../features/onsen/context/representations'
 import { getFarms } from '../../graph/graph-client'
 
@@ -41,42 +43,30 @@ const _FarmsPage: FC<InferGetServerSidePropsType<typeof getServerSideProps>> = (
 export const FarmsPage: FC = () => {
   const router = useRouter()
   const chainId = Number(router.query.chainId as string)
-  // const connect = useConnect()
-  // const { data: account } = useAccount()
-  // const { connecting, reconnecting } = useWalletState(connect, account?.address)
+  const { activeChain } = useNetwork()
+  const { data: account } = useAccount()
+  const connect = useConnect()
   const { data: incentiveRepresentations, isValidating } = useSWR<IncentiveRepresentation[]>(
     `/onsen/api/farms/${chainId}`,
     fetcher
   )
 
-  const incentives = useMemo(
-    () => incentiveRepresentations?.map((incentive) => new Incentive({ incentive })),
-    [incentiveRepresentations]
-  )
-
   return (
-    <div className="px-2 pt-16">
-      <Link passHref={true} href="/farm/create">
-        <Button className="transition-all hover:ring-4 btn btn-blue btn-filled btn-default w-full text-sm sm:text-base text-slate-50 px-8 h-[52px] sm:!h-[56px] rounded-2xl">
-          create incentive
-        </Button>
-      </Link>
-      {incentives?.length ? (
-        Object.values(incentives).map((incentive) => (
-          <div key={incentive.id}>
-            {incentive.id} {``}
-            {incentive.liquidityStaked.toExact()} {``}
-            {incentive.liquidityStaked.currency.symbol} {``}
-            {incentive.rewardRemaining.toExact()} {``}
-            {incentive.rewardRemaining.currency.symbol} {``}
-          </div>
-        ))
-      ) : (
-        <div>
-          <i>No incentives found..</i>
-        </div>
-      )}
-    </div>
+    <Layout>
+      <div>
+        <Link passHref={true} href="/farm/create">
+          <Button className="transition-all hover:ring-4 btn btn-blue btn-filled btn-default w-full text-sm sm:text-base text-slate-50 px-8 h-[52px] sm:!h-[56px] rounded-2xl">
+            create incentive
+          </Button>
+        </Link>
+      </div>
+      <IncentiveTable
+        incentives={incentiveRepresentations}
+        chainId={activeChain?.id}
+        loading={isValidating}
+        placeholder="No incoming incentives found"
+      />
+    </Layout>
   )
 }
 
