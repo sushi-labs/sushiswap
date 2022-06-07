39f6f6c86f0c182cf7b37060df1db48dc264f7de
matthewlilley <hello@matthewlilley.com>
2022-06-07 22:56:33 +0100

    chore(apps/furo): use local graphclient types for now

--- apps/furo/pages/api/vesting/[chainId]/[id]/index.ts
+++ apps/furo/pages/api/vesting/[chainId]/[id]/index.ts
@@ -1,6 +1,7 @@
-import type { Stream } from '@sushiswap/graph-client'
 import { getVesting } from 'lib'
 import type { NextApiRequest, NextApiResponse } from 'next'
+
+import type { Stream } from '.graphclient'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { chainId, id } = req.query
