84da3ddd2908d03c08d9e4995091d5ddef3dfd48
Chilli Chelli <ramin@sushi.com>
2022-06-07 16:23:05 +0200

    fix(UIKit): add network icons

--- packages/wagmi/components/Account/Balance.tsx
+++ packages/wagmi/components/Account/Balance.tsx
@@ -2,7 +2,7 @@ import { ExclamationCircleIcon } from '@heroicons/react/outline'
 import { Amount, Native } from '@sushiswap/currency'
 import { useIsMounted } from '@sushiswap/hooks'
 import { JSBI } from '@sushiswap/math'
-import { Loader, Typography } from '@sushiswap/ui'
+import { Loader, NetworkIcon, Typography } from '@sushiswap/ui'
 import { useBalance, useNetwork } from 'wagmi'
 
 export type Props = {
@@ -23,14 +23,21 @@ export function Balance({ address }: Props): JSX.Element {
   }
 
   return (
-    <Typography weight={700} className="flex gap-1 text-slate-200" as="span">
-      {isMounted &&
-        activeChain &&
-        data &&
-        Amount.fromRawAmount(Native.onChain(activeChain.id), JSBI.BigInt(data.value)).toSignificant(4)}
-      <Typography weight={700} variant="sm" className="text-slate-500" as="span">
-        {activeChain ? Native.onChain(activeChain.id).symbol : 'ETH'}
+    <>
+      <Typography weight={700} className="flex gap-1 items-center text-slate-200" as="span">
+        {activeChain?.id && (
+          <NetworkIcon type="naked" chainId={activeChain.id} width={24} height={24} className="-ml-1.5" />
+        )}
+        <Typography weight={700} className="flex gap-1 items-baseline text-slate-200" as="span">
+          {isMounted &&
+            activeChain &&
+            data &&
+            Amount.fromRawAmount(Native.onChain(activeChain.id), JSBI.BigInt(data.value)).toSignificant(4)}
+          <Typography weight={700} variant="sm" className="text-slate-500" as="span">
+            {activeChain ? Native.onChain(activeChain.id).symbol : 'ETH'}
+          </Typography>
+        </Typography>
       </Typography>
-    </Typography>
+    </>
   )
 }
