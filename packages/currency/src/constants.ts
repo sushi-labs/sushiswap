c5ddbe0cf4f79b59c42ed18d66a740528511f6db
matthewlilley <hello@matthewlilley.com>
2022-06-15 17:47:22 +0100

    feat(packages/currency): generator for hardhat exports to address mapping

--- packages/currency/src/constants.ts
+++ packages/currency/src/constants.ts
@@ -335,6 +335,27 @@ export const DAI_ADDRESS: AddressMap = {
   [ChainId.OPTIMISM]: '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1',
 }
 
+export interface ContractExport {
+  address: string
+  abi: any[]
+  linkedData?: any
+}
+
+export interface Export {
+  chainId: string
+  name: string
+  contracts: { [name: string]: ContractExport }
+}
+
+export type MultiExport = {
+  [chainId: string]: Export[]
+}
+
+// TODO: Use to generate typed address maps from hardhat-deploy exports
+export function exportToContractMap(name: string, multiExport: MultiExport) {
+  return Object.fromEntries(Object.entries(multiExport).map((entry) => [entry[0], entry[1][0].contracts[name]]))
+}
+
 export function addressMapToTokenMap(
   { decimals, symbol, name }: { decimals: number; symbol?: string; name?: string },
   map: AddressMap
@@ -343,7 +364,7 @@ export function addressMapToTokenMap(
     Object.entries(map).map(([chainId, address]) => [
       chainId,
       new Token({
-        chainId: Number(chainId),
+        chainId,
         address,
         decimals,
         symbol,
