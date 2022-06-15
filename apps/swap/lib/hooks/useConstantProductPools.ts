4f2ac85f0303162d043a0b76a56fff46cbfedda6
matthewlilley <hello@matthewlilley.com>
2022-06-15 18:16:59 +0100

    chore(apps/swap): cleanup constant products pools hook

--- apps/swap/lib/hooks/useConstantProductPools.ts
+++ apps/swap/lib/hooks/useConstantProductPools.ts
@@ -1,6 +1,6 @@
 import { Interface } from '@ethersproject/abi'
 import { Amount, Token, Type as Currency } from '@sushiswap/currency'
-import { computeConstantProductPoolAddress, ConstantProductPool, Fee } from '@sushiswap/exchange'
+import { ConstantProductPool } from '@sushiswap/exchange'
 import CONSTANT_PRODUCT_POOL_ABI from 'abis/constant-product-pool.json'
 import { useMultipleContractSingleData, useSingleContractMultipleData } from 'lib/state/multicall'
 import { useMemo } from 'react'
@@ -43,15 +43,13 @@ export type PoolWithState<T> =
   | PoolWithStateNotExists
   | PoolWithStateInvalid
 
-type PoolInput = [Currency | undefined, Currency | undefined, Fee | undefined, boolean | undefined]
-
 interface PoolData {
   address: string
   token0: Token
   token1: Token
 }
 
-export function useGetAllExistedPools(
+export function useConstantProductPools(
   chainId: number,
   currencies: [Currency | undefined, Currency | undefined][]
 ): PoolWithState<ConstantProductPool>[] {
@@ -185,77 +183,4 @@ export function poolListCompare(
     if (!p1.reserve1.equalTo(p2.reserve1)) return 9
   }
   return true
-}
-
-export function useConstantProductPools(chainId: number, pools: PoolInput[]): PoolWithState<ConstantProductPool>[] {
-  const { data: latestBlockNumber } = useBlockNumber({ chainId })
-  const contract = useConstantProductPoolFactoryContract(chainId)
-  const poolsAddresses = useMemo(
-    () =>
-      pools.reduce<(string | undefined)[]>((acc, [tokenA, tokenB, fee, twap]) => {
-        const address =
-          tokenA &&
-          tokenB &&
-          fee &&
-          twap !== undefined &&
-          tokenA.chainId === tokenB.chainId &&
-          !tokenA.equals(tokenB) &&
-          contract?.address
-            ? computeConstantProductPoolAddress({
-                factoryAddress: contract.address,
-                tokenA: tokenA.wrapped,
-                tokenB: tokenB.wrapped,
-                fee,
-                twap,
-              })
-            : undefined
-
-        acc.push(address && !acc.includes(address) ? address : undefined)
-        return acc
-      }, []),
-    [contract?.address, pools]
-  )
-
-  const results = useMultipleContractSingleData(
-    chainId,
-    latestBlockNumber,
-    poolsAddresses,
-    POOL_INTERFACE,
-    'getReserves'
-  )
-  return useMemo(() => {
-    return results.map((result, i) => {
-      const { result: reserves, loading } = result
-      const tokenA = pools[i][0]?.wrapped
-      const tokenB = pools[i][1]?.wrapped
-      const fee = pools[i]?.[2]
-      const twap = pools[i]?.[3]
-      if (loading) return { state: PoolState.LOADING }
-      if (!reserves) return { state: PoolState.NOT_EXISTS }
-      if (!tokenA || !tokenB || tokenA.equals(tokenB)) return { state: PoolState.INVALID }
-
-      const { _reserve0: reserve0, _reserve1: reserve1 } = reserves
-      const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA]
-      return {
-        state: PoolState.EXISTS,
-        pool: new ConstantProductPool(
-          Amount.fromRawAmount(token0, reserve0.toString()),
-          Amount.fromRawAmount(token1, reserve1.toString()),
-          fee,
-          twap
-        ),
-      }
-    })
-  }, [results, pools])
-}
-
-export function useConstantProductPool(
-  chainId: number,
-  tokenA?: Currency,
-  tokenB?: Currency,
-  fee?: Fee,
-  twap?: boolean
-): PoolWithState<ConstantProductPool> {
-  const inputs: [PoolInput] = useMemo(() => [[tokenA, tokenB, fee, twap]], [tokenA, tokenB, fee, twap])
-  return useConstantProductPools(chainId, inputs)[0]
 }
