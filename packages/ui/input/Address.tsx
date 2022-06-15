e1a54ec60a997bc7c03d20f3c438999b1e519bcd
matthewlilley <hello@matthewlilley.com>
2022-06-15 17:52:43 +0100

    refactor(packakges/ui): address input component to be type of search

--- packages/ui/input/Address.tsx
+++ packages/ui/input/Address.tsx
@@ -4,6 +4,7 @@ import React, { forwardRef } from 'react'
 import { DEFAULT_INPUT_CLASSNAME, ERROR_INPUT_CLASSNAME } from './index'
 
 export type AddressProps = Omit<React.HTMLProps<HTMLInputElement>, 'as' | 'onChange' | 'value'> & {
+  id: string
   error?: boolean
   value: string | undefined
   onChange(x: string): void
@@ -14,7 +15,8 @@ const matchSpaces = /\s+/g
 export const Address = forwardRef<HTMLInputElement, AddressProps>(
   (
     {
-      value,
+      id,
+      value = '',
       onChange,
       placeholder = 'Address or ENS name',
       title = 'Address or ENS name',
@@ -27,14 +29,15 @@ export const Address = forwardRef<HTMLInputElement, AddressProps>(
     return (
       <>
         <input
+          id={id}
           ref={ref}
           title={title}
           placeholder={placeholder}
-          value={value || ''}
-          type="text"
+          value={value}
+          type="search"
           className={classNames(DEFAULT_INPUT_CLASSNAME, error ? ERROR_INPUT_CLASSNAME : '', className)}
           onChange={(event) => onChange && onChange(event.target.value.replace(matchSpaces, ''))}
-          inputMode="text"
+          inputMode="search"
           autoComplete="off"
           autoCorrect="off"
           autoCapitalize="off"
