108b817df083df99f029f4ae7f548a7b59275609
OlaStenberg <hello@olastenberg.com>
2022-06-17 16:10:20 +0200

    refactor(api/price): remove maintainer source code

--- apis/price/config.ts
+++ apis/price/config.ts
@@ -17,19 +17,3 @@ export const SUPPORTED_CHAINS = [
 ]
 
 export const GRAPH_HOST = 'api.thegraph.com'
-
-export const LEGACY_SUBGRAPH_NAME: Record<string | number, string> = {
-  [ChainId.ARBITRUM]: 'sushiswap/arbitrum-exchange',
-  [ChainId.AVALANCHE]: 'sushiswap/avalanche-exchange',
-  [ChainId.BSC]: 'sushiswap/bsc-exchange',
-  [ChainId.CELO]: 'jiro-ono/sushitestsubgraph',
-  [ChainId.ETHEREUM]: 'sushiswap/exchange',
-  [ChainId.FANTOM]: 'sushiswap/fantom-exchange',
-  [ChainId.GNOSIS]: 'sushiswap/xdai-exchange',
-  // [ChainId.HECO]: 'heco-exchange/heco',
-  // [ChainId.HARMONY]: 'sushiswap/harmony-exchange',
-  [ChainId.MOONBEAM]: 'sushiswap/moonbeam-exchange',
-  [ChainId.MOONRIVER]: 'sushiswap/moonriver-exchange',
-  [ChainId.FUSE]: 'sushiswap/fuse-exchange',
-  [ChainId.POLYGON]: 'sushiswap/matic-exchange',
-}
