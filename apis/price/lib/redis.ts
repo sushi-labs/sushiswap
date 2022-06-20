f75ec8072bec9f1c4cf9e75cb2d00a805be59216
OlaStenberg <hello@olastenberg.com>
2022-06-20 07:26:34 +0200

    fix(apis/price): validate redis envs

--- apis/price/lib/redis.ts
+++ apis/price/lib/redis.ts
@@ -1,5 +1,8 @@
 import Redis from 'ioredis'
 
+if (!process.env.UPSTASH_REDIS_REST_URL) throw new Error('UPSTASH_REDIS_REST_URL is required for redis')
+if (!process.env.UPSTASH_REDIS_REST_PASSWORD) throw new Error('UPSTASH_REDIS_REST_PASSWORD is required for redis')
+
 const path = `rediss://:${process.env.UPSTASH_REDIS_REST_PASSWORD}@${process.env.UPSTASH_REDIS_REST_URL}`
 
 const redis = new Redis(path, {
