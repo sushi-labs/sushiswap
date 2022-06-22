b3aea1e7ae4dba10d61db7c2a1368af1506123f3
matthewlilley <hello@matthewlilley.com>
2022-06-22 08:13:05 +0100

    feat(packages/currency): add toAmount on Share

--- packages/currency/src/Share.ts
+++ packages/currency/src/Share.ts
@@ -1,6 +1,7 @@
-import { BigintIsh, Fraction, JSBI, MAX_UINT128 } from '@sushiswap/math'
+import { BigintIsh, Fraction, JSBI, MAX_UINT128, ZERO } from '@sushiswap/math'
 import invariant from 'tiny-invariant'
 
+import { Amount } from './Amount'
 import { Type } from './Type'
 
 export class Share<T extends Type> extends Fraction {
@@ -16,5 +17,17 @@ export class Share<T extends Type> extends Fraction {
     invariant(JSBI.lessThanOrEqual(this.quotient, MAX_UINT128), 'SHARE')
     this.currency = currency
     this.scale = JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(currency.decimals))
+  }
+
+  public toAmount(rebase: { base: JSBI; elastic: JSBI }, roundUp = false) {
+    return Amount.fromRawAmount(
+      this.currency,
+      JSBI.GT(rebase.base, 0)
+        ? JSBI[roundUp ? 'add' : 'subtract'](
+            JSBI.divide(JSBI.multiply(this.quotient, rebase.elastic), rebase.base),
+            JSBI.BigInt(1)
+          )
+        : ZERO
+    )
   }
 }
