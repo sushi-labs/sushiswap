ea9f1f23b5c46fae61897e236e56bbf405b414cb
matthewlilley <hello@matthewlilley.com>
2022-06-26 13:17:23 +0100

    refactor(api/price): use date-fns and flatten

--- apis/price/api/v0/[chainId].ts
+++ apis/price/api/v0/[chainId].ts
@@ -1,21 +1,29 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { getUnixTime } from 'date-fns'
 
 import { SUPPORTED_CHAINS } from '../../config'
 import redis from '../../lib/redis'
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-  const { chainId } = request.query
-  if (!SUPPORTED_CHAINS.includes(Number(chainId))) {
+  const chainId = request.query.chainId as string
+  if (!SUPPORTED_CHAINS.includes(chainId)) {
     response
       .status(400)
       .json({ message: 'Unsupported network. Supported chain ids: '.concat(SUPPORTED_CHAINS.join(', ')) })
   }
-  const data = await redis.hget('prices', chainId as string)
+
+  const data = await redis.hget('prices', chainId)
+
   if (!data) {
     return response.status(204)
   }
+
   const json = JSON.parse(data)
-  const updatedSecondsAgo = Number((Date.now() / 1000).toFixed()) - json.meta.updatedAtTimestamp
-  json.meta.updatedSecondsAgo = updatedSecondsAgo
-  return response.status(200).json(json)
+
+  const now = getUnixTime(Date.now())
+
+  return response.status(200).json({
+    ...json,
+    updatedSecondsAgo: now - json.updatedAtTimestamp,
+  })
 }
