5fdbf855dd83518430979c287de1dde979d88665
matthewlilley <hello@matthewlilley.com>
2022-06-26 07:37:20 +0100

    fix(packakges/ui): currency icon native

--- packages/ui/currency/Icon.tsx
+++ packages/ui/currency/Icon.tsx
@@ -5,33 +5,60 @@ import { WrappedTokenInfo } from '@sushiswap/token-lists'
 import Image, { ImageProps } from 'next/image'
 import { useMemo } from 'react'
 
-const BLOCKCHAIN: Record<number, string> = {
-  [ChainId.ETHEREUM]: 'ethereum',
-  [ChainId.BSC]: 'binance',
-  [ChainId.CELO]: 'celo',
-  [ChainId.FANTOM]: 'fantom',
-  [ChainId.AVALANCHE_TESTNET]: 'fuji',
-  [ChainId.FUSE]: 'fuse',
-  [ChainId.HARMONY]: 'harmony',
-  [ChainId.HECO]: 'heco',
-  [ChainId.POLYGON]: 'polygon',
-  [ChainId.MOONRIVER]: 'moonriver',
-  [ChainId.OKEX]: 'okex',
-  [ChainId.PALM]: 'palm',
-  [ChainId.TELOS]: 'telos',
-  [ChainId.GNOSIS]: 'gnosis',
-  [ChainId.ARBITRUM]: 'arbitrum',
-  [ChainId.AVALANCHE]: 'avalanche',
-  [ChainId.OPTIMISM]: 'optimism',
-  [ChainId.MOONBEAM]: 'moonbeam',
+const AvalancheLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/avax.jpg'
+const BinanceCoinLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/bnb.jpg'
+const EthereumLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/eth.jpg'
+const FantomLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/ftm.jpg'
+const HarmonyLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/one.jpg'
+const HecoLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/heco.jpg'
+const MaticLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/polygon.jpg'
+const MoonbeamLogo = 'https://raw.githubusercontent.com/sushiswap/icons/master/network/moonbeam.jpg'
+const OKExLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/okt.jpg'
+const xDaiLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/xdai.jpg'
+const CeloLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/celo.jpg'
+const PalmLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/palm.jpg'
+const MovrLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/movr.jpg'
+const FuseLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/fuse.jpg'
+const TelosLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/telos.jpg'
+
+const LOGO: Record<number, string> = {
+  [ChainId.ETHEREUM]: EthereumLogo,
+  [ChainId.KOVAN]: EthereumLogo,
+  [ChainId.RINKEBY]: EthereumLogo,
+  [ChainId.ROPSTEN]: EthereumLogo,
+  [ChainId.GÖRLI]: EthereumLogo,
+  [ChainId.FANTOM]: FantomLogo,
+  [ChainId.FANTOM_TESTNET]: FantomLogo,
+  [ChainId.POLYGON]: MaticLogo,
+  [ChainId.POLYGON_TESTNET]: MaticLogo,
+  [ChainId.GNOSIS]: xDaiLogo,
+  [ChainId.BSC]: BinanceCoinLogo,
+  [ChainId.BSC_TESTNET]: BinanceCoinLogo,
+  [ChainId.AVALANCHE]: AvalancheLogo,
+  [ChainId.AVALANCHE_TESTNET]: AvalancheLogo,
+  [ChainId.HECO]: HecoLogo,
+  [ChainId.HECO_TESTNET]: HecoLogo,
+  [ChainId.HARMONY]: HarmonyLogo,
+  [ChainId.HARMONY_TESTNET]: HarmonyLogo,
+  [ChainId.OKEX]: OKExLogo,
+  [ChainId.OKEX_TESTNET]: OKExLogo,
+  [ChainId.ARBITRUM]: EthereumLogo,
+  [ChainId.ARBITRUM_TESTNET]: EthereumLogo,
+  [ChainId.CELO]: CeloLogo,
+  [ChainId.PALM]: PalmLogo,
+  [ChainId.PALM_TESTNET]: PalmLogo,
+  [ChainId.MOONRIVER]: MovrLogo,
+  [ChainId.FUSE]: FuseLogo,
+  [ChainId.TELOS]: TelosLogo,
+  [ChainId.HARDHAT]: EthereumLogo,
+  [ChainId.MOONBEAM]: MoonbeamLogo,
+  [ChainId.OPTIMISM]: EthereumLogo,
 }
 
 export function Icon({ currency, ...rest }: { currency: Currency } & Omit<ImageProps, 'src'>) {
   const src = useMemo(() => {
     if (currency.isNative) {
-      return `https://raw.githubusercontent.com/sushiswap/list/master/logos/token-logos/network/${
-        BLOCKCHAIN[currency.chainId]
-      }/${currency.wrapped.address}.jpg`
+      return LOGO[currency.chainId]
     }
 
     if (currency instanceof WrappedTokenInfo && currency.logoURI) {
