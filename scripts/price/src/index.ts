bb87a693aab2fdaa404acf0fddfb82675064a5d7
matthewlilley <hello@matthewlilley.com>
2022-06-26 13:14:34 +0100

    refactor(scripts/price): simplify

--- scripts/price/src/index.ts
+++ scripts/price/src/index.ts
@@ -1,27 +1,41 @@
+import 'dotenv/config'
+
+import { getUnixTime } from 'date-fns'
+
 import { SUPPORTED_CHAINS } from './config'
 import { getPrices } from './lib/graph'
-import { pricesToJson } from './lib/mapper'
 import redis from './lib/redis'
 
 export async function execute() {
-  const chainPrices: ReturnType<typeof getPrices>[] = []
-  console.log('Updating prices for chains: '.concat(SUPPORTED_CHAINS.join(', ')))
-  for (const chainId of SUPPORTED_CHAINS as unknown as string[]) {
-    chainPrices.push(getPrices(chainId))
-  }
-
-  await (
-    await Promise.all(chainPrices)
-  ).forEach(async (result) => {
-    if (result?.chainId) {
-      const json = pricesToJson(result.chainId, result.data)
-      await redis.hset('prices', result.chainId, JSON.stringify(json))
-    } else {
-      console.log(`no result, ignore.`)
-    }
-  })
-
-  console.log(`Finished updating prices.`)
+  console.log(`Updating prices for chains: ${SUPPORTED_CHAINS.join(', ')}`)
+  const results = await Promise.all(SUPPORTED_CHAINS.map((chainId) => getPrices(chainId)))
+  await redis.hset(
+    'prices',
+    Object.fromEntries(
+      results.map(({ chainId, data }) => {
+        const ethPrice = data.legacy_exchange_bundle?.ethPrice
+        const updatedAtBlock = data.legacy_exchange__meta?.block.number
+        const updatedAtTimestamp = getUnixTime(Date.now())
+        return [
+          chainId,
+          JSON.stringify({
+            chainId,
+            ...Object.fromEntries(
+              data.legacy_exchange_tokens
+                .filter((token) => token.derivedETH != 0)
+                .map((token) => {
+                  const price = Number(token.derivedETH) * Number(ethPrice)
+                  return [token.id, price]
+                })
+            ),
+            updatedAtBlock,
+            updatedAtTimestamp,
+          }),
+        ]
+      })
+    )
+  )
+  console.log(`Finished updating prices for chains: ${SUPPORTED_CHAINS.join(', ')}`)
   process.exit()
 }
 execute()
--- scripts/price/src/lib/graph.ts
+++ scripts/price/src/lib/graph.ts
@@ -1,10 +1,10 @@
+import { ChainId } from '@sushiswap/chain'
+
 import { getBuiltGraphSDK } from '../../.graphclient'
-import { LEGACY_SUBGRAPH_NAME, SUPPORTED_CHAINS } from '../config'
+import { GRAPH_HOST, LEGACY_SUBGRAPH_NAME, SUPPORTED_CHAINS } from '../config'
 
-const GRAPH_HOST = 'api.thegraph.com'
-
-export const getPrices = async (chainId: string) => {
-  if (!SUPPORTED_CHAINS.includes(Number(chainId))) {
+export const getPrices = async (chainId: ChainId) => {
+  if (!SUPPORTED_CHAINS.includes(chainId)) {
     throw Error(`Unsupported Chain ${chainId}`)
   }
   const sdk = await getBuiltGraphSDK({ chainId, host: GRAPH_HOST, name: LEGACY_SUBGRAPH_NAME[chainId] })
