238bc35d4d7c0410b734fb030b31de6c2b18717a
matthewlilley <hello@matthewlilley.com>
2022-06-28 08:29:37 +0100

    chore: use REDIS_URL

--- apis/price/lib/redis.ts
+++ apis/price/lib/redis.ts
@@ -2,8 +2,6 @@ import Redis from 'ioredis'
 
 if (!process.env.REDIS_URL) throw new Error('REDIS_URL is required')
 
-const redis = new Redis(process.env.REDIS_URL, {
-  maxRetriesPerRequest: 1, // TODO: remove this later, limitation of testing a free redis server
-})
+const redis = new Redis(process.env.REDIS_URL)
 
 export default redis
--- scripts/price/src/lib/redis.ts
+++ scripts/price/src/lib/redis.ts
@@ -1,11 +1,7 @@
 import Redis from 'ioredis'
 
-if (!process.env.UPSTASH_REDIS_REST_URL) throw new Error('UPSTASH_REDIS_REST_URL is required for redis')
-if (!process.env.UPSTASH_REDIS_REST_PASSWORD) throw new Error('UPSTASH_REDIS_REST_PASSWORD is required for redis')
+if (!process.env.REDIS_URL) throw new Error('REDIS_URL is required')
 
-const path = `rediss://:${process.env.UPSTASH_REDIS_REST_PASSWORD}@${process.env.UPSTASH_REDIS_REST_URL}`
-const redis = new Redis(path, {
-  maxRetriesPerRequest: 1, // TODO: remove this later, limitation of testing a free redis server
-})
+const redis = new Redis(process.env.REDIS_URL)
 
 export default redis
