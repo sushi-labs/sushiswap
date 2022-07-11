3586aa02ceeb1fc4234a3d7a33c48c2ffb5eb82c
matthewlilley <hello@matthewlilley.com>
2022-07-11 01:44:58 +0100

    feat(apps/kashi): order by server side plus defualt

--- apps/kashi/pages/index.tsx
+++ apps/kashi/pages/index.tsx
@@ -1,32 +1,29 @@
 import { ChainId } from '@sushiswap/chain'
 import { formatPercent } from '@sushiswap/format'
-import { Link, Table } from '@sushiswap/ui'
+import { Table } from '@sushiswap/ui'
 import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table'
+import get from 'lodash.get'
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 
 import { getBuiltGraphSDK, KashiPair as KashiPairDTO } from '.graphclient'
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const orderBy = query.orderBy || 'kpi.supplyAPR'
   const sdk = await getBuiltGraphSDK()
   const { crossChainKashiPairs: data } = await sdk.CrossChainKashiPairs({
     chainIds: [ChainId.ARBITRUM, ChainId.POLYGON],
   })
-
-  console.log({ data })
-
-  // data.sort((a, b) => {
-  //   if (Number(a.totalAsset.base) > Number(b.totalAsset.base)) {
-  //     return -1
-  //   }
-  //   if (Number(a.totalAsset.base) < Number(b.totalAsset.base)) {
-  //     return 1
-  //   }
-  //   return 0
-  // })
-
   return {
     props: {
-      data,
+      data: data.sort((a, b) => {
+        if (Number(get(a, orderBy)) > Number(get(b, orderBy))) {
+          return -1
+        }
+        if (Number(get(a, orderBy)) < Number(get(b, orderBy))) {
+          return 1
+        }
+        return 0
+      }),
     },
   }
 }
@@ -51,7 +48,7 @@ const columns: ColumnDef<KashiPairDTO>[] = [
     header: () => 'Asset',
     cell: (cell) => (
       <>
-        <div>{cell.getValue().name}</div>
+        <div className="font-semibold">{cell.getValue().name}</div>
         <div className="text-sm font-light text-slate-400">{cell.getValue().symbol}</div>
       </>
     ),
@@ -65,7 +62,7 @@ const columns: ColumnDef<KashiPairDTO>[] = [
     header: () => 'Collateral',
     cell: (cell) => (
       <>
-        <div>{cell.getValue().name}</div>
+        <div className="font-semibold">{cell.getValue().name}</div>
         <div className="text-sm font-light text-slate-400">{cell.getValue().symbol}</div>
       </>
     ),
@@ -77,7 +74,7 @@ const columns: ColumnDef<KashiPairDTO>[] = [
   {
     accessorKey: 'totalAsset',
     header: () => 'Supplied',
-    cell: (cell) => cell.getValue().base,
+    cell: (cell) => <div className="font-semibold">{cell.getValue().base}</div>,
     footer: (props) => props.column.id,
     meta: {
       align: 'right',
@@ -86,7 +83,7 @@ const columns: ColumnDef<KashiPairDTO>[] = [
   {
     accessorKey: 'kpi',
     header: () => 'Supply APR',
-    cell: (cell) => formatPercent(cell.getValue().supplyAPR / 1e18),
+    cell: (cell) => <div className="font-semibold">{formatPercent(cell.getValue().supplyAPR / 1e18)}</div>,
     footer: (props) => props.column.id,
     meta: {
       align: 'right',
@@ -95,7 +92,7 @@ const columns: ColumnDef<KashiPairDTO>[] = [
   {
     accessorKey: 'totalBorrow',
     header: () => 'Borrowed',
-    cell: (cell) => cell.getValue().base,
+    cell: (cell) => <div className="font-semibold">{cell.getValue().base}</div>,
     footer: (props) => props.column.id,
     meta: {
       align: 'right',
@@ -104,34 +101,34 @@ const columns: ColumnDef<KashiPairDTO>[] = [
   {
     accessorKey: 'kpi',
     header: () => 'Borrow APR',
-    cell: (cell) => formatPercent(cell.getValue().borrowAPR / 1e18),
+    cell: (cell) => <div className="font-semibold">{formatPercent(cell.getValue().borrowAPR / 1e18)}</div>,
     footer: (props) => props.column.id,
     meta: {
       align: 'right',
     },
   },
   // Display Column
-  {
-    id: 'supply',
-    header: () => 'Supply',
-    cell: (props) => (
-      <Link.Internal href={`/${props.row.original.chainId}/${props.row.original.id}/supply`}>Supply</Link.Internal>
-    ),
-    meta: {
-      align: 'center',
-    },
-  },
+  // {
+  //   id: 'supply',
+  //   header: () => 'Supply',
+  //   cell: (props) => (
+  //     <Link.Internal href={`/${props.row.original.chainId}/${props.row.original.id}/supply`}>Supply</Link.Internal>
+  //   ),
+  //   meta: {
+  //     align: 'center',
+  //   },
+  // },
   // Display Column
-  {
-    id: 'borrow',
-    header: () => 'Borrow',
-    cell: (props) => (
-      <Link.Internal href={`/${props.row.original.chainId}/${props.row.original.id}/borrow`}>Borrow</Link.Internal>
-    ),
-    meta: {
-      align: 'center',
-    },
-  },
+  // {
+  //   id: 'borrow',
+  //   header: () => 'Borrow',
+  //   cell: (props) => (
+  //     <Link.Internal href={`/${props.row.original.chainId}/${props.row.original.id}/borrow`}>Borrow</Link.Internal>
+  //   ),
+  //   meta: {
+  //     align: 'center',
+  //   },
+  // },
 ]
 
 export default function Index({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
@@ -168,7 +165,7 @@ export default function Index({ data }: InferGetServerSidePropsType<typeof getSe
               {table.getRowModel().rows.map((row) => (
                 <Table.tr key={row.id}>
                   {row.getVisibleCells().map((cell) => (
-                    <Table.td key={cell.id} align={(cell.column.columnDef.meta as any)?.align}>
+                    <Table.td key={cell.id} align={(cell.column.columnDef.meta as any)?.align} className="py-2">
                       {flexRender(cell.column.columnDef.cell, cell.getContext())}
                     </Table.td>
                   ))}
