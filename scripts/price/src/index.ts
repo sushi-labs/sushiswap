c41c2df56a290a3eb957ac1a61fdef92f99599b9
LufyCZ <lufy@sushi.com>
2022-07-12 19:02:51 +0000

    feat(scripts/price): refactor, trident support

--- scripts/price/src/index.ts
+++ scripts/price/src/index.ts
@@ -1,80 +1,121 @@
 import 'dotenv/config'
 
+import { ChainId } from '@sushiswap/chain'
+import { WNATIVE_ADDRESS } from '@sushiswap/core-sdk'
 import { getUnixTime } from 'date-fns'
 
 import { getBuiltGraphSDK } from '../.graphclient'
 import { SUSHISWAP_CHAINS, SUSHISWAP_SUBGRAPH_NAME, TRIDENT_CHAINS, TRIDENT_SUBGRAPH_NAME } from './config'
 import redis from './redis'
 
-export async function execute() {
-  console.log(`Updating prices for chains: ${SUSHISWAP_CHAINS.join(', ')}`)
-
-  const sushiSwapResults = await Promise.all(
+async function getSushiSwapResults() {
+  const results = await Promise.all(
     SUSHISWAP_CHAINS.map((chainId) => {
-      if (!SUSHISWAP_CHAINS.includes(chainId)) {
-        throw Error(`Unsupported Chain ${chainId}`)
-      }
       const sdk = getBuiltGraphSDK({ chainId, name: SUSHISWAP_SUBGRAPH_NAME[chainId] })
-      return sdk.SushiSwapTokens({ first: 100000 })
+      return sdk.SushiSwapTokens({ first: 100000, where: { derivedETH_gt: 0 } })
     })
   )
 
-  const tridentResults = await Promise.all(
+  return results.map((result, i) => {
+    const nativePrice = Number(result.bundle?.ethPrice)
+    const updatedAtBlock = Number(result._meta?.block.number)
+
+    return {
+      chainId: SUSHISWAP_CHAINS[i],
+      updatedAtBlock,
+      tokens: result.tokens.map((token) => ({
+        id: token.id,
+        priceUSD: token.derivedETH * nativePrice,
+        liquidityNative: Number(token.liquidity),
+      })),
+    }
+  })
+}
+
+async function getTridentResults() {
+  const results = await Promise.all(
     TRIDENT_CHAINS.map((chainId) => {
-      if (!TRIDENT_CHAINS.includes(chainId)) {
-        throw Error(`Unsupported Chain ${chainId}`)
-      }
       const sdk = getBuiltGraphSDK({ chainId, name: TRIDENT_SUBGRAPH_NAME[chainId] })
-      return sdk.TridentTokens({ first: 100000 })
+      return sdk.TridentTokens({
+        first: 100000,
+        where: {
+          derivedNative_gt: 0,
+        },
+        native: WNATIVE_ADDRESS[chainId].toLowerCase(),
+      })
     })
   )
+
+  return results.map((result, i) => {
+    const nativePrice = Number(result.native?.derivedUSD)
+    const updatedAtBlock = Number(result._meta?.block.number)
+
+    return {
+      chainId: TRIDENT_CHAINS[i],
+      updatedAtBlock,
+      tokens: result.tokenPrices.map((tokenPrice) => ({
+        id: tokenPrice.id,
+        priceUSD: tokenPrice.derivedNative * nativePrice,
+        liquidityNative: Number(tokenPrice.token.kpi?.liquidity),
+      })),
+    }
+  })
+}
+
+export async function execute() {
+  console.log(
+    `Updating prices for chains: ${[...SUSHISWAP_CHAINS, ...TRIDENT_CHAINS]
+      .map((chainId) => ChainId[chainId])
+      .join(', ')}`
+  )
+
+  const results = (await Promise.all([getSushiSwapResults(), getTridentResults()])).flat()
+  const chainIds = Array.from(new Set(results.map((result) => result.chainId)))
+  // Legacy Exchange and Trident tokens combined, higher liquidity wins
+  const combined = chainIds.map((chainId) => {
+    // Get all sources for specific chain (Legacy, Trident or Legacy+Trident)
+    const sources = results.filter((result) => result.chainId === chainId)
+
+    let tokens
+
+    // No need to go through everything if there's just going to be one entry anyway
+    if (sources.length === 1) {
+      tokens = sources[0].tokens
+    } else {
+      const allTokens = sources.flatMap((result) => result.tokens)
+      const seenTokens = new Map<string, { priceUSD: number; liquidityNative: number }>()
+      allTokens.forEach((token) => {
+        const previousBestToken = seenTokens.get(token.id)
+
+        if (previousBestToken) {
+          if (previousBestToken.liquidityNative < token.liquidityNative) {
+            seenTokens.set(token.id, { priceUSD: token.priceUSD, liquidityNative: token.liquidityNative })
+          }
+        } else {
+          seenTokens.set(token.id, { priceUSD: token.priceUSD, liquidityNative: token.liquidityNative })
+        }
+      })
+      tokens = Array.from(seenTokens.entries()).map(([id, { priceUSD }]) => ({ id, priceUSD }))
+    }
+
+    return { chainId, updatedAtBlock: sources[0].updatedAtBlock, updatedAtTimestamp: getUnixTime(Date.now()), tokens }
+  })
 
   await redis.hset(
     'prices',
-    Object.fromEntries([
-      ...SUSHISWAP_CHAINS.map((chainId, i) => {
-        const ethPrice = sushiSwapResults[i].bundle?.ethPrice
-        const updatedAtBlock = sushiSwapResults[i]._meta?.block.number
-        const updatedAtTimestamp = getUnixTime(Date.now())
-        return [
+    Object.fromEntries(
+      combined.map(({ chainId, tokens, updatedAtBlock, updatedAtTimestamp }) => [
+        chainId,
+        JSON.stringify({
           chainId,
-          JSON.stringify({
-            chainId,
-            ...Object.fromEntries(
-              sushiSwapResults[i].tokens
-                .filter((token) => token.derivedETH != 0)
-                .map((token) => {
-                  const price = Number(token.derivedETH) * Number(ethPrice)
-                  return [token.id, price]
-                })
-            ),
-            updatedAtBlock,
-            updatedAtTimestamp,
-          }),
-        ]
-      }),
-      ...TRIDENT_CHAINS.map((chainId, i) => {
-        const updatedAtBlock = tridentResults[i]._meta?.block.number
-        const updatedAtTimestamp = getUnixTime(Date.now())
-        return [
-          chainId,
-          JSON.stringify({
-            chainId,
-            ...Object.fromEntries(
-              tridentResults[i].tokens
-                .filter((token) => token.price.derivedNative != 0)
-                .map((token) => {
-                  return [token.id, Number(token.price.derivedUSD)]
-                })
-            ),
-            updatedAtBlock,
-            updatedAtTimestamp,
-          }),
-        ]
-      }),
-    ])
+          ...tokens.reduce((acc, token) => ({ ...acc, [token.id]: token.priceUSD })),
+          updatedAtBlock,
+          updatedAtTimestamp,
+        }),
+      ])
+    )
   )
-  console.log(`Finished updating prices for chains: ${SUSHISWAP_CHAINS.join(', ')}`)
+  console.log(`Finished updating prices`)
   process.exit()
 }
 execute()
