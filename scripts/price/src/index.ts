0a8672522092dfb0a9ba111f8ec52c43ac13f8c8
matthewlilley <hello@matthewlilley.com>
2022-07-12 14:41:30 +0100

    feat(scripts/price): trident prices

--- scripts/price/src/index.ts
+++ scripts/price/src/index.ts
@@ -3,36 +3,47 @@ import 'dotenv/config'
 import { getUnixTime } from 'date-fns'
 
 import { getBuiltGraphSDK } from '../.graphclient'
-import { GRAPH_HOST, LEGACY_SUBGRAPH_NAME, SUPPORTED_CHAINS } from './config'
+import { GRAPH_HOST, LEGACY_SUBGRAPH_NAME, SUSHISWAP_CHAINS, TRIDENT_CHAINS, TRIDENT_SUBGRAPH_NAME } from './config'
 import redis from './redis'
 
 export async function execute() {
-  console.log(`Updating prices for chains: ${SUPPORTED_CHAINS.join(', ')}`)
+  console.log(`Updating prices for SushiSwap chains: ${SUSHISWAP_CHAINS.join(', ')}`)
 
-  const results = await Promise.all(
-    SUPPORTED_CHAINS.map((chainId) => {
-      if (!SUPPORTED_CHAINS.includes(chainId)) {
+  const sushiSwapResults = await Promise.all(
+    SUSHISWAP_CHAINS.map((chainId) => {
+      if (!SUSHISWAP_CHAINS.includes(chainId)) {
         throw Error(`Unsupported Chain ${chainId}`)
       }
       console.log({ chainId, host: GRAPH_HOST, subgraphName: LEGACY_SUBGRAPH_NAME[chainId] })
       const sdk = getBuiltGraphSDK({ chainId, host: GRAPH_HOST, subgraphName: LEGACY_SUBGRAPH_NAME[chainId] })
-      return sdk.TokenPrices()
+      return sdk.SushiSwapTokenPrices({ first: 100000 })
+    })
+  )
+
+  const tridentResults = await Promise.all(
+    TRIDENT_CHAINS.map((chainId) => {
+      if (!TRIDENT_CHAINS.includes(chainId)) {
+        throw Error(`Unsupported Chain ${chainId}`)
+      }
+      console.log({ chainId, host: GRAPH_HOST, subgraphName: TRIDENT_SUBGRAPH_NAME[chainId] })
+      const sdk = getBuiltGraphSDK({ chainId, host: GRAPH_HOST, subgraphName: TRIDENT_SUBGRAPH_NAME[chainId] })
+      return sdk.TridentTokenPrices({ first: 100000 })
     })
   )
 
   await redis.hset(
     'prices',
-    Object.fromEntries(
-      SUPPORTED_CHAINS.map((chainId, i) => {
-        const ethPrice = results[i].bundle?.ethPrice
-        const updatedAtBlock = results[i]._meta?.block.number
+    Object.fromEntries([
+      ...SUSHISWAP_CHAINS.map((chainId, i) => {
+        const ethPrice = sushiSwapResults[i].bundle?.ethPrice
+        const updatedAtBlock = sushiSwapResults[i]._meta?.block.number
         const updatedAtTimestamp = getUnixTime(Date.now())
         return [
           chainId,
           JSON.stringify({
             chainId,
             ...Object.fromEntries(
-              results[i].tokens
+              sushiSwapResults[i].tokens
                 .filter((token) => token.derivedETH != 0)
                 .map((token) => {
                   const price = Number(token.derivedETH) * Number(ethPrice)
@@ -43,10 +54,29 @@ export async function execute() {
             updatedAtTimestamp,
           }),
         ]
-      })
-    )
+      }),
+      TRIDENT_CHAINS.map((chainId, i) => {
+        const updatedAtBlock = tridentResults[i]._meta?.block.number
+        const updatedAtTimestamp = getUnixTime(Date.now())
+        return [
+          chainId,
+          JSON.stringify({
+            chainId,
+            ...Object.fromEntries(
+              tridentResults[i].tokenPrices
+                .filter((price) => price.derivedUSD != 0)
+                .map((price) => {
+                  return [price.id, Number(price.derivedUSD)]
+                })
+            ),
+            updatedAtBlock,
+            updatedAtTimestamp,
+          }),
+        ]
+      }),
+    ])
   )
-  console.log(`Finished updating prices for chains: ${SUPPORTED_CHAINS.join(', ')}`)
+  // console.log(`Finished updating prices for chains: ${SUSHISWAP_CHAINS.join(', ')}`)
   process.exit()
 }
 execute()
