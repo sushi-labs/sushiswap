7ab4ad92d8c5fa904fb899faf79daf68f294f822
matthewlilley <hello@matthewlilley.com>
2022-07-12 13:28:21 +0100

    refactor(scripts/price): simplify

--- scripts/price/src/index.ts
+++ scripts/price/src/index.ts
@@ -1,27 +1,36 @@
 import 'dotenv/config'
 
-import { getUnixTime } from 'date-fns'
-
-import { SUPPORTED_CHAINS } from './config'
-import { getPrices } from './lib/graph'
-import redis from './lib/redis'
+import { GRAPH_HOST, LEGACY_SUBGRAPH_NAME, SUPPORTED_CHAINS } from './config'
+import redis from './redis'
+import { getBuiltGraphSDK } from '.graphclient'
 
 export async function execute() {
   console.log(`Updating prices for chains: ${SUPPORTED_CHAINS.join(', ')}`)
-  const results = await Promise.all(SUPPORTED_CHAINS.map((chainId) => getPrices(chainId)))
+
+  const results = await Promise.all(
+    SUPPORTED_CHAINS.map((chainId) => {
+      if (!SUPPORTED_CHAINS.includes(chainId)) {
+        throw Error(`Unsupported Chain ${chainId}`)
+      }
+      console.log({ chainId, host: GRAPH_HOST, subgraphName: LEGACY_SUBGRAPH_NAME[chainId] })
+      const sdk = getBuiltGraphSDK({ chainId, host: GRAPH_HOST, subgraphName: LEGACY_SUBGRAPH_NAME[chainId] })
+      return sdk.TokenPrices()
+    })
+  )
+
   await redis.hset(
     'prices',
     Object.fromEntries(
       results.map(({ chainId, data }) => {
-        const ethPrice = data.legacy_exchange_bundle?.ethPrice
-        const updatedAtBlock = data.legacy_exchange__meta?.block.number
+        const ethPrice = data.bundle?.ethPrice
+        const updatedAtBlock = data._meta?.block.number
         const updatedAtTimestamp = getUnixTime(Date.now())
         return [
           chainId,
           JSON.stringify({
             chainId,
             ...Object.fromEntries(
-              data.legacy_exchange_tokens
+              data.tokens
                 .filter((token) => token.derivedETH != 0)
                 .map((token) => {
                   const price = Number(token.derivedETH) * Number(ethPrice)
