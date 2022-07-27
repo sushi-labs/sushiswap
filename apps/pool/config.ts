423f0f78aaa8c06a52d5db3cfac8ce605246c329
Chilli Chelli <ramin@sushi.com>
2022-07-27 03:04:17 +0200

    fix(apps/pool): add graph config

--- apps/pool/config.ts
+++ apps/pool/config.ts
@@ -10,18 +10,9 @@ const PENDING_KAVA_HOST_ENDPOINT = 'api.thegraph.com/subgraphs/id'
 
 export const GRAPH_HOST = {
   [ChainId.ETHEREUM]: GRAPH_HOST_ENDPOINT,
-  [ChainId.GNOSIS]: GRAPH_HOST_ENDPOINT,
   [ChainId.POLYGON]: GRAPH_HOST_ENDPOINT,
-  [ChainId.FANTOM]: GRAPH_HOST_ENDPOINT,
-  [ChainId.BSC]: GRAPH_HOST_ENDPOINT,
-  [ChainId.HARMONY]: GRAPH_HOST_ENDPOINT,
   [ChainId.AVALANCHE]: GRAPH_HOST_ENDPOINT,
-  [ChainId.CELO]: GRAPH_HOST_ENDPOINT,
   [ChainId.ARBITRUM]: GRAPH_HOST_ENDPOINT,
-  [ChainId.MOONRIVER]: GRAPH_HOST_ENDPOINT,
-  [ChainId.OKEX]: GRAPH_HOST_ENDPOINT,
-  [ChainId.HECO]: GRAPH_HOST_ENDPOINT,
-  [ChainId.FUSE]: GRAPH_HOST_ENDPOINT,
   [ChainId.MOONBEAM]: GRAPH_HOST_ENDPOINT,
 }
 
@@ -31,35 +22,17 @@ export const CHAIN_NAME: Record<number | string, string> = {
 }
 
 export const EXCHANGE_SUBGRAPH_NAME: Record<number | string, string> = {
-  [ChainId.ETHEREUM]: 'sushiswap/exchange',
-  [ChainId.GNOSIS]: 'sushiswap/xdai-exchange',
+  [ChainId.ETHEREUM]: 'sushiswap/exchange-ethereum',
   [ChainId.POLYGON]: 'sushiswap/exchange-polygon',
-  [ChainId.FANTOM]: 'sushiswap/exchange-fantom',
-  [ChainId.BSC]: 'sushiswap/bsc-exchange',
-  [ChainId.HARMONY]: 'sushiswap/harmony-exchange',
   [ChainId.AVALANCHE]: 'sushiswap/exchange-avalanche',
-  [ChainId.CELO]: 'sushiswap/exchange-celo',
   [ChainId.ARBITRUM]: 'sushiswap/exchange-arbitrum-backup',
-  [ChainId.MOONRIVER]: 'sushiswap/exchange-moonriver',
-  [ChainId.OKEX]: 'okex-exchange/oec',
-  [ChainId.HECO]: 'heco-exchange/heco',
-  [ChainId.FUSE]: 'sushiswap/exchange-fuse',
   [ChainId.MOONBEAM]: 'sushiswap/exchange-moonbeam',
 }
 
 export const ENABLED_NETWORKS = [
   ChainId.ETHEREUM,
-  ChainId.GNOSIS,
   ChainId.POLYGON,
-  ChainId.FANTOM,
-  ChainId.BSC,
-  ChainId.HARMONY,
   ChainId.AVALANCHE,
-  ChainId.CELO,
   ChainId.ARBITRUM,
-  ChainId.MOONRIVER,
-  ChainId.OKEX,
-  ChainId.HECO,
-  ChainId.FUSE,
   ChainId.MOONBEAM,
 ]
