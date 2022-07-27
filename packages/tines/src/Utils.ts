4b9977f5357db12fb5f7366d15deba6e1003b1a7
ilya <bretnwood@gmail.com>
2022-07-27 23:40:38 +0300

    fix: Tines: StableSwap calcInByOut

--- packages/tines/src/Utils.ts
+++ packages/tines/src/Utils.ts
@@ -17,8 +17,9 @@ export function DEBUG_MODE_ON(on: boolean) {
 export function closeValues(a: number, b: number, accuracy: number, logInfoIfFalse = ''): boolean {
   if (accuracy === 0) return a === b
   if (Math.abs(a) < 1 / accuracy) return Math.abs(a - b) <= 10
+  if (Math.abs(b) < 1 / accuracy) return Math.abs(a - b) <= 10
   const res = Math.abs(a / b - 1) < accuracy
-  if (!res && logInfoIfFalse) {
+  if (!res) {
     console.log('Expected close: ', a, b, accuracy, logInfoIfFalse)
     // debugger
   }
