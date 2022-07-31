9ee527cf3f0a2d2a70f5c678169d006f4d610a2c
Chilli Chelli <ramin@sushi.com>
2022-07-31 22:02:12 +0200

    fix(apps/pool): neon sign

--- apps/pool/components/SushiBarSection/SushiBarSectionDesktop.tsx
+++ apps/pool/components/SushiBarSection/SushiBarSectionDesktop.tsx
@@ -12,7 +12,6 @@ import { FC, useCallback, useState } from 'react'
 import useSWR from 'swr'
 import { useAccount, useContractWrite, useNetwork } from 'wagmi'
 
-import xSushiSign from '../../public/images/SushiBarNeonSign.png'
 import { SushiBarInput } from './SushiBarInput'
 
 const SUSHI_TOKEN = SUSHI[ChainId.ETHEREUM]
@@ -66,7 +65,7 @@ export const SushiBarSectionDesktop: FC = () => {
         <div className="p-5 flex flex-col lg:flex-row rounded-2xl bg-white bg-opacity-[0.02]">
           <div className="grid grid-cols-[72px_auto] items-center gap-4 px-5">
             <div className="relative w-[72px] h-[72px]">
-              <Image src={xSushiSign} layout="fill" />
+              <Image src="https://sushi.com/pool/images/SushiBarNeonSign.png" layout="fill" />
             </div>
             <div className="flex flex-col">
               <h4 className="font-bold text-slate-50 mb-1 whitespace-nowrap">Sushi Bar</h4>
