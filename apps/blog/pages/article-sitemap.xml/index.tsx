51fd0aaf8e68aec047baf18a5f035c132dff3cbd
LufyCZ <lufy@sushi.com>
2022-08-07 21:23:26 +0000

    feat(apps/blog): sitemap

---
+++ apps/blog/pages/article-sitemap.xml/index.tsx
@@ -0,0 +1,23 @@
+import { GetServerSideProps } from 'next'
+import { getServerSideSitemap, ISitemapField } from 'next-sitemap'
+
+import { getAllArticlesBySlug } from '../../lib/api'
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const {
+    articles: { data: articles },
+  } = await getAllArticlesBySlug()
+
+  const slugs = articles.map((article) => article.attributes.slug)
+
+  const fields = slugs.map<ISitemapField>((slug) => ({
+    loc: `${ctx.req.headers.host}/blog/${slug}`,
+    changeFreq: 'never',
+  }))
+
+  return getServerSideSitemap(ctx, fields)
+}
+
+export default function Sitemap() {
+  // Default export to prevent next.js errors
+}
