77ccbc97cf917a9161f855700636a632b0679e16
Matthew Lilley <hello@matthewlilley.com>
2022-08-20 06:46:04 +0000

    config(apps/pool): seo

---
+++ apps/pool/next-seo.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+/** @type {import('next-seo').DefaultSeoProps} */
+export default {
+  titleTemplate: '%s | Sushi',
+  title: 'Sushi Liquidity',
+  defaultTitle: 'Sushi Liquidity',
+  description: 'Sushi Liquidity is a app for managing the liquidity of SushiSwap & SushiLend.',
+  //   canonical: 'https://sushi.com/pool',
+  //   mobileAlternate: {
+  //     media: '',
+  //     href: '',
+  //   },
+  //   languageAlternates: [{ hrefLang: "en", href: "https://sushi.com/pool" }],
+  twitter: {
+    handle: '@sushiswap',
+    site: '@sushiswap',
+    cardType: 'summary_large_image',
+  },
+  openGraph: {
+    // url: 'https://sushi.com/pool',
+    type: 'website',
+    title: 'Sushi Liquidity',
+    description: 'Sushi Liquidity is a app for managing the liquidity of SushiSwap & SushiLend.',
+    // images: [],
+    // videos: [],
+    // locale: 'en_IE',
+    site_name: 'Sushi',
+  },
+}
