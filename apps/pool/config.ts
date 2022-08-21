7acf1351196cfe6caac763fda3ac9da4e1c91ee9
Matthew Lilley <hello@matthewlilley.com>
2022-08-21 17:26:53 +0000

    fix(apps/pool): subgraph config

--- apps/pool/config.ts
+++ apps/pool/config.ts
@@ -27,26 +27,25 @@ export const GRAPH_HOST = {
   [ChainId.ARBITRUM]: GRAPH_HOST_ENDPOINT,
   [ChainId.BSC]: GRAPH_HOST_ENDPOINT,
   [ChainId.CELO]: GRAPH_HOST_ENDPOINT,
+  [ChainId.FANTOM]: GRAPH_HOST_ENDPOINT,
+  [ChainId.FUSE]: GRAPH_HOST_ENDPOINT,
   [ChainId.GNOSIS]: GRAPH_HOST_ENDPOINT,
   [ChainId.MOONBEAM]: GRAPH_HOST_ENDPOINT,
   [ChainId.MOONRIVER]: GRAPH_HOST_ENDPOINT,
-  [ChainId.FANTOM]: GRAPH_HOST_ENDPOINT,
-  [ChainId.FUSE]: GRAPH_HOST_ENDPOINT,
   [ChainId.POLYGON]: GRAPH_HOST_ENDPOINT,
 }
 
 export const EXCHANGE_SUBGRAPH_NAME: Record<number | string, string> = {
   [ChainId.ETHEREUM]: 'sushiswap/sushiswap-ethereum',
   [ChainId.AVALANCHE]: 'sushiswap/sushiswap-avalanche',
-  [ChainId.ARBITRUM]: 'sushi-labs/sushiswap-arbitrum',
+  [ChainId.ARBITRUM]: 'sushiswap/sushiswap-arbitrum',
   [ChainId.BSC]: 'sushiswap/sushiswap-bsc',
   [ChainId.CELO]: 'sushiswap/sushiswap-celo',
+  [ChainId.FANTOM]: 'sushiswap/sushiswap-fantom',
+  [ChainId.FUSE]: 'sushiswap/sushiswap-fuse',
   [ChainId.GNOSIS]: 'sushiswap/sushiswap-gnosis',
   [ChainId.MOONBEAM]: 'sushiswap/sushiswap-moonbeam',
   [ChainId.MOONRIVER]: 'sushiswap/sushiswap-moonriver',
-  [ChainId.FANTOM]: 'sushi-labs/sushiswap-fantom',
-  [ChainId.FUSE]: 'sushiswap/sushiswap-fuse',
-  // [ChainId.GNOSIS]: 'sushi-labs/sushiswap-gnosis',
 }
 
 export const STAKING_SUBGRAPH_NAME: Record<number | string, string> = {
@@ -60,11 +59,11 @@ export const AMM_ENABLED_NETWORKS = [
   ChainId.ARBITRUM,
   ChainId.BSC,
   ChainId.CELO,
+  ChainId.FANTOM,
+  ChainId.FUSE,
   ChainId.GNOSIS,
   ChainId.MOONBEAM,
   ChainId.MOONRIVER,
-  ChainId.FUSE,
-  ChainId.FANTOM,
   // ChainId.HARMONY,
   // ChainId.POLYGON,
 ]
--- apps/pool/pages/api/pools.tsx
+++ apps/pool/pages/api/pools.tsx
@@ -4,6 +4,7 @@ import { getPools, GetPoolsQuery } from '../../lib/api'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader('Cache-Control', 'public, s-maxage=900, stale-while-revalidate=3600')
-  const pairs = await getPools(req.query as unknown as GetPoolsQuery)
+  const query = req.query as unknown
+  const pairs = await getPools(query as GetPoolsQuery)
   res.status(200).send(JSON.stringify(pairs))
 }
