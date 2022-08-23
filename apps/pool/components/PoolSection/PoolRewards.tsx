ea4d1a65e39e3af19274bd4dd98f1012b0b831da
Chilli Chelli <ramin@sushi.com>
2022-08-23 20:21:24 +0200

    feat(packages/wagmi): add farm rewards hook

--- apps/pool/components/PoolSection/PoolRewards.tsx
+++ apps/pool/components/PoolSection/PoolRewards.tsx
@@ -1,16 +1,19 @@
+import { formatPercent } from '@sushiswap/format'
 import { Currency, Table, Typography } from '@sushiswap/ui'
-import { FC } from 'react'
+import { useFarmRewards } from '@sushiswap/wagmi'
+import React, { FC } from 'react'
 
-import { useTokensFromPair } from '../../lib/hooks'
 import { PairWithAlias } from '../../types'
 
 interface PoolRewardsProps {
   pair: PairWithAlias
 }
 
-// TODO rewards
 export const PoolRewards: FC<PoolRewardsProps> = ({ pair }) => {
-  const { token0 } = useTokensFromPair(pair)
+  const { data: rewards } = useFarmRewards()
+
+  const farm = rewards?.[pair.chainId]?.farms?.[pair.id]
+  const totalAPR = farm?.incentives.reduce((acc, cur) => acc + (cur.apr || 0), 0) || 0
 
   return (
     <div className="flex flex-col gap-4 w-full">
@@ -19,7 +22,7 @@ export const PoolRewards: FC<PoolRewardsProps> = ({ pair }) => {
           Rewards
         </Typography>
         <Typography variant="sm" weight={400} className="text-slate-400">
-          Reward APR: <span className="font-bold text-slate-50">12%</span>
+          Reward APR: <span className="font-bold text-slate-50">{totalAPR > 0 ? formatPercent(totalAPR) : 'n/a'}</span>
         </Typography>
       </div>
       <Table.container className="w-full">
@@ -35,21 +38,33 @@ export const PoolRewards: FC<PoolRewardsProps> = ({ pair }) => {
             </Table.thr>
           </Table.thead>
           <Table.tbody>
-            <Table.tr>
-              <Table.td>
-                <div className="flex gap-3 items-center">
-                  <Currency.Icon currency={token0} width={24} height={24} />
-                  <Typography weight={700} variant="sm" className="text-slate-50">
-                    {token0.symbol}
+            {farm?.incentives ? (
+              farm?.incentives.map((incentive, idx) => (
+                <Table.tr key={idx}>
+                  <Table.td>
+                    <div className="flex gap-3 items-center">
+                      <Currency.Icon currency={incentive.rewardToken} width={24} height={24} />
+                      <Typography weight={700} variant="sm" className="text-slate-50">
+                        {incentive.rewardToken.symbol}
+                      </Typography>
+                    </div>
+                  </Table.td>
+                  <Table.td>
+                    <Typography variant="sm" weight={600} className="text-slate-50">
+                      {incentive.rewardPerDay} {incentive.rewardToken.symbol} per day
+                    </Typography>
+                  </Table.td>
+                </Table.tr>
+              ))
+            ) : (
+              <Table.tr>
+                <Table.td colSpan={2}>
+                  <Typography variant="xs" className="text-slate-400 italic w-full text-center">
+                    No rewards found
                   </Typography>
-                </div>
-              </Table.td>
-              <Table.td>
-                <Typography variant="sm" weight={600} className="text-slate-50">
-                  69.74 SUSHI per day
-                </Typography>
-              </Table.td>
-            </Table.tr>
+                </Table.td>
+              </Table.tr>
+            )}
           </Table.tbody>
         </Table.table>
       </Table.container>
