4619e6581f018fd9d2c7caa1724ea159d029c47c
Chilli Chelli <ramin@sushi.com>
2022-08-25 16:38:15 +0200

    fix(apps/pool): position overview

--- apps/pool/components/AddSection/AddSectionMyPosition.tsx
+++ apps/pool/components/AddSection/AddSectionMyPosition.tsx
@@ -29,10 +29,10 @@ export const AddSectionMyPosition: FC<AddSectionMyPositionProps> = ({ pair }) =>
   return (
     <>
       <div className="p-5 pb-2 flex flex-col gap-2">
-        <Typography variant="sm" weight={600} className="text-slate-50">
-          My Liquidity Position
-        </Typography>
-        <div className="flex flex-col gap-1.5">
+        <div className="flex gap-1 justify-between items-center">
+          <Typography variant="sm" weight={600} className="text-slate-50">
+            My Liquidity Position
+          </Typography>
           <Typography variant="xs" weight={500} className="text-slate-400">
             {balance ? (
               formatUSD(Number(value0) + Number(value1))
@@ -40,32 +40,31 @@ export const AddSectionMyPosition: FC<AddSectionMyPositionProps> = ({ pair }) =>
               <div className="bg-slate-700 rounded-full h-[16px] my-0.5 animate-pulse w-[60px]" />
             )}
           </Typography>
-          <div className="flex">
-            <Currency.IconList iconWidth={16} iconHeight={16}>
-              <Currency.Icon currency={token0} />
-              <Currency.Icon currency={token1} />
-            </Currency.IconList>
+        </div>
+        <div className="flex flex-col gap-1.5">
+          <div className="flex items-center gap-1.5">
+            <div className="w-4 h-4">
+              <Currency.Icon currency={token0} width={16} height={16} />
+            </div>
             <Typography variant="xs" weight={500} className="flex items-center gap-1 text-slate-400">
-              {balance ? (
-                <>
-                  {underlying0?.toSignificant(3)} {underlying0?.currency.symbol} + {underlying1?.toSignificant(3)}{' '}
-                  {underlying1?.currency.symbol}
-                </>
-              ) : (
-                <>
-                  <div className="bg-slate-700 rounded-full h-[12px] animate-pulse w-[60px]" /> +{' '}
-                  <div className="bg-slate-700 rounded-full h-[12px] animate-pulse w-[60px]" />
-                </>
-              )}
+              {balance && underlying0?.toSignificant(3)} {underlying0?.currency.symbol}
+            </Typography>
+          </div>
+          <div className="flex items-center gap-1.5">
+            <div className="w-4 h-4">
+              <Currency.Icon currency={token1} width={16} height={16} />
+            </div>
+            <Typography variant="xs" weight={500} className="flex items-center gap-1 text-slate-400">
+              {balance && underlying1?.toSignificant(3)} {underlying1?.currency.symbol}
             </Typography>
           </div>
         </div>
       </div>
       <div className="p-5 flex flex-col gap-2">
-        <Typography variant="sm" weight={600} className="text-slate-50">
-          My Staked Position
-        </Typography>
-        <div className="flex flex-col gap-1.5">
+        <div className="flex gap-1 justify-between items-center">
+          <Typography variant="sm" weight={600} className="text-slate-50">
+            My Staked Position
+          </Typography>
           <Typography variant="xs" weight={500} className="text-slate-400">
             {balance ? (
               formatUSD(Number(value0) + Number(value1))
@@ -73,23 +72,22 @@ export const AddSectionMyPosition: FC<AddSectionMyPositionProps> = ({ pair }) =>
               <div className="bg-slate-700 rounded-full h-[16px] my-0.5 animate-pulse w-[60px]" />
             )}
           </Typography>
-          <div className="flex">
-            <Currency.IconList iconWidth={16} iconHeight={16}>
-              <Currency.Icon currency={token0} />
-              <Currency.Icon currency={token1} />
-            </Currency.IconList>
+        </div>
+        <div className="flex flex-col gap-1.5">
+          <div className="flex items-center gap-1.5">
+            <div className="w-4 h-4">
+              <Currency.Icon currency={token0} width={16} height={16} />
+            </div>
             <Typography variant="xs" weight={500} className="flex items-center gap-1 text-slate-400">
-              {balance ? (
-                <>
-                  {underlying0?.toSignificant(3)} {underlying0?.currency.symbol} + {underlying1?.toSignificant(3)}{' '}
-                  {underlying1?.currency.symbol}
-                </>
-              ) : (
-                <>
-                  <div className="bg-slate-700 rounded-full h-[12px] animate-pulse w-[60px]" /> +{' '}
-                  <div className="bg-slate-700 rounded-full h-[12px] animate-pulse w-[60px]" />
-                </>
-              )}
+              {balance && underlying0?.toSignificant(3)} {underlying0?.currency.symbol}
+            </Typography>
+          </div>
+          <div className="flex items-center gap-1.5">
+            <div className="w-4 h-4">
+              <Currency.Icon currency={token1} width={16} height={16} />
+            </div>
+            <Typography variant="xs" weight={500} className="flex items-center gap-1 text-slate-400">
+              {balance && underlying1?.toSignificant(3)} {underlying1?.currency.symbol}
             </Typography>
           </div>
         </div>
