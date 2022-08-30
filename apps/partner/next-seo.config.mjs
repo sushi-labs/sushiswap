e5b9efefa3f9c21ab8566ce36df951e1dcf4d578
LufyCZ <lufy@sushi.com>
2022-08-30 14:28:55 +0000

    feat(apps/partner): basic seo

---
+++ apps/partner/next-seo.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+/** @type {import('next-seo').DefaultSeoProps} */
+export default {
+  titleTemplate: '%s | Sushi',
+  title: 'Partner',
+  defaultTitle: 'Partner',
+  description: 'Sushi Partner is a platform for sushi-partner interaction.',
+  //   canonical: 'https://sushi.com/analytics',
+  //   mobileAlternate: {
+  //     media: '',
+  //     href: '',
+  //   },
+  //   languageAlternates: [{ hrefLang: "en", href: "https://sushi.com/analytics" }],
+  twitter: {
+    handle: '@sushiswap',
+    site: '@sushiswap',
+    cardType: 'summary_large_image',
+  },
+  openGraph: {
+    // url: 'https://sushi.com/analytics',
+    type: 'website',
+    title: 'Partner',
+    description: 'Sushi Partner is a platform for sushi-partner interaction.',
+    // images: [],
+    // videos: [],
+    // locale: 'en_IE',
+    site_name: 'Sushi',
+  },
+}
