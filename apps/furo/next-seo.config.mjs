ab0c15eef0489ded44dff3e1493ca68f57140cfb
LufyCZ <lufy@sushi.com>
2022-08-30 14:14:58 +0000

    feat(apps/furo): add basic seo

---
+++ apps/furo/next-seo.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+/** @type {import('next-seo').DefaultSeoProps} */
+export default {
+  titleTemplate: '%s | Sushi',
+  title: 'Furo',
+  defaultTitle: 'Furo',
+  description: 'Earn, stream and automate your DAO salaries and token vesting with Furo.',
+  //   canonical: 'https://sushi.com/furo',
+  //   mobileAlternate: {
+  //     media: '',
+  //     href: '',
+  //   },
+  //   languageAlternates: [{ hrefLang: "en", href: "https://sushi.com/furo" }],
+  twitter: {
+    handle: '@sushiswap',
+    site: '@sushiswap',
+    cardType: 'summary_large_image',
+  },
+  openGraph: {
+    // url: 'https://sushi.com/furo',
+    type: 'website',
+    title: 'Furo',
+    description: 'and token vesting with Furo. and token vesting with Furo.',
+    // images: [],
+    // videos: [],
+    // locale: 'en_IE',
+    site_name: 'Sushi',
+  },
+}
