39ea4c85c1701b7efc51e60a2845669263ca0d59
chillichelli <ramin@erfani.dev>
2022-09-05 04:41:12 +0200

    fix(apps/pool): remove logs

--- apps/pool/components/PoolsSection/Tables/PairPositionCell.tsx
+++ apps/pool/components/PoolsSection/Tables/PairPositionCell.tsx
@@ -18,9 +18,8 @@ export const PairPositionCell: FC<CellWithBalanceProps> = ({ row }) => {
 
 const _PairPositionCell: FC<CellWithBalanceProps> = () => {
   const { value1, value0 } = usePoolPosition()
-  const { value0: stakedValue0, value1: stakedValue1, underlying0, underlying1 } = usePoolPositionStaked()
+  const { value0: stakedValue0, value1: stakedValue1 } = usePoolPositionStaked()
 
-  console.log(value0, value1, stakedValue0, stakedValue1, underlying0?.toSignificant(6), underlying1?.toSignificant(6))
   return (
     <Typography variant="sm" weight={600} className="text-slate-50 text-right">
       {formatUSD(value0 + value1 + stakedValue0 + stakedValue1)}
