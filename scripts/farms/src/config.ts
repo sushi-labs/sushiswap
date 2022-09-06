60340b8e270ae6a022792017f46af218614b4d5c
LufyCZ <lufy@sushi.com>
2022-09-06 08:04:54 +0000

    chore(scripts/farm): enable polygon and kava

--- scripts/farms/src/config.ts
+++ scripts/farms/src/config.ts
@@ -1,7 +1,7 @@
 import { ChainId } from '@sushiswap/chain'
 
 export const MINICHEF_SUBGRAPH_NAME: Record<number | string, string> = {
-  // [ChainId.POLYGON]: 'jiro-ono/minichef-staging-updates', // new trident subgraph not synced yet
+  [ChainId.POLYGON]: 'jiro-ono/minichef-staging-updates', // new trident subgraph not synced yet
   [ChainId.GNOSIS]: 'jiro-ono/gnosis-minichef-staging',
   // [ChainId.HARMONY]: 'sushiswap/harmony-minichef', // subgraph broken
   [ChainId.ARBITRUM]: 'jiro-ono/arbitrum-minichef-staging',
@@ -10,6 +10,6 @@ export const MINICHEF_SUBGRAPH_NAME: Record<number | string, string> = {
   [ChainId.FUSE]: 'sushiswap/fuse-minichef',
   [ChainId.FANTOM]: 'sushiswap/fantom-minichef',
   [ChainId.MOONBEAM]: 'sushiswap/moonbeam-minichef',
-  // [ChainId.KAVA]: 'sushiswap/kava-minichef', //block subgraph not synced yet
+  [ChainId.KAVA]: 'sushiswap/kava-minichef', //block subgraph not synced yet
   [ChainId.METIS]: 'sushiswap/metis-minichef',
 }
