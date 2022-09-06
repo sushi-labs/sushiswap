c3f6c94bcbbc98fb4b5cda98b3ee9e888c5eb36a
Matthew Lilley <hello@matthewlilley.com>
2022-09-06 20:52:04 +0000

    chore(packages/config/wagmi): simplify

--- packages/config/wagmi/src/index.ts
+++ packages/config/wagmi/src/index.ts
@@ -1,16 +1,6 @@
-import { SafeConnector } from '@gnosis.pm/safe-apps-wagmi'
 import { ChainId } from '@sushiswap/chain'
-import { allChains, configureChains, CreateClientConfig } from 'wagmi'
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
-import { InjectedConnector } from 'wagmi/connectors/injected'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
-import { alchemyProvider } from 'wagmi/providers/alchemy'
-import { publicProvider } from 'wagmi/providers/public'
 
-const alchemyId = process.env.ALCHEMY_ID || process.env.NEXT_PUBLIC_ALCHEMY_ID
-const infuraId = process.env.INFURA_ID || process.env.NEXT_PUBLIC_INFURA_ID
-
-const otherChains = [
+export const otherChains = [
   {
     id: ChainId.AVALANCHE,
     name: 'Avalanche',
@@ -265,44 +255,3 @@ const otherChains = [
     },
   },
 ]
-
-const { chains, provider, webSocketProvider } = configureChains(
-  [...allChains, ...otherChains],
-  [
-    alchemyProvider({ alchemyId }),
-    publicProvider(),
-    // infuraProvider({ infuraId }),
-  ]
-)
-
-export const config: CreateClientConfig = {
-  provider,
-  webSocketProvider,
-  autoConnect: false,
-  connectors() {
-    return [
-      new InjectedConnector({
-        chains,
-        options: {
-          shimDisconnect: true,
-        },
-      }),
-      new WalletConnectConnector({
-        chains,
-        // TODO: Flesh out wallet connect options?
-        options: {
-          qrcode: true,
-        },
-      }),
-      new CoinbaseWalletConnector({
-        // TODO: Flesh out coinbase wallet connect options?
-        chains,
-        options: {
-          appName: 'Sushi 2.0',
-          appLogoUrl: 'https://raw.githubusercontent.com/sushiswap/art/master/sushi/logo.svg',
-        },
-      }),
-      new SafeConnector({ chains }),
-    ]
-  },
-}
--- packages/wagmi/client.ts
+++ packages/wagmi/client.ts
@@ -1,6 +1,54 @@
-import { config } from '@sushiswap/wagmi-config'
-import { createClient } from 'wagmi'
+import { SafeConnector } from '@gnosis.pm/safe-apps-wagmi'
+import { otherChains } from '@sushiswap/wagmi-config'
+import { allChains, configureChains, createClient, CreateClientConfig } from 'wagmi'
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
 
 export type Client = ReturnType<typeof createClient>
 
-export const client: Client = createClient(config)
+const alchemyId = process.env.ALCHEMY_ID || process.env.NEXT_PUBLIC_ALCHEMY_ID
+const infuraId = process.env.INFURA_ID || process.env.NEXT_PUBLIC_INFURA_ID
+
+const { chains, provider, webSocketProvider }: CreateClientConfig = configureChains(
+  [...allChains, ...otherChains],
+  [
+    alchemyProvider({ alchemyId }),
+    publicProvider(),
+    // infuraProvider({ infuraId }),
+  ]
+)
+
+export const client: Client = createClient({
+  provider,
+  webSocketProvider,
+  autoConnect: false,
+  connectors() {
+    return [
+      new InjectedConnector({
+        chains,
+        options: {
+          shimDisconnect: true,
+        },
+      }),
+      new WalletConnectConnector({
+        chains,
+        // TODO: Flesh out wallet connect options?
+        options: {
+          qrcode: true,
+        },
+      }),
+      new CoinbaseWalletConnector({
+        // TODO: Flesh out coinbase wallet connect options?
+        chains,
+        options: {
+          appName: 'Sushi 2.0',
+          appLogoUrl: 'https://raw.githubusercontent.com/sushiswap/art/master/sushi/logo.svg',
+        },
+      }),
+      new SafeConnector({ chains }),
+    ]
+  },
+})
