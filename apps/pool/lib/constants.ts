5d435de9b4c25754ef1778fb1189eb371d751758
chillichelli <ramin@erfani.dev>
2022-09-06 19:53:59 +0200

    fix(apps/pool): master chef string mismatch

--- apps/pool/lib/constants.ts
+++ apps/pool/lib/constants.ts
@@ -4,6 +4,6 @@ export const L2_DEADLINE_FROM_NOW = 60 * 5
 
 export const CHEF_TYPE_MAP = {
   MiniChef: Chef.MINICHEF,
-  MasterChef: Chef.MASTERCHEF,
+  MasterChefV1: Chef.MASTERCHEF,
   MasterChefV2: Chef.MASTERCHEF_V2,
 }
