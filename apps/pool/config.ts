3475039bab8236a3bb0cefdd9cb9fcce82d82963
Matthew Lilley <hello@matthewlilley.com>
2022-09-15 11:29:06 +0000

    fix(apps/pool): typing

--- apps/pool/config.ts
+++ apps/pool/config.ts
@@ -1,10 +1,10 @@
 import { ChainId } from '@sushiswap/chain'
 
-export const TRIDENT_ENABLED_NETWORKS = [
+export const TRIDENT_ENABLED_NETWORKS: ChainId[] = [
   // ChainId.OPTIMISM, ChainId.POLYGON, ChainId.METIS, ChainId.KAVA
 ]
 
-export const AMM_ENABLED_NETWORKS = [
+export const AMM_ENABLED_NETWORKS: ChainId[] = [
   ChainId.ETHEREUM,
   // ChainId.AVALANCHE,
   // ChainId.ARBITRUM,
@@ -19,4 +19,4 @@ export const AMM_ENABLED_NETWORKS = [
   // ChainId.HARMONY,
 ]
 
-export const SUPPORTED_CHAIN_IDS = [...TRIDENT_ENABLED_NETWORKS, ...AMM_ENABLED_NETWORKS]
+export const SUPPORTED_CHAIN_IDS: ChainId[] = [...TRIDENT_ENABLED_NETWORKS, ...AMM_ENABLED_NETWORKS]
