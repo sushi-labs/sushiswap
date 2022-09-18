88df1c588402947d4e1528b337ac00b40c6e9031
Chilli Chelli <ramin@sushi.com>
2022-09-18 11:22:52 +0200

    fix(apps/pool): color on add widgets

--- apps/pool/components/NewPositionSection/SelectFeeWidget.tsx
+++ apps/pool/components/NewPositionSection/SelectFeeWidget.tsx
@@ -17,7 +17,7 @@ export const FEE_MAP = [Fee.LOW, Fee.MEDIUM, Fee.DEFAULT, Fee.HIGH]
 
 export const SelectFeeWidget: FC<SelectFeeWidgetProps> = memo(({ selectedNetwork, fee, setFee }) => {
   return (
-    <Widget id="selectFee" maxWidth={400} className="bg-slate-800">
+    <Widget id="selectFee" maxWidth={400} className="!bg-slate-800">
       <Widget.Content>
         <Disclosure>
           {() => (
--- apps/pool/components/NewPositionSection/SelectNetworkWidget.tsx
+++ apps/pool/components/NewPositionSection/SelectNetworkWidget.tsx
@@ -13,7 +13,7 @@ interface SelectNetworkWidgetProps {
 
 export const SelectNetworkWidget: FC<SelectNetworkWidgetProps> = memo(({ selectedNetwork, onSelect }) => {
   return (
-    <Widget id="selectNetwork" maxWidth={400} className="bg-slate-800">
+    <Widget id="selectNetwork" maxWidth={400} className="!bg-slate-800">
       <Widget.Content>
         <Disclosure>
           {() => (
