32d8372dc8f5e69e06733bccd31aff9a45079504
Matthew Lilley <hello@matthewlilley.com>
2022-09-18 00:01:39 +0000

    chore(apps/pool): enable trident networks

--- apps/analytics/config.ts
+++ apps/analytics/config.ts
@@ -1,20 +1,18 @@
 import { ChainId } from '@sushiswap/chain'
 
-export const TRIDENT_ENABLED_NETWORKS = [
-  // ChainId.OPTIMISM, ChainId.POLYGON, ChainId.METIS, ChainId.KAVA
-]
+export const TRIDENT_ENABLED_NETWORKS = [ChainId.OPTIMISM, ChainId.POLYGON, ChainId.METIS, ChainId.KAVA]
 
 export const AMM_ENABLED_NETWORKS = [
   ChainId.ETHEREUM,
-  // ChainId.AVALANCHE,
-  // ChainId.ARBITRUM,
-  // ChainId.BSC,
-  // ChainId.CELO,
-  // ChainId.FANTOM,
-  // ChainId.FUSE,
-  // ChainId.GNOSIS,
-  // ChainId.MOONBEAM,
-  // ChainId.MOONRIVER,
+  ChainId.AVALANCHE,
+  ChainId.ARBITRUM,
+  ChainId.BSC,
+  ChainId.CELO,
+  ChainId.FANTOM,
+  ChainId.FUSE,
+  ChainId.GNOSIS,
+  ChainId.MOONBEAM,
+  ChainId.MOONRIVER,
   // ChainId.ARBITRUM_NOVA,
   // ChainId.HARMONY,
 ]
