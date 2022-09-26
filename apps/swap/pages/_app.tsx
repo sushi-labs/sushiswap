567e74ca8ec363c44e534b5370ad0bc82310eb4b
matthewlilley <hello@matthewlilley.com>
2022-09-26 12:54:08 +0100

    chore(apps/swap): add bg radial

--- apps/swap/pages/_app.tsx
+++ apps/swap/pages/_app.tsx
@@ -64,6 +64,7 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
               <App.Footer />
               <ToastContainer className="mt-[50px]" />
             </App.Shell>
+            <div className="z-[-1] bg-gradient-radial fixed inset-0 bg-scroll bg-clip-border transform pointer-events-none" />
           </ThemeProvider>
         </Provider>
       </WagmiConfig>
