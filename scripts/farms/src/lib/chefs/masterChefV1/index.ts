d23857a2942bf18cea27a2b5968cd80236f28263
LufyCZ <lufy@sushi.com>
2022-09-26 18:14:51 +0000

    refactor(scripts/farm): extract fetchers

--- scripts/farms/src/lib/chefs/masterChefV1/index.ts
+++ scripts/farms/src/lib/chefs/masterChefV1/index.ts
@@ -1,49 +1,13 @@
 import { ChainId } from '@sushiswap/chain'
-import { IMasterChef, MasterChef } from '@sushiswap/core'
-import MasterChefV1ABI from '@sushiswap/core/abi/MasterChef.json'
 import { SUSHI } from '@sushiswap/currency'
-import { readContract, ReadContractConfig, readContracts, ReadContractsConfig } from '@wagmi/core'
 import { daysInYear, secondsInDay } from 'date-fns'
 import { Farm } from 'src/types'
 
 import { MASTERCHEF_ADDRESS } from '../../../config'
 import { getAverageBlockTime, getPairs, getTokenBalancesOf, getTokens } from '../../common'
+import { getPoolInfos, getPoolLength, getTotalAllocPoint } from './fetchers'
 
 const SUSHI_PER_BLOCK = 100
-
-async function getPoolLength() {
-  const poolLengthCall: ReadContractConfig = {
-    addressOrName: MASTERCHEF_ADDRESS[ChainId.ETHEREUM],
-    chainId: ChainId.ETHEREUM,
-    contractInterface: MasterChefV1ABI,
-    functionName: 'poolLength',
-  }
-  return readContract<IMasterChef, Awaited<ReturnType<MasterChef['poolLength']>>>(poolLengthCall)
-}
-
-async function getTotalAllocPoint() {
-  const totalAllocPointCall: ReadContractConfig = {
-    addressOrName: MASTERCHEF_ADDRESS[ChainId.ETHEREUM],
-    chainId: ChainId.ETHEREUM,
-    contractInterface: MasterChefV1ABI,
-    functionName: 'totalAllocPoint',
-  }
-  return readContract<IMasterChef, Awaited<ReturnType<MasterChef['totalAllocPoint']>>>(totalAllocPointCall)
-}
-
-async function getPoolInfos(poolLength: number) {
-  const poolInfoCalls: ReadContractsConfig['contracts'] = [...Array(poolLength)].map((_, i) => ({
-    addressOrName: MASTERCHEF_ADDRESS[ChainId.ETHEREUM],
-    args: [i],
-    chainId: ChainId.ETHEREUM,
-    contractInterface: MasterChefV1ABI,
-    functionName: 'poolInfo',
-  }))
-  return readContracts<Awaited<ReturnType<MasterChef['poolInfo']>>[]>({
-    allowFailure: true,
-    contracts: poolInfoCalls,
-  })
-}
 
 export async function getMasterChefV1(): Promise<{ chainId: ChainId; farms: Record<string, Farm> }> {
   const [poolLength, totalAllocPoint, [{ derivedUSD: sushiPriceUSD }], averageBlockTime] = await Promise.all([
--- scripts/farms/src/lib/chefs/masterChefV2/index.ts
+++ scripts/farms/src/lib/chefs/masterChefV2/index.ts
@@ -1,104 +1,19 @@
 import { ChainId } from '@sushiswap/chain'
-import { MasterChefV2 } from '@sushiswap/core'
-import MasterChefV2ABI from '@sushiswap/core/abi/MasterChefV2.json'
 import { SUSHI } from '@sushiswap/currency'
-import { readContract, ReadContractConfig, readContracts, ReadContractsConfig } from '@wagmi/core'
 import { daysInYear, secondsInDay } from 'date-fns'
-import { BigNumber } from 'ethers'
 import { Farm } from 'src/types'
 
 import { MASTERCHEF_V2_ADDRESS } from '../../../config'
 import { divBigNumberToNumber, getAverageBlockTime, getPairs, getTokenBalancesOf, getTokens } from '../../common'
-
-async function getPoolLength() {
-  const poolLengthCall: ReadContractConfig = {
-    addressOrName: MASTERCHEF_V2_ADDRESS[ChainId.ETHEREUM],
-    chainId: ChainId.ETHEREUM,
-    contractInterface: MasterChefV2ABI,
-    functionName: 'poolLength',
-  }
-  return readContract<MasterChefV2, Awaited<ReturnType<MasterChefV2['poolLength']>>>(poolLengthCall)
-}
-
-async function getTotalAllocPoint() {
-  const totalAllocPointCall: ReadContractConfig = {
-    addressOrName: MASTERCHEF_V2_ADDRESS[ChainId.ETHEREUM],
-    chainId: ChainId.ETHEREUM,
-    contractInterface: MasterChefV2ABI,
-    functionName: 'totalAllocPoint',
-  }
-  return readContract<MasterChefV2, Awaited<ReturnType<MasterChefV2['totalAllocPoint']>>>(totalAllocPointCall)
-}
-
-async function getSushiPerBlock() {
-  const sushiPerBlockCall: ReadContractConfig = {
-    addressOrName: MASTERCHEF_V2_ADDRESS[ChainId.ETHEREUM],
-    chainId: ChainId.ETHEREUM,
-    contractInterface: MasterChefV2ABI,
-    functionName: 'sushiPerBlock',
-  }
-  return readContract<MasterChefV2, Awaited<ReturnType<MasterChefV2['sushiPerBlock']>>>(sushiPerBlockCall)
-}
-
-async function getPoolInfos(poolLength: number) {
-  const poolInfoCalls: ReadContractsConfig['contracts'] = [...Array(poolLength)].map((_, i) => ({
-    addressOrName: MASTERCHEF_V2_ADDRESS[ChainId.ETHEREUM],
-    args: [i],
-    chainId: ChainId.ETHEREUM,
-    contractInterface: MasterChefV2ABI,
-    functionName: 'poolInfo',
-  }))
-  return readContracts<Awaited<ReturnType<MasterChefV2['poolInfo']>>[]>({
-    allowFailure: true,
-    contracts: poolInfoCalls,
-  })
-}
-
-async function getLpTokens(poolLength: number) {
-  const lpTokenCalls: ReadContractsConfig['contracts'] = [...Array(poolLength)].map((_, i) => ({
-    addressOrName: MASTERCHEF_V2_ADDRESS[ChainId.ETHEREUM],
-    args: [i],
-    chainId: ChainId.ETHEREUM,
-    contractInterface: MasterChefV2ABI,
-    functionName: 'lpToken',
-  }))
-  return readContracts<Awaited<ReturnType<MasterChefV2['lpToken']>>[]>({
-    allowFailure: true,
-    contracts: lpTokenCalls,
-  })
-}
-
-async function getRewarders(poolLength: number) {
-  const rewarderCalls: ReadContractsConfig['contracts'] = [...Array(poolLength)].map((_, i) => ({
-    addressOrName: MASTERCHEF_V2_ADDRESS[ChainId.ETHEREUM],
-    args: [i],
-    chainId: ChainId.ETHEREUM,
-    contractInterface: MasterChefV2ABI,
-    functionName: 'rewarder',
-  }))
-  return readContracts<Awaited<ReturnType<MasterChefV2['rewarder']>>[]>({
-    allowFailure: true,
-    contracts: rewarderCalls,
-  })
-}
-
-async function getRewarderInfos() {
-  const { getBuiltGraphSDK } = await import('../../../../.graphclient')
-  const sdk = getBuiltGraphSDK()
-
-  const { rewarders } = await sdk.MasterChefV2Rewarders({
-    where: {
-      id_not: '0x0000000000000000000000000000000000000000',
-      rewardToken_not: '0x0000000000000000000000000000000000000000',
-    },
-  })
-
-  return rewarders.map((rewarder) => ({
-    id: rewarder.id,
-    rewardToken: rewarder.rewardToken as string,
-    rewardPerSecond: BigNumber.from(rewarder.rewardPerSecond),
-  }))
-}
+import {
+  getLpTokens,
+  getPoolInfos,
+  getPoolLength,
+  getRewarderInfos,
+  getRewarders,
+  getSushiPerBlock,
+  getTotalAllocPoint,
+} from './fetchers'
 
 export async function getMasterChefV2(): Promise<{ chainId: ChainId; farms: Record<string, Farm> }> {
   const [poolLength, totalAllocPoint, sushiPerBlock, rewarderInfos, averageBlockTime] = await Promise.all([
