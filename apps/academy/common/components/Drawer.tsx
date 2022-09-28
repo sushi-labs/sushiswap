e3e47baca0f7cf48b03787c00363d6856c6519e5
0xIvan <0xivanivan@gmail.com>
2022-09-28 20:59:26 +0800

    Add mobile menu

--- apps/academy/common/components/Drawer.tsx
+++ apps/academy/common/components/Drawer.tsx
@@ -1,23 +1,70 @@
+import { Transition } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import { IconButton } from '@sushiswap/ui'
 import classNames from 'classnames'
-import React from 'react'
+import React, { FC, Fragment, ReactNode, useEffect, useRef, useState } from 'react'
+import ReactDOM from 'react-dom'
 
-export const Drawer = ({ children, isOpen, onClose, header }) => {
+interface Drawer {
+  isOpen: boolean
+  onClose: () => void
+  children: ReactNode
+  className?: string
+}
+
+export const Drawer: FC<Drawer> = ({ children, isOpen, onClose, className }) => {
+  const [render, setRender] = useState(false)
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (ref.current) setRender(true)
+  }, [])
+
   return (
-    <div
-      className={classNames(
-        'bg-slate-900 w-screen transform ease-in-out duration-300 z-[1080] h-screen fixed top-0 overflow-y-auto px-6 pt-6',
-        isOpen ? '-translate-x-0' : '-translate-x-full'
-      )}
-    >
-      <div className="flex items-center justify-between">
-        <header className="text-2xl font-bold">{header}</header>
-        <IconButton className="w-6 h-6" onClick={onClose}>
-          <XMarkIcon />
-        </IconButton>
-      </div>
-      <div className="mt-5">{children}</div>
+    <div ref={ref}>
+      {render &&
+        ReactDOM.createPortal(
+          <Transition.Root appear show={isOpen} unmount={false} as={Fragment}>
+            <div
+              className={classNames(className, 'absolute right-0 top-0 bottom-0 w-full translate-x-[100%] z-[1080]')}
+            >
+              <Transition.Child
+                enter="ease-in-out duration-500"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in-out duration-500"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+                unmount={false}
+              >
+                <div
+                  aria-hidden="true"
+                  onClick={onClose}
+                  className="translate-x-[-100%] absolute inset-0 bg-slate-900 transition-opacity"
+                />
+              </Transition.Child>
+              <Transition.Child
+                enter="transform transition ease-in-out duration-300"
+                enterFrom="translate-x-0"
+                enterTo="translate-x-[-100%]"
+                leave="transform transition ease-in-out duration-500"
+                leaveFrom="translate-x-[-100%]"
+                leaveTo="translate-x-0"
+                unmount={false}
+              >
+                <div className={classNames('px-[30px] py-[34px] relative', className)}>
+                  <div className="absolute right-6 top-[38px]" onClick={onClose}>
+                    <IconButton className="w-6 h-6 text-slate-50/50" onClick={onClose}>
+                      <XMarkIcon />
+                    </IconButton>
+                  </div>
+                  {children}
+                </div>
+              </Transition.Child>
+            </div>
+          </Transition.Root>,
+          ref?.current
+        )}
     </div>
   )
 }
--- apps/academy/common/components/Header.tsx
+++ apps/academy/common/components/Header.tsx
@@ -1,8 +1,12 @@
 import { Listbox } from '@headlessui/react'
 import { Bars3Icon, ChevronDownIcon } from '@heroicons/react/24/outline'
-import { App, classNames, Link, Select, Typography } from '@sushiswap/ui'
+import { App, classNames, IconButton, Link, Select, SushiIcon, Typography } from '@sushiswap/ui'
 import { AppType } from '@sushiswap/ui/app/Header'
-import { FC } from 'react'
+import { useDisclosure } from 'common/hooks'
+import { SushiTransparentIcon, TriangleIcon } from 'common/icons'
+import { FC, useState } from 'react'
+
+import { Drawer } from './Drawer'
 
 export const Header: FC = () => {
   // TODO: links
@@ -34,67 +38,92 @@ export const Header: FC = () => {
       ],
     },
   ]
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [sectionOpen, setSectionOpen] = useState('')
+  const toggleSection = (section: string) => {
+    setSectionOpen((prev) => (prev === section ? '' : section))
+  }
+  const handleClose = () => {
+    setSectionOpen('')
+    onClose()
+  }
+
   return (
-    <App.Header appType={AppType.Academy} withScrollBackground>
-      <nav className="hidden md:flex gap-14">
-        {headerLinks.map(({ title, options }, i) => (
-          <Select
-            key={title}
-            button={
-              <Listbox.Button
-                type="button"
-                className="flex items-center gap-1 font-medium hover:text-slate-200 text-slate-300"
-              >
-                <span className="text-sm">{title}</span>
-                <ChevronDownIcon className="w-3 h-3" aria-hidden="true" />
-              </Listbox.Button>
-            }
-          >
-            <Select.Options
-              className={classNames(
-                'w-[217px] max-w-[240px] !bg-slate-700 -ml-5 mt-5 max-h-[unset] p-6 gap-6 flex flex-col',
-                i === headerLinks.length - 1 && 'right-0 xl:right-auto'
-              )}
+    <>
+      <App.Header appType={AppType.Academy} withScrollBackground>
+        <nav className="hidden md:flex gap-14">
+          {headerLinks.map(({ title, options }, i) => (
+            <Select
+              key={title}
+              button={
+                <Listbox.Button
+                  type="button"
+                  className="flex items-center gap-1 font-medium hover:text-slate-200 text-slate-300"
+                >
+                  <span className="text-sm">{title}</span>
+                  <ChevronDownIcon className="w-3 h-3" aria-hidden="true" />
+                </Listbox.Button>
+              }
             >
-              {options.map(({ option, href }, i) => (
-                <Link.Internal href={href} key={i}>
-                  <Typography weight={500} variant="sm">
-                    {option}
-                  </Typography>
-                </Link.Internal>
-              ))}
-            </Select.Options>
-          </Select>
-        ))}
-      </nav>
-      <nav className="md:hidden">
-        <Select
-          button={
-            <Listbox.Button type="button" className="p-1.5 bg-slate-900 rounded">
-              <Bars3Icon className="w-5 h-5" aria-hidden="true" />
-            </Listbox.Button>
-          }
-        >
-          <Select.Options className="w-[240px] !bg-slate-700 -ml-5 mt-5 max-h-[unset] p-6 gap-6 flex flex-col right-0">
-            {headerLinks.map(({ title, options }) => (
-              <div key={title}>
-                <Typography variant="xs" weight={600} className="mb-2 uppercase text-slate-400">
-                  {title}
-                </Typography>
-                <div className="px-2 space-y-2">
-                  {options.map(({ option, href }, i) => (
-                    <Link.Internal href={href} key={i}>
-                      <Typography weight={500} variant="sm">
-                        {option}
+              <Select.Options
+                className={classNames(
+                  'w-[217px] max-w-[240px] !bg-slate-700 -ml-5 mt-5 max-h-[unset] p-6 gap-6 flex flex-col',
+                  i === headerLinks.length - 1 && 'right-0 xl:right-auto'
+                )}
+              >
+                {options.map(({ option, href }, i) => (
+                  <Link.Internal href={href} key={i}>
+                    <Typography weight={500} variant="sm">
+                      {option}
+                    </Typography>
+                  </Link.Internal>
+                ))}
+              </Select.Options>
+            </Select>
+          ))}
+        </nav>
+
+        <nav className="md:hidden">
+          <IconButton type="button" className="p-1.5 bg-slate-900 rounded" onClick={onOpen}>
+            <Bars3Icon className="w-5 h-5" aria-hidden="true" />
+          </IconButton>
+
+          <Drawer isOpen={isOpen} onClose={handleClose} className="flex flex-col h-screen">
+            <>
+              <div className="flex items-center justify-between w-full">
+                <SushiIcon width={32} height={32} />
+              </div>
+              <div className="mt-[62px] grid gap-12">
+                {headerLinks.map(({ title, options }) => (
+                  <div key={title}>
+                    <button className="flex items-center gap-6" onClick={() => toggleSection(title)}>
+                      <TriangleIcon className={title === sectionOpen && 'rotate-90'} />
+                      <Typography variant="h3" weight={700} className="text-slate-50">
+                        {title}
                       </Typography>
-                    </Link.Internal>
-                  ))}
+                    </button>
+                    {sectionOpen === title && (
+                      <div className="grid gap-[28px] pb-1.5 pl-6 mt-9">
+                        {options.map(({ option, href }, i) => (
+                          <Link.Internal href={href} key={i}>
+                            <Typography weight={500} className="text-slate-400">
+                              {option}
+                            </Typography>
+                          </Link.Internal>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                ))}
+                <div className="absolute bottom-0 right-0">
+                  <SushiTransparentIcon />
                 </div>
               </div>
-            ))}
-          </Select.Options>
-        </Select>
-      </nav>
-    </App.Header>
+            </>
+          </Drawer>
+        </nav>
+      </App.Header>
+    </>
   )
 }
