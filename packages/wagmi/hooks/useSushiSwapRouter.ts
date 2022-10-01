0a7b258d9fb4eb0ee44d8472cdefbb3b68f7e54b
matthewlilley <hello@matthewlilley.com>
2022-10-01 22:56:15 +0100

    fix(packages/wagmi): useSushiSwapRouter and config safety

--- packages/wagmi/hooks/useSushiSwapRouter.ts
+++ packages/wagmi/hooks/useSushiSwapRouter.ts
@@ -1,28 +1,32 @@
-import sushiswapArtifact from '@sushiswap/sushiswap/artifacts/contracts/UniswapV2Router02.sol/UniswapV2Router02.json'
+import { ChainId } from '@sushiswap/chain'
 import sushiswapExports from '@sushiswap/sushiswap/exports.json'
-import { useContract, useSigner } from 'wagmi'
+import { useMemo } from 'react'
+import { useSigner } from 'wagmi'
+import { getContract } from 'wagmi/actions'
+
+type Exports = typeof sushiswapExports
 
 export const getSushiSwapKlimaRouterContractConfig = (chainId: number | undefined) => ({
-  addressOrName: '0x85B5cc3ec95AE5D0b02E7c17e53F97C4B02a78e4',
+  addressOrName: chainId === ChainId.POLYGON ? '0x85B5cc3ec95AE5D0b02E7c17e53F97C4B02a78e4' : '',
   contractInterface:
-    sushiswapExports[chainId as unknown as keyof Omit<typeof sushiswapExports, '31337'>]?.[0]?.contracts
-      ?.UniswapV2Router02?.abi ?? sushiswapArtifact.abi,
+    sushiswapExports[chainId?.toString() as keyof Exports]?.[0]?.contracts?.UniswapV2Router02?.abi ?? [],
 })
 
 // TODO CELO NOT FOUND?
 export const getSushiSwapRouterContractConfig = (chainId: number | undefined) => ({
   addressOrName:
-    sushiswapExports[chainId as unknown as keyof Omit<typeof sushiswapExports, '31337'>]?.[0]?.contracts
-      ?.UniswapV2Router02?.address ?? '',
+    sushiswapExports[chainId?.toString() as keyof Exports]?.[0]?.contracts?.UniswapV2Router02?.address ?? '',
   contractInterface:
-    sushiswapExports[chainId as unknown as keyof Omit<typeof sushiswapExports, '31337'>]?.[0]?.contracts
-      ?.UniswapV2Router02?.abi ?? [],
+    sushiswapExports[chainId?.toString() as keyof Exports]?.[0]?.contracts?.UniswapV2Router02?.abi ?? [],
 })
 
 export function useSushiSwapRouterContract(chainId: number | undefined) {
   const { data: signerOrProvider } = useSigner()
-  return useContract({
-    ...getSushiSwapRouterContractConfig(chainId),
-    signerOrProvider,
-  })
+  return useMemo(() => {
+    if (!chainId || !(chainId in sushiswapExports)) return
+    return getContract({
+      ...getSushiSwapRouterContractConfig(chainId),
+      signerOrProvider,
+    })
+  }, [chainId, signerOrProvider])
 }
