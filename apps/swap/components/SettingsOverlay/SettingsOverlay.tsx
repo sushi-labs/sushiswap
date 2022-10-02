0386a84772dbc97471cd67bf2dcdf26bc71e5ba3
matthewlilley <hello@matthewlilley.com>
2022-10-02 08:17:41 +0100

    feat(apps/swpa): make sushi guard & carbon offset more interactive

--- apps/swap/components/SettingsOverlay/SettingsOverlay.tsx
+++ apps/swap/components/SettingsOverlay/SettingsOverlay.tsx
@@ -1,6 +1,6 @@
 import { CogIcon } from '@heroicons/react/outline'
 import { ChainId } from '@sushiswap/chain'
-import { IconButton, Overlay, SlideIn } from '@sushiswap/ui'
+import { classNames, IconButton, Overlay, SlideIn } from '@sushiswap/ui'
 import { useSettings } from 'lib/state/storage'
 import { FC, useState } from 'react'
 
@@ -16,23 +16,48 @@ interface SettingsOverlay {
 
 export const SettingsOverlay: FC<SettingsOverlay> = ({ chainId }) => {
   const [open, setOpen] = useState(false)
-  const [{ carbonOffset }] = useSettings()
+  const [{ carbonOffset, sushiGuard }, { updateCarbonOffset, updateSushiGuard }] = useSettings()
   return (
     <>
-      <div className="flex flex-row gap-4">
-        {chainId === ChainId.POLYGON && carbonOffset && (
-          <svg width="20" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="m10.708 2.45.19.003c.321.007.642.034.953.081a1.083 1.083 0 0 0 1.233-.918 1.09 1.09 0 0 0-.912-1.24 9.993 9.993 0 0 0-1.073-.1l-.15-.004C5.573.151 1.122 4.412 1.001 9.781c-.12 5.369 4.141 9.826 9.505 9.947a9.734 9.734 0 0 0 9.375-6.433 1.087 1.087 0 1 0-2.046-.739 7.567 7.567 0 0 1-7.278 4.991c-4.161-.094-7.47-3.552-7.38-7.717.1-4.1 3.45-7.367 7.53-7.38ZM22.86 3.915c.35-.446.02-1.112-.722-1.138H19.23a8.597 8.597 0 0 0-6.126 2.538 8.653 8.653 0 0 0-1.744 2.48 8.71 8.71 0 0 0-.712 2.401A4.08 4.08 0 0 0 7.67 8.901H5.364c-.431.017-.451.437-.12.799l5.384 5.382c.38.309.742.288 1.072 0L22.86 3.914Zm-10.88 9.182c-.08.082-.22.07-.22-.067v-1.584c0-4.12 3.348-7.472 7.47-7.472h1.403c.22 0 .32.16.18.299-.41.406-2.075 2.067-8.833 8.824Z"
-              fill="#fff"
-              stroke="#fff"
-              strokeWidth=".005"
-              strokeMiterlimit="2"
-              strokeLinejoin="round"
-            />
-          </svg>
+      <div className="grid grid-flow-col gap-4">
+        {chainId === ChainId.ETHEREUM && (
+          <IconButton
+            className={classNames(
+              'hover:animate-pulse min-w-5 min-h-5 flex items-center',
+              sushiGuard && 'text-green-400'
+            )}
+            onClick={() => updateSushiGuard(!sushiGuard)}
+          >
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="m10 18.75-3.86-2.058a6.865 6.865 0 0 1-3.64-6.067V2.5a1.251 1.251 0 0 1 1.25-1.25h12.5A1.251 1.251 0 0 1 17.5 2.5v8.125a6.864 6.864 0 0 1-3.64 6.067L10 18.75ZM3.75 2.5v8.125a5.616 5.616 0 0 0 2.979 4.964L10 17.333l3.271-1.744a5.616 5.616 0 0 0 2.979-4.964V2.5H3.75Z"
+                fill="currentColor"
+              />
+              <path d="M10 15.798V3.75h5v6.753a4.375 4.375 0 0 1-2.313 3.858L10 15.798Z" fill="currentColor" />
+            </svg>
+          </IconButton>
         )}
-        <IconButton className="hover:animate-spin-slow" onClick={() => setOpen(true)}>
+        {chainId === ChainId.POLYGON && (
+          <IconButton
+            className={classNames(
+              'hover:animate-pulse min-w-5 min-h-5 flex items-center',
+              carbonOffset && 'text-green-400'
+            )}
+            onClick={() => updateCarbonOffset(!carbonOffset)}
+          >
+            <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="m8.808 2.145.172.002c.29.007.58.03.86.073a.978.978 0 0 0 1.113-.828.984.984 0 0 0-.824-1.12 9.234 9.234 0 0 0-.968-.09L9.025.18a8.783 8.783 0 0 0-.398 17.56 8.785 8.785 0 0 0 8.462-5.805.981.981 0 1 0-1.846-.667 6.83 6.83 0 0 1-6.57 4.505 6.824 6.824 0 0 1-6.661-6.966 6.826 6.826 0 0 1 6.796-6.66Zm10.968 1.32c.317-.401.018-1.003-.651-1.026H16.5a7.76 7.76 0 0 0-5.53 2.291 7.81 7.81 0 0 0-1.574 2.238 7.862 7.862 0 0 0-.642 2.167 3.683 3.683 0 0 0-2.688-1.168H3.985c-.39.015-.408.395-.109.721l4.86 4.858c.344.279.67.26.968 0l10.072-10.08Zm-9.819 8.29c-.072.074-.199.063-.199-.06v-1.43A6.75 6.75 0 0 1 16.5 3.52h1.267c.2 0 .29.144.163.27-.37.366-1.873 1.866-7.973 7.964Z"
+                fill="currentColor"
+                stroke="currentColor"
+                strokeWidth=".005"
+                strokeMiterlimit="2"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </IconButton>
+        )}
+        <IconButton className="flex items-center hover:animate-spin-slow min-w-5 min-h-5" onClick={() => setOpen(true)}>
           <CogIcon width={20} height={20} />
         </IconButton>
       </div>
