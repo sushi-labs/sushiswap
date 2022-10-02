80a4e0fe03f06701ec9f00fa206d1186ca9bcab7
0xIvan <0xivanivan@gmail.com>
2022-10-03 00:32:42 +0800

    Selected topic styling

--- apps/academy/common/components/Categories.tsx
+++ apps/academy/common/components/Categories.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@sushiswap/ui'
+import { Button, classNames } from '@sushiswap/ui'
 import { Dispatch, FC, SetStateAction } from 'react'
 
 import { CategoryEntity } from '../../.mesh'
@@ -20,8 +20,11 @@ export const Categories: FC<Categories> = ({ categories, selected, onSelect }) =
             size="md"
             onClick={() => onSelect(category.id)}
             key={category.id}
-            color={selected === category.id ? 'blue' : 'gray'}
-            className="!px-[14px] sm:!px-4 !h-9 sm:h-10 font-medium !text-xs sm:!text-sm sm:font-normal rounded-full !bg-slate-800"
+            color="gray"
+            className={classNames(
+              '!px-[14px] sm:!px-4 !h-9 sm:h-10 font-medium !text-xs sm:!text-sm sm:font-normal rounded-full !bg-slate-800 hover:ring-1',
+              selected === category.id && 'ring-1'
+            )}
           >
             {category?.attributes?.name}
           </Button>
