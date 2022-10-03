42e2095ab0cf859d6795ceded8451bf52b6cb5db
matthewlilley <hello@matthewlilley.com>
2022-10-03 15:34:57 +0100

    fix(apps/stable): force use of correct bytecode for cpp

--- packages/exchange/src/ConstantProductPool/computeConstantProductPoolAddress.ts
+++ packages/exchange/src/ConstantProductPool/computeConstantProductPoolAddress.ts
@@ -3,9 +3,9 @@ import { getCreate2Address } from '@ethersproject/address'
 import { keccak256 } from '@ethersproject/solidity'
 import { ChainId } from '@sushiswap/chain'
 import { Token } from '@sushiswap/currency'
-import constantProductPoolArtifact from '@sushiswap/trident/artifacts/contracts/pool/constant-product/ConstantProductPool.sol/ConstantProductPool.json'
 import EXPORTS from '@sushiswap/trident/exports/all.json'
-import constantProductPoolArtifactPreview from 'trident-preview/artifacts/contracts/pool/constant-product/ConstantProductPool.sol/ConstantProductPool.json'
+import constantProductPoolArtifactV0 from 'trident-v0/artifacts/contracts/pool/constant-product/ConstantProductPool.sol/ConstantProductPool.json'
+import constantProductPoolArtifactV1 from 'trident-v1/artifacts/contracts/pool/constant-product/ConstantProductPool.sol/ConstantProductPool.json'
 
 import { Fee } from '../Fee'
 import { computeInitCodeHash } from './computeInitCodeHash'
@@ -35,11 +35,11 @@ export const computeConstantProductPoolAddress = ({
     token0.chainId === ChainId.POLYGON
       ? // Compute init code hash based off the bytecode, deployData & masterDeployerAddress
         computeInitCodeHash({
-          creationCode: constantProductPoolArtifactPreview.bytecode,
+          creationCode: constantProductPoolArtifactV0.bytecode,
           deployData,
           masterDeployerAddress: (EXPORTS as any)[token0.chainId][0].contracts.MasterDeployer.address,
         })
-      : keccak256(['bytes'], [constantProductPoolArtifact.bytecode])
+      : keccak256(['bytes'], [constantProductPoolArtifactV1.bytecode])
 
   // Compute pool address
   return getCreate2Address(factoryAddress, keccak256(['bytes'], [deployData]), CONSTANT_PRODUCT_POOL_INIT_CODE_HASH)
