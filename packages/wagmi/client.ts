396cbe4458b1a8420e16f3cc1034182dfa3b1936
matthewlilley <hello@matthewlilley.com>
2022-10-03 20:31:48 +0100

    fix(packages/wagmi): only use single provider for now

--- packages/wagmi/client.ts
+++ packages/wagmi/client.ts
@@ -5,8 +5,8 @@ import { allChains, Chain, configureChains, createClient, CreateClientConfig } f
 import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
-import { alchemyProvider } from 'wagmi/providers/alchemy'
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
+// import { alchemyProvider } from 'wagmi/providers/alchemy'
+// import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 import { publicProvider } from 'wagmi/providers/public'
 export type Client = ReturnType<typeof createClient>
 
@@ -16,18 +16,19 @@ const infuraId = process.env.INFURA_ID || process.env.NEXT_PUBLIC_INFURA_ID
 const { chains, provider }: CreateClientConfig & { chains: Chain[] } = configureChains(
   [...allChains, ...otherChains],
   [
-    jsonRpcProvider({
-      priority: 0,
-      rpc: (chain) => {
-        if (chain.id !== 1) return null
-        return {
-          http: `https://api.securerpc.com/v1`,
-          webSocket: `wss://api.securerpc.com/v1`,
-        }
-      },
-    }),
-    alchemyProvider({ apiKey: alchemyId, priority: 1 }),
-    publicProvider({ priority: 2 }),
+    // jsonRpcProvider({
+    //   priority: 0,
+    //   rpc: (chain) => {
+    //     if (chain.id !== 1) return null
+    //     return {
+    //       http: `https://api.securerpc.com/v1`,
+    //       webSocket: `wss://api.securerpc.com/v1`,
+    //     }
+    //   },
+    // }),
+    // alchemyProvider({ apiKey: alchemyId, priority: 1 }),
+    // publicProvider({ priority: 2 }),
+    publicProvider(),
     // infuraProvider({ infuraId }),
   ],
   { pollingInterval: 8_000 }
