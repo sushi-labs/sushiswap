af3d127e2739dc66fa51c0b2e807abe2f9c11cab
matthewlilley <hello@matthewlilley.com>
2022-10-04 19:54:51 +0100

    fix(apps/earn): lower stale time for single resources

--- apps/invest/pages/api/user/[id].ts
+++ apps/invest/pages/api/user/[id].ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { getUser, GetUserQuery } from '../../../lib/api'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59')
   if (req.query.id === 'undefined') {
     return res.status(200).send({ user: [] })
   }
