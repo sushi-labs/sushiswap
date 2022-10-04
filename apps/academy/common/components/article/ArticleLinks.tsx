3c8152d6bd131d8961ef5997bb26d03d7a1fbcf0
0xIvan <0xivanivan@gmail.com>
2022-10-05 00:48:00 +0800

    Add title to sharing

--- apps/academy/common/components/article/ArticleLinks.tsx
+++ apps/academy/common/components/article/ArticleLinks.tsx
@@ -1,6 +1,6 @@
 import { EnvelopeIcon, LinkIcon } from '@heroicons/react/24/outline'
-import { TwitterIcon } from '@sushiswap/ui'
-import { FC } from 'react'
+import { Tooltip, TwitterIcon } from '@sushiswap/ui'
+import { FC, useState } from 'react'
 
 import { ArticleEntity } from '../../../.mesh'
 
@@ -9,36 +9,49 @@ interface ArticleLinks {
 }
 
 export const ArticleLinks: FC<ArticleLinks> = ({ article }) => {
+  const shareText = `Check out this Sushi article: ${article?.attributes.title}`
+  const [showTooltip, setShowTooltip] = useState(false)
+  const handleTooltipTimer = () => {
+    setShowTooltip(true)
+    setTimeout(() => {
+      setShowTooltip(false)
+    }, 1000)
+  }
+
   return (
     <section>
-      {/* <hr className="my-12 border border-slate-200/5" /> */}
-      {/* <h2 className="mb-6 text-base font-semibold text-slate-200">Share article</h2> */}
       <div className="flex gap-6">
         <a
           target="_blank"
           title="Share on Twitter"
-          href={`http://twitter.com/share?url=https://sushi.com/academy/articles/${article?.attributes?.slug}`}
+          href={`http://twitter.com/share?text=${shareText}&url=https://sushi.com/academy/articles/${article?.attributes?.slug}`}
           rel="noreferrer"
         >
           <TwitterIcon width={20} height={20} className="cursor-pointer text-blue hover:text-blue-400" />
         </a>
         <a
           title="Share by Email"
-          href={`mailto:?subject=${encodeURI(
-            article?.attributes?.title || ''
-          )}&body=Checkout this new SushiSwap Blog article ${encodeURI(
+          href={`mailto:?subject=${encodeURI(article?.attributes?.title || '')}&body=${shareText} ${encodeURI(
             `https://sushi.com/academy/articles/${article?.attributes?.slug}`
           )}`}
         >
           <EnvelopeIcon width={20} height={20} className="cursor-pointer text-blue hover:text-blue-400" />
         </a>
-        <LinkIcon
-          width={20}
-          height={20}
-          className="cursor-pointer text-blue hover:text-blue-400"
-          onClick={() =>
-            navigator.clipboard.writeText(`https://sushi.com/academy/articles/${article?.attributes?.slug}`)
+        <Tooltip
+          button={
+            <LinkIcon
+              width={20}
+              height={20}
+              className="cursor-pointer text-blue hover:text-blue-400"
+              onClick={() => {
+                navigator.clipboard.writeText(`https://sushi.com/academy/articles/${article?.attributes?.slug}`)
+                handleTooltipTimer()
+              }}
+            />
           }
+          panel={<>Copied to clipboard</>}
+          placement="bottom"
+          visible={showTooltip}
         />
       </div>
     </section>
