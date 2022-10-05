8dcf48be8eada2e002dd1319b098ea717d3e0f87
matthewlilley <hello@matthewlilley.com>
2022-10-05 16:59:42 +0100

    fix(apps/earn): set stale time for single resources to ~10m, max age 100m

--- apps/invest/pages/api/bar.tsx
+++ apps/invest/pages/api/bar.tsx
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { getSushiBar } from '../../lib/api'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader('Cache-Control', 'public, s-maxage=900, stale-while-revalidate=3600')
+  res.setHeader('Cache-Control', 'public, s-maxage=100, stale-while-revalidate=599')
   const bar = await getSushiBar()
   res.status(200).send(bar)
 }
