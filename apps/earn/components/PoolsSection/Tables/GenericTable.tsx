b58f85f3d3f28902aaa84d18919a4697826d66d4
matthewlilley <hello@matthewlilley.com>
2022-10-06 09:20:39 +0100

    fix(apps/earn): anchor

--- apps/earn/components/PoolsSection/Tables/GenericTable.tsx
+++ apps/earn/components/PoolsSection/Tables/GenericTable.tsx
@@ -1,5 +1,5 @@
 import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/solid'
-import { classNames, Link, LoadingOverlay, Table, Tooltip, Typography } from '@sushiswap/ui'
+import { classNames, LoadingOverlay, Table, Tooltip, Typography } from '@sushiswap/ui'
 import { flexRender, RowData, Table as ReactTableType } from '@tanstack/react-table'
 import React, { ReactNode, useState } from 'react'
 
@@ -89,9 +89,11 @@ export const GenericTable = <T extends { id: string }>({
                                 style={{ maxWidth: headers[i].getSize(), width: headers[i].getSize() }}
                                 key={cell.id}
                               >
-                                <Link.Internal href={`/${row.original.id}`}>
-                                  <a>{flexRender(cell.column.columnDef.cell, cell.getContext())}</a>
-                                </Link.Internal>
+                                {/* <Link.Internal href={`/${row.original.id}`}> */}
+                                <a href={`/earn/${row.original.id}`}>
+                                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </a>
+                                {/* </Link.Internal> */}
                               </Table.td>
                             )
                           })}
@@ -115,9 +117,11 @@ export const GenericTable = <T extends { id: string }>({
                     {row.getVisibleCells().map((cell, i) => {
                       return (
                         <Table.td style={{ maxWidth: headers[i].getSize(), width: headers[i].getSize() }} key={cell.id}>
-                          <Link.Internal href={`/${row.original.id}`}>
-                            <a>{flexRender(cell.column.columnDef.cell, cell.getContext())}</a>
-                          </Link.Internal>
+                          {/* <Link.Internal href={`/${row.original.id}`}> */}
+                          <a href={`/earn/${row.original.id}`}>
+                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                          </a>
+                          {/* </Link.Internal> */}
                         </Table.td>
                       )
                     })}
