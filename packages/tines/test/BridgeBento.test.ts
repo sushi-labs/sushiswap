ab43b1463c232601f5249362e8238b532e0cb119
ilya <bretnwood@gmail.com>
2022-10-10 15:17:12 +0300

    feat: Tines: zero Bento Bridge full

--- packages/tines/test/BridgeBento.test.ts
+++ packages/tines/test/BridgeBento.test.ts
@@ -2,35 +2,68 @@ import { BigNumber, BigNumberish } from '@ethersproject/bignumber'
 import seedrandom from 'seedrandom'
 import { _1e12 } from '../../math/dist'
 import { RToken } from '../dist'
-import { BridgeBento, closeValues, getBigNumber } from '../src'
+import { BridgeBento, getBigNumber } from '../src'
+
+function closeValues(a: number, b: number, accuracy: number, logInfoIfFalse = ''): boolean {
+  if (a === b) return true
+  if (accuracy === 0) return a === b
+  if (a == 0) return b < accuracy
+  if (b == 0) return a < accuracy
+  const res = Math.abs(a / b - 1) < accuracy
+  return res
+}
+
+function calcPrecision(a: number, b: number): number {
+  if (a === b) return 0
+  if (a == 0) return b
+  if (b == 0) return a
+  return Math.abs(a / b - 1)
+}
 
 function expectCloseValues(
   v1: BigNumberish,
   v2: BigNumberish,
-  precision: number,
+  precisionExpected: number,
   description = '',
   additionalInfo = ''
 ) {
   const a = typeof v1 == 'number' ? v1 : parseFloat(v1.toString())
   const b = typeof v2 == 'number' ? v2 : parseFloat(v2.toString())
-  const res = closeValues(a, b, precision)
-  if (!res) {
+  const precision = calcPrecision(a, b)
+  if (precision > precisionExpected) {
     console.log('Close values expectation failed:', description)
     console.log('v1 =', a)
     console.log('v2 =', b)
-    console.log('precision =', Math.abs(a / b - 1), ', expected <', precision)
+    console.log('precision =', precision, ', expected <', precisionExpected)
     if (additionalInfo != '') {
       console.log(additionalInfo)
     }
   }
-  expect(res).toBeTruthy()
-  return res
+  expect(precision <= precisionExpected).toBeTruthy()
+  return precision <= precisionExpected
 }
 
 function checkBridging(bridge: BridgeBento, amount: number) {
   const share = bridge.calcOutByIn(amount, true).out
+
   const amount2 = bridge.calcOutByIn(share, false).out
   expectCloseValues(amount, amount2, 1e-10)
+
+  const amount3 = bridge.calcInByOut(share, true).inp
+  expectCloseValues(amount, amount3, 1e-10)
+
+  const share2 = bridge.calcInByOut(amount, false).inp
+  expectCloseValues(share, share2, 1e-10)
+
+  if (amount != 0) {
+    const price = bridge.calcCurrentPriceWithoutFee(true)
+    expectCloseValues(share / amount, price, 1e-10)
+  }
+
+  if (share != 0) {
+    const price = bridge.calcCurrentPriceWithoutFee(false)
+    expectCloseValues(amount / share, price, 1e-10)
+  }
 }
 
 export function getRandomExp(rnd: () => number, min: number, max: number) {
@@ -80,7 +113,7 @@ describe('Bento Bridge test', () => {
     checkBridging(bridge, 0)
     for (let i = 0; i < 100; ++i) {
       const amount = getRandomExp(rnd, 1, 1e20)
-      checkBridging(bridge, 0)
+      checkBridging(bridge, amount)
     }
   })
 })
