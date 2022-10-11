edd639cadc2b7980046286c8afb11b6dd352860b
s bacha <sam@manifoldfinance.com>
2022-10-11 03:48:56 -0700

    build(nextjs): add meta data
    WIP

--- apps/swap/next.config.mjs
+++ apps/swap/next.config.mjs
@@ -8,10 +8,27 @@ const withTranspileModules = transpileModules([
   '@sushiswap/ui',
 ])
 
+const date = new Date();
+const GIT_COMMIT_SHA=`git rev-parse --verify HEAD`
+const GIT_COMMIT_SHA_SHORT=`git rev-parse --short HEAD`
+const NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA = typeof process.env.GIT_COMMIT_SHA === 'string' && process.env.GIT_COMMIT_SHA.substring(0, 8);
+
+
 // @ts-check
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   basePath: '/swap',
+  poweredByHeader: false,
+  productionBrowserSourceMaps: false,
+  httpAgentOptions: {
+    keepAlive: false,
+  },
+ generateBuildId: async () => {
+    return 'my-build-id'
+  },
+//  analyticsId: process.env.VERCEL_ANALYTICS_ID || '',
   reactStrictMode: true,
   swcMinify: true,
   images: {
@@ -20,6 +37,14 @@ const nextConfig = {
   },
   experimental: {
     nextScriptWorkers: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [{ key: 'Web-Build', value: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA }]
+      }
+    ];
   },
   async redirects() {
     return [
@@ -62,3 +87,7 @@ const nextConfig = {
 }
 
 export default withTranspileModules(nextConfig)
+
+console.log('process.env.VERCEL_GIT_COMMIT_SHA: ', process.env.GIT_COMMIT_SHA_SHORT);
+console.log('process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA: ', process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA);
+console.log('next.config.mjs', JSON.stringify(module.exports, null, 2))
