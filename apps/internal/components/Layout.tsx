a1aaff6956644374958e41e6c0c4e95a06d086dc
matthewlilley <hello@matthewlilley.com>
2022-10-11 20:01:28 +0100

    chore(apps/internal): token page layout

--- apps/internal/components/Layout.tsx
+++ apps/internal/components/Layout.tsx
@@ -6,11 +6,13 @@ type Props = {
   maxWidth?: MaxWidth
   backdrop?: React.ReactNode
   className?: string
+  breadcrumbs?: BreadcrumbLink[]
 }
 
 export function Layout({ children, maxWidth = '5xl', backdrop, className, breadcrumbs }: Props) {
   return (
     <Container maxWidth={maxWidth} className={classNames(className, 'lg:mx-auto px-4 h-full')}>
+      {breadcrumbs && <Breadcrumb home="/" links={breadcrumbs} />}
       <div className="pb-4 mt-10 mb-4 lg:mb-40 lg:mt-20">
         <Backdrop backdrop={backdrop}>{children}</Backdrop>
       </div>
