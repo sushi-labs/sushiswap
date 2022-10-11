66f891696fefbb6bd881c03999d6a87e0bff5255
matthewlilley <hello@matthewlilley.com>
2022-10-11 14:40:05 +0100

    fix(apps/furo): link paths

--- apps/furo/pages/_app.tsx
+++ apps/furo/pages/_app.tsx
@@ -43,12 +43,12 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1" />
-        <link rel="manifest" href="/site.webmanifest?v=1" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#fa52a0" />
-        <link rel="shortcut icon" href="/favicon.ico?v=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/furo/apple-touch-icon.png?v=1" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/furo/favicon-32x32.png?v=1" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/furo/favicon-16x16.png?v=1" />
+        <link rel="manifest" href="/furo/site.webmanifest?v=1" />
+        <link rel="mask-icon" href="/furo/safari-pinned-tab.svg?v=1" color="#fa52a0" />
+        <link rel="shortcut icon" href="/furo/favicon.ico?v=1" />
         <meta name="msapplication-TileColor" content="#2b5797" />
         <meta name="theme-color" content="#FA52A0" />
       </Head>
