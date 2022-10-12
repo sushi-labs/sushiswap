940acd9deaacdc074efd831260c74f3751268516
matthewlilley <hello@matthewlilley.com>
2022-10-12 19:28:08 +0100

    feat: add html lang to apps

--- apps/furo/pages/_document.tsx
+++ apps/furo/pages/_document.tsx
@@ -5,7 +5,7 @@ import Document, { Head, Html, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Inter&display=optional" rel="stylesheet" />
         </Head>
