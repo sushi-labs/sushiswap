fce7b3f9668af64e18eb2191cfcd693f86d4c41a
matthewlilley <hello@matthewlilley.com>
2022-10-13 08:33:45 +0100

    chore: disable next-pwa until custom runtime cache crafted which avoids serving stale bundles

--- apps/swap/next.config.mjs
+++ apps/swap/next.config.mjs
@@ -1,10 +1,10 @@
-import nextPwa from 'next-pwa'
+// import nextPwa from 'next-pwa'
 import transpileModules from 'next-transpile-modules'
 import { withAxiom } from 'next-axiom'
 
-const withPwa = nextPwa({
-  dest: 'public',
-})
+// const withPwa = nextPwa({
+//   dest: 'public',
+// })
 
 const withTranspileModules = transpileModules([
   '@sushiswap/redux-token-lists',
@@ -66,4 +66,5 @@ const nextConfig = {
   },
 }
 
-export default withAxiom(withPwa(withTranspileModules(nextConfig)))
+// export default withAxiom(withPwa(withTranspileModules(nextConfig)))
+export default withAxiom(withTranspileModules(nextConfig))
