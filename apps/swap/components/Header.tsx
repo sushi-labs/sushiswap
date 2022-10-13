cf24c900feb42b2c5e1a66355eaa3267fc5104f9
matthewlilley <hello@matthewlilley.com>
2022-10-13 01:24:10 +0100

    feat(packages/ui): extract buy crypto button

--- apps/swap/components/Header.tsx
+++ apps/swap/components/Header.tsx
@@ -1,40 +1,14 @@
-import { App, AppType } from '@sushiswap/ui'
+import { App, AppType, BuyCrypto } from '@sushiswap/ui'
 import { NotificationCentre, Wallet } from '@sushiswap/wagmi'
-import React, { FC, useMemo } from 'react'
+import React, { FC } from 'react'
 import { useAccount } from 'wagmi'
 
 import { SUPPORTED_CHAIN_IDS } from '../config'
 import { useNotifications } from '../lib/state/storage'
 
-const TRANSAK_NETWORKS = [
-  'ethereum',
-  'arbitrum',
-  'optimism',
-  'bsc',
-  'polygon',
-  'avaxcchain',
-  'celo',
-  'fantom',
-  'moonriver',
-]
-
 export const Header: FC = () => {
   const { address } = useAccount()
   const [notifications, { clearNotifications }] = useNotifications(address)
-  const buyUrl = useMemo(() => {
-    const params = new URLSearchParams()
-    params.append('apiKey', '5baa5495-64a5-4bcb-af71-febf3e54b07e')
-    if (address) {
-      params.append('walletAddress', address)
-    }
-    params.append('networks', TRANSAK_NETWORKS.join(','))
-    params.append('redirectURL', 'https://www.sushi.com/swap')
-    params.append('isAutoFillUserData', 'true')
-    params.append('hideMenu', 'true')
-    params.append('isFeeCalculationHidden', 'true')
-    // params.append('themeColor', '#3B82F6')
-    return `https://global.transak.com/?${params.toString()}`
-  }, [address])
   return (
     <App.Header
       withScrollBackground={true}
@@ -44,7 +18,7 @@ export const Header: FC = () => {
           <App.NavItemInternal href="https://sushi.com/swap" label="Swap" />
           <App.NavItemInternal href="https://sushi.com/earn" label="Earn" />
           {/* <App.NavItemInternal href="https://sushi.com/bridge" label="Bridge" /> */}
-          <App.NavItemExternal href={buyUrl} label="Buy" />
+          <BuyCrypto address={address} />
         </App.NavItemList>
       }
     >
