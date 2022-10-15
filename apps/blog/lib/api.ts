2a3f6664cc3be3f04a3b0b4af754fc575ecddf92
matthewlilley <hello@matthewlilley.com>
2022-10-15 14:26:09 +0100

    chore(apps/blog): cleanup blog api and revalidte 60

--- apps/blog/lib/api.ts
+++ apps/blog/lib/api.ts
@@ -1,45 +1,34 @@
 import { ArticleFiltersInput, CategoryFiltersInput, getMeshSDK, PaginationArg } from '../.mesh'
 
-export const getArticleAndMoreArticles = async (slug: string, preview: Record<string, unknown> | null) => {
-  const sdk = getMeshSDK()
+const sdk = getMeshSDK()
 
-  try {
-    return (
-      (await sdk.articleAndMoreArticles({
-        filters: {
-          slug: { eq: slug },
-          articleType: { eq: 'blog' },
-        },
-        filters_ne: { slug: { not: { eq: slug } } },
-        publicationState: preview ? 'PREVIEW' : 'LIVE',
-      })) ?? []
-    )
-  } catch (e) {
-    console.error(e)
-  }
+export const getArticleAndMoreArticles = async (slug: string, preview: Record<string, unknown> | null) => {
+  return sdk.articleAndMoreArticles({
+    filters: {
+      slug: { eq: slug },
+      articleType: { eq: 'blog' },
+    },
+    filters_ne: { slug: { not: { eq: slug } } },
+    publicationState: preview ? 'PREVIEW' : 'LIVE',
+  })
 }
 
 export const getAllArticlesBySlug = async () => {
-  const sdk = getMeshSDK()
-  return await sdk.getAllArticlesWithSlug()
+  return sdk.getAllArticlesWithSlug()
 }
 
 export const getGlobalSEO = async () => {
-  const sdk = getMeshSDK()
-  return await sdk.getGlobalSEO()
+  return sdk.getGlobalSEO()
 }
 
 export const getPreviewPostBySlug = async (slug: string) => {
-  const sdk = getMeshSDK()
-  return await sdk.getPreviewPostBySlug({ slug })
+  return sdk.getPreviewPostBySlug({ slug })
 }
 
 export const getArticles = async (variables?: { filters?: ArticleFiltersInput; pagination?: PaginationArg }) => {
-  const sdk = getMeshSDK()
-  return await sdk.getArticles({ ...variables, filters: { ...variables?.filters, articleType: { eq: 'blog' } } })
+  return sdk.getArticles({ ...variables, filters: { ...variables?.filters, articleType: { eq: 'blog' } } })
 }
 
 export const getCategories = async (filters?: CategoryFiltersInput) => {
-  const sdk = getMeshSDK()
-  return await sdk.getCategories({ filters })
+  return sdk.getCategories({ filters })
 }
