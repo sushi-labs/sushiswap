29019f06f7b3ebe5a10e9ec81b30870c389a2e14
matthewlilley <hello@matthewlilley.com>
2022-10-20 20:33:32 +0100

    feat(packages/chainlink): init

--- apps/kashi/components/Table/HealthCell.tsx
+++ apps/kashi/components/Table/HealthCell.tsx
@@ -6,7 +6,7 @@ import { CellProps } from './types'
 export const HealthCell: FC<CellProps> = ({ row }) => {
   return (
     <Typography variant="sm" weight={500} className="truncate text-slate-50">
-      {row.marketHealth.toFixed(2)}
+      ...
     </Typography>
   )
 }
