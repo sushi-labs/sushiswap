fa7b31faeaade319a64d4cc68a565ea4ffd56b31
matthewlilley <hello@matthewlilley.com>
2022-10-22 22:46:06 +0100

    fix(apps/earn): user api return 422 when no id

--- apps/earn/pages/api/user/[id].ts
+++ apps/earn/pages/api/user/[id].ts
@@ -4,6 +4,7 @@ import { getUser, GetUserQuery } from '../../../lib/api'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59')
+  if (!req.query?.id) res.status(422)
   const user = await getUser(req.query as GetUserQuery)
   res.status(200).json(user)
 }
