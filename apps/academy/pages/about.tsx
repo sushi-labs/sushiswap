f94d133066134ff0f12768934206693e41d47aed
0xIvan <0xivanivan@gmail.com>
2022-10-23 23:04:57 +0200

    Update roadmap

--- apps/academy/pages/about.tsx
+++ apps/academy/pages/about.tsx
@@ -1,9 +1,11 @@
 import { Disclosure, Transition } from '@headlessui/react'
-import { Chip, classNames, Container } from '@sushiswap/ui'
+import { classNames, Container, Link } from '@sushiswap/ui'
 import sushiPulse from 'common/assets/sushi-pulse.png'
 import sushidilly from 'common/assets/sushidilly.png'
 import { defaultSidePadding } from 'common/helpers'
 import { TriangleIcon } from 'common/icons'
+import { roadmapSections } from 'common/roadmap'
+import { format } from 'date-fns'
 import { NextPage } from 'next'
 import Image from 'next/image'
 
@@ -64,74 +66,61 @@ const About: NextPage = () => {
           <article className="mt-[100px]">
             <h2 className="font-medium text-slate-50 text-xl sm:text-4xl">Product Roadmap Sushi 2.0</h2>
             <p className="text-slate-300 mt-5 sm:mt-[30px] text-sm sm:text-xl">
-              Neque veniam esse harum ea tenetur non sit. Reiciendis exercitation incidunt quasi. Facere earum sit
-              repellat parquia diema repellendus sed est aut. Est ipsa inventore iure fugit voluptate.
+              On 11th May 2022 the
+              <Link.External
+                href="https://snapshot.org/#/sushigov.eth/proposal/QmVfGe7ZqUS4HZhpxssPXPKJF8sCctcEsN1Sybo3p4F4zb"
+                className="mx-2 underline"
+              >
+                Implementation Proposal
+              </Link.External>
+              for
+              <Link.External
+                href="https://mirror.xyz/0x4bb4c1B0745ef7B4642fEECcd0740deC417ca0a0/dFUiMYBl_xp2aN_F_Hc1abgP8NMv5Js5UYnup77MN0k"
+                className="mx-2 underline"
+              >
+                {"Sushi 2.0 'A Restructure for the Road Ahead'"}
+              </Link.External>
+              was passed by the community with 92% of the votes (9.6 mln SushiPowah) in favor. The current team has
+              collectively worked together on a long term roadmap & vision for what we would like to accomplish for
+              Sushi. This roadmap has been outlined below.
             </p>
             <div className="mt-8 sm:mt-10 grid gap-6">
-              <Disclosure as="div" className="bg-slate-800/50 rounded-lg">
-                {({ open }) => (
-                  <>
-                    <Disclosure.Button
-                      as="div"
-                      className="grid cursor-pointer px-4 pt-4 pb-6 sm:px-8 sm:pt-8 sm:pb-8 gap-2"
-                    >
-                      <div className="flex justify-between">
-                        <h4 className="font-semibold sm:font-bold text-lg sm:text-3xl text-slate-50">Miso Launchpad</h4>
-                        <div className="flex items-center justify-center rounded-full bg-slate-800 w-6 h-6 sm:w-[42px] sm:h-[42px]">
-                          <TriangleIcon
-                            className={classNames('transition sm:w-4 sm:h-4 h-3 w-3', open && 'transform rotate-90')}
-                          />
+              {roadmapSections.map(({ product, description, lastUpdated }) => (
+                <Disclosure as="div" className="bg-slate-800/50 rounded-lg" key={product}>
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button
+                        as="div"
+                        className="grid cursor-pointer px-4 pt-4 pb-6 sm:px-8 sm:pt-8 sm:pb-8 gap-2"
+                      >
+                        <div className="flex justify-between">
+                          <h4 className="font-semibold sm:font-bold text-lg sm:text-3xl text-slate-50">{product}</h4>
+                          <div className="flex items-center justify-center rounded-full bg-slate-800 w-6 h-6 sm:w-[42px] sm:h-[42px]">
+                            <TriangleIcon
+                              className={classNames('transition sm:w-4 sm:h-4 h-3 w-3', open && 'transform rotate-90')}
+                            />
+                          </div>
                         </div>
-                      </div>
-                      <div className="flex sm:gap-[14px] flex-wrap">
-                        <span className="sm:text-base text-slate-400">Last update on 23 sep, 2022:</span>
-                        <div className="flex gap-1 sm:gap-[14px] flex-wrap">
-                          {['Dashboard Designs', 'Crosschain Support', 'New UI'].map((label, i, a) => (
-                            <>
-                              <span className="sm:hidden">
-                                {label}
-                                {i !== a.length - 1 && ','}
-                              </span>
-                              <Chip label={label} key={label} className="sm:inline-flex hidden" />
-                            </>
-                          ))}
-                        </div>
-                      </div>
-                    </Disclosure.Button>
-                    {open && <hr className="border border-slate-700 mx-4 sm:mx-8" />}
-                    <Transition
-                      className="transition-[max-height] overflow-hidden"
-                      enter="duration-300 ease-in-out"
-                      enterFrom="transform max-h-0"
-                      enterTo="transform max-h-max"
-                      leave="transition-[max-height] duration-250 ease-in-out"
-                      leaveFrom="transform max-h-max"
-                      leaveTo="transform max-h-0"
-                    >
-                      <Disclosure.Panel className="text-gray-500 px-4 py-6 sm:px-8 sm:py-8">
-                        Our solution to launching a new token or introducing price discovery for many kinds of `assets
-                        on release. Lots of work over the last few months has gone into integrating Miso directly into
-                        our main Sushi app, and we now have a better system in place to scale up the completely
-                        permissionless framework. Continued work will be done in regards to introducing new forms of
-                        recipes, or scripts per se that have unified steps that are taken during an auction and on
-                        release of a new token. The auction creation process can then cater to any project’s pre and
-                        post-launch plans. Some of these enhancements include: Gnosis Safe or Minimal Multisig
-                        Deployment on launch. Liquidity Lockups or direct Liquidity Launching to spin up liquidity pairs
-                        the moment an auction ends. Vesting schedule put into place at the end of an auction. Direct
-                        incentive deployment, to establish well incentivized farms for new projects looking to build
-                        their liquidity on Sushi from day one. In the past Miso was gatekept, or required team
-                        involvement to help projects launch tokens and provide assistance during the process. Today,
-                        Miso is now completely permissionless and anyone has the ability to spin up new auctions at any
-                        time. Permissionless Miso has been deployed onto a number of major networks, and is available to
-                        use for any project regardless of the chain they decide to launch on. Decentralized curation or
-                        signaling of Miso Auctions is being explored, and we will continue to seek ways we can further
-                        improve the experience for users to ensure that they are participating in legitimate auctions.
-                        Read Less
-                      </Disclosure.Panel>
-                    </Transition>
-                  </>
-                )}
-              </Disclosure>
+                        <span className="sm:text-base text-slate-400">
+                          Last updated on {format(new Date(lastUpdated), 'd MMM, y')}
+                        </span>
+                      </Disclosure.Button>
+                      {open && <hr className="border border-slate-700 mx-4 sm:mx-8" />}
+                      <Transition
+                        className="transition-[max-height] overflow-hidden"
+                        enter="duration-300 ease-in-out"
+                        enterFrom="transform max-h-0"
+                        enterTo="transform max-h-max"
+                        leave="transition-[max-height] duration-250 ease-in-out"
+                        leaveFrom="transform max-h-max"
+                        leaveTo="transform max-h-0"
+                      >
+                        <Disclosure.Panel className="px-4 py-6 sm:px-8 sm:py-8">{description}</Disclosure.Panel>
+                      </Transition>
+                    </>
+                  )}
+                </Disclosure>
+              ))}
             </div>
           </article>
         </Container>
