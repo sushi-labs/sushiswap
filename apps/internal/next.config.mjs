aeb88232237e95a39c571e6b0a66c647a5a81e70
matthewlilley <hello@matthewlilley.com>
2022-11-09 03:07:34 +0000

    fix: compilation

--- apps/internal/next.config.mjs
+++ apps/internal/next.config.mjs
@@ -1,10 +1,6 @@
 import transpileModules from 'next-transpile-modules'
 
-const withTranspileModules = transpileModules([
-  '@sushiswap/redux-token-lists',
-  '@sushiswap/ui',
-  '@sushiswap/graph-client',
-])
+const withTranspileModules = transpileModules(['@sushiswap/redux-token-lists', '@sushiswap/ui'])
 
 // @ts-check
 /** @type {import('next').NextConfig} */
