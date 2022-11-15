03adf695456a12066954e32c74c93f33f95b2046
Chilli Chelli <ramin@sushi.com>
2022-11-15 09:12:27 +0100

    feat(apps/root): background on explainers

--- apps/_root/components/Story/Section3/Guard.tsx
+++ apps/_root/components/Story/Section3/Guard.tsx
@@ -15,8 +15,8 @@ export const Guard: FC = () => {
           <GuardImage />
           <div className="flex flex-col justify-center gap-3">
             <AnimateSharedLayout>
-              <div className="flex flex-col items-center lg:items-start">
-                <motion.div layoutId={`custody-title`} className="flex flex-col items-center lg:items-start z-[2001]">
+              <motion.div layoutId={`guard-container`} className="flex flex-col items-center lg:items-start">
+                <motion.div layoutId={`guard-title`} className="flex flex-col items-center lg:items-start z-[2001]">
                   <Typography variant="h1" weight={600} className="text-center lg:text-left">
                     Keep more profits and earn gas refunds.
                   </Typography>
@@ -24,7 +24,6 @@ export const Guard: FC = () => {
                     Enable SushiGuard and say no to MEV attacks.
                   </Typography>
                 </motion.div>
-
                 <Button
                   onClick={() => setOpen(true)}
                   className="!p-0 mt-3"
@@ -33,7 +32,7 @@ export const Guard: FC = () => {
                 >
                   Learn More
                 </Button>
-              </div>
+              </motion.div>
               <AnimatePresence>
                 {open && (
                   <>
@@ -43,23 +42,54 @@ export const Guard: FC = () => {
                       exit={{ opacity: 0, transition: { duration: 0.15 } }}
                       transition={{ duration: 0.2, delay: 0.15 }}
                       style={{ pointerEvents: 'auto' }}
-                      className="z-[2000] fixed bg-[rgba(0,0,0,0.6)] will-change-[opacity] inset-0 w-full backdrop-blur"
+                      className="z-[2001] fixed bg-[rgba(0,0,0,0.6)] will-change-[opacity] inset-0 w-full"
                       onClick={() => setOpen(false)}
                     />
                     <article className="w-full h-full pointer-events-none top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] fixed z-[2001] overflow-hidden px-[40px] py-0 flex items-center justify-center">
                       <motion.div
+                        layoutId={`guard-container`}
                         className={classNames(
-                          'h-[auto] max-w-[700px] overflow-hidden pointer-events-none relative w-full h-full m-[0_auto]'
+                          'bg-neutral-900 p-[35px] rounded-xl h-[auto] max-w-[700px] overflow-hidden pointer-events-none relative w-full h-full m-[0_auto]'
                         )}
                       >
                         <div className="flex flex-col items-center lg:items-start">
-                          <motion.div layoutId={`custody-title`} className="flex flex-col items-center lg:items-start">
+                          <motion.div layoutId={`guard-title`} className="flex flex-col items-center lg:items-start">
                             <Typography variant="h1" weight={600} className="text-center lg:text-left">
-                              Keep more profits and earn gas refund.
+                              Keep more profits and <br /> earn gas refund.
                             </Typography>
                             <Typography variant="lg" weight={400} className="text-center lg:text-left mt-2">
                               Enable SushiGuard and say no to MEV attacks.
                             </Typography>
+                          </motion.div>
+                          <motion.div animate className="prose !prose-invert prose-neutral prose-dark mt-10">
+                            <h3>What is OpenMEV?</h3>
+                            <p>
+                              OpenMEV aims to provide a credible neutral platform for facilitating both aggregation and
+                              direct communication channels between block validators, block producers and block
+                              synchronizers for the Ethereum and EVM-based networks.
+                            </p>
+                            <p>Example use cases include:</p>
+                            <ul>
+                              <li>
+                                Users that would like to communicate their preferred transaction order within a block.
+                              </li>
+                              <li>Account abstraction via private mempool </li>
+                            </ul>
+                            <p>
+                              OpenMEV is built on top of SecureRPC. SecureRPC provides users with an accessible,
+                              convenient and secure infrastructure for transaction routing and execution.
+                            </p>
+                            <ul>
+                              <li>
+                                Accessible: allow ordinary users to easily discover the trading risk and value on the
+                                network.
+                              </li>
+                              <li>
+                                Convenient: enable backrunning swaps automatically so that more opportunities to capture
+                                profits at a lower cost.
+                              </li>
+                              <li>Secure: make transactions on the blockchain network more secure and private.</li>
+                            </ul>
                           </motion.div>
                         </div>
                       </motion.div>
