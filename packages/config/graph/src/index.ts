923207b95dcdd8f4c1a8e508647cb79383fd6b82
HHK <hhk.contact@protonmail.com>
2022-11-15 15:35:33 +0100

    fix: update ethereum subgraph as sushi-0m return incorrect volume because of MEV

--- packages/config/graph/src/index.ts
+++ packages/config/graph/src/index.ts
@@ -139,7 +139,7 @@ export const EXCHANGE_SUBGRAPH_NAME: Record<number, string> = {
 }
 
 export const SUSHISWAP_SUBGRAPH_NAME = {
-  [ChainId.ETHEREUM]: 'sushi-0m/sushiswap-ethereum',
+  [ChainId.ETHEREUM]: 'sushiswap-subgraphs/sushiswap-ethereum',
   [ChainId.AVALANCHE]: 'sushi-0m/sushiswap-avalanche',
   [ChainId.ARBITRUM]: 'sushi-0m/sushiswap-arbitrum',
   [ChainId.BSC]: 'sushi-0m/sushiswap-bsc',
