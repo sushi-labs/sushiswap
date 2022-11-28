58271347cc7dd57eb53bc65ee71d48beecbc707f
Ola Stenberg <hello@olastenberg.com>
2022-11-28 14:14:36 +0100

    feat(apis/pool-list): add redis middleware

--- apis/pool-list/api/v0/[chainId]/base.ts
+++ apis/pool-list/api/v0/[chainId]/base.ts
@@ -1,63 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import prisma from '../../../lib/prisma'
+import { getBase } from '../../../lib/api'
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   const chainId = request.query.chainId as string
   const excludeTokensArg = request.query.excludeTokens as string
   const excludeTokens = excludeTokensArg ? excludeTokensArg.split(',') : []
+  const PROTOCOLS = ['SushiSwap'] // Hardcoded for now, should be a string array param?
 
-  const PROTOCOL = 'SushiSwap' // Hardcoded for now, should be a string array param?
-
-  const topPools = await prisma.pool.findMany({
-    take: 500,
-    where: {
-      chainId: chainId.toString(),
-      protocol: { in: [PROTOCOL] },
-      AND: {
-        token0: {
-          address: {
-            notIn: excludeTokens,
-          },
-        },
-        token1: {
-          address: {
-            notIn: excludeTokens,
-          },
-        },
-      }
-    },
-    select: {
-      address: true,
-      protocol: true,
-      type: true,
-      version: true,
-      token0: {
-        select: {
-          address: true,
-          symbol: true,
-          name: true,
-          decimals: true,
-        },
-      },
-      token1: {
-        select: {
-          address: true,
-          symbol: true,
-          name: true,
-          decimals: true,
-        },
-      },
-    },
-    // include: {
-    //   token0: true,
-    //   token1: true,
-    // },
-    orderBy: {
-      liquidityUSD: 'desc',
-    },
-  })
-
-  return response.status(200).json({
-    pools: topPools,
-  })
+  return await getBase(response, chainId, excludeTokens, PROTOCOLS)
 }
---
+++ apis/pool-list/lib/api.ts
@@ -0,0 +1,173 @@
+import { Prisma, Token } from '@prisma/client'
+import type { VercelResponse } from '@vercel/node'
+import { createPrismaRedisCache } from 'prisma-redis-middleware'
+import prisma from './prisma'
+import redis from './redis'
+
+const cacheMiddleware = createPrismaRedisCache({
+  models: [{ model: 'Pool', cacheTime: 300 }],
+  storage: { type: 'redis', options: { client: redis, invalidation: { referencesTTL: 300 } } },
+  cacheTime: 60,
+  onHit: (key: string) => {
+    console.log('Hit: ✅', key)
+  },
+  onMiss: (key: string) => {
+    console.log('Miss: ❌', key)
+  },
+})
+
+prisma.$use(cacheMiddleware)
+
+export async function getPools(response: VercelResponse, chainId: string, tokenAddress: string, protocols: string[]) {
+  const tokenWithPools = await prisma.token.findFirst({
+    where: {
+      address: tokenAddress,
+      chainId: chainId.toString(),
+    },
+    include: {
+      pools0: {
+        select: {
+          address: true,
+          protocol: true,
+          type: true,
+          version: true,
+          token0: {
+            select: {
+              address: true,
+              symbol: true,
+              name: true,
+              decimals: true,
+            },
+          },
+          token1: {
+            select: {
+              address: true,
+              symbol: true,
+              name: true,
+              decimals: true,
+            },
+          },
+        },
+        where: {
+          liquidityUSD: { gt: 100 },
+          protocol: { in: protocols },
+        },
+      },
+      pools1: {
+        select: {
+          address: true,
+          protocol: true,
+          type: true,
+          version: true,
+          token0: {
+            select: {
+              address: true,
+              symbol: true,
+              name: true,
+              decimals: true,
+            },
+          },
+          token1: {
+            select: {
+              address: true,
+              symbol: true,
+              name: true,
+              decimals: true,
+            },
+          },
+        },
+        where: {
+          liquidityUSD: { gt: 100 },
+          protocol: { in: protocols },
+        },
+      },
+    },
+  })
+
+  if (!tokenWithPools) {
+    await prisma.$disconnect()
+    return response.status(204).json({ message: 'No token found.' })
+  }
+  if (tokenWithPools.pools0.length === 0 && tokenWithPools.pools1.length === 0) {
+    await prisma.$disconnect()
+    return response.status(204).json({ message: 'Token found, but no pools.' })
+  }
+
+  const pools = Array.from(
+    new Set(
+      [
+        tokenWithPools.pools0.map((pool) => ({
+          address: pool.address,
+          protocol: pool.protocol,
+          type: pool.type,
+          version: pool.version,
+          token: pool.token0.address.toLowerCase() === tokenAddress.toString() ? pool.token1 : pool.token0,
+        })),
+        ,
+        tokenWithPools.pools1.map((pool) => ({
+          address: pool.address,
+          protocol: pool.protocol,
+          type: pool.type,
+          version: pool.version,
+          token: pool.token0.address.toLowerCase() === tokenAddress.toString() ? pool.token1 : pool.token0,
+        })),
+        ,
+      ].flat()
+    )
+  )
+  const result = {
+    token: {
+      address: tokenWithPools.address,
+      symbol: tokenWithPools.symbol,
+      name: tokenWithPools.name,
+      decimals: tokenWithPools.decimals,
+      pools,
+    },
+  }
+  await prisma.$disconnect()
+  return response.status(200).json(result)
+}
+
+export async function getBase(
+  response: VercelResponse,
+  chainId: string,
+  excludeTokens: string[],
+  protocols: string[],
+  size: number = 500
+) {
+  const pools = await prisma.pool.findMany({
+    take: size,
+    where: {
+      chainId: chainId.toString(),
+      protocol: { in: protocols },
+      AND: {
+        token0Id: {
+          notIn: excludeTokens,
+        },
+        token1Id: {
+          notIn: excludeTokens,
+        },
+      },
+    },
+    select: {
+      address: true,
+      protocol: true,
+      type: true,
+      version: true,
+    },
+    // include: {
+    //   token0: true,
+    //   token1: true,
+    // },
+    orderBy: {
+      liquidityUSD: 'desc',
+    },
+  })
+
+  if (!pools) {
+    await prisma.$disconnect()
+    return response.status(204).json({ message: 'No pools found.' })
+  }
+  await prisma.$disconnect()
+  return response.status(200).json(pools)
+}
