6bcd110d98f9421ccfe4c1c6bd747eae702a423a
matthewlilley <hello@matthewlilley.com>
2022-11-29 19:56:24 +0000

    chore: cleanup route processor

--- protocols/routeprocessor/.eslintrc.js
+++ protocols/routeprocessor/.eslintrc.js
@@ -1,11 +1,1 @@
-module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-};
+module.exports = require('@sushiswap/eslint-config/hardhat')
