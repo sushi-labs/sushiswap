2a042260ddc00552a3a232240af95b4368026c6f
matthewlilley <hello@matthewlilley.com>
2022-12-08 16:20:24 +0000

    fix: guard missing param and ensure it is casted

--- apis/tokens/api/v0/[chainId].ts
+++ apis/tokens/api/v0/[chainId].ts
@@ -3,7 +3,8 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { getTokens } from '../../lib/api'
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-  const chainId = request.query.chainId as string
+  const chainId = request.query.chainId
+  if (!chainId) throw Error("chainId is required!")
   const tokens = await getTokens(chainId)
   return response.status(200).json(tokens)
 }
--- apis/tokens/lib/api.ts
+++ apis/tokens/lib/api.ts
@@ -29,7 +29,7 @@ export async function getTokens(chainId = 1) {
     },
     where: {
       AND: {
-        chainId,
+        chainId: Number(chainId),
         status: 'APPROVED',
       },
     },
