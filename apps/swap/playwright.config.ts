e1e72debef4e85330b5634b16e8996dfc3d40ca8
matthewlilley <hello@matthewlilley.com>
2022-12-10 22:49:49 +0000

    chore: optimize workflow

--- apps/swap/playwright.config.ts
+++ apps/swap/playwright.config.ts
@@ -39,7 +39,8 @@ const config: PlaywrightTestConfig = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
+    // trace: 'retain-on-failure',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
@@ -74,6 +75,11 @@ const config: PlaywrightTestConfig = {
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   outputDir: 'test/results/',
+
+  webServer: {
+    command: 'pnpm run start',
+    // port: 3000,
+  },
 
   /* Run your local dev server before starting the tests */
   // webServer: {
