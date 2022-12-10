355da9bff78f1618b20946ef01f0442a9d5f104c
matthewlilley <hello@matthewlilley.com>
2022-12-10 19:21:41 +0000

    test(apps/swap): trace retain on failure

--- apps/swap/playwright.config.ts
+++ apps/swap/playwright.config.ts
@@ -39,8 +39,7 @@ const config: PlaywrightTestConfig = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    // trace: 'retain-on-failure',
-    trace: 'on',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
