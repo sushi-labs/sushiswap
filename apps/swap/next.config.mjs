94218b34bcdbcdcce5a19e45ce41588f4c388045
matthewlilley <hello@matthewlilley.com>
2022-12-10 01:17:46 +0000

    chore: config for sentry

--- apps/swap/next.config.mjs
+++ apps/swap/next.config.mjs
@@ -87,10 +87,23 @@ const nextConfig = {
   },
 }
 
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+}
+
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
 export default withTranspileModules(
   withSentryConfig(nextConfig, {
+    ...sentryWebpackPluginOptions,
     // Additional config options for the Sentry Webpack plugin. Keep in mind that
     // the following options are set automatically, and overriding them is not
     // recommended:
