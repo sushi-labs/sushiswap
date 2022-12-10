3d473062e1b4a4738cc9a2c7b140c2577381345b
LufyCZ <lufy@sushi.com>
2022-12-10 02:08:52 +0000

    chore(apps/blog): remove typescript ignore

--- apps/blog/next.config.mjs
+++ apps/blog/next.config.mjs
@@ -26,13 +26,6 @@ const nextConfig = {
       },
     ]
   },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 }
 
 export default withTranspileModules(nextConfig)
