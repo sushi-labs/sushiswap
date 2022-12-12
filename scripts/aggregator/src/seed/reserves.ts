f28a2a0c61cac9a7cf6da65cd36a0bf5b3386a7c
Ola Stenberg <hello@olastenberg.com>
2022-12-12 16:13:48 +0100

    refactor(scripts/aggregator): add params to reserve script, make it chain specific instead of protocol

--- scripts/aggregator/src/seed/reserves.ts
+++ scripts/aggregator/src/seed/reserves.ts
@@ -1,31 +1,53 @@
-import { Pool, Prisma, PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
 import { ChainId } from '@sushiswap/chain'
 import { readContracts } from '@wagmi/core'
 import { performance } from 'perf_hooks'
 
 import IUniswapV2PairArtifact from '@uniswap/v2-core/build/IUniswapV2Pair.json' assert { type: 'json' }
+import { PoolType, ProtocolVersion } from '../config.js'
 import '../lib/wagmi.js'
-import { PoolType, ProtocolName, ProtocolVersion } from '../config.js'
 
 const prisma = new PrismaClient()
-
-async function main() {
-  // TODO: pass in params, e.g. chainId, protocol, version
-  // Hardcoded for now.
-  const chainId = ChainId.BSC
-  const protocol = ProtocolName.PANCAKESWAP
-  const version = ProtocolVersion.V2
-  const type = PoolType.CONSTANT_PRODUCT_POOL
-  const pools = await getPools(chainId, protocol, version, type)
-  const poolsWithReserve = await getReserves(chainId, pools)
-  await updatePoolsWithReserve(chainId, poolsWithReserve)
-
+if (process.env.CHAIN_ID === undefined) {
+  throw new Error('CHAIN_ID env var not set')
 }
 
-async function getPools(chainId: ChainId, protocol: string, version: string, type: string) {
+if (process.env.VERSION === undefined || process.env.TYPE === undefined) {
+  throw new Error('VERSION, and TYPE env vars must be set, e.g. VERSION=V2 TYPE=CONSTANT_PRODUCT_POOL.')
+}
+
+const CHAIN_ID = Number(process.env.CHAIN_ID) as ChainId
+const TYPE = process.env.TYPE
+
+if (TYPE !== PoolType.CONSTANT_PRODUCT_POOL) {
+  throw new Error(
+    `Pool type not supported, ${TYPE}. Current implementation only supports ${PoolType.CONSTANT_PRODUCT_POOL}`
+  )
+}
+
+if (!Object.values(ProtocolVersion).includes(process.env.VERSION as ProtocolVersion)) {
+  throw new Error(
+    `Protocol version (${process.env.VERSION}) not supported, supported versions: ${[
+      ProtocolVersion.V2,
+      ProtocolVersion.LEGACY,
+      ProtocolVersion.TRIDENT,
+    ].join(',')}`
+  )
+}
+
+const VERSIONS = ['V2', 'LEGACY', 'TRIDENT']
+
+async function main() {
+  console.log(`CHAIN_ID: ${CHAIN_ID}, VERSIONS: ${VERSIONS}, TYPE: ${TYPE}`)
+  const pools = await getPools(CHAIN_ID, VERSIONS, TYPE)
+  const poolsWithReserve = await getReserves(CHAIN_ID, pools)
+  await updatePoolsWithReserve(CHAIN_ID, poolsWithReserve)
+}
+
+async function getPools(chainId: ChainId, versions: string[], type: string) {
   const startTime = performance.now()
 
-  const batchSize = 25000
+  const batchSize = 2500
   let cursor = null
   const results = []
   let totalCount = 0
@@ -33,11 +55,11 @@ async function getPools(chainId: ChainId, protocol: string, version: string, typ
     const requestStartTime = performance.now()
     let result = []
     if (!cursor) {
-      result = await getPoolsAddresses(chainId, protocol, version, type, batchSize)
+      result = await getPoolsAddresses(chainId, versions, type, batchSize)
     } else {
-      result = await getPoolsAddresses(chainId, protocol, version, type, batchSize, 1, { id: cursor })
+      result = await getPoolsAddresses(chainId, versions, type, batchSize, 1, { id: cursor })
     }
-    cursor = result.length > 0 ? result[result.length - 1].id : null
+    cursor = result.length > batchSize ? result[result.length - 1].id : null
     totalCount += result.length
     results.push(result)
     const requestEndTime = performance.now()
@@ -58,13 +80,39 @@ async function getPools(chainId: ChainId, protocol: string, version: string, typ
 
 async function getPoolsAddresses(
   chainId: ChainId,
-  protocol: string,
-  version: string,
+  versions: string[],
   type: string,
   take?: number,
   skip?: number,
   cursor?: Prisma.PoolWhereUniqueInput
 ): Promise<{ id: string; address: string }[]> {
+  // const tokensWithPools = await prisma.token.findMany({
+  //   take,
+  //   skip,
+  //   cursor,
+  //   include: {
+  //     pools0: {
+
+  //     },
+  //     pools1: {
+
+  //     },
+  //   },
+  //   where: {
+  //     chainId,
+  //     status: 'APPROVED',
+  //   },
+  // })
+  // const pools = [tokensWithPools.flatMap((token) => token.pools0), tokensWithPools.flatMap((token) => token.pools1)].flat()
+  // const unique = new Set(pools.map((pool) => pool))
+
+  // return Array.from(unique).map((pool) => {
+  //   return {
+  //     id: pool.id,
+  //     address: pool.address,
+  //   }
+  // })
+
   return prisma.pool.findMany({
     take,
     skip,
@@ -75,15 +123,16 @@ async function getPoolsAddresses(
     },
     where: {
       chainId,
-      protocol,
-      version,
+      version: {
+        in: versions,
+      },
       type,
-      // token0: {
-      //   status: "APPROVED"
-      // },
-      // token1: {
-      //   status: "APPROVED"
-      // }
+      token0: {
+        status: 'APPROVED',
+      },
+      token1: {
+        status: 'APPROVED',
+      },
     },
   })
 }
@@ -109,7 +158,6 @@ async function getReserves(chainId: ChainId, poolAddresses: string[]) {
       contracts: batch,
     })
 
-
     let failures = 0
     const mappedPools = poolAddresses.slice(i, max).reduce<PoolWithReserve[]>((prev, address, i) => {
       if (!reserves[i] || !reserves[i].reserve0 || !reserves[i].reserve1) {
@@ -126,21 +174,30 @@ async function getReserves(chainId: ChainId, poolAddresses: string[]) {
       ]
     }, [])
 
-
     if (failures > 0) {
       console.log(`Failed to fetch reserves for ${failures} pools.`)
     }
     const batchEndTime = performance.now()
     totalFailedCount += failures
     totalSuccessCount += mappedPools.length
-    console.log(`Fetched reserves, ${totalSuccessCount}/${poolAddresses.length} (${((batchEndTime - batchStartTime) / 1000).toFixed(1)}s). `)
+    console.log(
+      `Fetched reserves, ${totalSuccessCount}/${poolAddresses.length} (${(
+        (batchEndTime - batchStartTime) /
+        1000
+      ).toFixed(1)}s). `
+    )
 
     poolsWithReserve.push(...mappedPools)
   }
 
   const endTime = performance.now()
 
-  console.log(`Successfully fetched reserves for ${totalSuccessCount} pools, fails: ${totalFailedCount} (${((endTime - startTime) / 1000).toFixed(1)}s). `)
+  console.log(
+    `Successfully fetched reserves for ${totalSuccessCount} pools, fails: ${totalFailedCount} (${(
+      (endTime - startTime) /
+      1000
+    ).toFixed(1)}s). `
+  )
   return poolsWithReserve
 }
 
@@ -165,7 +222,11 @@ async function updatePoolsWithReserve(chainId: ChainId, pools: PoolWithReserve[]
     const startTime = performance.now()
     const responses = await Promise.all(requests)
     const endTime = performance.now()
-    console.log(`Updated ${responses.length} pools, ${updatedCount}/${pools.length} (${((endTime - startTime) / 1000).toFixed(1)}s).`)
+    console.log(
+      `Updated ${responses.length} pools, ${updatedCount}/${pools.length} (${((endTime - startTime) / 1000).toFixed(
+        1
+      )}s).`
+    )
     updatedCount += responses.length
   }
   const endTime = performance.now()
