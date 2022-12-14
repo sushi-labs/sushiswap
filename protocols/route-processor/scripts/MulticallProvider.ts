aaf7147bc515cf371d8e0e955c44d0f171f8f8b4
Ilya Lyalin <bretnwood@gmail.com>
2022-12-14 17:50:04 +0300

    fix: Multicall merge issue

--- protocols/route-processor/scripts/MulticallProvider.ts
+++ protocols/route-processor/scripts/MulticallProvider.ts
@@ -1,10 +1,18 @@
 import { ContractCallContext, ContractCallResults, Multicall } from 'ethereum-multicall'
-import { ethers } from 'ethers'
+import { BigNumber, ethers } from 'ethers'
+
+interface SeriaCall {
+  contract: string
+  abi: any[]
+  method: string
+  methodParameters: any
+}
 
 export class MultiCallProvider {
   multicall: Multicall
   prepairingCallcontext?: ContractCallContext[]
-  prepairingCall?: Promise<ContractCallResults>
+  prepairingCall?: Promise<any[][]>
+  seriaLength: Map<number, number> = new Map()
   nextSeriaId = 0
 
   constructor(chainDataProvider: ethers.providers.BaseProvider) {
@@ -12,64 +20,90 @@ export class MultiCallProvider {
   }
 
   // aggregate several multicalls in one
-  async call(inp: ContractCallContext[]): Promise<ContractCallResults> {
+  async call(inp: ContractCallContext[]): Promise<any[][]> {
     //console.log('Multicall order', inp.length)
     if (!this.prepairingCallcontext) {
       this.prepairingCallcontext = inp
-      this.prepairingCall = new Promise<ContractCallResults>((res, _rej) => {
+      this.prepairingCall = new Promise<any[][]>((res, _rej) => {
         setTimeout(async () => {
           //console.log('Multicall call', this.prepairingCallcontext?.length)
           const input = this.prepairingCallcontext as ContractCallContext[]
           this.prepairingCallcontext = undefined
           this.prepairingCall = undefined
-          res(await this.multicall.call(input))
+          this.nextSeriaId = 0
+          const { results } = await this.multicall.call(input)
+          const serias: any[][] = []
+          for (const r in results) {
+            const [elementSeria, index] = r.split('_')
+            const seria = parseInt(elementSeria)
+            const retContext = results[r].callsReturnContext[0]
+            if (serias[seria] === undefined) serias[seria] = new Array(this.seriaLength.get(seria) || 0)
+            serias[parseInt(elementSeria)][parseInt(index)] = retContext.success ? retContext.returnValues : undefined
+          }
+          res(serias)
         }, 0)
       })
     } else {
       this.prepairingCallcontext = this.prepairingCallcontext.concat(inp)
     }
-    return this.prepairingCall as Promise<ContractCallResults>
+    return this.prepairingCall as Promise<any[][]>
+  }
+
+  async seriaCall(calls: SeriaCall[]): Promise<any[]> {
+    const seria = this.nextSeriaId++
+    const getReservesCalls: ContractCallContext[] = calls.map((call, i) => ({
+      reference: `${seria}_${i}`,
+      contractAddress: call.contract,
+      abi: call.abi,
+      calls: [{ reference: '', methodName: call.method, methodParameters: call.methodParameters }],
+    }))
+    this.seriaLength.set(seria, getReservesCalls.length)
+    const serias = await this.call(getReservesCalls) // can be mixed with other calls
+    return serias[seria] || []
   }
 
   async multiContractCall(contracts: string[], abi: any[], method: string, methodParameters: any): Promise<any[]> {
     if (contracts.length == 0) return []
 
-    const seria = '' + this.nextSeriaId++
-    const getReservesCalls: ContractCallContext[] = contracts.map((contract, i) => ({
-      reference: `${seria}_i`,
-      contractAddress: contract,
-      abi,
-      calls: [{ reference: '', methodName: method, methodParameters }],
-    }))
-    const { results }: ContractCallResults = await this.call(getReservesCalls) // can be mixed with other calls
-    const res = new Array(contracts.length)
-    for (const r in results) {
-      const [elementSeria, index] = r.split('_')
-      if (elementSeria !== seria || index == undefined) continue
-
-      const retContext = results[r].callsReturnContext[0]
-      res[parseInt(index)] = retContext.success ? retContext.returnValues : undefined
-    }
-    return res
+    return await this.seriaCall(
+      contracts.map((contract) => ({
+        contract,
+        abi,
+        method,
+        methodParameters,
+      }))
+    )
   }
 
   async multiDataCall(contract: string, abi: any[], method: string, methodParameters: any[]): Promise<any[]> {
-    const seria = '' + this.nextSeriaId++
-    const getReservesCalls: ContractCallContext[] = methodParameters.map((data, i) => ({
-      reference: `${seria}_i`,
-      contractAddress: contract,
-      abi,
-      calls: [{ reference: '', methodName: method, methodParameters: methodParameters[i] }],
-    }))
-    const { results }: ContractCallResults = await this.call(getReservesCalls) // can be mixed with other calls
-    const res = new Array(methodParameters.length)
-    for (const r in results) {
-      const [elementSeria, index] = r.split('_')
-      if (elementSeria !== seria || index == undefined) continue
-
-      const retContext = results[r].callsReturnContext[0]
-      res[parseInt(index)] = retContext.success ? retContext.returnValues : undefined
-    }
-    return res
+    return await this.seriaCall(
+      methodParameters.map((data) => ({
+        contract,
+        abi,
+        method,
+        methodParameters: data,
+      }))
+    )
   }
+}
+
+export function convertToNumbers(arr: any[]): (number | undefined)[] {
+  return arr.map((a) => {
+    if (a === undefined) return undefined
+    return parseInt(a[0].hex, 16)
+  })
+}
+
+export function convertToBigNumber(arr: any[]): (BigNumber | undefined)[] {
+  return arr.map((a) => {
+    if (a === undefined) return undefined
+    return BigNumber.from(a[0].hex)
+  })
+}
+
+export function convertToBigNumberPair(arr: any[]): ([BigNumber, BigNumber] | undefined)[] {
+  return arr.map((a) => {
+    if (a === undefined) return undefined
+    return [BigNumber.from(a[0].hex), BigNumber.from(a[1].hex)]
+  })
 }
