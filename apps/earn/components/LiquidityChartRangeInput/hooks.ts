3324cf7023129ea2c43663e8369f468450c1dfef
chillichelli <ramin@erfani.dev>
2022-12-22 15:59:04 +0000

    fix(apps/earn): build errors

--- apps/earn/components/LiquidityChartRangeInput/hooks.ts
+++ apps/earn/components/LiquidityChartRangeInput/hooks.ts
@@ -1,9 +1,6 @@
 import { Fee } from '@sushiswap/amm'
 import { Type } from '@sushiswap/currency'
-import { TickProcessed, usePoolActiveLiquidity } from 'hooks/usePoolTickData'
-import { useCallback, useMemo } from 'react'
-
-import { ChartEntry } from './types'
+import { useMemo } from 'react'
 
 export function useDensityChartData({
   currencyA,
@@ -14,36 +11,44 @@ export function useDensityChartData({
   currencyB: Type | undefined
   feeAmount: Fee | undefined
 }) {
-  const { isLoading, error, data } = usePoolActiveLiquidity(currencyA, currencyB, feeAmount)
+  // const { isLoading, error, data } = usePoolActiveLiquidity(currencyA, currencyB, feeAmount)
 
-  const formatData = useCallback(() => {
-    if (!data?.length) {
-      return undefined
-    }
+  // const formatData = useCallback(() => {
+  //   if (!data?.length) {
+  //     return undefined
+  //   }
+  //
+  //   const newData: ChartEntry[] = []
+  //
+  //   for (let i = 0; i < data.length; i++) {
+  //     const t: TickProcessed = data[i]
+  //
+  //     const chartEntry = {
+  //       activeLiquidity: parseFloat(t.liquidityActive.toString()),
+  //       price0: parseFloat(t.price0),
+  //     }
+  //
+  //     if (chartEntry.activeLiquidity > 0) {
+  //       newData.push(chartEntry)
+  //     }
+  //   }
+  //
+  //   return newData
+  // }, [data])
 
-    const newData: ChartEntry[] = []
-
-    for (let i = 0; i < data.length; i++) {
-      const t: TickProcessed = data[i]
-
-      const chartEntry = {
-        activeLiquidity: parseFloat(t.liquidityActive.toString()),
-        price0: parseFloat(t.price0),
-      }
-
-      if (chartEntry.activeLiquidity > 0) {
-        newData.push(chartEntry)
-      }
-    }
-
-    return newData
-  }, [data])
+  // return useMemo(() => {
+  //   return {
+  //     isLoading,
+  //     error,
+  //     formattedData: !isLoading ? formatData() : undefined,
+  //   }
+  // }, [isLoading, error, formatData])
 
   return useMemo(() => {
     return {
-      isLoading,
-      error,
-      formattedData: !isLoading ? formatData() : undefined,
+      isLoading: false,
+      error: undefined,
+      formattedData: [],
     }
-  }, [isLoading, error, formatData])
+  }, [])
 }
