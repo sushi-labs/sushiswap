9d7572e2252c9c7293ef5b3910336f6bb122ccdb
matthewlilley <hello@matthewlilley.com>
2022-12-29 19:58:47 +0000

    feat: extract base prettier and seperate next, react & solidity configs

--- packages/config/prettier/src/index.ts
+++ packages/config/prettier/src/index.ts
@@ -1,11 +1,3 @@
-import { Options } from 'prettier'
+import base from './base'
 
-const options: Options = {
-  endOfLine: 'lf',
-  printWidth: 120,
-  tabWidth: 2,
-  singleQuote: true,
-  semi: false,
-}
-
-export = options
+export = base
--- packages/config/prettier/src/next.ts
+++ packages/config/prettier/src/next.ts
@@ -1,11 +1,10 @@
 import { Options } from 'prettier'
 
+import base from './base'
+
 export const options: Options = {
-  endOfLine: 'lf',
-  printWidth: 120,
-  tabWidth: 2,
-  singleQuote: true,
-  semi: false,
+  ...base,
+  plugins: [require('prettier-plugin-tailwindcss')],
 }
 
 export default options
