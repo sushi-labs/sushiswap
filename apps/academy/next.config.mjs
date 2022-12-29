380358df15805444c9819c5a10a9d6a175fb81b5
LufyCZ <lufy@sushi.com>
2022-12-29 22:24:38 +0000

    chore: remove ts skips in academy and blog

--- apps/academy/next.config.mjs
+++ apps/academy/next.config.mjs
@@ -5,12 +5,6 @@ const withTranspileModules = transpileModules(['@sushiswap/ui'])
 // @ts-check
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    ignoreBuildErrors: true,
-  },
   basePath: '/academy',
   reactStrictMode: true,
   swcMinify: false,
