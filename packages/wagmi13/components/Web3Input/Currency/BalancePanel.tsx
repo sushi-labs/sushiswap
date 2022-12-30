10fa57bd8158b9be76043a6b1d137da0141c339e
chillichelli <ramin@erfani.dev>
2022-12-30 22:04:01 +0000

    feat(apps/swap13): set magicnumber to const

--- packages/wagmi13/components/Web3Input/Currency/BalancePanel.tsx
+++ packages/wagmi13/components/Web3Input/Currency/BalancePanel.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import { CreditCardIcon } from '@heroicons/react/20/solid'
-import { Amount, Native, tryParseAmount, Type } from '@sushiswap/currency'
+import { Amount, Native, Type } from '@sushiswap/currency'
 import { FundSource } from '@sushiswap/hooks'
 import { Skeleton } from '@sushiswap/ui13/components/skeleton'
 import React, { FC, useCallback } from 'react'
 
 import { CurrencyInputProps } from './index'
+import { JSBI } from '@sushiswap/math'
 
 type BalancePanel = Pick<
   CurrencyInputProps,
@@ -16,6 +17,8 @@ type BalancePanel = Pick<
   account: string | undefined
   balance: Record<FundSource, Amount<Type>> | undefined
 }
+
+const MIN_NATIVE_CURRENCY_FOR_GAS: JSBI = JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(16)) // .01 ETH
 
 export const BalancePanel: FC<BalancePanel> = ({
   id,
@@ -29,8 +32,11 @@ export const BalancePanel: FC<BalancePanel> = ({
 
   const onClick = useCallback(() => {
     if (onChange && balance?.[fundSource]?.greaterThan(0)) {
-      if (balance?.[fundSource].currency.isNative && balance?.[fundSource].greaterThan('1000000000000000')) {
-        const hundred = Amount.fromRawAmount(Native.onChain(balance[fundSource].currency.chainId), '1000000000000000')
+      if (balance?.[fundSource].currency.isNative && balance?.[fundSource].greaterThan(MIN_NATIVE_CURRENCY_FOR_GAS)) {
+        const hundred = Amount.fromRawAmount(
+          Native.onChain(balance[fundSource].currency.chainId),
+          MIN_NATIVE_CURRENCY_FOR_GAS
+        )
         onChange(balance[fundSource].subtract(hundred).toFixed())
       } else {
         onChange(balance?.[fundSource]?.greaterThan(0) ? balance[fundSource].toFixed() : '')
