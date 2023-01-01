d3bb7d0bb41927672f1c771284555481d280edaa
matthewlilley <hello@matthewlilley.com>
2023-01-01 05:01:09 +0000

    chore(apis/swap): cleanuip shortname handling and reuse

--- apis/swap/api/index.ts
+++ apis/swap/api/index.ts
@@ -1,20 +1,11 @@
 import { ChainId } from '@sushiswap/chain'
 import {
-  DAI,
-  FRAX,
-  LUSD,
-  MAI,
-  MIM,
-  Native,
+  isShortName,
+  isShortNameToCurrencySupported,
   nativeCurrencyIds,
-  SUSHI,
+  ShortName,
+  shortNameToCurrency,
   Token,
-  UNI,
-  USDC,
-  USDT,
-  WBTC,
-  WETH9,
-  WNATIVE,
 } from '@sushiswap/currency'
 import { DataFetcher, Router } from '@sushiswap/router'
 import type { VercelRequest, VercelResponse } from '@vercel/node'
@@ -25,45 +16,6 @@ import { z } from 'zod'
 
 const fetch = (url: RequestInfo, init?: RequestInit) =>
   import('node-fetch').then(({ default: fetch }) => fetch(url, init))
-
-const CHAIN_ID_SHORT_CURRENCY_NAME_TO_CURRENCY = {
-  [ChainId.ETHEREUM]: {
-    NATIVE: Native.onChain(ChainId.ETHEREUM),
-    WNATIVE: WETH9[ChainId.ETHEREUM],
-    ETH: Native.onChain(ChainId.ETHEREUM),
-    WETH: WETH9[ChainId.ETHEREUM],
-    WBTC: WBTC[ChainId.ETHEREUM],
-    USDC: USDC[ChainId.ETHEREUM],
-    USDT: USDT[ChainId.ETHEREUM],
-    DAI: DAI[ChainId.ETHEREUM],
-    FRAX: FRAX[ChainId.ETHEREUM],
-    MIM: MIM[ChainId.ETHEREUM],
-    SUSHI: SUSHI[ChainId.ETHEREUM],
-    MAI: MAI[ChainId.ETHEREUM],
-    UNI: UNI[ChainId.ETHEREUM],
-    LUSD: LUSD[ChainId.ETHEREUM],
-  },
-  [ChainId.POLYGON]: {
-    NATIVE: Native.onChain(ChainId.POLYGON),
-    WNATIVE: WNATIVE[ChainId.POLYGON],
-    MATIC: Native.onChain(ChainId.POLYGON),
-    WMATIC: WNATIVE[ChainId.POLYGON],
-    WETH: WETH9[ChainId.POLYGON],
-    WBTC: WBTC[ChainId.POLYGON],
-    USDC: USDC[ChainId.POLYGON],
-    USDT: USDT[ChainId.POLYGON],
-    DAI: DAI[ChainId.POLYGON],
-    FRAX: FRAX[ChainId.POLYGON],
-    MIM: MIM[ChainId.POLYGON],
-    SUSHI: SUSHI[ChainId.POLYGON],
-    MAI: MAI[ChainId.POLYGON],
-    UNI: UNI[ChainId.POLYGON],
-  },
-} as const
-
-type ShortCurrencyNameChainId = keyof typeof CHAIN_ID_SHORT_CURRENCY_NAME_TO_CURRENCY
-
-type ShortCurrencyName = keyof typeof CHAIN_ID_SHORT_CURRENCY_NAME_TO_CURRENCY[ShortCurrencyNameChainId]
 
 const schema = z.object({
   chainId: z.coerce
@@ -130,17 +82,14 @@ const handler = async (request: VercelRequest, response: VercelResponse) => {
 
   // console.log({ chainId, fromTokenId, toTokenId, amount, gasPrice, to })
 
-  const isShortNameSupported = chainId in CHAIN_ID_SHORT_CURRENCY_NAME_TO_CURRENCY
-
-  const SHORT_CURRENCY_NAME_TO_CURRENCY = CHAIN_ID_SHORT_CURRENCY_NAME_TO_CURRENCY[chainId as ShortCurrencyNameChainId]
-
-  const fromTokenIdIsShortName = fromTokenId in SHORT_CURRENCY_NAME_TO_CURRENCY
-  const toTokenIdIsShortName = toTokenId in SHORT_CURRENCY_NAME_TO_CURRENCY
+  const isShortNameSupported = isShortNameToCurrencySupported(chainId)
+  const fromTokenIdIsShortName = isShortName(chainId, fromTokenId)
+  const toTokenIdIsShortName = isShortName(chainId, toTokenId)
 
   // Limited to predefined short names and tokens from our db for now
   const fromToken =
     isShortNameSupported && fromTokenIdIsShortName
-      ? SHORT_CURRENCY_NAME_TO_CURRENCY[fromTokenId as ShortCurrencyName]
+      ? shortNameToCurrency(chainId, fromTokenId as ShortName)
       : new Token({
           chainId,
           ...tokenSchema.parse(
@@ -151,7 +100,7 @@ const handler = async (request: VercelRequest, response: VercelResponse) => {
   // Limited to predefined short names and tokens from our db for now
   const toToken =
     isShortNameSupported && toTokenIdIsShortName
-      ? SHORT_CURRENCY_NAME_TO_CURRENCY[toTokenId as ShortCurrencyName]
+      ? shortNameToCurrency(chainId, toTokenId as ShortName)
       : new Token({
           chainId,
           ...tokenSchema.parse(
