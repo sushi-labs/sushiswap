8b55394004044d083490e05f7f0e05660ee6aa17
chillichelli <ramin@erfani.dev>
2023-01-02 02:25:09 +0000

    feat(apps/swap13): add price to widget title

--- apps/swap13/ui/widget/WidgetTitle.tsx
+++ apps/swap13/ui/widget/WidgetTitle.tsx
@@ -8,15 +8,46 @@ import { NetworkIcon } from '@sushiswap/ui13/components/icons'
 import { NetworkSelector, NetworkSelectorOnSelectCallback } from '@sushiswap/ui13/components/networkselector'
 import { Tooltip } from '@sushiswap/ui13/components/Tooltip'
 import { AppType } from '@sushiswap/ui13/types'
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 
 import { SUPPORTED_CHAIN_IDS } from '../../config'
 import { useSwapActions, useSwapState } from '../TradeProvider'
+import { usePrices } from '@sushiswap/react-query'
+import { Amount, Price, Token, tryParseAmount } from '@sushiswap/currency'
+import { formatNumber, formatUSD } from '@sushiswap/format'
 
 export const WidgetTitle = () => {
   const [invert, setInvert] = useState(false)
   const { appType, network0, network1, token1, token0 } = useSwapState()
   const { setNetwork1, setNetwork0 } = useSwapActions()
+  const { data: prices0 } = usePrices({ chainId: network0 })
+  const { data: prices1 } = usePrices({ chainId: network1 })
+
+  const [inputUSD, outputUSD, price] = useMemo(() => {
+    if (!prices0 || !prices1) {
+      return ['0.00', '0.00', '0.00']
+    }
+
+    const token0Price = tryParseAmount('1', token0)?.multiply(prices0[token0.wrapped.address])
+    const token1Price = tryParseAmount('1', token1)?.multiply(prices1[token1.wrapped.address])
+    const dummy0 = new Token({ address: token0.wrapped.address, chainId: 1, decimals: token0.decimals })
+    const dummy1 = new Token({ address: token1.wrapped.address, chainId: 1, decimals: token1.decimals })
+
+    if (token0Price && token1Price) {
+      const price = new Price({
+        baseAmount: Amount.fromRawAmount(dummy0, token0Price.quotient.toString()),
+        quoteAmount: Amount.fromRawAmount(dummy1, token1Price.quotient.toString()),
+      })
+
+      return [
+        token0Price.toFixed(2),
+        token1Price.toFixed(2),
+        invert ? price.invert().toSignificant(4) : price.toSignificant(4),
+      ]
+    }
+
+    return ['0.00', '0.00', '0.00']
+  }, [invert, prices0, prices1, token0, token1])
 
   const handleSelect0 = useCallback<NetworkSelectorOnSelectCallback>(
     (el, close) => {
@@ -76,10 +107,15 @@ export const WidgetTitle = () => {
       )}
       <button
         onClick={() => setInvert((invert) => !invert)}
-        className="text-sm flex items-center gap-1 font-semibold text-blue-600 hover:text-blue-800 cursor-pointer"
+        className="text-sm flex items-center gap-1 font-bold text-blue-600 hover:text-blue-800 cursor-pointer"
       >
-        <ArrowTrendingUpIcon width={16} height={16} />1 {invert ? token1.symbol : token0.symbol} = 0.05{' '}
-        {invert ? token0.symbol : token1.symbol}
+        <ArrowTrendingUpIcon width={16} height={16} />
+        <span className="flex items-baseline gap-1">
+          1 {invert ? token0.symbol : token1.symbol}{' '}
+          <span className="font-normal text-xs">(${invert ? inputUSD : outputUSD})</span> = {price}{' '}
+          {invert ? token1.symbol : token0.symbol}{' '}
+          <span className="font-normal text-xs">(${invert ? outputUSD : inputUSD})</span>
+        </span>
       </button>
     </div>
   )
