7de32f7b3b80d2d3bf78031d787d9f5ba8e61b4c
Chilli Chelli <ramin@sushi.com>
2023-01-03 16:45:14 +0000

    fix(packages/swap13): add approval texthelp

--- apps/swap13/ui/widget/SwapButton.tsx
+++ apps/swap13/ui/widget/SwapButton.tsx
@@ -2,14 +2,17 @@
 
 import { Button } from '@sushiswap/ui13/components/button'
 import Container from '@sushiswap/ui13/components/Container'
-import React, { FC, useCallback, useState } from 'react'
+import React, { FC, ReactNode, useCallback, useState } from 'react'
 
 import { ConfirmationDialog } from '../ConfirmationDialog'
 import { useSwapActions, useSwapState } from '../TradeProvider'
+import { Loader } from '@sushiswap/ui13/components/Loader'
+import { useTrade } from '../../lib/useTrade'
 
 export const SwapButton: FC = () => {
   const [open, setOpen] = useState(false)
-  const { token0, token1, review } = useSwapState()
+  const { value, token0, token1, review } = useSwapState()
+  const { data: trade } = useTrade()
   const { setReview } = useSwapActions()
 
   const onClick = useCallback(() => {
@@ -20,18 +23,29 @@ export const SwapButton: FC = () => {
     }
   }, [review, setReview])
 
+  let disabled = false
+  let title: ReactNode = 'Review Swap'
+
+  if (review) title = `Swap ${token0.symbol} for ${token1.symbol}`
+  if (!review) title = 'Review Swap'
+  if (!value) {
+    title = 'Enter amount'
+    disabled = true
+  }
+  if (value && !trade) {
+    title = <Loader size={16} className="text-white" />
+    disabled = true
+  }
+
   return (
     <>
       <div className="fixed bottom-6 sm:p-4 !pb-0 left-4 right-4 w-[unset] sm:!mr-[var(--scroll-lock-safe-area)]">
-        <Container maxWidth={520} className="mx-auto">
-          <Button fullWidth size="xl" onClick={onClick} className="z-[1082]">
-            {review ? (
-              <>
-                Swap {token0.symbol} for {token1.symbol}
-              </>
-            ) : (
-              'Review Swap'
-            )}
+        <Container maxWidth={520} className="mx-auto flex flex-col gap-2">
+          <p className="text-center text-xs text-blue cursor-pointer">
+            What is an approval? <span className="text-gray-600">(show button when user needs to do approval)</span>
+          </p>
+          <Button fullWidth size="xl" onClick={onClick} disabled={disabled} className="z-[1082]">
+            {title}
           </Button>
         </Container>
       </div>
