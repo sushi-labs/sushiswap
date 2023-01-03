c26dbf8bc695b2486d55048558879a2cc8e4973c
matthewlilley <hello@matthewlilley.com>
2023-01-03 05:11:21 +0000

    test: tines to route processor test boilerplate

---
+++ packages/router/src/TinesToRouteProcessor.test.ts
@@ -0,0 +1,14 @@
+import { TinesToRouteProcessor } from './TinesToRouteProcessor'
+
+describe('TinesToRouteProcessor', () => {
+  describe('constructor', () => {
+    let tinesToRouteProcessor: TinesToRouteProcessor
+    it('instanciates', () => {
+      // tinesToRouteProcessor = new TinesToRouteProcessor()
+      expect(tinesToRouteProcessor).toBeInstanceOf(TinesToRouteProcessor)
+    })
+  })
+  describe('#getRouteProcessorCode', () => {
+    //
+  })
+})
