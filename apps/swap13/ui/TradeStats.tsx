5e711ab7bd60ee0cc9e8bb6806cf2ef53c61519c
matthewlilley <hello@matthewlilley.com>
2023-01-08 04:41:31 +0000

    fix: expected skeleton and add route

--- apps/swap13/ui/TradeStats.tsx
+++ apps/swap13/ui/TradeStats.tsx
@@ -9,12 +9,12 @@ import { useSwapState } from './TradeProvider'
 import { useTrade } from '../lib/useTrade'
 import { Skeleton } from '@sushiswap/ui13/components/skeleton'
 import numeral from 'numeral'
+import { Button } from '@sushiswap/ui13/components/button'
 
 export const TradeStats: FC = () => {
-  const { value, token1, recipient } = useSwapState()
+  const { value, token0, token1, recipient } = useSwapState()
   const { data: slippageTolerance } = useSlippageTolerance()
   const { isLoading, data: trade } = useTrade()
-
   return (
     <Transition
       show={!!value}
@@ -26,7 +26,7 @@ export const TradeStats: FC = () => {
       leaveTo="transform translate-y-[16px] opacity-0"
     >
       <div className="w-full px-3 flex flex-col gap-1">
-        <div className="flex justify-between items-center gap-2">
+        {/* <div className="flex justify-between items-center gap-2">
           <span className="text-sm text-gray-700 dark:text-slate-400">Price Impact</span>
           <span className="text-sm font-semibold text-green text-right">
             {isLoading ? (
@@ -35,22 +35,36 @@ export const TradeStats: FC = () => {
               `${numeral(trade?.priceImpact ?? 0).format('0.00%')}`
             )}
           </span>
-        </div>
-        <div className="flex justify-between items-center">
-          <span className="text-sm text-gray-700 dark:text-slate-400">Network Fee</span>
-          <span className="text-sm font-semibold text-gray-700 text-right dark:text-slate-400">
-            {isLoading ? <Skeleton.Box className="h-4 py-0.5 w-[60px] rounded-md" /> : `~$${trade?.gasSpent ?? '0.00'}`}
-          </span>
-        </div>
+        </div> */}
         <div className="flex justify-between items-center gap-2">
-          <span className="text-sm text-gray-700 dark:text-slate-400">
-            Minimum received after slippage ({slippageTolerance === 'AUTO' ? '0.5' : slippageTolerance}%)
-          </span>
+          <span className="text-sm text-gray-700 dark:text-slate-400">Minimum received</span>
           <span className="text-sm font-semibold text-gray-700 text-right dark:text-slate-400">
             {isLoading ? (
               <Skeleton.Box className="h-4 py-0.5 w-[60px] rounded-md" />
             ) : (
               `${trade?.minAmountOut?.toSignificant(6) ?? '0.00'} ${token1.symbol}`
+            )}
+          </span>
+        </div>
+        <div className="flex justify-between items-center gap-2">
+          <span className="text-sm text-gray-700 dark:text-slate-400">Slippage tolerance</span>
+          <span className="text-sm font-semibold text-gray-700 text-right dark:text-slate-400">
+            {slippageTolerance === 'AUTO' ? '0.5' : slippageTolerance}%
+          </span>
+        </div>
+        <div className="flex justify-between items-center">
+          <span className="text-sm text-gray-700 dark:text-slate-400">Transaction cost</span>
+          <span className="text-sm font-semibold text-gray-700 text-right dark:text-slate-400">
+            {isLoading ? <Skeleton.Box className="h-4 py-0.5 w-[60px] rounded-md" /> : `~$${trade?.gasSpent ?? '0.00'}`}
+          </span>
+        </div>
+        <div className="flex justify-between items-center">
+          <span className="text-sm text-gray-700 dark:text-slate-400">Route</span>
+          <span className="text-sm font-semibold text-gray-700 text-right dark:text-slate-400">
+            {isLoading ? (
+              <Skeleton.Box className="h-4 py-0.5 w-[60px] rounded-md" />
+            ) : (
+              <button type="button">{`${token0?.symbol} > ${token1?.symbol}`}</button>
             )}
           </span>
         </div>
@@ -60,7 +74,7 @@ export const TradeStats: FC = () => {
           <div className="flex flex-col justify-end">
             <span className="text-xl font-semibold text-gray-900 dark:text-slate-100">
               {isLoading ? (
-                <Skeleton.Box className="h-[20px] my-[4px] w-[60px] rounded-md" />
+                <Skeleton.Box className="h-[20px] my-[4px] w-[150px] rounded-md" />
               ) : (
                 `${trade?.amountOut?.toSignificant(6) ?? '0.00'} ${token1.symbol}`
               )}
