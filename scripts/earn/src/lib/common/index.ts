7277be6103e837eb489f183ed4faf417e240002c
Ola Stenberg <hello@olastenberg.com>
2023-01-10 11:38:13 +0100

    feat(scripts/earn): init earn db from archive branch

---
+++ scripts/earn/src/lib/common/index.ts
@@ -0,0 +1,4 @@
+export * from './blocks.js'
+export * from './pairs.js'
+export * from './tokens.js'
+export * from './utils.js'
---
+++ scripts/earn/src/lib/index.ts
@@ -0,0 +1,4 @@
+export * from './chefs/index.js'
+export * from './common/index.js'
+export * from './redis.js'
+export * from './wagmi.js'
