0c47ddca705ba68dcc8f983ab4927953d5ad5b12
Ola Stenberg <hello@olastenberg.com>
2023-01-16 21:03:24 +0100

    refactor(apis/incentives): exclude unnecessary fields from query

--- apis/incentives/lib/api.ts
+++ apis/incentives/lib/api.ts
@@ -2,8 +2,23 @@ import prisma from '@sushiswap/database'
 
 export async function getIncentivesByChainId(chainId: number) {
   const incentives = await prisma.incentive.findMany({
-    include: {
-      rewardToken: true,
+    select: {
+      id: true,
+      chainId: true,
+      pid: true,
+      type: true,
+      apr: true,
+      rewardPerDay: true,
+      poolId: true,
+      rewardToken: {
+        select: {
+          id: true,
+          address: true,
+          name: true,
+          symbol: true,
+          decimals: true,
+        },
+      },
     },
     where: { chainId },
   })
@@ -13,20 +28,49 @@ export async function getIncentivesByChainId(chainId: number) {
 
 export async function getIncentives() {
   const incentives = await prisma.incentive.findMany({
-    include: {
-      rewardToken: true,
+    select: {
+      id: true,
+      chainId: true,
+      pid: true,
+      type: true,
+      apr: true,
+      rewardPerDay: true,
+      poolId: true,
+      rewardToken: {
+        select: {
+          id: true,
+          address: true,
+          name: true,
+          symbol: true,
+          decimals: true,
+        },
+      },
     },
   })
   await prisma.$disconnect()
   return incentives ? incentives : []
 }
 
-
 export async function getIncentivesByPoolId(chainId: number, address: string) {
   const poolId = `${chainId}:${address.toLowerCase()}`
   const incentives = await prisma.incentive.findMany({
-    include: {
-      rewardToken: true,
+    select: {
+      id: true,
+      chainId: true,
+      pid: true,
+      type: true,
+      apr: true,
+      rewardPerDay: true,
+      poolId: true,
+      rewardToken: {
+        select: {
+          id: true,
+          address: true,
+          name: true,
+          symbol: true,
+          decimals: true,
+        },
+      },
     },
     where: { poolId },
   })
