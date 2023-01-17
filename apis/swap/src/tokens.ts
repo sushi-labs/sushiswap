41cd3f7342e258c717f2ed4538d119e8bd500a96
LufyCZ <lufy@sushi.com>
2023-01-17 20:24:37 +0000

    feat(apis/swap): pre-fetch token cache

--- apis/swap/src/tokens.ts
+++ apis/swap/src/tokens.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 
-import type { ChainId } from '@sushiswap/chain'
+import { ChainId } from '@sushiswap/chain'
 import {
   currencyFromShortCurrencyName,
   isShortCurrencyName,
@@ -12,28 +12,44 @@ import fetch from 'node-fetch'
 import { z } from 'zod'
 
 const tokenSchema = z.object({
-  address: z.coerce.string(),
+  address: z.coerce.string().transform((address) => address.toLowerCase()),
   symbol: z.string(),
   name: z.string(),
   decimals: z.coerce.number().int().gte(0),
 })
 
 const tokenCache: Map<ChainId, Map<string, typeof tokenSchema['_type']>> = new Map()
+function getCache(chainId: ChainId, tokenId: string) {
+  return tokenCache.get(chainId)?.get(tokenId.toLowerCase())
+}
+function setCache(chainId: ChainId, tokenId: string, token: typeof tokenSchema['_type']) {
+  if (!tokenCache.get(chainId)) tokenCache.set(chainId, new Map())
+  tokenCache.get(chainId)?.set(tokenId.toLowerCase(), token)
+}
 
+// ! Only Polygon tokens are currently pre-cached
+async function populateCache() {
+  const chainIds = [ChainId.POLYGON]
+
+  for (const chainId of chainIds) {
+    const tokens = z
+      .array(tokenSchema)
+      .parse(await fetch(`https://tokens.sushi.com/v0/${chainId}`).then((data) => data.json()))
+    tokens.forEach((token) => setCache(chainId, token.address, token))
+  }
+}
+populateCache()
+
+// Currently unused, since the cache will be populated with all tokens
 async function fetcher(chainId: ChainId, tokenId: string) {
-  const cachedChain = tokenCache.get(chainId)
-  const cachedToken = cachedChain?.get(tokenId)
+  const cachedToken = getCache(chainId, tokenId)
   if (cachedToken) return cachedToken
 
   const token = tokenSchema.parse(
-    await (await fetch(`https://tokens.sushi.com/v0/${chainId}/${getAddress(tokenId)}`)).json()
+    await fetch(`https://tokens.sushi.com/v0/${chainId}/${getAddress(tokenId)}`).then((data) => data.json())
   )
 
-  if (!cachedChain) {
-    tokenCache.set(chainId, new Map())
-  }
-
-  tokenCache.get(chainId)?.set(tokenId, token)
+  setCache(chainId, tokenId, token)
 
   return token
 }
