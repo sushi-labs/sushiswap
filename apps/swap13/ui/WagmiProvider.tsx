c3a56e27cb9d97adac5ada9318595698814d4314
matthewlilley <hello@matthewlilley.com>
2023-01-18 23:03:07 +0000

    chore: rm apps/swap13

--- apps/swap13/ui/WagmiProvider.tsx
+++
@@ -1,7 +0,0 @@
-import { client } from '@sushiswap/wagmi13'
-import { FC, ReactNode } from 'react'
-import { WagmiConfig } from 'wagmi'
-
-export const WagmiProvider: FC<{ children: ReactNode }> = ({ children }) => (
-  <WagmiConfig client={client}>{children}</WagmiConfig>
-)
