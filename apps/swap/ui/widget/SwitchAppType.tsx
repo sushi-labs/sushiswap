a6de379cb28dd022ccc21dd0a909f1e44314db44
Chilli Chelli <ramin@sushi.com>
2023-01-19 04:46:20 +0000

    fix(apps/swap): show limit button

--- apps/swap/ui/widget/SwitchAppType.tsx
+++ apps/swap/ui/widget/SwitchAppType.tsx
@@ -1,12 +1,9 @@
 'use client'
 
-import { AppType } from '@sushiswap/ui13/types'
 import React, { FC } from 'react'
-
-import { useSwapActions, useSwapState } from '../trade/TradeProvider'
-import { Tab } from '@sushiswap/ui13/components/tabs'
 import { Button } from '@sushiswap/ui13/components/button'
 import { Tooltip } from '@sushiswap/ui13/components/Tooltip'
+import NonSSRWrapper from '../NonSSRWrapper'
 
 export const SwitchAppType: FC = () => {
   return (
@@ -14,11 +11,13 @@ export const SwitchAppType: FC = () => {
       <Button size="sm" variant="outlined" color="default">
         Swap
       </Button>
-      {/* <Tooltip description="Coming Soon™">
-        <Button className="pointer-events-none" size="sm" variant="empty" color="default">
-          Limit
-        </Button>
-      </Tooltip> */}
+      <NonSSRWrapper>
+        <Tooltip description="Coming soon!">
+          <Button className="pointer-events-none opacity-40" size="sm" variant="empty" color="default">
+            Limit
+          </Button>
+        </Tooltip>
+      </NonSSRWrapper>
     </div>
   )
 }
