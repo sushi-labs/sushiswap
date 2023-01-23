9bc840584792ca75e101c9295f1a1fee102dc93a
LufyCZ <lufy@sushi.com>
2023-01-23 22:32:16 +0000

    feat(apis/incentives): filtering by poolIds

--- apis/incentives/api/v0/index.ts
+++ apis/incentives/api/v0/index.ts
@@ -1,9 +1,29 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { z } from 'zod'
 
-import { getIncentives } from '../../lib/api'
+import { getIncentives, getIncentivesByPoolIds } from '../../lib/api'
 
-const handler = async (_request: VercelRequest, response: VercelResponse) => {
-  const incentives = await getIncentives()
+const schema = z.object({
+  pools: z
+    .string()
+    .optional()
+    .transform((pools) => pools?.split(',')),
+})
+
+const handler = async (request: VercelRequest, response: VercelResponse) => {
+  const result = schema.safeParse(request.query)
+  if (!result.success) {
+    return response.status(400).json(result.error.format())
+  }
+
+  let incentives
+
+  if (result.data.pools) {
+    incentives = await getIncentivesByPoolIds(result.data.pools)
+  } else {
+    incentives = await getIncentives()
+  }
+
   return response.status(200).json(incentives)
 }
 
