0d1d6ff4fa0dc2e643719c3f4d79a4ab049fccd0
LufyCZ <lufy@sushi.com>
2023-02-02 16:53:48 +0000

    feat(apis/pools): use getPools for getPool

--- apis/pools/lib/api.ts
+++ apis/pools/lib/api.ts
@@ -8,94 +8,6 @@ import type { PoolsApiSchema } from '../api/v0/'
 import type { PoolCountApiSchema } from '../api/v0/count'
 import type { PoolApiSchema } from '../api/v0/[chainId]/[address]'
 
-const poolSelect = {
-  id: true,
-  address: true,
-  name: true,
-  chainId: true,
-  version: true,
-  type: true,
-  swapFee: true,
-  twapEnabled: true,
-  totalSupply: true,
-  liquidityUSD: true,
-  volumeUSD: true,
-  feeApr: true,
-  incentiveApr: true,
-  totalApr: true,
-  isIncentivized: true,
-  fees1d: true,
-  fees1w: true,
-  volume1d: true,
-  volume1w: true,
-  isBlacklisted: true,
-  token0: {
-    select: {
-      id: true,
-      address: true,
-      name: true,
-      symbol: true,
-      decimals: true,
-    },
-  },
-  token1: {
-    select: {
-      id: true,
-      address: true,
-      name: true,
-      symbol: true,
-      decimals: true,
-    },
-  },
-  incentives: {
-    select: {
-      id: true,
-      pid: true,
-      chainId: true,
-      chefType: true,
-      apr: true,
-      rewarderAddress: true,
-      rewarderType: true,
-      rewardPerDay: true,
-      rewardToken: {
-        select: {
-          id: true,
-          address: true,
-          name: true,
-          symbol: true,
-          decimals: true,
-        },
-      },
-    },
-  },
-}
-
-export async function getPool(args: (typeof PoolApiSchema)['_output']) {
-  const id = `${args.chainId}:${args.address.toLowerCase()}`
-  const pool = await prisma.sushiPool.findFirstOrThrow({
-    select: poolSelect,
-    where: {
-      id,
-    },
-  })
-
-  type Pool = DecimalToString<
-    typeof pool & {
-      type: PoolType
-      version: PoolVersion
-      incentives: Array<
-        (typeof pool)['incentives'][0] & {
-          chefType: ChefType
-          rewarderType: RewarderType
-        }
-      >
-    }
-  >
-
-  await prisma.$disconnect()
-  return pool as unknown as Pool
-}
-
 type PrismaArgs = NonNullable<Parameters<typeof prisma.sushiPool.findMany>['0']>
 
 function parseWhere(args: typeof PoolsApiSchema._output | typeof PoolCountApiSchema._output) {
@@ -144,6 +56,16 @@ function parseWhere(args: typeof PoolsApiSchema._output | typeof PoolCountApiSch
   return where
 }
 
+export async function getPool(args: typeof PoolApiSchema._output) {
+  const id = `${args.chainId}:${args.address.toLowerCase()}`
+
+  // Need to specify take and orderBy to make TS happy
+  const [pool] = [await getPools({ ids: [id], take: 1, orderBy: 'liquidityUSD' })]
+
+  await prisma.$disconnect()
+  return pool
+}
+
 export async function getPools(args: typeof PoolsApiSchema._output) {
   const take = args.take
   const orderBy: PrismaArgs['orderBy'] = { [args.orderBy]: args.orderDir }
@@ -163,7 +85,67 @@ export async function getPools(args: typeof PoolsApiSchema._output) {
     ...cursor,
     where,
     orderBy,
-    select: poolSelect,
+    select: {
+      id: true,
+      address: true,
+      name: true,
+      chainId: true,
+      version: true,
+      type: true,
+      swapFee: true,
+      twapEnabled: true,
+      totalSupply: true,
+      liquidityUSD: true,
+      volumeUSD: true,
+      feeApr: true,
+      incentiveApr: true,
+      totalApr: true,
+      isIncentivized: true,
+      fees1d: true,
+      fees1w: true,
+      volume1d: true,
+      volume1w: true,
+      isBlacklisted: true,
+      token0: {
+        select: {
+          id: true,
+          address: true,
+          name: true,
+          symbol: true,
+          decimals: true,
+        },
+      },
+      token1: {
+        select: {
+          id: true,
+          address: true,
+          name: true,
+          symbol: true,
+          decimals: true,
+        },
+      },
+      incentives: {
+        select: {
+          id: true,
+          pid: true,
+          chainId: true,
+          chefType: true,
+          apr: true,
+          rewarderAddress: true,
+          rewarderType: true,
+          rewardPerDay: true,
+          rewardToken: {
+            select: {
+              id: true,
+              address: true,
+              name: true,
+              symbol: true,
+              decimals: true,
+            },
+          },
+        },
+      },
+    },
   })
 
   // TODO: Get rid of manual enums when the DB is migrated
