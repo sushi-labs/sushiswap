2a99f3f6a2d6280bf1f84924c733b657713b83eb
matthewlilley <hello@matthewlilley.com>
2023-02-06 13:42:41 +0000

    chore: price unknown warning

--- packages/wagmi13/components/Web3Input/Currency/PricePanel.tsx
+++ packages/wagmi13/components/Web3Input/Currency/PricePanel.tsx
@@ -32,7 +32,23 @@ export const PricePanel: FC<PricePanel> = ({ loading, price, currency, value, us
   return (
     <p className="font-medium text-lg py-1 select-none text-gray-500 dark:text-slate-400">
       {!loading && price?.equalTo(ZERO) ? (
-        <span className="text-sm italic">N/A</span>
+        <span className="text-sm flex items-center">
+          Price is unknown{' '}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 ml-1"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+            />
+          </svg>
+        </span>
       ) : (
         <>
           $ {big}.<span className="text-sm font-semibold">{portion}</span>
