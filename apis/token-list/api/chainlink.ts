6a61e36214634c3b506d150b639d060b856f6705
LufyCZ <lufy@sushi.com>
2023-02-07 19:26:19 +0000

    fix(apis/token-list): fix

--- apis/token-list/api/chainlink.ts
+++ apis/token-list/api/chainlink.ts
@@ -1,6 +1,6 @@
-import CHAINLINK_TOKEN_LIST from '@sushiswap/chainlink-token-list'
+import * as CHAINLINK_TOKEN_LIST from '@sushiswap/chainlink-token-list' assert { type: 'json' }
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-export default (request: VercelRequest, response: VercelResponse) => {
+export default (_request: VercelRequest, response: VercelResponse) => {
   return response.status(200).json(CHAINLINK_TOKEN_LIST)
 }
