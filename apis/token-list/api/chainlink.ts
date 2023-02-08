df3d0523a160fb55056f5396b0be1771a36186e6
Ola Stenberg <hello@olastenberg.com>
2023-02-08 12:14:06 +0100

    feat: revert token-list api back to master

--- apis/token-list/api/chainlink.ts
+++ apis/token-list/api/chainlink.ts
@@ -1,6 +1,6 @@
-import * as CHAINLINK_TOKEN_LIST from '@sushiswap/chainlink-token-list' assert { type: 'json' }
+import CHAINLINK_TOKEN_LIST from '@sushiswap/chainlink-token-list'
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-export default (_request: VercelRequest, response: VercelResponse) => {
+export default (request: VercelRequest, response: VercelResponse) => {
   return response.status(200).json(CHAINLINK_TOKEN_LIST)
 }
