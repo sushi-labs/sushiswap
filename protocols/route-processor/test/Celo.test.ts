349ac71822dcfbd26b371c85517bd6ddd1e22d2a
Ilya Lyalin <bretnwood@gmail.com>
2023-02-10 19:22:16 +0300

    feat: Celo test

---
+++ protocols/route-processor/test/Celo.test.ts
@@ -0,0 +1,113 @@
+import { ChainId } from '@sushiswap/chain'
+import { Native, Token, USDC, WNATIVE } from '@sushiswap/currency'
+import { DataFetcher, Router } from '@sushiswap/router'
+import { getBigNumber, RouteStatus } from '@sushiswap/tines'
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
+
+const RouteProcessorAddr = '0x9B3fF703FA9C8B467F5886d7b61E61ba07a9b51c'
+
+const cUSD = new Token({
+  chainId: ChainId.CELO,
+  address: '0x765DE816845861e75A25fCA122bb6898B8B1282a',
+  decimals: 18,
+  symbol: 'cUSD',
+  name: 'Celo Dollar',
+})
+
+const delay = async (ms: number) => new Promise((res) => setTimeout(res, ms))
+
+if (process.env.INFURA_API_KEY) {
+  describe('Celo test', () => {
+    it.only('Celo test', async () => {
+      const chainId = ChainId.CELO
+      const fromToken = Native.onChain(chainId)
+      const toToken = cUSD
+      const to = '0x0000000000000000000000000000000000000001'
+
+      const provider = new ethers.providers.JsonRpcProvider(
+        `https://celo-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+        42220
+      )
+
+      const dataFetcher = new DataFetcher(provider, chainId)
+      dataFetcher.startDataFetching()
+
+      let route
+      for (let i = 0; i < 10; ++i) {
+        // try to find a route
+        route = Router.findBestRoute(dataFetcher, fromToken, getBigNumber(10 * 1e18), toToken, 50 * 1e9)
+        if (route.status == RouteStatus.Success) break
+        await delay(1000)
+      }
+      expect(route?.status).equal(RouteStatus.Success)
+
+      if (route) {
+        //console.log(route.legs)
+
+        const rpParams = Router.routeProcessorParams(
+          dataFetcher,
+          route,
+          fromToken,
+          toToken,
+          to, //WNATIVE[chainId].address, // to
+          RouteProcessorAddr
+        )
+
+        console.log(rpParams)
+
+        //const RouteProcessor = new Contract(RouteProcessorAddr, RPData.abi, provider.getSigner())
+
+        const coder = new ethers.utils.AbiCoder()
+        const callArgs = coder.encode(
+          ['address', 'uint256', 'address', 'uint256', 'address', 'bytes'],
+          [
+            rpParams.tokenIn,
+            0, //rpParams.amountIn,
+            rpParams.tokenOut,
+            rpParams.amountOutMin,
+            rpParams.to,
+            rpParams.routeCode,
+            //'0x04471EcE3750Da237f93B8E339c536989b8978a438014DA9471c101e0cac906E52DF4f00943b21863efFffff0a4DA9471c101e0cac906E52DF4f00943b21863efF471EcE3750Da237f93B8E339c536989b8978a438010000000000000000000000000000000000000001',
+          ]
+        )
+        const callData = '0x2646478b' + callArgs.substring(2)
+
+        // Simple one pool swap code
+        // 04 471EcE3750Da237f93B8E339c536989b8978a438 01 4DA9471c101e0cac906E52DF4f00943b21863efF ffff
+        // distributeERC20Shares<celo_token>01<pool addr><share>
+        // 0a 4DA9471c101e0cac906E52DF4f00943b21863efF 471EcE3750Da237f93B8E339c536989b8978a438 01 471EcE3750Da237f93B8E339c536989b8978a438
+        // swapUniswapPool pool celo direction to(celo)
+
+        // const p = await provider.perform('call', {
+        //   transaction: {
+        //     to: RouteProcessorAddr,
+        //     data: callData,
+        //   },
+        // })
+        // console.log(p)
+        const val = '0x' + Number(parseInt(rpParams.value?.toString() as string)).toString(16)
+        console.log('val', val)
+
+        const call = await ethers.utils.fetchJson(
+          `https://celo-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+          JSON.stringify({
+            method: 'eth_call',
+            params: [
+              {
+                from: WNATIVE[chainId].address,
+                to: RouteProcessorAddr,
+                value: val,
+                data: callData,
+              },
+              'latest',
+            ],
+            id: 1,
+            jsonrpc: '2.0',
+          })
+        )
+        console.log(call)
+      }
+    })
+  })
+}
