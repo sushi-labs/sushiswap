5963cffeccacfeb92c9a42b92cdddc312e9eb842
Ilya Lyalin <bretnwood@gmail.com>
2023-02-11 19:30:17 +0300

    fix: RP deploy code

--- protocols/route-processor/deploy/GetDeployCode.ts
+++ protocols/route-processor/deploy/GetDeployCode.ts
@@ -1,17 +1,17 @@
+import { BENTOBOX_ADDRESS } from '@sushiswap/address'
 import { ChainId, chainName } from '@sushiswap/chain'
-import { BentoBox } from '@sushiswap/router'
 import { ethers } from 'hardhat'
 
 const chainId = parseInt(process.argv[2]) as ChainId
 console.log(`Deployment code for network ${chainName[chainId]} (${chainId})`)
-console.log('BentoBox: ', BentoBox[chainId])
+console.log('BentoBox: ', BENTOBOX_ADDRESS[chainId])
 
 getCode()
 
 async function getCode() {
   const RouteProcessor = await ethers.getContractFactory('RouteProcessor')
   const deplTrans = RouteProcessor.getDeployTransaction(
-    BentoBox[chainId] || '0x0000000000000000000000000000000000000000'
+    BENTOBOX_ADDRESS[chainId] || '0x0000000000000000000000000000000000000000'
   )
   console.log(deplTrans.data)
 }
