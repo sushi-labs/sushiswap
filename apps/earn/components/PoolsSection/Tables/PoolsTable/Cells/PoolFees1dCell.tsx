6fde4375e0cb6030e6c5a683bd2d6cc863806219
LufyCZ <lufy@sushi.com>
2023-02-13 19:58:08 +0000

    fix(apps/earn): merge

--- apps/earn/components/PoolsSection/Tables/PoolsTable/Cells/PoolFees1dCell.tsx
+++ apps/earn/components/PoolsSection/Tables/PoolsTable/Cells/PoolFees1dCell.tsx
@@ -4,11 +4,7 @@ import { FC } from 'react'
 
 import { CellProps } from './types'
 
-<<<<<<<< HEAD:apps/earn/components/PoolsSection/Tables/PoolsTable/Cells/PoolFees24hCell.tsx
-export const PoolFees24hCell: FC<CellProps> = ({ row }) => {
-========
 export const PoolFees1dCell: FC<CellProps> = ({ row }) => {
->>>>>>>> origin/feature/add-incentive-integration-v2:apps/earn/components/PoolsSection/Tables/PoolsTable/Cells/PoolFees1dCell.tsx
   const fees = formatUSD(row.fees1d)
 
   return (
--- apps/earn/components/PoolsSection/Tables/SharedCells/PoolVolume1dCell.tsx
+++ apps/earn/components/PoolsSection/Tables/SharedCells/PoolVolume1dCell.tsx
@@ -4,11 +4,7 @@ import { FC } from 'react'
 
 import { Row } from './types'
 
-<<<<<<<< HEAD:apps/earn/components/PoolsSection/Tables/SharedCells/PoolVolume24hCell.tsx
-export const PoolVolume24hCell: FC<Row<{ volume1d: string | number }>> = ({ row }) => {
-========
 export const PoolVolume1dCell: FC<Row<{ volume1d: string | number }>> = ({ row }) => {
->>>>>>>> origin/feature/add-incentive-integration-v2:apps/earn/components/PoolsSection/Tables/SharedCells/PoolVolume1dCell.tsx
   const volume = formatUSD(row.volume1d)
 
   return (
