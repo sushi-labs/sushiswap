6eadecdd01fa6b31c961ba6777a27228024359c5
Ilya Lyalin <bretnwood@gmail.com>
2023-02-13 23:00:55 +0300

    feat: RP2 base distrib functions

--- protocols/route-processor/contracts/RouteProcessor2.sol
+++ protocols/route-processor/contracts/RouteProcessor2.sol
@@ -91,192 +91,59 @@ contract RouteProcessor {
     address to,
     bytes memory route
   ) private returns (uint256 amountOut) {
-    uint256 amountInAcc = 0;
-    uint256 balanceInitial = tokenOut == NATIVE_ADDRESS ? 
-      address(to).balance : IERC20(tokenOut).balanceOf(to);
+    uint256 balanceInInitial = tokenIn == NATIVE_ADDRESS ? address(to).balance : IERC20(tokenIn).balanceOf(to);
+    uint256 balanceOutInitial = tokenOut == NATIVE_ADDRESS ? address(to).balance : IERC20(tokenOut).balanceOf(to);
 
     uint256 stream = InputStream.createStream(route);
     while (stream.isNotEmpty()) {
       uint8 commandCode = stream.readUint8();
-      if (commandCode < 10) {
-        if (commandCode == 4)
-          distributeERC20Shares(stream); // distribute ERC20 tokens from this router to pools
-        else if (commandCode == 3)
-          amountInAcc += distributeERC20Amounts(stream, tokenIn); // initial distribution
-        else if (commandCode == 5)
-          amountInAcc += wrapAndDistributeERC20Amounts(stream); // wrap natives and initial distribution        
-        else if (commandCode == 6) 
-          unwrapNative(to, stream);
-        else if (commandCode == 7)
-          amountInAcc += distributeERC20AmountsFromRP(stream, tokenIn); // initial distribution
-        else revert('Unknown command code');
-      } else if (commandCode < 20) {
-        if (commandCode == 10)
-          swapUniswapPool(stream); // Sushi/Uniswap pool swap
-        else if (commandCode == 11)
-          swapUniV3(0, stream); // UniV3 pool swap  TODO: input amount!
-      } else if (commandCode < 24) {
-        if (commandCode == 20) bentoDepositAmountFromBento(stream, tokenIn);
-        else if (commandCode == 21) swapTrident(stream);
-        else if (commandCode == 23) bentoWithdrawShareFromRP(stream, tokenIn);
-        else revert('Unknown command code');
-      } else {
-        if (commandCode == 24) amountInAcc += distributeBentoShares(stream, tokenIn);
-        else if (commandCode == 25) distributeBentoPortions(stream);
-        else if (commandCode == 26) bentoDepositAllFromBento(stream);
-        else if (commandCode == 27) bentoWithdrawAllFromRP(stream);
-        else revert('Unknown command code');
-      }
+      if (commandCode == 1) processMyERC20(stream);
+      else if (commandCode == 2) processUserERC20(stream, amountIn);
+      else if (commandCode == 3) processNative(stream);
+      else revert('RouteProcessor: Unknown command code');
     }
 
-    require(amountInAcc == amountIn, 'Wrong amountIn value');
-    uint256 balanceFinal = tokenOut == NATIVE_ADDRESS ? 
-      address(to).balance : IERC20(tokenOut).balanceOf(to);
-    require(balanceFinal >= balanceInitial + amountOutMin, 'Minimal ouput balance violation');
+    uint256 balanceInFinal = tokenIn == NATIVE_ADDRESS ? address(to).balance : IERC20(tokenIn).balanceOf(to);
+    require(balanceInFinal + amountIn >= balanceInInitial, 'RouteProcessor: Minimal imput balance violation');
 
-    amountOut = balanceFinal - balanceInitial;
+    uint256 balanceOutFinal = tokenOut == NATIVE_ADDRESS ? address(to).balance : IERC20(tokenOut).balanceOf(to);
+    require(balanceOutFinal >= balanceOutInitial + amountOutMin, 'RouteProcessor: Minimal ouput balance violation');
+
+    amountOut = balanceOutFinal - balanceOutInitial;
   }
 
-  /// @notice Transfers input tokens sent to BentoBox to a pool
-  /// @notice Expected to be called for initial liquidity transfer from user to BentoBox, so we know exact amounts
-  /// @param stream [Pool, Amount]. Pool into which an amount of tokens will be transferred
-  /// @param token Address of the token to transfer
-  function bentoDepositAmountFromBento(uint256 stream, address token) private {
-    address to = stream.readAddress();
-    uint256 amount = stream.readUint();
-    bentoBox.deposit(token, address(bentoBox), to, amount, 0);
-  }
-
-  /// @notice Transfers all available input tokens from BentoBox to a pool
-  /// @param stream [Pool, Token]. Pool into which all tokens will be transferred 
-  function bentoDepositAllFromBento(uint256 stream) private {
-    address to = stream.readAddress();
-    address token = stream.readAddress();
-
-    uint256 amount = IERC20(token).balanceOf(address(bentoBox)) +
-      bentoBox.strategyData(token).balance -
-      bentoBox.totals(token).elastic;
-    bentoBox.deposit(token, address(bentoBox), to, amount, 0);
-  }
-
-  /// @notice Withdraws BentoBox tokens from BentoBox to an address
-  /// @param stream [To, Amount]. Destination where an amount of token will be transferred
-  /// @param token Token to transfer
-  function bentoWithdrawShareFromRP(uint256 stream, address token) private {
-    address to = stream.readAddress();
-    uint256 amount = stream.readUint();
-    bentoBox.withdraw(token, address(this), to, amount, 0);
-  }
-
-  /// @notice Withdraws all available BentoBox tokens from BentoBox to an address
-  /// @param stream [Token, To]. Token which will be transferred to a destination
-  function bentoWithdrawAllFromRP(uint256 stream) private {
-    address token = stream.readAddress();
-    address to = stream.readAddress();
-    uint256 amount = bentoBox.balanceOf(token, address(this));
-    bentoBox.withdraw(token, address(this), to, 0, amount);
-  }
-
-  /// @notice Performs a Trident pool swap
-  /// @param stream [Pool, SwapData]. Pool against a swap defined by SwapData will be executed
-  function swapTrident(uint256 stream) private {
-    address pool = stream.readAddress();
-    bytes memory swapData = stream.readBytes();
-    IPool(pool).swap(swapData);
-  }
-
-  /// @notice Performs a Sushi/UniswapV2 pool swap
-  /// @param stream [Pool, TokenIn, Direction, To]
-  /// @return amountOut Amount of the output token
-  function swapUniswapPool(uint256 stream) private returns (uint256 amountOut) {
-    address pool = stream.readAddress();
-    address tokenIn = stream.readAddress();
-    uint8 direction = stream.readUint8();
-    address to = stream.readAddress();
-
-    (uint256 r0, uint256 r1, ) = IUniswapV2Pair(pool).getReserves();
-    require(r0 > 0 && r1 > 0, 'Wrong pool reserves');
-    (uint256 reserveIn, uint256 reserveOut) = direction == 1 ? (r0, r1) : (r1, r0);
-
-    uint256 amountIn = IERC20(tokenIn).balanceOf(pool) - reserveIn;
-    uint256 amountInWithFee = amountIn * 997;
-    amountOut = (amountInWithFee * reserveOut) / (reserveIn * 1000 + amountInWithFee);
-    (uint256 amount0Out, uint256 amount1Out) = direction == 1 ? (uint256(0), amountOut) : (amountOut, uint256(0));
-    IUniswapV2Pair(pool).swap(amount0Out, amount1Out, to, new bytes(0));
-  }
-
-  /// @notice Distributes input ERC20 tokens from msg.sender to addresses. Tokens should be approved
-  /// @notice Expected to be called for initial liquidity transfer from the user to pools, so we know exact amounts
-  /// @param stream [ArrayLength, ...[To, Amount][]]. An array of destinations and token amounts
-  /// @param token Token to distribute
-  /// @return amountTotal Total amount distributed
-  function distributeERC20Amounts(uint256 stream, address token) private returns (uint256 amountTotal) {
-    uint8 num = stream.readUint8();
-    amountTotal = 0;
-    for (uint256 i = 0; i < num; ++i) {
-      address to = stream.readAddress();
-      uint256 amount = stream.readUint();
-      amountTotal += amount;
-      IERC20(token).safeTransferFrom(msg.sender, to, amount);
-    }
-  }
-
-  /// @notice Distributes input ERC20 tokens from this contract to addresses. Tokens should be approved
-  /// @notice Expected to be called for initial liquidity transfer from the user to pools, so we know exact amounts
-  /// @param stream [ArrayLength, ...[To, Amount][]]. An array of destinations and token amounts
-  /// @param token Token to distribute
-  /// @return amountTotal Total amount distributed
-  function distributeERC20AmountsFromRP(uint256 stream, address token) private returns (uint256 amountTotal) {
-    uint8 num = stream.readUint8();
-    amountTotal = 0;
-    for (uint256 i = 0; i < num; ++i) {
-      address to = stream.readAddress();
-      uint256 amount = stream.readUint();
-      amountTotal += amount;
-      IERC20(token).safeTransfer(to, amount);
-    }
-  }
-
-  /// @notice Wraps all native inputs and distributes wrapped ERC20 tokens from RouteProcessor to addresses
-  /// @notice Expected to be called for initial liquidity transfer from the user to pools, so we know exact amounts
-  /// @param stream [WrapToken, ArrayLength, ...[To, Amount][]]. An array of destinations and token amounts
-  /// @return amountTotal Total amount distributed
-  function wrapAndDistributeERC20Amounts(uint256 stream) private returns (uint256 amountTotal) {
+  function processNative(uint256 stream) private {
     address token = stream.readAddress();
     IWETH(token).deposit{value: address(this).balance}();
-    uint8 num = stream.readUint8();
-    amountTotal = 0;
-    for (uint256 i = 0; i < num; ++i) {
-      address to = stream.readAddress();
-      uint256 amount = stream.readUint();
-      amountTotal += amount;
-      IERC20(token).safeTransfer(to, amount);
-    }
-    require(address(this).balance == 0, "RouteProcessor: invalid input amount");
   }
 
-  /// @notice Distributes input BentoBox tokens from msg.sender to addresses. Tokens should be approved
-  /// @notice Expected to be called for initial liquidity transfer from the user to pools, so we know exact amounts
-  /// @param stream [ArrayLength, ...[To, ShareAmount][]]. An array of destinations and token share amounts
-  /// @param token Token to distribute
-  /// @return sharesTotal Total shares distributed
-  function distributeBentoShares(uint256 stream, address token) private returns (uint256 sharesTotal) {
-    uint8 num = stream.readUint8();
-    sharesTotal = 0;
-    for (uint256 i = 0; i < num; ++i) {
-      address to = stream.readAddress();
-      uint256 share = stream.readUint();
-      sharesTotal += share;
-      bentoBox.transfer(token, msg.sender, to, share);
-    }
-  }
+  // function processERC20(uint256 stream, uint256 amountIn) private {
+  //   address token = stream.readAddress();
+  //   uint8 num = stream.readUint8();
+  //   address from; // = num <= 127 ? msg.sender : address(this);
+  //   uint256 amountTotal; // =  amountIn > 0 ? amountIn : IERC20(token).balanceOf(address(this))
+  //    // - 1;     // slot undrain protection
+  //   if (num < 128) { // take liquidity for swap from msg.sender
+  //     from = msg.sender;
+  //     amountTotal = amountIn;
+  //   } else { // take liquidity for swap from this contract
+  //     unchecked {num -= 128;}
+  //     from = address(this);
+  //     amountTotal = IERC20(token).balanceOf(address(this))
+  //       - 1;     // slot undrain protection
+  //   }
 
-  /// @notice Distributes ERC20 tokens from RouteProcessor to addresses
-  /// @notice Quantity for sending is determined by share in 1/65535
-  /// @notice During routing we can't predict in advance the actual value of internal swaps because of slippage,
-  /// @notice so we have to work with shares - not fixed amounts
-  /// @param stream [Token, ArrayLength, ...[To, ShareAmount][]]. Token to distribute. An array of destinations and token share amounts
-  function distributeERC20Shares(uint256 stream) private {
+  //   unchecked {
+  //     for (uint256 i = 0; i < num; ++i) {
+  //       uint16 share = stream.readUint16();
+  //       uint256 amount = (amountTotal * share) / 65535;
+  //       amountTotal -= amount;
+  //       swap(stream, from, token, amount);
+  //     }
+  //   }
+  // }
+
+  function processMyERC20(uint256 stream) private {
     address token = stream.readAddress();
     uint8 num = stream.readUint8();
     uint256 amountTotal = IERC20(token).balanceOf(address(this))
@@ -284,45 +151,50 @@ contract RouteProcessor {
 
     unchecked {
       for (uint256 i = 0; i < num; ++i) {
-        address to = stream.readAddress();
         uint16 share = stream.readUint16();
         uint256 amount = (amountTotal * share) / 65535;
         amountTotal -= amount;
-        IERC20(token).safeTransfer(to, amount);
+        swap(stream, address(this), token, amount);
       }
     }
   }
-
-  /// @notice Distributes BentoBox tokens from RouteProcessor to addresses
-  /// @notice Quantity for sending is determined by portions in 1/65535.
-  /// @notice During routing we can't predict in advance the actual value of internal swaps because of slippage,
-  /// @notice so we have to work with portions - not fixed amounts
-  /// @param stream [Token, ArrayLength, ...[To, ShareAmount][]]. Token to distribute. An array of destinations and token share amounts
-  function distributeBentoPortions(uint256 stream) private {
+  
+  function processUserERC20(uint256 stream, uint256 amountTotal) private {
     address token = stream.readAddress();
     uint8 num = stream.readUint8();
-    uint256 amountTotal = bentoBox.balanceOf(token, address(this))
-      - 1;     // slot undrain protection
 
     unchecked {
       for (uint256 i = 0; i < num; ++i) {
-        address to = stream.readAddress();
         uint16 share = stream.readUint16();
         uint256 amount = (amountTotal * share) / 65535;
         amountTotal -= amount;
-        bentoBox.transfer(token, address(this), to, amount);
+        swap(stream, msg.sender, token, amount);
       }
     }
   }
 
-  /// @notice Unwraps the Native Token
-  /// @param receiver Destination of the unwrapped token
-  /// @param stream [Token]. Token to unwrap native
-  function unwrapNative(address receiver, uint256 stream) private {
-    address token = stream.readAddress();
-    IWETH(token).withdraw( IERC20(token).balanceOf(address(this))
-      - 1);     // slot undrain protection
-    payable(receiver).transfer(address(this).balance);
+  function swap(uint256 stream, address from, address tokenIn, uint256 amountIn) private {
+    uint8 poolType = stream.readUint8();
+    if (poolType == 0) swapUniV2(stream, from, tokenIn, amountIn);
+    //else if (poolType == 1) swapUniV3(stream, tokenIn, amountIn);
+    revert('RouteProcessor: Unknown pool type');
+  }
+
+  function swapUniV2(uint256 stream, address from, address tokenIn, uint256 amountIn) private {
+    address pool = stream.readAddress();
+    uint8 direction = stream.readUint8();
+    address to = stream.readAddress();
+
+    IERC20(tokenIn).safeTransferFrom(from, to, amountIn);
+
+    (uint256 r0, uint256 r1, ) = IUniswapV2Pair(pool).getReserves();
+    require(r0 > 0 && r1 > 0, 'Wrong pool reserves');
+    (uint256 reserveIn, uint256 reserveOut) = direction == 1 ? (r0, r1) : (r1, r0);
+
+    uint256 amountInWithFee = amountIn * 997;
+    uint256 amountOut = (amountInWithFee * reserveOut) / (reserveIn * 1000 + amountInWithFee);
+    (uint256 amount0Out, uint256 amount1Out) = direction == 1 ? (uint256(0), amountOut) : (amountOut, uint256(0));
+    IUniswapV2Pair(pool).swap(amount0Out, amount1Out, to, new bytes(0));
   }
 
   /// @dev The minimum value that can be returned from #getSqrtRatioAtTick. Equivalent to getSqrtRatioAtTick(MIN_TICK)
@@ -363,11 +235,11 @@ contract RouteProcessor {
     int256 amount1Delta,
     bytes calldata data
   ) external {
-    require(msg.sender == lastCalledPool, 'uniswapV3SwapCallback: call from unknown source');
+    require(msg.sender == lastCalledPool, 'RouteProcessor.uniswapV3SwapCallback: call from unknown source');
     lastCalledPool = IMPOSSIBLE_POOL_ADDRESS;
     address tokenIn = abi.decode(data, (address));
     int256 amount = amount0Delta > 0 ? amount0Delta : amount1Delta;
-    require(amount > 0, 'uniswapV3SwapCallback: no positive amount');
+    require(amount > 0, 'RouteProcessor.uniswapV3SwapCallback: no positive amount');
     IERC20(tokenIn).safeTransfer(msg.sender, uint256(amount));
   }
 }
