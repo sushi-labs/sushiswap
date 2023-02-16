c72ea4a628a620aafd1e441a748ce7b928483b64
matthewlilley <hello@matthewlilley.com>
2023-02-16 17:26:37 +0000

    fix: out of order wagmi read contracts results

--- packages/graph-client/lib/wagmi.ts
+++ packages/graph-client/lib/wagmi.ts
@@ -1,6 +1,7 @@
 import { ChainId } from '@sushiswap/chain'
+import { isPromiseFulfilled } from '@sushiswap/validate'
 import { allChains, allProviders } from '@sushiswap/wagmi-config'
-import { Address, configureChains, createClient, erc20ABI, readContracts } from '@wagmi/core'
+import { Address, configureChains, createClient, erc20ABI, readContract } from '@wagmi/core'
 import { BigNumber } from 'ethers'
 
 const { provider } = configureChains(allChains, allProviders)
@@ -10,25 +11,46 @@ createClient({ provider })
 export async function fetchBalances(
   args: { token: string; user: string; chainId: ChainId }[]
 ): Promise<Record<string, string>> {
-  const _balances = await readContracts({
-    allowFailure: true,
-    contracts: args.map(
-      ({ token, user, chainId }) =>
-        ({
-          address: token as Address,
-          functionName: 'balanceOf',
-          args: [user as Address],
-          chainId,
-          abi: erc20ABI,
-        } as const)
-    ),
-  })
+  // const _balances = await readContracts({
+  //   allowFailure: true,
+  //   contracts: args.map(
+  //     ({ token, user, chainId }) =>
+  //       ({
+  //         address: token as Address,
+  //         functionName: 'balanceOf',
+  //         args: [user as Address],
+  //         chainId,
+  //         abi: erc20ABI,
+  //       } as const)
+  //   ),
+  // })
 
-  const balances = args.map((arg, i) => ({ ...arg, value: !_balances[i] ? BigNumber.from(0) : _balances[i] }))
+  const balances = await Promise.allSettled(
+    args.map(({ token, user, chainId }) =>
+      readContract({
+        address: token as Address,
+        functionName: 'balanceOf',
+        args: [user as Address],
+        chainId,
+        abi: erc20ABI,
+      } as const)
+    )
+  ).then((promiseSettledResults) => {
+    if (!Array.isArray(promiseSettledResults)) {
+      console.error('balance readContract failed')
+      return []
+    }
+    return promiseSettledResults.map((promiseSettledResult, i) => {
+      return {
+        ...args[i],
+        value: isPromiseFulfilled(promiseSettledResult) ? promiseSettledResult.value : BigNumber.from(0),
+      }
+    })
+  })
 
   return Object.fromEntries(
     balances
-      .filter(({ value }) => BigNumber.isBigNumber(value) && value.gt(0))
+      // .filter(({ value }) => BigNumber.isBigNumber(value) && value.gt(0))
       .map((balance) => [`${balance.chainId}:${balance.token}`, balance.value.toString()])
   )
 }
