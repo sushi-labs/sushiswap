3721893383a5339eb4072a035db12ca14ee3f4b1
LufyCZ <lufy@sushi.com>
2023-03-01 21:53:35 +0000

    fix(apps/earn): remove matcher, wasn't matching index

--- apps/earn/middleware.ts
+++ apps/earn/middleware.ts
@@ -31,16 +31,3 @@ export async function middleware(req: NextRequest) {
 
   if (pathname.startsWith('/')) return NextResponse.next()
 }
-
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
-}
