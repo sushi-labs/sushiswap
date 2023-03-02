6c94cfe897621517b64f1edfe101f0be177cf204
LufyCZ <lufy@sushi.com>
2023-03-02 03:51:14 +0000

    fix(apps/earn): swapFee on breadcrumbs

--- apps/earn/pages/[id]/index.tsx
+++ apps/earn/pages/[id]/index.tsx
@@ -28,7 +28,7 @@ import { ChainId } from '@sushiswap/chain'
 const LINKS = (pool: Pool): BreadcrumbLink[] => [
   {
     href: `/${pool.id}`,
-    label: `${pool.name} - ${POOL_TYPE_MAP[pool.type]} - ${formatPercent(pool.swapFee * 100)}`,
+    label: `${pool.name} - ${POOL_TYPE_MAP[pool.type]} - ${formatPercent(pool.swapFee)}`,
   },
 ]
 
