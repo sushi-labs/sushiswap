46f365cfa24fe8ee0fdd23f00cc9fafe05f62f06
matthewlilley <hello@matthewlilley.com>
2023-03-03 04:51:09 +0000

    fix: usd formatter

--- packages/format/src/price.ts
+++ packages/format/src/price.ts
@@ -1,9 +1,9 @@
 import numeral from 'numeral'
 
 export const formatUSD = (value: string | number, inputString = '$0.00a') => {
-  if (value < 0.000001) return numeral(value).format('$0.000000a')
-  if (value < 0.00001) return numeral(value).format('$0.00000a')
-  if (value < 0.0001) return numeral(value).format('$0.0000a')
+  if (value < 0.000001) return '$0.00'
+  if (value < 0.0001) return numeral(value).format('$0.000000a')
+  if (value < 0.001) return numeral(value).format('$0.0000a')
   if (value < 0.01) return numeral(value).format('$0.000a')
   return numeral(value).format(inputString)
 }
