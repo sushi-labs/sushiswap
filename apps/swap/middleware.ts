fffd9b2ab506ab884edff20202b5975f24111bbc
matthewlilley <hello@matthewlilley.com>
2023-03-08 12:04:28 +0000

    fix: redirects from legacy swap/xswap paths

--- apps/swap/middleware.ts
+++ apps/swap/middleware.ts
@@ -1,11 +1,36 @@
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl
+  const { pathname, searchParams, search } = req.nextUrl
 
   if (pathname === '/') {
     const url = req.nextUrl.clone()
-    url.pathname = '/1:NATIVE/1:SUSHI'
+
+    if (search === '') {
+      url.pathname = '/1:NATIVE/1:SUSHI'
+    } else {
+      if (searchParams.has('chainId') && searchParams.has('token0') && searchParams.has('token1')) {
+        const chainId = searchParams.get('chainId')
+        const token0 = searchParams.get('token0')
+        const token1 = searchParams.get('token1')
+        url.search = ''
+        url.pathname = `/${chainId}:${token0}/${chainId}:${token1}`
+      } else if (
+        searchParams.has('srcChainId') &&
+        searchParams.has('srcToken') &&
+        searchParams.has('dstChainId') &&
+        searchParams.has('dstToken')
+      ) {
+        const srcChainId = searchParams.get('srcChainId')
+        const srcToken = searchParams.get('srcToken')
+        const dstChainId = searchParams.get('dstChainId')
+        const dstToken = searchParams.get('dstToken')
+        // ?srcToken=ETH&dstToken=ETH&srcChainId=1&dstChainId=42161
+        url.search = ''
+        url.pathname = `/${srcChainId}:${srcToken}/${dstChainId}:${dstToken}`
+      }
+    }
+
     return NextResponse.redirect(url)
   }
 
