fe962666db417d8a9455d38d865511466e49d4a0
matthewlilley <hello@matthewlilley.com>
2023-03-09 19:57:18 +0000

    fix: cache header for tokens

--- apis/tokens/api/v0/index.ts
+++ apis/tokens/api/v0/index.ts
@@ -3,6 +3,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { getTokens } from '../../lib/api.js'
 
 const handler = async (_request: VercelRequest, response: VercelResponse) => {
+  response.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate=600')
   const tokens = await getTokens()
   return response.status(200).json(tokens)
 }
