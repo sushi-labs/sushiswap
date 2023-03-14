91fce912d3fa6e92039fb97e2594bbf30eb25744
Chilli Chelli <ramin@sushi.com>
2023-03-14 17:31:26 +0000

    fix(apps/swap): pairs should be sushiswap liq provider

--- packages/wagmi/future/hooks/pools/actions/getAllPoolsCodeMap.ts
+++ packages/wagmi/future/hooks/pools/actions/getAllPoolsCodeMap.ts
@@ -1,6 +1,5 @@
 import { UsePoolsParams } from '../types'
 import { getAllPools } from './getAllPools'
-import { convertPoolOrPairtoRPool } from '@sushiswap/amm/dist/Trade/convertPoolOrPairtoRPool'
 import { ConstantProductPoolCode } from '@sushiswap/router/dist/pools/ConstantProductPool'
 import { BridgeBento, ConstantProductRPool, StableSwapRPool } from '@sushiswap/tines'
 import { BentoPoolCode } from '@sushiswap/router/dist/pools/BentoPool'
@@ -9,12 +8,16 @@ import { BentoBridgePoolCode } from '@sushiswap/router/dist/pools/BentoBridge'
 import { getBentoBoxContractConfig } from '../../../../hooks'
 import { isBentoBoxV1ChainId } from '@sushiswap/bentobox'
 import { PoolCode } from '@sushiswap/router/dist/pools/PoolCode'
+import { ConstantProductPool, Pair, StablePool } from '@sushiswap/amm'
+import { convertPoolOrPairtoRPool } from '@sushiswap/amm/dist/Trade/convertPoolOrPairtoRPool'
 
 export const getAllPoolsCodeMap = async (variables: Omit<UsePoolsParams, 'enabled'>) => {
   const { pairs, stablePools, constantProductPools, bridgeBentoPools } = await getAllPools(variables)
 
   const rPools = [
-    ...[...(pairs || []), ...(stablePools || []), ...(constantProductPools || [])].map(convertPoolOrPairtoRPool),
+    ...(pairs || []),
+    ...(stablePools || []),
+    ...(constantProductPools || []),
     ...(bridgeBentoPools || []),
   ]
 
@@ -22,19 +25,30 @@ export const getAllPoolsCodeMap = async (variables: Omit<UsePoolsParams, 'enable
     ? getBentoBoxContractConfig(variables.chainId).address
     : undefined
 
-  return rPools.reduce<Map<string, PoolCode>>((acc, cur) => {
-    if (cur instanceof ConstantProductRPool) {
-      acc.set(cur.address, new ConstantProductPoolCode(cur, LiquidityProviders.SushiSwap, 'SushiSwap'))
-    }
-
-    if (cur instanceof StableSwapRPool) {
-      acc.set(cur.address, new BentoPoolCode(cur, LiquidityProviders.Trident, 'Trident'))
-    }
-
+  const rp = rPools.reduce<Map<string, PoolCode>>((acc, cur) => {
     if (cur instanceof BridgeBento && bentoAddress) {
       acc.set(cur.address, new BentoBridgePoolCode(cur, LiquidityProviders.Trident, 'Trident', bentoAddress))
+      return acc
+    }
+
+    if (!(cur instanceof BridgeBento)) {
+      const rpool = convertPoolOrPairtoRPool(cur)
+      if (cur instanceof Pair && rpool instanceof ConstantProductRPool) {
+        acc.set(rpool.address, new ConstantProductPoolCode(rpool, LiquidityProviders.SushiSwap, 'SushiSwap'))
+      }
+
+      if (cur instanceof ConstantProductPool && rpool instanceof ConstantProductRPool) {
+        acc.set(rpool.address, new ConstantProductPoolCode(rpool, LiquidityProviders.Trident, 'Trident'))
+      }
+
+      if (cur instanceof StablePool && rpool instanceof StableSwapRPool) {
+        acc.set(rpool.address, new BentoPoolCode(rpool, LiquidityProviders.Trident, 'Trident'))
+      }
     }
 
     return acc
   }, new Map<string, PoolCode>())
+
+  console.log(rp)
+  return rp
 }
