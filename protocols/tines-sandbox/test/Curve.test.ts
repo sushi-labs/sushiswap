344ce36ae197dd7b6638820344d673420fe27195
Ilya Lyalin <bretnwood@gmail.com>
2023-03-14 23:25:44 +0300

    feat: tines-sandbox: first curve test

--- protocols/tines-sandbox/test/Curve.test.ts
+++ protocols/tines-sandbox/test/Curve.test.ts
@@ -1,64 +1,106 @@
 import { erc20Abi } from '@sushiswap/abi'
-import { CurvePool, RToken } from '@sushiswap/tines'
-import { Contract } from 'ethers'
+import { CurvePool, getBigNumber, RToken } from '@sushiswap/tines'
+import { expect } from 'chai'
+import { BigNumber, Contract, Signer } from 'ethers'
 import { ethers } from 'hardhat'
 
 import { setTokenBalance } from '../src/SetTokenBalance'
+
+function closeValues(_a: number | BigNumber, _b: number | BigNumber, accuracy: number): boolean {
+  const a: number = typeof _a == 'number' ? _a : parseInt(_a.toString())
+  const b: number = typeof _b == 'number' ? _b : parseInt(_b.toString())
+  if (accuracy === 0) return a === b
+  if (Math.abs(a) < 1 / accuracy) return Math.abs(a - b) <= 10
+  if (Math.abs(b) < 1 / accuracy) return Math.abs(a - b) <= 10
+  return Math.abs(a / b - 1) < accuracy
+}
+
+function expectCloseValues(_a: number | BigNumber, _b: number | BigNumber, accuracy: number, logInfoIfFalse = '') {
+  const res = closeValues(_a, _b, accuracy)
+  if (!res) {
+    console.log(`Expected close: ${_a}, ${_b}, ${accuracy} ${logInfoIfFalse}`)
+    // debugger
+    expect(res).equal(true)
+  }
+  return res
+}
 
 interface PoolInfo {
   poolContract: Contract
   tokenContracts: Contract[]
   poolTines: CurvePool
+  user: Signer
+  userAddress: string
 }
 
-async function createCurvePool(address: string): Promise<PoolInfo> {
+async function createCurvePoolInfo(poolAddress: string, user: Signer, initialBalance: bigint): Promise<PoolInfo> {
   const chainId = ethers.provider.network.chainId
   const poolContract = new Contract(
-    address,
+    poolAddress,
     [
       'function exchange(int128 i, int128 j, uint256 dx, uint256 min_dy) returns (string)',
-      'function A() returns (uint256)',
-      'function fee() returns (uint256)',
-      'function coins(uint256) returns (address)',
-      'function balances(uint256) returns (uint256)',
+      'function A() pure returns (uint256)',
+      'function fee() pure returns (uint256)',
+      'function coins(uint256) pure returns (address)',
+      'function balances(uint256) pure returns (uint256)',
     ],
-    ethers.provider
+    user
   )
 
+  const userAddress = await user.getAddress()
   const tokenContracts = []
   const tokenTines: RToken[] = []
   for (let i = 0; i < 100; ++i) {
+    let token
     try {
-      const token = await poolContract.coins(i)
-      tokenContracts.push(new Contract(token, erc20Abi, ethers.provider))
-      tokenTines.push({ address: token, name: token, symbol: token, chainId })
+      token = await poolContract.coins(i)
     } catch (e) {
       break
     }
+
+    const res = await setTokenBalance(token, userAddress, initialBalance)
+    expect(res).equal(true)
+
+    const tokenContract = new Contract(token, erc20Abi, user)
+    await tokenContract.approve(poolAddress, initialBalance.toString())
+
+    tokenContracts.push(tokenContract)
+    tokenTines.push({ address: token, name: token, symbol: token, chainId })
   }
 
   const A = await poolContract.A()
   const fee = await poolContract.fee()
   const reserves = await Promise.all(tokenContracts.map((_, i) => poolContract.balances(i)))
 
-  const poolTines = new CurvePool(address, tokenTines[0], tokenTines[1], fee / 1e10, A, reserves[0], reserves[1])
+  const poolTines = new CurvePool(poolAddress, tokenTines[0], tokenTines[1], fee / 1e10, A, reserves[0], reserves[1])
 
   return {
     poolContract,
     tokenContracts,
     poolTines,
+    user,
+    userAddress,
   }
 }
 
-// async function checkSwap(poolInfo: PoolInfo, from: number, to: number, amountIn: number) {
-//   const balanceOutBefore = await poolInfo.tokenContracts[to].
-// }
+async function checkSwap(poolInfo: PoolInfo, from: number, to: number, amountIn: number) {
+  const expectedOut = poolInfo.poolTines.calcOutByIn(amountIn, from < to)
+
+  const balanceOutBefore = await poolInfo.tokenContracts[to].balanceOf(poolInfo.userAddress)
+  await poolInfo.poolContract.exchange(from, to, getBigNumber(amountIn), 0)
+  const balanceOutAfter = await poolInfo.tokenContracts[to].balanceOf(poolInfo.userAddress)
+  const realOutBN = balanceOutAfter.sub(balanceOutBefore)
+  const realOut = parseInt(realOutBN.toString())
+
+  expectCloseValues(realOut, expectedOut.out, 1e-9)
+}
 
 it('test', async () => {
-  const res = await setTokenBalance(
-    '0x853d955acef822db058eb8505911ed77f175b99e',
-    '0x0000000000000000000000000000000000000010',
-    1000n
+  const [user] = await ethers.getSigners()
+  const poolInfo = await createCurvePoolInfo(
+    '0xdcef968d416a41cdac0ed8702fac8128a64241a2', // fraxusdc
+    user,
+    BigInt(1e30)
   )
-  console.log(res)
+  await checkSwap(poolInfo, 0, 1, 1e20)
 })
