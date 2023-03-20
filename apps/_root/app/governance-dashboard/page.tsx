c26e972684173c2354573c68151dd954ca5e8ced
0xIvan <0xivanivan@gmail.com>
2023-03-20 23:00:57 +0900

    Add bookmarks with styling

--- apps/_root/app/governance-dashboard/page.tsx
+++ apps/_root/app/governance-dashboard/page.tsx
@@ -3,22 +3,33 @@
 import { Tab } from '@headlessui/react'
 import { classNames } from '@sushiswap/ui'
 import Container from '@sushiswap/ui/future/components/Container'
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 
 import { Hero } from './components'
 
-const DATE_FILTER = ['month', 'quarter', 'year'] as const
-type DateFilterKey = (typeof DATE_FILTER)[number]
-type DateFilter = { key: DateFilterKey; title: string }
+const DATE_FILTERS = ['Last Month', 'Last Quarter', 'Last Year'] as const
+type DateFilter = (typeof DATE_FILTERS)[number]
 
-const FILTERS: DateFilter[] = [
-  { key: 'month', title: 'Last Month' },
-  { key: 'quarter', title: 'Last Quarter' },
-  { key: 'year', title: 'Last Year' },
-]
+const GOV_STATUS = ['Snapshot Vote', 'Proposal', 'Discussion']
+type GovernanceStatus = (typeof GOV_STATUS)[number]
+
+type DateDispatch = { type: 'date'; payload: DateFilter }
+type GovStatusDispatch = { type: 'govStatus'; payload: GovernanceStatus }
+
+const INITIAL_FILTERS = {
+  date: 'Last Month',
+  govStatus: 'Snapshot Vote',
+}
+
+function reducer(state, { type, payload }: DateDispatch | GovStatusDispatch) {
+  return {
+    ...state,
+    [type]: payload,
+  }
+}
 
 export default function GovernanceDashboard() {
-  const [dateFilter, setDateFilter] = useState(FILTERS[0])
+  const [filters, dispatch] = useReducer(reducer, INITIAL_FILTERS)
 
   return (
     <Tab.Group>
@@ -30,23 +41,57 @@ export default function GovernanceDashboard() {
               <div className="flex justify-between items-center">
                 <h2 className="font-bold text-2xl">Latest @ Sushi</h2>
                 <div className="flex gap-2">
-                  {FILTERS.map((filter) => (
+                  {DATE_FILTERS.map((filter) => (
                     <button
                       className={classNames(
                         'rounded-full h-10 px-4 ring-1 ring-slate-700/40',
-                        dateFilter.key === filter.key ? 'bg-slate-700/40' : 'hover:bg-slate-700/20'
+                        filters.date === filter ? 'bg-slate-700/40' : 'hover:bg-slate-700/20'
                       )}
-                      key={filter.key}
-                      onClick={() => setDateFilter(filter)}
+                      key={filter}
+                      onClick={() => dispatch({ type: 'date', payload: filter })}
                     >
-                      {filter.title}
+                      {filter}
                     </button>
                   ))}
                 </div>
               </div>
-              <div className="flex gap-6">
-                <div>bookmarks</div>
-                <div>cards</div>
+              <div className="grid w-full grid-cols-[1fr,2fr] gap-6">
+                <div>
+                  {GOV_STATUS.map((status, i) => (
+                    <div
+                      className={classNames(
+                        'hover:cursor-pointer rounded-t-2xl relative',
+                        filters.govStatus === status
+                          ? 'bg-gradient-to-r from-[#0993EC] to-[#F338C3] pt-0.5 px-0.5'
+                          : 'hover:-translate-y-1 transition-transform eae-in-out',
+                        i && '-mt-4'
+                      )}
+                      key={status}
+                      onClick={() => dispatch({ type: 'govStatus', payload: status })}
+                      style={{ zIndex: i + 1 }}
+                    >
+                      <div className="flex justify-between items-center px-5 py-5 pb-10 bg-gradient-to-b from-[#212939] to-[#101728] rounded-t-2xl gap-10">
+                        <div className="flex items-center gap-2">
+                          <div
+                            className={classNames(
+                              'rounded-sm w-3 h-3',
+                              !i ? 'bg-red' : i === 1 ? 'bg-yellow' : 'bg-blue'
+                            )}
+                          />
+                          <span className="font-medium">{status}</span>
+                        </div>
+                        <div className="h-6 w-10 flex items-center justify-center bg-slate-500/60 rounded-lg">2</div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                <div className="relative p-1">
+                  <div className="flex gap-6 h-full">
+                    <div className="ring-1 rounded-lg h-full w-1/2"> card 1</div>
+                    <div className="ring-1 rounded-lg h-full w-1/2"> card 2</div>
+                  </div>
+                  <div className="bg-gradient-to-r to-[#101728] w-1/2 from-transparent h-full top-0 absolute right-0" />
+                </div>
               </div>
             </section>
           </Tab.Panel>
