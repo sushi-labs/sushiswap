387751e01ea94473e645ea0911c0f45a73ed77f2
Chilli Chelli <ramin@sushi.com>
2023-03-20 11:50:30 +0000

    fix(apps/earn): build error

--- packages/format/src/price.ts
+++ packages/format/src/price.ts
@@ -1,9 +1,9 @@
 import numeral from 'numeral'
 
-export const formatUSD = (value: string | number, inputString = '$0.00a') => {
+export const formatUSD = (value: string | number, inputString = '$0.00a'): string => {
   if (value < 0.000001) return '$0.00'
   if (value < 0.0001) return numeral(value).format('$0.000000a')
   if (value < 0.001) return numeral(value).format('$0.0000a')
   if (value < 0.01) return numeral(value).format('$0.000a')
-  return Intl.NumberFormat('en', { notation: 'compact' })
+  return Intl.NumberFormat('en', { notation: 'compact' }).format(Number(value))
 }
