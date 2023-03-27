c2a17ff717f84cc83682df99d67a48b06a651378
matthewlilley <hello@matthewlilley.com>
2023-03-27 20:25:19 +0100

    chore: port v3 sdk

---
+++ packages/v3-sdk/src/utils/mostSignificantBit.test.ts
@@ -0,0 +1,30 @@
+import { JSBI, MAX_UINT256 } from '@sushiswap/math'
+
+import { ONE } from '../internalConstants'
+import { mostSignificantBit } from './mostSignificantBit'
+
+describe('mostSignificantBit', () => {
+  it('throws for zero', () => {
+    expect(() => mostSignificantBit(JSBI.BigInt(0))).toThrow('ZERO')
+  })
+  it('correct value for every power of 2', () => {
+    for (let i = 1; i < 256; i++) {
+      const x = JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(i))
+      expect(mostSignificantBit(x)).toEqual(i)
+    }
+  })
+  it('correct value for every power of 2 - 1', () => {
+    for (let i = 2; i < 256; i++) {
+      const x = JSBI.subtract(JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(i)), JSBI.BigInt(1))
+      expect(mostSignificantBit(x)).toEqual(i - 1)
+    }
+  })
+
+  it('succeeds for MaxUint256', () => {
+    expect(mostSignificantBit(MAX_UINT256)).toEqual(255)
+  })
+
+  it('throws for MaxUint256 + 1', () => {
+    expect(() => mostSignificantBit(JSBI.add(MAX_UINT256, ONE))).toThrow('MAX')
+  })
+})
