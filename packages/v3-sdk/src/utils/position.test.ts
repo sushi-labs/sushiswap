c99f183683065e08476df7e2b9fb0ab9e79d5477
matthewlilley <hello@matthewlilley.com>
2023-03-27 20:25:19 +0100

    chore: port v3 sdk

---
+++ packages/v3-sdk/src/utils/position.test.ts
@@ -0,0 +1,26 @@
+import { JSBI } from '@sushiswap/math'
+
+import { ZERO } from '../internalConstants'
+import { PositionLibrary } from '.'
+
+describe('PositionLibrary', () => {
+  describe('#getTokensOwed', () => {
+    it('0', () => {
+      const [tokensOwed0, tokensOwed1] = PositionLibrary.getTokensOwed(ZERO, ZERO, ZERO, ZERO, ZERO)
+      expect(tokensOwed0).toEqual(ZERO)
+      expect(tokensOwed1).toEqual(ZERO)
+    })
+
+    it('non-0', () => {
+      const [tokensOwed0, tokensOwed1] = PositionLibrary.getTokensOwed(
+        ZERO,
+        ZERO,
+        JSBI.BigInt(1),
+        JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(128)),
+        JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(128))
+      )
+      expect(tokensOwed0).toEqual(JSBI.BigInt(1))
+      expect(tokensOwed1).toEqual(JSBI.BigInt(1))
+    })
+  })
+})
