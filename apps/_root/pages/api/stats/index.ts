9a74b82f0b6cc4241855b3c829883cc42bee2a28
LufyCZ <lufy@sushi.com>
2023-04-01 14:40:34 +0000

    chore(apps/root): purge sushi-data

--- apps/_root/pages/api/stats/index.ts
+++ apps/_root/pages/api/stats/index.ts
@@ -1,17 +1,25 @@
 import { formatNumber, formatUSD } from '@sushiswap/format'
-import sushiData from '@sushiswap/sushi-data'
 import stringify from 'fast-json-stable-stringify'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import getBentoTVL from '../../../functions/graph/fetchers/bentobox'
 import { getLegacyExchangeData } from '../../../functions/graph/fetchers/exchange'
 import { getTridentExchangeData } from '../../../functions/graph/queries/trident'
+import { SUSHI_ADDRESS } from '@sushiswap/currency'
+import { ChainId } from '@sushiswap/chain'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader('Cache-Control', 'public, s-maxage=900, stale-while-revalidate=3600')
 
+  const getSushiPriceUSD = async () => {
+    {
+      const prices = await fetch('https://token-price.sushi.com/v0/1').then((data) => data.json())
+      return prices[SUSHI_ADDRESS[ChainId.ETHEREUM].toLowerCase()]
+    }
+  }
+
   const [sushiPrice, bentoTVL, legacyExchangeData, tridentExchangeData] = await Promise.all([
-    sushiData.sushi.priceUSD(),
+    getSushiPriceUSD(),
     getBentoTVL(),
     getLegacyExchangeData(),
     getTridentExchangeData(),
