fb473fd63db0ea4f408d8f9e65c14b6c505cc84f
Ola Stenberg <hello@olastenberg.com>
2023-04-02 13:38:47 +0200

    fix(packages/router): add decimals on RToken

--- packages/router/src/liquidity-providers/NativeWrapProvider.ts
+++ packages/router/src/liquidity-providers/NativeWrapProvider.ts
@@ -19,7 +19,7 @@ export class NativeWrapProvider extends LiquidityProvider {
       name: native.name,
       symbol: native.symbol,
       chainId: chainId,
-      // decimals: 18,
+      decimals: 18,
     }
     const bridge = new BridgeUnlimited(WNATIVE_ADDRESS[chainId], nativeRToken, WNATIVE[chainId] as RToken, 0, 50_000)
     this.poolCodes = [new NativeWrapBridgePoolCode(bridge, LiquidityProviders.NativeWrap)]
