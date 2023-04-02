cf0c9f335a59d692d0ae4eeb188e7aeb5407b58c
matthewlilley <hello@matthewlilley.com>
2023-04-02 01:53:01 +0100

    chore: addresses and cleanup

--- packages/wagmi/future/hooks/pools/actions/getConcentratedPositionOwner.ts
+++ packages/wagmi/future/hooks/pools/actions/getConcentratedPositionOwner.ts
@@ -12,7 +12,7 @@ export const getConcentratedPositionOwners = async ({
       ({ tokenId, chainId }) =>
         ({
           chainId,
-          address: '0xC36442b4a4522E871399CD717aBDD847Ab11FE88',
+          address: '0xF0cBce1942A68BEB3d1b73F0dd86C8DCc363eF49',
           abi: [
             {
               inputs: [
