b8ce702320e69e21f89c54e772eca8b7ee916dac
Chilli Chelli <ramin@sushi.com>
2023-04-19 13:44:44 +0100

    fix(apps/root): add page layout

--- apps/_root/app/claims/rp2/page.tsx
+++ apps/_root/app/claims/rp2/page.tsx
@@ -18,39 +18,70 @@ const RP2ClaimPage = () => {
   return (
     <>
       <Header />
-      <Container className="mx-auto py-40 prose !prose-invert prose-slate">
+      <Container className="mx-auto py-10 prose !prose-invert prose-slate">
         <div className="flex flex-col">
-          <h1 className="text-2xl font-medium tracking-tight text-white md:text-3xl">Sushi RouteProcessor2 Claim</h1>
+          <h2>Sushi RouteProcessor2 Claim</h2>
+          <p className="font-bold">
+            Please revoke any token approvals you have on RouteProcessor2. Once you have revoked approvals, you will be
+            able to claim any lost funds resulting from the exploit.
+          </p>
+          <p>If your funds rest in the whitehat contract, they are claimable here.</p>
+          <List className="!pt-0 pb-3 relative">
+            <List.Control>
+              {!address ? (
+                <List.KeyValue flex title="No user connected">
+                  <ConnectButton size="xs" color="blue" hideChevron />
+                </List.KeyValue>
+              ) : claims.length > 0 ? (
+                claims.map(([chainId, claim]) => (
+                  <ClaimItem account={address} key={claim.index} claim={claim} chainId={chainId} />
+                ))
+              ) : (
+                <List.KeyValue flex title="No claims found">
+                  <span className="h-[28px]" />
+                </List.KeyValue>
+              )}
+            </List.Control>
+          </List>
           <p>
-            Firstly, please know that {"Sushi's"} Swap web app is safe to use now. You likely have no exposure if you{' '}
-            {"haven't"} interacted with Sushi in the past ten days, as the exploited contract is less than ten days old.
-            But check approvals with the link above as a good security practice.
+            If your funds do not reside in the whitehat address, you must submit an email to{' '}
+            <a href="mailto:security@sushi.com">security@sushi.com</a> or #open-a-ticket and include the transaction
+            ID(s) and blockchain(s) data for the lost funds.
           </p>
           <p>
-            If you do not see your claim here, your funds have been blackhacked. The Sushi team will establish a claim
-            process, which the user can opt-in to, and {"we'll"} manage claims on a case-by-case basis. More details
-            surrounding this process will come shortly.
+            Blackhat funds will take longer to process, as the team will verify the legitimacy against on-chain data
+            that validates them and will get paid accordingly.
           </p>
         </div>
-
-        <List className="pt-6 relative">
-          <List.Label>Claims found on every network</List.Label>
-          <List.Control>
-            {!address ? (
-              <List.KeyValue flex title="No user connected">
-                <ConnectButton size="xs" color="blue" hideChevron />
-              </List.KeyValue>
-            ) : claims.length > 0 ? (
-              claims.map(([chainId, claim]) => (
-                <ClaimItem account={address} key={claim.index} claim={claim} chainId={chainId} />
-              ))
-            ) : (
-              <List.KeyValue flex title="No claims found">
-                <span className="h-[28px]" />
-              </List.KeyValue>
-            )}
-          </List.Control>
-        </List>
+        <hr />
+        <h2>My funds were lost in this exploit. What can I do?</h2>
+        <p>
+          Part of the lost funds got swept by whitehat security teams and part of them are lost to blackhat hackers.
+        </p>
+        <ol>
+          <li>If your funds rest in the whitehat contract, they will be claimable from this page.</li>
+          <li>
+            If your funds do not reside in the whitehat address, you must submit an email to{' '}
+            <a href="mailto:security@sushi.com">security@sushi.com</a> or #open-a-ticket and include the transaction
+            ID(s) and blockchain(s) data for the lost funds.
+          </li>
+        </ol>
+        <hr />
+        <h2>What is RouteProcessor2 Vulnerability? What happened?</h2>
+        <p>
+          There was an approval bug in a Sushi contract called RouteProcessor2 and it was exploited by hackers. Money
+          was taken from users wallets. No other contract is affected. Sushi liquidity is safe.
+        </p>
+        <hr />
+        <h2>Can we use Sushi safely now?</h2>
+        <p>
+          The user interface has been updated to remove the affected contract. It is safe to trade and provide liquidity
+          on sushi.
+        </p>
+        <p>
+          If you have tokens approved for RouteProcessor2 on any network, you should revoke the approvals as soon as
+          possible.
+        </p>
       </Container>
     </>
   )
