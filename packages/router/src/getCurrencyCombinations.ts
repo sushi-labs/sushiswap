ba350ed9ec809caeabec014cd832fa42c389c8c2
matthewlilley <hello@matthewlilley.com>
2023-04-26 08:50:44 +0100

    fix: use same currency combo func for v3

--- packages/router/src/getCurrencyCombinations.ts
+++ packages/router/src/getCurrencyCombinations.ts
@@ -28,7 +28,7 @@ export function getCurrencyCombinations(chainId: ChainId, currencyA: Type, curre
     // token B against all bases
     ...bases.map((base): [Token, Token] => [tokenB, base]),
     // each base against all bases
-    ...basePairs, 
+    ...basePairs,
   ]
     .filter((tokens): tokens is [Token, Token] => Boolean(tokens[0] && tokens[1]))
     .filter(([t0, t1]) => t0.address !== t1.address)
@@ -46,28 +46,4 @@ export function getCurrencyCombinations(chainId: ChainId, currencyA: Type, curre
 
       return true
     })
-}
-
-
-export function getV3CurrencyCombinations(chainId: ChainId, currencyA: Type, currencyB: Type) {
-  const [tokenA, tokenB] = chainId ? [currencyA?.wrapped, currencyB?.wrapped] : [undefined, undefined]
-
-  const common = chainId in BASES_TO_CHECK_TRADES_AGAINST ? BASES_TO_CHECK_TRADES_AGAINST[chainId] : []
-
-
-  if (!tokenA || !tokenB) {
-    return []
-  }
-
-  return [
-    // the direct pair
-    [tokenA, tokenB],
-    // token A against all bases
-    ...common.map((common): [Token, Token] => [tokenA, common]),
-    // token B against all bases
-    ...common.map((common): [Token, Token] => [tokenB, common]),
-
-  ]
-    .filter((tokens): tokens is [Token, Token] => Boolean(tokens[0] && tokens[1]))
-    .filter(([t0, t1]) => t0.address !== t1.address)
 }
