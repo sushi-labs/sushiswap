b9add45f734176e7ea788b521e992f7103128f79
Ilya Lyalin <bretnwood@gmail.com>
2023-04-28 01:37:37 +0300

    fix: RP tests multicall viem=>ethers

--- protocols/route-processor/test/Router3.test.ts
+++ protocols/route-processor/test/Router3.test.ts
@@ -1,3 +1,4 @@
+import { Interface } from '@ethersproject/abi'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { erc20Abi, weth9Abi } from '@sushiswap/abi'
 import { bentoBoxV1Address, BentoBoxV1ChainId } from '@sushiswap/bentobox'
@@ -149,6 +150,68 @@ interface TestEnvironment {
   dataFetcher: DataFetcher
 }
 
+async function switchMulticallToEthers(client: any) {
+  //const oldFunction = client.multicall
+  const [user] = await ethers.getSigners()
+  const multicallContract = new Contract(
+    '0xca11bde05977b3631167028862be2a173976ca11',
+    [
+      'function aggregate3(tuple(address target, bool allowFailure, bytes callData)[] calls) payable returns (tuple(bool success, bytes returnData)[] returnData)',
+    ],
+    user
+  )
+  client.multicall = async (arg: any) => {
+    const ifaces: Interface[] = []
+    const calls = arg.contracts.map((call: any, i: number) => {
+      const iface = new ethers.utils.Interface(call.abi)
+      ifaces[i] = iface
+      const callData = iface.encodeFunctionData(call.functionName, call.args)
+      return {
+        target: call.address,
+        allowFailure: arg.allowFailure,
+        callData,
+      }
+    })
+
+    const result0 = await multicallContract.callStatic.aggregate3(calls, { gasLimit: 1_000_000_000 })
+    const result = result0.map((res: any, i: number) => {
+      try {
+        const result0 = ifaces[i].decodeFunctionResult(arg.contracts[i].functionName, res.returnData)
+        let result = []
+        for (let i = 0; i < result0.length; ++i) {
+          if (result0[i] === undefined) continue
+          result[i] = result0[i] instanceof BigNumber ? result0[i].toBigInt() : result0[i]
+        }
+        if (result.length == 1) result = result[0]
+        return {
+          result,
+          status: res.success ? 'success' : 'failure',
+        }
+      } catch (e) {
+        return {
+          result: undefined,
+          status: 'failure',
+        }
+      }
+    })
+
+    // correctness check
+    // const etalon = await oldFunction(arg)
+    // if (etalon.length !== result.length) console.error('Length wrong')
+    // etalon.forEach((e: any, j: number) => {
+    //   const r = result[j]
+    //   if (e.status !== r.status) console.error('Status wrong', j, e, r)
+    //   if (e.result instanceof Array) {
+    //     e.result.forEach((a, i) => {
+    //       if (a !== r.result[i]) console.error('Result wrong 1', j, i, e, r)
+    //     })
+    //   } else if (e.result !== r.result) console.error('Result wrong 0', j, e, r)
+    // })
+
+    return result
+  }
+}
+
 async function getTestEnvironment(): Promise<TestEnvironment> {
   //console.log('Prepare Environment:')
 
@@ -171,6 +234,7 @@ async function getTestEnvironment(): Promise<TestEnvironment> {
     },
     transport: custom(network.provider),
   })
+  await switchMulticallToEthers(client)
   //console.log('    Create DataFetcher ...')
   const provider = ethers.provider
   const chainId = network.config.chainId as ChainId
@@ -452,7 +516,7 @@ describe('End-to-end RouteProcessor3 test', async function () {
     const amountIn = getBigNumber(1000000 * 1e18)
     intermidiateResult[0] = amountIn
     intermidiateResult = await updMakeSwap(env, Native.onChain(chainId), token, intermidiateResult)
-    const permit = await makePermit(env, token, intermidiateResult[0])
+    const permit = await makePermit(env, token, intermidiateResult[0] as BigNumber)
     intermidiateResult = await updMakeSwap(
       env,
       token,
