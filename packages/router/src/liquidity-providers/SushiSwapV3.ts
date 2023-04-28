56dfced419c8232cba3bcf0c4f6f47b75006c351
matthewlilley <hello@matthewlilley.com>
2023-04-28 17:33:38 +0100

    chore: failing test for directional data fetcher bug

--- packages/router/src/liquidity-providers/SushiSwapV3.ts
+++ packages/router/src/liquidity-providers/SushiSwapV3.ts
@@ -11,17 +11,20 @@ export class SushiSwapV3Provider extends UniswapV3BaseProvider {
       [ChainId.FANTOM]: '0x7770978eED668a3ba661d51a773d3a992Fc9DDCB',
       [ChainId.OPTIMISM]: '0x9c6522117e2ed1fE5bdb72bb0eD5E3f2bdE7DBe0',
       [ChainId.POLYGON]: '0x917933899c6a5F8E37F31E19f92CdBFF7e8FF0e2',
+      [ChainId.ARBITRUM_NOVA]: '0xaa26771d497814E81D305c511Efbb3ceD90BF5bd',
     } as const
     const initCodeHash = {
       [ChainId.FANTOM]: '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54',
       [ChainId.OPTIMISM]: '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54',
       [ChainId.POLYGON]: '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54',
+      [ChainId.ARBITRUM_NOVA]: '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54',
     } as const
 
     const tickLens = {
       [ChainId.FANTOM]: '0xD75F5369724b513b497101fb15211160c1d96550',
       [ChainId.OPTIMISM]: '0x0367a647A68f304f2A6e453c25033a4249d7F2C6',
       [ChainId.POLYGON]: '0x9fdeA1412e50D78B25aCE4f96d35801647Fdf7dA',
+      [ChainId.ARBITRUM_NOVA]: '0xF60e5f4A44a510742457D8064ffd360B12d8D9AF',
     } as const
     super(chainId, web3Client, factory, initCodeHash, tickLens, databaseClient)
   }
