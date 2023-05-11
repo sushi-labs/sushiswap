963ed2cd1f0393269d0973d7b52c5f7eea849598
matthewlilley <hello@matthewlilley.com>
2023-05-11 06:11:28 +0100

    workflow(swap-e2e): remove only

--- apps/swap/test/pages/index.test.ts
+++ apps/swap/test/pages/index.test.ts
@@ -59,7 +59,7 @@ test.afterEach(async ({ page }) => {
   //
 })
 
-test.only('Wrap and unwrap', async ({ page }) => {
+test('Wrap and unwrap', async ({ page }) => {
   const inputBalance = page.getByTestId('swap-from-balance-button')
   const outputBalance = page.getByTestId('swap-to-balance-button')
 
