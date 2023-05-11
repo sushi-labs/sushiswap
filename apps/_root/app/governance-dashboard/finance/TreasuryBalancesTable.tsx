aaaf3c84348b1f455e85a6e1c324385c010b7dc9
0xIvan <0xivanivan@gmail.com>
2023-05-12 00:55:30 +0900

    Sort table on share

--- apps/_root/app/governance-dashboard/finance/TreasuryBalancesTable.tsx
+++ apps/_root/app/governance-dashboard/finance/TreasuryBalancesTable.tsx
@@ -4,15 +4,10 @@ import { ExternalLinkIcon } from '@heroicons/react/outline'
 import { Native, Token } from '@sushiswap/currency'
 import { shortenAddress } from '@sushiswap/format'
 import { Currency, ProgressBar, ProgressColor } from '@sushiswap/ui'
+import { ExternalLink } from '@sushiswap/ui/future/components/ExternalLink'
 import { GenericTable } from '@sushiswap/ui/future/components/table/GenericTable'
-import {
-  createColumnHelper,
-  getCoreRowModel,
-  getSortedRowModel,
-  SortingState,
-  useReactTable,
-} from '@tanstack/react-table'
-import React, { useState } from 'react'
+import { createColumnHelper, getCoreRowModel, getSortedRowModel, useReactTable } from '@tanstack/react-table'
+import React from 'react'
 
 import { formatNumber, TREASURY_ADDRESS, TreasuryBalance } from '../lib'
 
@@ -21,6 +16,7 @@ const columnHelper = createColumnHelper<TreasuryBalance>()
 const columns = [
   columnHelper.accessor('token', {
     header: 'Token',
+    enableSorting: false,
     cell: (info) => {
       const token = info.getValue()
       const currency = token
@@ -41,18 +37,22 @@ const columns = [
   }),
   columnHelper.accessor('portfolioShare', {
     header: 'Portfolio Share',
+
     cell: (info) => <ProgressBar progress={info.getValue()} color={ProgressColor.BLUE} className="w-[100px]" />,
   }),
   columnHelper.accessor('fiatBalance', {
     header: () => <div className="w-full text-right">Value</div>,
+    enableSorting: false,
     cell: (info) => <div className="w-full text-right">${formatNumber(+info.getValue())}</div>,
   }),
   columnHelper.accessor('balance', {
     header: () => <div className="w-full text-right">Amount</div>,
+    enableSorting: false,
     cell: (info) => <div className="w-full text-right">{formatNumber(+info.getValue())}</div>,
   }),
   columnHelper.accessor('fiatConversion', {
     header: () => <div className="w-full text-right">Price</div>,
+    enableSorting: false,
     cell: (info) => (
       <div className="w-full text-right">
         {(+info.getValue()).toLocaleString('EN', {
@@ -66,15 +66,12 @@ const columns = [
 ]
 
 export function TreasuryBalancesTable({ balances }: { balances: TreasuryBalance[] }) {
-  const [sorting, setSorting] = useState<SortingState>([{ id: 'portfolioShare', desc: true }])
-
   const table = useReactTable({
     data: balances,
     columns,
-    state: {
-      sorting,
+    initialState: {
+      sorting: [{ id: 'portfolioShare', desc: true }],
     },
-    onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
   })
@@ -83,13 +80,19 @@ export function TreasuryBalancesTable({ balances }: { balances: TreasuryBalance[
     <div className="rounded-lg bg-[#1A2031]">
       <div className="h-full w-full border-b border-slate-800 px-5 pt-5 pb-7">
         <h3 className="mt-3 text-xl font-semibold">Treasury Snapshot</h3>
-        <div className="flex items-center gap-2 hover:underline">
-          <span className="text-sm text-slate-400">Wallet Address: {shortenAddress(TREASURY_ADDRESS)}</span>
-          <ExternalLinkIcon className="h-4 w-4 text-slate-400" />
-        </div>
+        <ExternalLink
+          href={`https://etherscan.io/address/${TREASURY_ADDRESS}`}
+          className="group inline-flex text-sm text-slate-400"
+          endIcon={
+            <ExternalLinkIcon className="h-4 w-4 text-slate-400 group-hover:text-white group-focus:text-white group-active:text-white" />
+          }
+        >
+          <span>Wallet Address: {shortenAddress(TREASURY_ADDRESS)}</span>
+        </ExternalLink>
       </div>
       <GenericTable<TreasuryBalance>
         loading={false}
+        loadingOverlay={false}
         table={table}
         placeholder=""
         pageSize={Math.max(balances.length, 3)}
