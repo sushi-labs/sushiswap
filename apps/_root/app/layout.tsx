bc3b2f8effdb5833b159696704a72df007c71a14
matthewlilley <hello@matthewlilley.com>
2023-05-13 00:32:09 +0100

    fix: compilation

--- apps/_root/app/layout.tsx
+++ apps/_root/app/layout.tsx
@@ -14,7 +14,7 @@ import { QueryClientProvider } from 'components/QueryClientProvider'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html className="dark">
+    <html className="dark" lang="en">
       <Head>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1" />
