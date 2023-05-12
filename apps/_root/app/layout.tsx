9177b894941d3492a7b9ddb74bbf21a419f92b79
matthewlilley <hello@matthewlilley.com>
2023-05-12 22:55:55 +0100

    chore: reset to original layout

--- apps/_root/app/layout.tsx
+++ apps/_root/app/layout.tsx
@@ -12,16 +12,9 @@ import { Onramper } from '@sushiswap/wagmi/future/components'
 import { WagmiProvider } from '../components/WagmiProvider'
 import { QueryClientProvider } from '../components/QueryClientProvider'
 
-import { Inter } from 'next/font/google'
-
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-})
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${inter.className} dark`}>
+    <html className="dark" lang="en">
       <Head>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1" />
