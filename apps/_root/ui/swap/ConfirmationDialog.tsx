c0b8f6e3efc073c2cf3c55d28efc0f2e291a9d9a
matthewlilley <hello@matthewlilley.com>
2023-05-18 18:09:40 +0100

    fix: bug with enabled condition

--- apps/_root/ui/swap/ConfirmationDialog.tsx
+++ apps/_root/ui/swap/ConfirmationDialog.tsx
@@ -65,7 +65,7 @@ export const ConfirmationDialog: FC<ConfirmationDialogProps> = ({ children }) =>
     enabled:
       Boolean(trade?.writeArgs) &&
       appType === AppType.Swap &&
-      isRouteProcessorChainId(network0) &&
+      (isRouteProcessorChainId(network0) || isRouteProcessor3ChainId(network0)) &&
       approved &&
       trade?.route?.status !== 'NoWay',
     overrides: trade?.overrides,
