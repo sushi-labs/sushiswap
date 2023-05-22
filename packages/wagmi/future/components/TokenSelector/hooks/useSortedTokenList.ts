0b32342e55f61ee76e744a063c7b1c0255c3f7fb
Chilli Chelli <ramin@sushi.com>
2023-05-22 11:05:39 +0100

    fix(packages/wagmi): add explicit type

--- packages/wagmi/future/components/TokenSelector/hooks/useSortedTokenList.ts
+++ packages/wagmi/future/components/TokenSelector/hooks/useSortedTokenList.ts
@@ -40,7 +40,7 @@ export const useSortedTokenList = ({
     queryKey: ['sortedTokenList', { debouncedQuery, tokenMap, customTokenMap, balancesMap, pricesMap }],
     queryFn: async () => {
       const tokenMapValues = tokenMap ? Object.values(tokenMap) : []
-      const tokenMapIds = tokenMapValues ? tokenMapValues.map((el) => el.address) : []
+      const tokenMapIds: string[] = tokenMapValues ? tokenMapValues.map((el) => el.address) : []
       const customTokenMapValues = customTokenMap
         ? Object.values(customTokenMap).filter((el) => el.chainId === chainId && !tokenMapIds.includes(el.address))
         : []
