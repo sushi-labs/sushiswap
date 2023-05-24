c43055aaa0b18732d8ceedddb2b0cf2a52a5cf53
LufyCZ <lufy@sushi.com>
2023-05-24 15:50:18 +0000

    feat(apps/earn): concentrated selected %

---
+++ apps/earn/lib/hooks/usePoolsByTokenPair.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query'
+import { getPoolsByTokenPair } from 'lib/api'
+
+function usePoolsByTokenPair(tokenId0?: string, tokenId1?: string) {
+  return useQuery({
+    queryKey: ['poolsByTokenPair', tokenId0, tokenId1],
+    queryFn: () => {
+      if (!tokenId0 || !tokenId1) return []
+
+      return getPoolsByTokenPair(tokenId0.toLowerCase(), tokenId1.toLowerCase())
+    },
+    enabled: !!tokenId0 && !!tokenId1,
+  })
+}
+
+export { usePoolsByTokenPair }
