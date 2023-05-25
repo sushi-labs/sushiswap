47f3200797de5b7db94f944274746311cc128dfa
0xIvan <0xivanivan@gmail.com>
2023-05-25 16:27:54 +0900

    Render Tooltip only after mounted

--- apps/_root/app/governance-dashboard/components/InfoIconTooltip.tsx
+++ apps/_root/app/governance-dashboard/components/InfoIconTooltip.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import { InformationCircleIcon } from '@heroicons/react/24/outline'
+import { useIsMounted } from '@sushiswap/hooks'
 import { Tooltip } from '@sushiswap/ui/future/components/Tooltip'
 import React from 'react'
 
 export function InfoIconTooltip({ description }: { description: string }) {
-  return (
+  const isMounted = useIsMounted()
+  return isMounted ? (
     <Tooltip description={description}>
       <InformationCircleIcon className="h-4 w-4" strokeWidth={2} />
     </Tooltip>
-  )
+  ) : null
 }
