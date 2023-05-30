0f08374d5a4a66a3275a94daf48d0856255028a4
Chilli Chelli <ramin@sushi.com>
2023-05-30 17:25:18 +0100

    fix(apps/earn): filter test token

--- packages/react-query/src/hooks/rewards/useAngleRewards.ts
+++ packages/react-query/src/hooks/rewards/useAngleRewards.ts
@@ -44,23 +44,31 @@ export const angleRewardsSelect = (chainId: ChainId, data: Awaited<ReturnType<ty
         pools: Object.entries(data.pools).reduce<TransformedPools>((acc, [a, b]) => {
             acc[a] = {
                 ...b,
-                distributionData: b.distributionData.map(el => ({
-                    ...el,
-                    token: new Token({ chainId, address: el.token, symbol: el.tokenSymbol, decimals: 18})
-                })),
+                distributionData: b.distributionData.reduce<Array<Omit<z.infer<typeof angleRewardsPoolsValidator>['distributionData'][0], 'token'> & { token: Token}>>((acc, el) => {
+                    if (el.tokenSymbol !== 'aglaMerkl') {
+                        acc.push({
+                            ...el,
+                            token: new Token({ chainId, address: el.token, symbol: el.tokenSymbol, decimals: 18})
+                        })
+                    }
+
+                    return acc
+                }, []),
                 token0: new Token({ chainId, address: b.token0, symbol: b.tokenSymbol0, decimals: b.decimalToken0 }),
                 token1: new Token({ chainId, address: b.token1, symbol: b.tokenSymbol1, decimals: b.decimalToken1 }),
                 rewardsPerToken: Object.entries(b.rewardsPerToken).reduce<TransformedRewardsPerToken>((acc, [k, v]) => {
                     const token0 = new Token({chainId, address: k, decimals: v.decimals, symbol: v.symbol})
 
-                    acc[k] = {
-                        accumulatedSinceInception: Amount.fromRawAmount(token0, parseUnits(v.accumulatedSinceInception.toString(), v.decimals).toString()),
-                        breakdown: Object.entries(v.breakdown).reduce<Record<string, Amount<Token>>>((acc, [i, j]) => {
-                            acc[i] = Amount.fromRawAmount(token0, parseUnits(j.toString(), v.decimals).toString())
-                            return acc
-                        }, {}),
-                        symbol: v.symbol,
-                        unclaimed: Amount.fromRawAmount(token0, v.unclaimedUnformatted)
+                    if (token0.symbol !== 'aglaMerkl') {
+                        acc[k] = {
+                            accumulatedSinceInception: Amount.fromRawAmount(token0, parseUnits(v.accumulatedSinceInception.toString(), v.decimals).toString()),
+                            breakdown: Object.entries(v.breakdown).reduce<Record<string, Amount<Token>>>((acc, [i, j]) => {
+                                acc[i] = Amount.fromRawAmount(token0, parseUnits(j.toString(), v.decimals).toString())
+                                return acc
+                            }, {}),
+                            symbol: v.symbol,
+                            unclaimed: Amount.fromRawAmount(token0, v.unclaimedUnformatted)
+                        }
                     }
                     return acc
                 }, {})
