94dd935f8cae817e52bb1d5cb241c81d285d971b
matthewlilley <hello@matthewlilley.com>
2023-06-08 12:35:49 +0100

    chore: extract trident

--- protocols/trident/contracts/interfaces/IStablePoolFactory.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-pragma solidity >=0.8.0;
-
-import "./IMasterDeployerV2.sol";
-
-interface IStablePoolFactory {
-    function getDeployData() external view returns (bytes memory, IMasterDeployerV2);
-}
--- protocols/trident/contracts/interfaces/IWETH9.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-pragma solidity >=0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-interface IWETH9 is IERC20 {
-    function deposit() external payable;
-    function withdraw(uint256) external;
-}
