77771d052b8b2e2a582093e4fa3bd31c44550fc5
LufyCZ <lufy@sushi.com>
2023-06-09 19:37:10 +0000

    feat(apps/swap): thundercore banner

---
+++ apps/_root/ui/swap/widget/ThunderCoreBanner.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const ThunderCoreBanner = () => {
+  return (
+    <div className="rounded-[10px] py-4 px-6 bg-gradient-to-r from-[#2d68af] to-[#479dbb] space-y-3 mt-4">
+      <div className="space-y-1">
+        <div className="-skew-x-[8deg] text-sm font-bold">Join the Thundercore Trading Competition</div>
+        <div className="-skew-x-[8deg] text-lg font-black">Grab a Share of $5,500 in Token Airdrop!</div>
+      </div>
+      <div className="font-medium text-[9px] ">Campaign Period: 6/13 08:00 (UTC) — 6/21 08:00 (UTC)</div>
+    </div>
+  )
+}
