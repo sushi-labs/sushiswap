61eb0fe566fa57963a042b87f671f5eeebe43df2
matthewlilley <hello@matthewlilley.com>
2023-06-15 14:14:21 +0100

    refactor: partner

--- apps/_root/app/swap/providers.tsx
+++ apps/_root/app/swap/providers.tsx
@@ -3,13 +3,16 @@
 import { SwapProvider } from 'ui/swap/trade/TradeProvider'
 import { TokenProvider } from 'ui/swap/token/TokenProvider'
 import { SplashController } from '@sushiswap/ui/future/components/SplashController'
+import { ThemeProvider } from '@sushiswap/ui'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <TokenProvider>
-      <SplashController>
-        <SwapProvider>{children}</SwapProvider>
-      </SplashController>
-    </TokenProvider>
+    <ThemeProvider>
+      <TokenProvider>
+        <SplashController>
+          <SwapProvider>{children}</SwapProvider>
+        </SplashController>
+      </TokenProvider>
+    </ThemeProvider>
   )
 }
