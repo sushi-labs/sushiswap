d96e72e7bc9ea2f319eb71890a0976cc2bc31ae5
Ola Stenberg <hello@olastenberg.com>
2023-06-16 13:59:47 +0200

    fix(packages/wagmi): disable erc20 approval if write function is undefined

--- packages/wagmi/src/future/systems/Checker/ApproveERC20.tsx
+++ packages/wagmi/src/future/systems/Checker/ApproveERC20.tsx
@@ -54,11 +54,10 @@ export const Component: FC<ApproveERC20Props> = ({
   if (state === ApprovalState.APPROVED || !enabled) {
     return <>{children}</>
   }
-  console.log({state, contract, enabled})
   return (
     <Button
       as={as}
-      disabled={state !== ApprovalState.NOT_APPROVED || !contract }
+      disabled={state !== ApprovalState.NOT_APPROVED || !write }
       loading={[ApprovalState.UNKNOWN, ApprovalState.LOADING, ApprovalState.PENDING].includes(state)}
       testdata-id={id}
       variant={variant}
