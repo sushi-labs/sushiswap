3135df37e9ab36bbdd7843c3033478a701473c3b
Ola Stenberg <hello@olastenberg.com>
2023-06-16 14:24:57 +0200

    test(apps/furo): switch url from localhost to 127.0.0.1

--- apps/furo/test/pages/vesting/index.test.ts
+++ apps/furo/test/pages/vesting/index.test.ts
@@ -224,7 +224,7 @@ async function mockSubgraph(page: Page) {
 }
 
 async function increaseEvmTime(unix: number) {
-  const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545', CHAIN_ID)
+  const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545', CHAIN_ID)
   // await provider.send('evm_increaseTime', [unix])
   await provider.send('evm_mine', [unix])
 }
