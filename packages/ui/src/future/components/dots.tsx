3170e723d53048b4cdf0ce6c3038f0f7a897ae18
Chilli Chelli <ramin@sushi.com>
2023-06-20 15:59:52 +0100

    fix(packages/ui): use slot instead

--- packages/ui/src/future/components/dots.tsx
+++ packages/ui/src/future/components/dots.tsx
@@ -1,23 +1,29 @@
 import classNames from 'classnames'
 import * as React from 'react'
+import { Slot } from '@radix-ui/react-slot'
 
-interface DotsProps {
+export interface DotsProps extends React.ButtonHTMLAttributes<HTMLSpanElement> {
+  asChild?: boolean
   children?: React.ReactNode
   className?: string
 }
 
-const Dots = React.forwardRef<HTMLSpanElement, DotsProps>(({ className, children, ...props }, ref) => (
-  <span
-    ref={ref}
-    className={classNames(
-      "after:inline-block after:content-['.'] after:animate-ellipsis after:w-4 after:text-left",
-      className
-    )}
-    {...props}
-  >
-    {children}
-  </span>
-))
+const Dots = React.forwardRef<HTMLButtonElement, DotsProps>(({ className, asChild, children, ...props }, ref) => {
+  const Comp = asChild ? Slot : 'span'
+
+  return (
+    <Comp
+      ref={ref}
+      className={classNames(
+        "after:inline-block after:content-['.'] after:animate-ellipsis after:w-4 after:text-left",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </Comp>
+  )
+})
 
 Dots.displayName = 'Dots'
 
