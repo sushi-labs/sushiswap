2ec40156b58ebb79f3942f69690e10f389c5d47b
Ola Stenberg <hello@olastenberg.com>
2023-06-21 22:46:50 +0200

    WIP

--- packages/address-mapper/src/index.ts
+++ packages/address-mapper/src/index.ts
@@ -1,33 +1,80 @@
 
 
 export class AddressMapper {
-    private addresses: Map<string, Map<number, string>> = new Map()
 
-    constructor(...addressLists: Record<number, string>[]) {
-        this.initialize(addressLists)
+    // constructor(...addressLists: Record<number, string>[]) {
+    //     this.initialize(addressLists)
+    // }
+
+    //  static generate(...addressLists: Record<number, string>[] | Record<number, string[]>[]) {
+    //     const addresses: Record<string, Record<number, string[]>> = {}
+    //     for (const addressList of addressLists) {
+            
+    //         Object.entries(addressList).forEach(([chainId, address]) => {
+    //             const id = `${chainId}:${address.toLowerCase()}`
+    //             if (!addresses[id]) {
+    //                 addresses[id] = {}
+    //             }
+    //             for (const [chainId, address] of Object.entries(addressList)) {
+    //                 const currentId = `${chainId}:${address.toLowerCase()}`
+    //                 if (id !== currentId) {
+    //                     if (!addresses[id][Number(chainId)]) {
+    //                         addresses[id][Number(chainId)] = []
+    //                     }
+    //                     addresses[id][Number(chainId)].push(address.toLowerCase())
+    //                 }
+    //             }
+    //         })
+    //     }
+    //     return addresses
+    // }
+    static generate(addressLists: (Record<number, string[]> | Record<number, string>)[]): Record<string, Record<number, string[]>> {
+        const result: Record<string, Record<number, string[]>> = {};
+
+    for (const addressList of addressLists) {
+        
+            Object.entries(addressList).forEach(([chainId, addresses]) => {
+                const hehe = Array.isArray(addresses) ? addresses : [addresses]
+                
+        //     Object.entries(hehe).forEach(([chainId, add]) => {
+        //         const id = `${chainId}:${add.toLowerCase()}`
+        //         if (!result[id]) {
+        //             result[id] = {}
+        //         }
+        //         for (const [chainId, address] of Object.entries(addressList)) {
+        //             const currentId = `${chainId}:${address.toLowerCase()}`
+        //             if (id !== currentId) {
+        //                 if (!result[id][Number(chainId)]) {
+        //                     result[id][Number(chainId)] = []
+        //                 }
+        //                 result[id][Number(chainId)].push(address.toLowerCase())
+        //             }
+        //         }
+        //     })
+        //     })
+        // }
+        console.log({result})
+        return result;
     }
+    // getAddresses(chainId: number, address: string, filterChainIds?: number[]) {
+    //     const id = `${chainId}:${address.toLowerCase()}`
+    //     if (!addresses.has(id)) return undefined
+    //     return !filterChainIds ? addresses.get(id) : new Map([...(addresses.get(id)?.entries() ?? [])].filter(([key]) => filterChainIds.includes(key)))
+    // }
 
-    private initialize(addressLists: Record<number, string>[]) {
-        for (const addressList of addressLists) {
+    static merge(...addressLists: Record<number, string>[]) {
+        const merged: Record<number, string[]> = {}
+        for (const addressList of addressLists) {   
             Object.entries(addressList).forEach(([chainId, address]) => {
-                const id = `${chainId}:${address.toLowerCase()}`
-                if (!this.addresses?.has(id)) {
-                    this.addresses?.set(id, new Map())
-                }
-                for (const [chainId, address] of Object.entries(addressList)) {
-                    const currentId = `${chainId}:${address.toLowerCase()}`
-                    if (id !== currentId) {
-                        this.addresses.get(id)?.set(Number(chainId), address.toLowerCase())
-                    }
+                if (!merged[chainId]) {
+                    merged[chainId] = [address.toLowerCase()]
+                } else {
+                    merged[chainId].push(address.toLowerCase())
                 }
             })
         }
-    }
-
-    getAddresses(chainId: number, address: string, filterChainIds?: number[]) {
-        const id = `${chainId}:${address.toLowerCase()}`
-        if (!this.addresses.has(id)) return undefined
-        return !filterChainIds ? this.addresses.get(id) : new Map([...(this.addresses.get(id)?.entries() ?? [])].filter(([key]) => filterChainIds.includes(key)))
+        return merged
+    
     }
 
 }
--- packages/address-mapper/test/main.test.ts
+++ packages/address-mapper/test/main.test.ts
@@ -2,66 +2,103 @@
 import { describe, expect, test } from "vitest";
 import { AddressMapper } from "../src/index.js";
 
-const usdcList = {
+const usdc_e_List = {
     1: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
     4: "0x1717a0d5c8705ee89a8ad6e808268d6a826c97a4",
     3: "0x0d9c8723b343a8368bebe0b5e89273ff8d712e3c"
 }
-const sushiList = {
-    1: '0x6b3595068778dd592e39a122f4f5a5cf09c90fe2',
-    4: '0x0769fd68dfb93167989c6f7254cd0d766fb2841f',
-    3: '0x0769fd68dfb93167989c6f7254cd0d766fb2841f',
-}
 
-const addressMapper: AddressMapper = new AddressMapper(usdcList, sushiList);
+
 
 describe("AddressMapper", () => {
-    test("when sushi kovan address is given, return eth and ropsten addresses", async () => {
-        const expected = new Map([
-            [1, "0x6b3595068778dd592e39a122f4f5a5cf09c90fe2"],
-            [4, "0x0769fd68dfb93167989c6f7254cd0d766fb2841f"],
-        ])
-        expect(addressMapper.getAddresses(3, "0x0769fd68dfb93167989c6f7254cd0d766fb2841f")).toEqual(expected)
+
+    const usdc_b_List = {
+        1: "0x0000000000000000000000000000000000000101",
+    }
+    
+    const usdc_merged = AddressMapper.merge(usdc_e_List, usdc_b_List)
+
+
+    test("merge", async () => {
+        const expected = {
+            1: ["0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", "0x0000000000000000000000000000000000000101"],
+            4: ["0x1717a0d5c8705ee89a8ad6e808268d6a826c97a4"],
+            3: ["0x0d9c8723b343a8368bebe0b5e89273ff8d712e3c"]
+        }
+        expect(usdc_merged).toEqual(expected)
     });
 
-    test.each(
-        [
-            {
-                chainId: 1, address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", filter: [3], expected: new Map([
-                    [3, "0x0d9c8723b343a8368bebe0b5e89273ff8d712e3c"],
-                ])
-            },
-            {
-                chainId: 3, address: "0x0d9c8723b343a8368bebe0b5e89273ff8d712e3c", filter: [1, 4], expected: new Map([
-                    [1, "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"],
-                    [4, "0x1717a0d5c8705ee89a8ad6e808268d6a826c97a4"],
-                ])
-            },
-            {
-                chainId: 3, address: "0x0d9c8723b343a8368bebe0b5e89273ff8d712e3c", filter: [1337], expected: new Map()
-            },
-        ])('only return addresses from networks: $filter', ({ chainId, address, filter, expected }) => {
-            expect(addressMapper.getAddresses(chainId, address, filter)).toEqual(expected)
-        })
 
-
-    describe("Ignore lower case", () => {
-        const testList = {
-            1: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-            4: "0x1717A0D5C8705EE89A8aD6E808268D6A826C97A4",
+    test("when kovan address is given, return all eth and ropsten addresses", async () => {
+        const usdc_merged = AddressMapper.merge(usdc_e_List, usdc_b_List)
+    
+        const actual = AddressMapper.generate([usdc_merged])
+        const expected = {
+            '1:0x0000000000000000000000000000000000000101': {
+                1: ["0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"],
+                3: ["0x0d9c8723b343a8368bebe0b5e89273ff8d712e3c"],
+                4: ["0x1717a0d5c8705ee89a8ad6e808268d6a826c97a4"],
+        },
+            '1:0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+                1: ["0x0000000000000000000000000000000000000101"],
+                3: ["0x0d9c8723b343a8368bebe0b5e89273ff8d712e3c"],
+                4: ["0x1717a0d5c8705ee89a8ad6e808268d6a826c97a4"],
+        },
+            '3:0x0d9c8723b343a8368bebe0b5e89273ff8d712e3c': {
+                1: ["0x0000000000000000000000000000000000000101", "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"],
+                3: [],
+                4: ["0x1717a0d5c8705ee89a8ad6e808268d6a826c97a4"],
+        },
+            '4:0x1717a0d5c8705ee89a8ad6e808268d6a826c97a4': {
+                1: ["0x0000000000000000000000000000000000000101", "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"],
+                3: ["0x0d9c8723b343a8368bebe0b5e89273ff8d712e3c"],
+                4: [],
+            }
         }
-        const mapper: AddressMapper = new AddressMapper(testList);
+       
+        expect(actual).toEqual(expected)
+    });
 
-        test.each(
-            [{
-                address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
-            }, {
-                address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
-            }]
-        )("when getAddresses() is called with $address, case sensitivity is ignored and result is returned", async ({ address }) => {
-            expect(mapper.getAddresses(1, address)).toEqual(new Map([
-                [4, "0x1717a0d5c8705ee89a8ad6e808268d6a826c97a4"],
-            ]))
-        })
-    })
+    // test.each(
+    //     [
+    //         {
+    //             chainId: 1, address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", filter: [3], expected: new Map([
+    //                 [3, ["0x0d9c8723b343a8368bebe0b5e89273ff8d712e3c"]],
+    //             ])
+    //         },
+    //         {
+    //             chainId: 3, address: "0x0d9c8723b343a8368bebe0b5e89273ff8d712e3c", filter: [1, 4], expected: new Map([
+    //                 [1, ["0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", "0x0000000000000000000000000000000000000101"]],
+    //                 [4, ["0x1717a0d5c8705ee89a8ad6e808268d6a826c97a4"]],
+    //             ])
+    //         },
+    //         {
+    //             chainId: 3, address: "0x0d9c8723b343a8368bebe0b5e89273ff8d712e3c", filter: [1337], expected: new Map()
+    //         },
+    //     ])('only return addresses from networks: $filter', ({ chainId, address, filter, expected }) => {
+    //         const actual = mappings[`${chainId}:${address}`]
+    //         expect(actual).toEqual(expected)
+    //     })
+
+
+    // describe("Ignore lower case", () => {
+    //     const testList = {
+    //         1: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    //         4: "0x1717A0D5C8705EE89A8aD6E808268D6A826C97A4",
+    //     }
+    //     const mapper: AddressMapper = new AddressMapper(testList);
+
+    //     test.each(
+    //         [{
+    //             address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+    //         }, {
+    //             address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+    //         }]
+    //     )("when getAddresses() is called with $address, case sensitivity is ignored and result is returned", async ({ address }) => {
+    //         expect(mapper.getAddresses(1, address)).toEqual(new Map([
+    //             [4, ["0x1717a0d5c8705ee89a8ad6e808268d6a826c97a4"]],
+    //         ]))
+    //     })
+    // })
+    
 });
