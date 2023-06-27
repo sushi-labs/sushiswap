da12078f678ed502bb23b1e304166652b03ab045
ParthMinddeft <parth.talsaniya@minddeft.net>
2023-06-27 19:22:35 +0530

    token swap static login

--- apps/aptos/app/swap/page.tsx
+++ apps/aptos/app/swap/page.tsx
@@ -12,11 +12,12 @@ import React, { useEffect, useState } from 'react'
 import { SwitchAppType } from 'widget/SwitchAppType'
 import { WidgetTitleV2 } from 'widget/WidgetTitleV2'
 import { WalletSelector } from '@aptos-labs/wallet-adapter-ant-design'
+import { Network, Provider } from 'aptos'
 
 export default function SwapPage() {
   // const token0 = 'APTOS'
   // const token1 = 'SUSHI'
-  const { wallet, account, connected, isLoading, connect, wallets } = useWallet()
+  const { wallet, account, connected, isLoading, connect, wallets, signAndSubmitTransaction } = useWallet()
   const [token0, setToken0] = useState(DEFAULT_TOKEN_LIST.tokens[0])
   const [token1, setToken1] = useState(DEFAULT_TOKEN_LIST.tokens[1])
   const [open, setOpen] = useState<boolean>(false)
@@ -27,6 +28,9 @@ export default function SwapPage() {
   const [isLoadingPrice, setLoadingPrice] = useState<boolean>(true)
   // console.log(wallets)
   // return <WidgetTitleV2 />
+
+  const provider = new Provider(Network.TESTNET)
+
   useEffect(() => {
     setLoadingPrice(true)
     if (connected) {
@@ -55,10 +59,38 @@ export default function SwapPage() {
     }
   }, [account, inverse, connected])
 
+  const swapToken = async () => {
+    if (!account) return []
+    const payload = {
+      function: '0xe8c9cd6be3b05d3d7d5e09d7f4f0328fe7639b0e41d06e85e3655024ad1a79c2::router::swap_exact_input',
+      type_arguments: [
+        '0xe05d610ddad41a45e61b1327f01fcc0a582eedae00683ef969b85fa892c4b4f::usdt::Tether',
+        '0x5728d69f8a1c64b4cdb59f7746fe1a847215716e77c4bc90128c1100da826946::matic::Polygon',
+      ],
+      arguments: ['10000000000', '0'],
+      type: 'entry_function_payload',
+    }
+    try {
+      // sign and submit transaction to chain
+      const response = await signAndSubmitTransaction(payload)
+      // wait for transaction
+      await provider.waitForTransaction(response.hash)
+    } catch (error: any) {
+    } finally {
+    }
+  }
+
   return (
     <>
       <Container maxWidth={520} className="p-4 mx-auto mt-16 mb-[86px] flex flex-col gap-4">
         <Drawer.Root>
+        <button
+            onClick={() => {
+              swapToken()
+            }}
+          >
+            Test swap
+          </button>
           <WidgetTitleV2 />
           <SwitchAppType />
           <div>
