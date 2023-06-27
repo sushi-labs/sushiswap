6138545ff076be9fe15ef14109469d4252ef8c35
yash-khatri-minddeft <yash.khatri@minddeft.net>
2023-06-27 12:12:19 +0530

    dev: exceed balance issue resolved

--- apps/aptos/app/swap/page.tsx
+++ apps/aptos/app/swap/page.tsx
@@ -1,47 +1,47 @@
 'use client'
 
-import { WalletSelector } from '@aptos-labs/wallet-adapter-ant-design'
 import { useWallet } from '@aptos-labs/wallet-adapter-react'
-import { Dialog, Transition } from '@headlessui/react'
+// import { Dialog, Transition } from '@headlessui/react'
 import { Drawer } from '@sushiswap/ui'
 import Container from '@sushiswap/ui/future/components/Container'
 import SwapTrade from 'components/SwapTrade'
 import DEFAULT_TOKEN_LIST from './../../config/tokenList.json'
 import TokenListDialog from 'components/TokenListDialog'
 import TradeInput from 'components/TradeInput'
-import { disconnect } from 'process'
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { SwitchAppType } from 'widget/SwitchAppType'
-import { ThunderCoreBanner } from 'widget/ThunderCoreBanner'
 import { WidgetTitleV2 } from 'widget/WidgetTitleV2'
 
 export default function SwapPage() {
   // const token0 = 'APTOS'
   // const token1 = 'SUSHI'
+  const { wallet, account, connected, isLoading, connect, wallets } = useWallet()
   const [token0, setToken0] = useState(DEFAULT_TOKEN_LIST.tokens[0])
   const [token1, setToken1] = useState(DEFAULT_TOKEN_LIST.tokens[1])
   const [open, setOpen] = useState<boolean>(false)
   const [inverse, setInverse] = useState<boolean>(false)
   const [balance, setBalance] = useState<number>(0)
-  const { wallet, account, connected, isLoading, connect, wallets } = useWallet()
   const [buttonMessage, setBttonMessage] = useState<string>('Connect Wallet')
-  const [filteredCoin, setFilteredCoin] = useState<object>({})
+  const [filteredCoin, setFilteredCoin] = useState<object | undefined>({})
   const [isLoadingPrice, setLoadingPrice] = useState<boolean>(true)
   // console.log(wallets)
   // return <WidgetTitleV2 />
   useEffect(() => {
-    if (account) {
+    setLoadingPrice(true)
+    if (connected) {
+      setBttonMessage('Enter Amount')
       fetch(`https://fullnode.testnet.aptoslabs.com/v1/accounts/${account?.address}/resources`)
         .then((res) => res.json())
         .then((data) => {
-          console.log(data)
           const coinData = data.filter((coin: object) => {
             if (!inverse) {
-              console.log(token0.address)
               return coin.type.includes(token0.address)
             }
             return coin.type.includes(token1.address)
           })
+          if (coinData.length) {
+            setLoadingPrice(false)
+          }
           console.log(coinData[0])
           setFilteredCoin(coinData[0])
         })
@@ -52,13 +52,11 @@ export default function SwapPage() {
       setLoadingPrice(false)
       setFilteredCoin({})
     }
-    if (connected) {
-      setBttonMessage('Enter Amount')
-    }
   }, [account, inverse, connected])
+
   return (
     <>
-      <Container maxWidth={520} className="p-4 mx-auto mt-16 mb-[86px] flex flex-col gap-4">
+      <Container maxWidth={520} className="flex flex-col gap-4">
         <Drawer.Root>
           <WidgetTitleV2 />
           <SwitchAppType />
@@ -96,7 +94,7 @@ export default function SwapPage() {
                 className="btn w-full font-medium flex items-center justify-center gap-2 cursor-pointer transition-all bg-blue hover:bg-blue-600 active:bg-blue-700 text-white px-6 h-[52px] rounded-xl text-base font-semibold"
                 disabled={isLoading}
               >
-                {isLoading ? <>Loading...</> : buttonMessage}
+                {buttonMessage}
               </button>
             </div>
             {/* <ThunderCoreBanner /> */}
