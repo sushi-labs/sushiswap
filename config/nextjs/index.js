da1e996f2bfea7ea1737326559b09a6be3990305
matthewlilley <hello@matthewlilley.com>
2023-06-29 01:10:13 +0100

    chore: init modularize imports

--- config/nextjs/index.js
+++ config/nextjs/index.js
@@ -5,6 +5,23 @@ const defaultNextConfig = {
   swcMinify: false,
   poweredByHeader: false,
   staticPageGenerationTimeout: 180,
+  modularizeImports: {
+    '@heroicons/react/solid': {
+      transform: '@heroicons/react/solid/{{member}}',
+    },
+    '@heroicons-v1/react/solid': {
+      transform: '@heroicons-v1/react/solid/{{member}}',
+    },
+    '@heroicons/react/outline': {
+      transform: '@heroicons/react/outline/{{member}}',
+    },
+    '@heroicons-v1/react/outline': {
+      transform: '@heroicons-v1/react/outline/{{member}}',
+    },
+    lodash: {
+      transform: 'lodash/{{member}}',
+    },
+  },
   experimental: {
     esmExternals: 'loose',
   },
