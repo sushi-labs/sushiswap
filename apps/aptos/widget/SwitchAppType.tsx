461703ec82ff10fd303f51e85c0466d7e00cc638
ParthMinddeft <parth.talsaniya@minddeft.net>
2023-06-29 12:51:59 +0530

    icon change

--- apps/aptos/widget/SwitchAppType.tsx
+++ apps/aptos/widget/SwitchAppType.tsx
@@ -1,21 +1,21 @@
-import { Button } from "@sushiswap/ui/future/components/button";
-import { FC } from "react";
+import { Button } from '@sushiswap/ui/future/components/button'
+import { FC } from 'react'
 import { Tooltip } from '@sushiswap/ui/future/components/Tooltip'
-import NonSSRWrapper from "NonSSRWrapper";
+import NonSSRWrapper from 'NonSSRWrapper'
 
 export const SwitchAppType: FC = () => {
-    return (
-        <div className="flex gap-2">
-            <Button size="sm" variant="outlined" color="default">
-                Swap
-            </Button>
-            <NonSSRWrapper>
-                <Tooltip description="Coming soon!">
-                    <Button className="pointer-events-none opacity-40" size="sm" variant="empty" color="default">
-                        Limit
-                    </Button>
-                </Tooltip>
-            </NonSSRWrapper>
-        </div>
-    )
+  return (
+    <div className="flex gap-2">
+      <Button size="sm" variant="outlined" color="default">
+        Swap
+      </Button>
+      <NonSSRWrapper>
+        <Tooltip description="Coming soon!">
+          <Button className="pointer-events-none opacity-40" size="sm" variant="empty" color="default">
+            Limit
+          </Button>
+        </Tooltip>
+      </NonSSRWrapper>
+    </div>
+  )
 }
