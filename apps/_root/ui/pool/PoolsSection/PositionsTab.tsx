977bf0ce3c4045907862f1b70b3222cf79803797
LufyCZ <lufy@sushi.com>
2023-07-01 00:52:45 +0000

    feat(apps/earn): split v2 into v2, trident

--- apps/_root/ui/pool/PoolsSection/PositionsTab.tsx
+++ apps/_root/ui/pool/PoolsSection/PositionsTab.tsx
@@ -1,4 +1,5 @@
 import { Tab } from '@headlessui/react'
+import { Protocol } from '@sushiswap/database'
 import { Container } from '@sushiswap/ui/components/container'
 import { Switch } from '@sushiswap/ui/components/switch'
 import { Toggle } from '@sushiswap/ui/components/toggle'
@@ -14,7 +15,7 @@ export const PositionsTab = () => {
   return (
     <Container maxWidth="7xl" className="px-4 mx-auto">
       <Tab.Group defaultIndex={0} selectedIndex={tabPositions} onChange={setTabPositions}>
-        <div className="flex flex-wrap justify-between items-center">
+        <div className="flex flex-wrap items-center justify-between">
           <div className="flex items-center gap-3 mb-4">
             <Tab as={Fragment}>
               {({ selected }) => (
@@ -36,9 +37,25 @@ export const PositionsTab = () => {
                 </Toggle>
               )}
             </Tab>
+            <Tab as={Fragment}>
+              {({ selected }) => (
+                <Toggle size="sm" pressed={selected}>
+                  <span className="mt-1">🍱</span>
+                  <span>Trident Stable</span>
+                </Toggle>
+              )}
+            </Tab>
+            <Tab as={Fragment}>
+              {({ selected }) => (
+                <Toggle size="sm" pressed={selected}>
+                  <span className="mt-1">🍱</span>
+                  <span>Trident Classic</span>
+                </Toggle>
+              )}
+            </Tab>
           </div>
           <div className="flex gap-3 items-center px-2.5">
-            <span className="font-medium text-sm text-gray-600 dark:text-slate-400">Include closed</span>
+            <span className="text-sm font-medium text-gray-600 dark:text-slate-400">Include closed</span>
             <Switch checked={hide} onCheckedChange={() => setHide((prev) => !prev)} />
           </div>
         </div>
@@ -47,7 +64,13 @@ export const PositionsTab = () => {
             <ConcentratedPositionsTable hideClosed={hide} />
           </Tab.Panel>
           <Tab.Panel>
-            <PositionsTable />
+            <PositionsTable protocol={Protocol.SUSHISWAP_V2} />
+          </Tab.Panel>
+          <Tab.Panel>
+            <PositionsTable protocol={Protocol.BENTOBOX_STABLE} />
+          </Tab.Panel>
+          <Tab.Panel>
+            <PositionsTable protocol={Protocol.BENTOBOX_CLASSIC} />
           </Tab.Panel>
         </Tab.Panels>
       </Tab.Group>
--- apps/_root/ui/pool/PoolsSection/Tables/PositionsTable/PositionsTable.tsx
+++ apps/_root/ui/pool/PoolsSection/Tables/PositionsTable/PositionsTable.tsx
@@ -1,3 +1,4 @@
+import { Protocol } from '@sushiswap/database'
 import { useBreakpoint } from '@sushiswap/hooks'
 import { GenericTable } from '@sushiswap/ui/components/table/GenericTable'
 import { useAccount } from '@sushiswap/wagmi'
@@ -13,7 +14,11 @@ import { PositionQuickHoverTooltip } from './PositionQuickHoverTooltip'
 
 const COLUMNS = [NAME_COLUMN, VALUE_COLUMN, APR_COLUMN] as any
 
-export const PositionsTable: FC = () => {
+interface PositionsTableProps {
+  protocol: Protocol
+}
+
+export const PositionsTable: FC<PositionsTableProps> = ({ protocol }) => {
   const { address } = useAccount()
   const { isSm } = useBreakpoint('sm')
   const { isMd } = useBreakpoint('md')
@@ -24,17 +29,21 @@ export const PositionsTable: FC = () => {
   const { data: positions, isValidating } = useUserPositions({ id: address, chainIds: SUPPORTED_CHAIN_IDS })
 
   const _positions = useMemo(() => {
+    if (!positions) return []
+
     const _tokenSymbols = tokenSymbols?.filter((el) => el !== '') || []
-    return (positions || [])
-      ?.filter((el) => chainIds.includes(el.chainId))
-      .filter((el) =>
-        _tokenSymbols.length > 0
-          ? _tokenSymbols.some((symbol) => {
-              return [el.pool?.token0.symbol, el.pool?.token1.symbol].includes(symbol.toUpperCase())
-            })
-          : true
-      )
-  }, [chainIds, tokenSymbols, positions])
+    const searchFiltered = positions.filter((el) =>
+      _tokenSymbols.length > 0
+        ? _tokenSymbols.some((symbol) => {
+            return [el.pool?.token0.symbol, el.pool?.token1.symbol].includes(symbol.toUpperCase())
+          })
+        : true
+    )
+    const chainFiltered = searchFiltered.filter((el) => chainIds.includes(el.chainId))
+    const protocolFiltered = chainFiltered.filter((el) => el.pool?.protocol === protocol)
+
+    return protocolFiltered
+  }, [positions, tokenSymbols, chainIds, protocol])
 
   const table = useReactTable<PositionWithPool>({
     data: _positions,
