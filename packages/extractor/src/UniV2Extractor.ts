e258a69853e2d35fdce9b5b44da01586a711d0da
Ilya Lyalin <bretnwood@gmail.com>
2023-07-02 19:26:08 +0300

    feat: ExtractorV2: +test

--- packages/extractor/src/UniV2Extractor.ts
+++ packages/extractor/src/UniV2Extractor.ts
@@ -1,7 +1,7 @@
 import { getReservesAbi } from '@sushiswap/abi'
 import { computePairAddress } from '@sushiswap/amm'
 import { Token } from '@sushiswap/currency'
-import { ConstantProductPoolCode, LiquidityProviders, PoolCode } from '@sushiswap/router'
+import { ConstantProductPoolCode, LiquidityProviders } from '@sushiswap/router'
 import { ConstantProductRPool, RToken } from '@sushiswap/tines'
 import { BigNumber } from 'ethers'
 import { Address, decodeEventLog, Log, parseAbiItem, PublicClient } from 'viem'
@@ -11,7 +11,7 @@ import { LogFilter } from './LogFilter'
 import { MultiCallAggregator } from './MulticallAggregator'
 import { warnLog } from './WarnLog'
 
-export interface Factory {
+export interface FactoryV2 {
   address: Address
   provider: LiquidityProviders
   fee: number
@@ -26,10 +26,11 @@ interface PoolState {
 const UniV2EventsAbi = [parseAbiItem('event Sync(uint112 reserve0, uint112 reserve1)')]
 
 export class UniV2Extractor {
-  factories: Factory[]
+  factories: FactoryV2[]
   client: PublicClient
   multiCallAggregator: MultiCallAggregator
   poolMap: Map<Address, PoolState> = new Map()
+  unexistedPools: Set<Address> = new Set()
   logFilter: LogFilter
   logging: boolean
   busyCounter: Counter
@@ -37,7 +38,7 @@ export class UniV2Extractor {
   /// @param client
   /// @param factories list of supported factories
   /// @param logging to write logs in console or not
-  constructor(client: PublicClient, factories: Factory[], logging = true) {
+  constructor(client: PublicClient, factories: FactoryV2[], logging = true) {
     this.client = client
     this.multiCallAggregator = new MultiCallAggregator(client)
     this.factories = factories
@@ -52,7 +53,9 @@ export class UniV2Extractor {
         let eventUnknown = 0
         let eventRemoved = 0
         logs.forEach((l) => {
-          const poolState = this.poolMap.get(l.address.toLowerCase() as Address)
+          const addr = l.address.toLowerCase() as Address
+          this.unexistedPools.delete(addr)
+          const poolState = this.poolMap.get(addr)
           if (!poolState) {
             ++eventUnknown
             return
@@ -78,7 +81,7 @@ export class UniV2Extractor {
           eventRemoved > 0 ? `${eventRemoved} removed` : '',
         ]
           .filter((e) => e !== '')
-          .join(',')
+          .join(', ')
 
         this.consoleLog(`Block ${blockNumber} ${logs.length} logs (${eventInfo}) jobs ${this.busyCounter.counter}`)
       } else {
@@ -88,6 +91,8 @@ export class UniV2Extractor {
       }
     })
   }
+
+  async start() {}
 
   async updatePoolState(poolState: PoolState) {
     poolState.isUpdated = false
@@ -99,50 +104,62 @@ export class UniV2Extractor {
     poolState.isUpdated = true
   }
 
-  async addPoolsForTokens(tokens: Token[]) {
-    const promises: Promise<ConstantProductPoolCode>[] = []
-    for (let i = 0, promiseIndex = 0; i < tokens.length; ++i) {
+  getPoolsForTokens(tokens: Token[]): {
+    prefetchedPools: ConstantProductPoolCode[]
+    fetchingPools: Promise<ConstantProductPoolCode[]>
+  } {
+    const prefetchedPools: ConstantProductPoolCode[] = []
+    const waitPools: Promise<ConstantProductPoolCode | undefined>[] = []
+    for (let i = 0; i < tokens.length; ++i) {
       for (let j = i + 1; j < tokens.length; ++j) {
+        const [t0, t1] = tokens[i].sortsBefore(tokens[j]) ? [tokens[i], tokens[j]] : [tokens[j], tokens[i]]
         this.factories.forEach((factory) => {
-          const [t0, t1] = tokens[i].sortsBefore(tokens[j]) ? [tokens[i], tokens[j]] : [tokens[j], tokens[i]]
           const addr = computePairAddress({
             factoryAddress: factory.address,
             tokenA: t0,
             tokenB: t1,
             initCodeHashManualOverride: factory.initCodeHash,
           }) as Address
-          const poolState = this.poolMap.get(addr.toLowerCase() as Address)
-          if (!poolState) {
-            promises[promiseIndex++] = this.multiCallAggregator
-              .callValue(addr, getReservesAbi, 'getReserves')
-              .then((reserves) => {
-                // TODO: keep a set of unexisted pools
-                const [reserve0, reserve1] = reserves as [bigint, bigint]
-                const pool = new ConstantProductRPool(
-                  addr,
-                  t0 as RToken,
-                  t1 as RToken,
-                  factory.fee,
-                  BigNumber.from(reserve0),
-                  BigNumber.from(reserve1)
-                )
-                const poolState: PoolState = {
-                  poolCode: new ConstantProductPoolCode(pool, factory.provider, factory.provider),
-                  isUpdated: true,
-                }
-                this.poolMap.set(addr.toLowerCase() as Address, poolState)
-                return poolState.poolCode
-              })
-          } else {
-            promises[promiseIndex++] = Promise.resolve(poolState.poolCode)
+          const addrL = addr.toLowerCase() as Address
+          const poolState = this.poolMap.get(addrL)
+          if (poolState) {
+            prefetchedPools.push(poolState.poolCode)
+            return
           }
+          if (this.unexistedPools.has(addrL)) return
+          const promise = this.multiCallAggregator.callValue(addr, getReservesAbi, 'getReserves').then(
+            (reserves) => {
+              const [reserve0, reserve1] = reserves as [bigint, bigint]
+              const pool = new ConstantProductRPool(
+                addr,
+                t0 as RToken,
+                t1 as RToken,
+                factory.fee,
+                BigNumber.from(reserve0),
+                BigNumber.from(reserve1)
+              )
+              const poolState: PoolState = {
+                poolCode: new ConstantProductPoolCode(pool, factory.provider, factory.provider),
+                isUpdated: true,
+              }
+              this.poolMap.set(addrL, poolState)
+              return poolState.poolCode
+            },
+            () => {
+              this.unexistedPools.add(addrL)
+              return undefined
+            }
+          )
+          waitPools.push(promise)
         })
       }
     }
-
-    const result = await Promise.allSettled(promises)
-
-    return result.map((r) => (r.status == 'fulfilled' ? r.value : undefined)).filter((r) => r !== undefined)
+    return {
+      prefetchedPools,
+      fetchingPools: Promise.all(waitPools).then(
+        (pools) => pools.filter((p) => p !== undefined) as ConstantProductPoolCode[]
+      ),
+    }
   }
 
   consoleLog(log: string) {
--- packages/extractor/src/index.ts
+++ packages/extractor/src/index.ts
@@ -1,3 +1,4 @@
 export * from './LogFilter'
 export * from './MulticallAggregator'
+export * from './UniV2Extractor'
 export * from './UniV3Extractor'
