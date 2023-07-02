479943822d73e54dc0571477d70da4c483b7319d
matthewlilley <hello@matthewlilley.com>
2023-07-02 21:17:53 +0100

    feat: init storybook

---
+++ apps/docs/.storybook/main.ts
@@ -0,0 +1,44 @@
+// .storybook/main.ts
+
+// Replace your-framework with the framework you are using (e.g., react-vite, vue3-vite)
+import type { StorybookConfig } from '@storybook/react-vite'
+
+import path from 'path'
+import { mergeConfig } from 'vite'
+
+const config: StorybookConfig = {
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.tsx', '../stories/**/*.stories.ts'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-mdx-gfm'],
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  typescript: {
+    reactDocgen: 'react-docgen', // 👈 react-docgen configured here.
+  },
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+
+  async viteFinal(config, { configType }) {
+    // customize the Vite config here
+    return mergeConfig(config, {
+      optimizeDeps: {
+        include: ['@sushiswap/chain', '@sushiswap/ui'],
+      },
+      define: {
+        'process.env': {},
+      },
+      resolve: {
+        alias: [
+          {
+            find: '@sushiswap/ui',
+            replacement: path.resolve(__dirname, '../../../packages/ui/'),
+          },
+        ],
+      },
+    })
+  },
+}
+
+export default config
---
+++ apps/docs/stories/button.stories.tsx
@@ -0,0 +1,21 @@
+// Button.stories.ts|tsx
+
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button } from '@sushiswap/ui'
+import * as React from 'react'
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+}
+
+export default meta
+type Story = StoryObj<typeof Button>
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/react/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+  render: () => <Button />,
+}
