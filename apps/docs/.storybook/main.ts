d02bf014459da21d7fa51e452c7162a03ee21454
Chilli Chelli <ramin@sushi.com>
2023-07-03 16:26:14 +0100

    feat(apps/docs): add chromatic

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -14,7 +14,6 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-docs',
-    'storybook-addon-react-docgen',
     {
       name: '@storybook/addon-styling',
       options: {
