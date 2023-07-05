a5ee1a154e07b61fa81027e6f8b38fff87f646f3
0xIvan <0xivanivan@gmail.com>
2023-07-05 15:25:17 +0800

    Don't remove ring on focus

--- apps/evm/app/governance-dashboard/components/FilterButton.tsx
+++ apps/evm/app/governance-dashboard/components/FilterButton.tsx
@@ -6,8 +6,8 @@ export function FilterButton(props: { children: ReactNode; isActive: boolean; on
   return (
     <button
       className={classNames(
-        'h-10 rounded-full px-4 text-sm font-medium ring-1 ring-blue/40 dark:ring-slate-700/40 transition ease-in-out text-blue dark:text-gray-50',
-        isActive ? 'bg-blue/10 dark:bg-slate-700/40' : 'hover:ring-blue-600 dark:hover:ring-slate-600'
+        'h-10 rounded-full px-4 text-sm font-medium focus:ring-1 hover:ring-blue-600 dark:hover:ring-slate-600 ring-1 ring-blue/40 dark:ring-slate-700/40 transition ease-in-out text-blue dark:text-gray-50',
+        isActive && 'bg-blue/10 dark:bg-slate-700/40'
       )}
       onClick={onClick}
     >
