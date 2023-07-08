10500a2e5dc0a0e9e3837ea591c9cdbbbdb1f0dc
LufyCZ <lufy@sushi.com>
2023-07-08 22:43:56 +0000

    feat(apps/pool): add outofrangeinc. to reward table

--- apps/evm/ui/pool/RewardsSection/Tables/RewardsTableV3/RewardsTableV3RowPopover.tsx
+++ apps/evm/ui/pool/RewardsSection/Tables/RewardsTableV3/RewardsTableV3RowPopover.tsx
@@ -28,7 +28,7 @@ export const RewardsTableV3RowPopover: FC<RewardTableV3CellProps> = ({ row }) =>
           </Badge>
         </div>
         <div className="flex flex-col items-baseline gap-[1px]">
-          <span className="text-sm font-medium flex items-baseline gap-1 text-gray-900 dark:text-slate-50">
+          <span className="flex items-baseline gap-1 text-sm font-medium text-gray-900 dark:text-slate-50">
             {unwrapToken(row.token0).symbol} <span className="font-normal text-gray-900 dark:text-slate-500">/</span>{' '}
             {unwrapToken(row.token1).symbol}
             <span className="text-xs text-gray-500 dark:text-slate-500">{row.poolFee}%</span>
@@ -40,71 +40,89 @@ export const RewardsTableV3RowPopover: FC<RewardTableV3CellProps> = ({ row }) =>
       </div>
       <List className="mt-4">
         <List.Control className="max-h-[300px] overflow-y-scroll scroll">
-          {ongoingFarms.map(({ start, end, amount, token, propFees, propToken0, propToken1 }, i) => (
-            <>
-              <List.Label className="!text-[10px] !px-4 pt-4 uppercase font-semibold !text-gray-400 !dark:text-slate-500">
-                Farm {i + 1}
-              </List.Label>
-              <List.KeyValue
-                flex
-                className="!items-start"
-                title={
-                  <div className="flex gap-1 items-center">
-                    Reward
-                    <Explainer>
-                      Your share calculated as your LP divided by the total LP in the pool multiplied by the rewards set
-                      per day.
-                    </Explainer>
+          {ongoingFarms.map(
+            ({ start, end, amount, token, propFees, propToken0, propToken1, isOutOfRangeIncentivized }, i) => (
+              <>
+                <List.Label className="!text-[10px] !px-4 pt-4 uppercase font-semibold !text-gray-400 !dark:text-slate-500">
+                  Farm {i + 1}
+                </List.Label>
+                <List.KeyValue
+                  flex
+                  className="!items-start"
+                  title={
+                    <div className="flex items-center gap-1">
+                      My Reward
+                      <Explainer>
+                        Your share calculated as your LP divided by the total LP in the pool multiplied by the rewards
+                        set per day.
+                      </Explainer>
+                    </div>
+                  }
+                  subtitle="per day"
+                >
+                  <div className="flex items-center gap-2">
+                    <Currency.Icon currency={token} width={18} height={18} />
+                    {rewardPerDay({
+                      start,
+                      end,
+                      amount,
+                      tvl: row.tvl ?? 0,
+                      userTVL: row.userTVL ?? 0,
+                      token,
+                    })?.toSignificant(6)}{' '}
+                    {unwrapToken(token).symbol}
                   </div>
-                }
-                subtitle="per day"
-              >
-                <div className="flex items-center gap-2">
-                  <Currency.Icon currency={token} width={18} height={18} />
-                  {rewardPerDay({
-                    start,
-                    end,
-                    amount,
-                    tvl: row.tvl ?? 0,
-                    userTVL: row.userTVL ?? 0,
-                    token,
-                  })?.toSignificant(6)}{' '}
-                  {unwrapToken(token).symbol}
-                </div>
-              </List.KeyValue>
-              <List.KeyValue className="!items-start" flex title="Duration">
-                <div className="flex flex-col">
-                  <span className="font-mediumt">{Math.floor((end - Date.now() / 1000) / 3600 / 24)} days left</span>
-                  <span className="text-xs text-gray-500 dark:text-slate-500">
-                    Ends at: {format(end * 1000, 'dd MMM yyyy hh:mm')}
-                  </span>
-                </div>
-              </List.KeyValue>
-              <List.KeyValue
-                flex
-                className="!items-start"
-                title={
-                  <div className="flex gap-1 items-center">
-                    Details
-                    <Explainer>
-                      Weight that fees earned by positions represent in their rewards score. A higher % means that more
-                      rewards will be attributed to positions that earn more fees during the distribution.
-                    </Explainer>
+                </List.KeyValue>
+                <List.KeyValue className="!items-start" flex title="Duration">
+                  <div className="flex flex-col">
+                    <span className="font-mediumt">{Math.floor((end - Date.now() / 1000) / 3600 / 24)} days left</span>
+                    <span className="text-xs text-gray-500 dark:text-slate-500">
+                      Ends at: {format(end * 1000, 'dd MMM yyyy hh:mm')}
+                    </span>
                   </div>
-                }
-                subtitle="Reward weights (%)"
-              >
-                <div className="flex flex-col">
-                  <span className="font-medium">
-                    {propFees}% / {propToken0}% / {propToken1}%
-                  </span>
-                  <span className="flex gap-1 items-center text-xs text-gray-500 dark:text-slate-500">
-                    Fees / {row.token0.symbol} / {row.token1.symbol}
-                  </span>
-                </div>
-              </List.KeyValue>
-            </>
-          ))}
+                </List.KeyValue>
+                <List.KeyValue
+                  flex
+                  className="!items-start"
+                  title={
+                    <div className="flex items-center gap-1">
+                      Details
+                      <Explainer>
+                        Weight that fees earned by positions represent in their rewards score. A higher % means that
+                        more rewards will be attributed to positions that earn more fees during the distribution.
+                      </Explainer>
+                    </div>
+                  }
+                  subtitle="Reward weights (%)"
+                >
+                  <div className="flex flex-col">
+                    <span className="font-medium">
+                      {propFees}% / {propToken0}% / {propToken1}%
+                    </span>
+                    <span className="flex items-center gap-1 text-xs text-gray-500 dark:text-slate-500">
+                      Fees / {row.token0.symbol} / {row.token1.symbol}
+                    </span>
+                  </div>
+                </List.KeyValue>
+                <List.KeyValue
+                  flex
+                  className="!items-start"
+                  title={
+                    <div className="flex items-center gap-1">
+                      Out of range Liquidity
+                      <Explainer>This farm does not incentivize out of range liquidity.</Explainer>
+                    </div>
+                  }
+                >
+                  <div className="flex flex-col">
+                    <span className="font-medium">
+                      {isOutOfRangeIncentivized ? 'Incentivized' : 'Not Incentivized'}
+                    </span>
+                  </div>
+                </List.KeyValue>
+              </>
+            )
+          )}
         </List.Control>
       </List>
     </div>
