8c9b1e2ea01d3c48784a43609cb5ac87ae6194d0
Chilli Chelli <ramin@sushi.com>
2023-07-19 14:23:27 +0100

    feat(packages/ui): simplify animation

--- packages/ui/src/components/splash-controller.tsx
+++ packages/ui/src/components/splash-controller.tsx
@@ -6,12 +6,10 @@ const Splash: FC = () => {
   return (
     <div className="fixed inset-0 bg-gray-100 dark:bg-slate-900 z-[1080] flex items-center justify-center">
       <div className="absolute top-[calc(50%-20px)] left-[calc(50%-20px)]">
-        <div className="rotate-45">
-          <div className="w-[50px] h-[50px] animate-walk absolute top-[-15px] left-[15px]  overflow-hidden">
-            <SushiIcon width={50} height={50} className="rotate-[-45deg]" />
-          </div>
+        <div className="w-[50px] h-[50px] animate-[bounce_.5s_linear_infinite_0.17s] absolute">
+          <SushiIcon width={50} height={50} />
         </div>
-        <div className="w-[50px] h-[5px] bg-black opacity-20 absolute top-[80px] left-0 rounded-[50%] animate-shadow" />
+        <div className="w-[50px] h-[5px] bg-black opacity-20 absolute top-[51px] left-0 rounded-[50%] animate-shadow" />
       </div>
     </div>
   )
