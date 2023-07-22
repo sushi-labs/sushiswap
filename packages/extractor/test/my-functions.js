5395c1e2bec2be971c8957c8ecdb96fb556370e7
matthewlilley <hello@matthewlilley.com>
2023-07-22 06:52:19 +0100

    test: polygon

--- packages/extractor/test/my-functions.js
+++ packages/extractor/test/my-functions.js
@@ -4,17 +4,18 @@ const {
   USDT,
   SUSHI,
   WBTC,
+  DAI,
   // eslint-disable-next-line @typescript-eslint/no-var-requires
 } = require('@sushiswap/currency')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { ChainId } = require('@sushiswap/chain')
 
 function setQuery(context, events, done) {
-  const chainIds = [ChainId.ETHEREUM, ChainId.ARBITRUM, ChainId.OPTIMISM]
+  const chainIds = [ChainId.ETHEREUM, ChainId.ARBITRUM, ChainId.OPTIMISM.ChainId.POLYGON]
   const chainId = chainIds[Math.floor(Math.random() * chainIds.length)]
   const tokensIn = [Native.onChain(chainId)]
   const tokenIn = tokensIn[Math.floor(Math.random() * tokensIn.length)]
-  const tokensOut = [WBTC[chainId], USDC[chainId], USDT[chainId], SUSHI[chainId]]
+  const tokensOut = [WBTC[chainId], DAI[chainId], USDC[chainId], USDT[chainId], SUSHI[chainId]]
   const tokenOut = tokensOut[Math.floor(Math.random() * tokensOut.length)]
 
   context.vars['query'] = {
