0fc3e9f0cddcb68574e072497e9683aeb26a6a83
LufyCZ <lufy@sushi.com>
2023-07-28 17:24:08 +0000

    refactor(packages/math): BigNumber -> bigint

--- packages/math/src/functions/minimum.ts
+++ packages/math/src/functions/minimum.ts
@@ -1,15 +1,13 @@
-import JSBI from 'jsbi'
-
 /**
  * Returns the smallest member of the array
  * @param values the values from which the smallest gets returned
  * @returns the smallest memmber of the array
  */
-export function minimum(...values: JSBI[]): JSBI {
+export function minimum(...values: bigint[]): bigint {
   let lowest = values[0]
   for (let i = 1; i < values.length; i++) {
     const value = values[i]
-    if (JSBI.LT(value, lowest)) {
+    if (value < lowest) {
       lowest = value
     }
   }
