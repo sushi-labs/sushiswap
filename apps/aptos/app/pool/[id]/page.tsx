cd7fd64ae13894a6897d16dcd27d00b9f2e80172
yash-khatri-minddeft <yash.khatri@minddeft.net>
2023-07-28 14:39:51 +0530

    pool page

--- apps/aptos/app/pool/[id]/page.tsx
+++ apps/aptos/app/pool/[id]/page.tsx
@@ -25,31 +25,34 @@ const _Pool = () => {
   const [chainId, ...address] = decodeURIComponent(router?.id).split(':')
   const tokenAddress = address.join(':')
   const { network } = useWallet()
-  const { data: pool } = usePool(network?.name.toLowerCase(), tokenAddress)
+  const { data: pool } = usePool(Number(chainId), tokenAddress)
   console.log(pool)
+
   return (
     <>
-      <Layout>
-        <div className="flex flex-col gap-9">
-          <div className="flex flex-col lg:grid lg:grid-cols-[568px_auto] gap-12">
-            <div className="flex flex-col order-1 gap-9">
-              <PoolHeader />
-              <hr className="my-3 border-t border-gray-900/5 dark:border-slate-200/5" />
-              <PoolComposition />
-            </div>
-            <div className="flex flex-col order-2 gap-4">
-              <AppearOnMount>
-                <div className="flex flex-col gap-10">
-                  <PoolPosition />
+      {pool && (
+        <Layout>
+          <div className="flex flex-col gap-9">
+            <div className="flex flex-col lg:grid lg:grid-cols-[568px_auto] gap-12">
+              <div className="flex flex-col order-1 gap-9">
+                <PoolHeader />
+                <hr className="my-3 border-t border-gray-900/5 dark:border-slate-200/5" />
+                <PoolComposition />
+              </div>
+              <div className="flex flex-col order-2 gap-4">
+                <AppearOnMount>
+                  <div className="flex flex-col gap-10">
+                    <PoolPosition />
+                  </div>
+                </AppearOnMount>
+                <div className="hidden lg:flex">
+                  <PoolButtons />
                 </div>
-              </AppearOnMount>
-              <div className="hidden lg:flex">
-                <PoolButtons />
               </div>
             </div>
           </div>
-        </div>
-      </Layout>
+        </Layout>
+      )}
     </>
   )
 }
--- apps/aptos/utils/usePool.ts
+++ apps/aptos/utils/usePool.ts
@@ -2,21 +2,29 @@ import { useQuery } from '@tanstack/react-query'
 import { Pool } from './usePools'
 const MAINNET_CONTRACT = process.env['MAINNET_CONTRACT'] || process.env['NEXT_PUBLIC_MAINNET_CONTRACT']
 const TESTNET_CONTRACT = process.env['TESTNET_CONTRACT'] || process.env['NEXT_PUBLIC_TESTNET_CONTRACT']
-export const getPoolQueryFn = async (network: string, address: string) => {
-  const CONTRACT_ADDRESS = network === 'testnet' ? TESTNET_CONTRACT : MAINNET_CONTRACT
+export const getPoolQueryFn = async (chainId: number, address: string) => {
+  const CONTRACT_ADDRESS = chainId === 2 ? TESTNET_CONTRACT : MAINNET_CONTRACT
+  const network = chainId === 2 ? 'testnet' : 'mainnet'
+  console.log(network)
   if (address) {
-    const response = await fetch(`${CONTRACT_ADDRESS}::swap::TokenPairMetadata<${address}>`)
+    const response = await fetch(
+      `https://fullnode.${network}.aptoslabs.com/v1/accounts/${CONTRACT_ADDRESS}/resource/${CONTRACT_ADDRESS}::swap::TokenPairMetadata<${address}>`
+    )
     if (response.status == 200) {
-      const data = await response.json()
-      return data
+      console.log(response)
+      const pair = await response.json()
+      pair.id =
+        chainId + ':' + pair?.data?.token_x_details?.token_address + ', ' + pair?.data?.token_y_details?.token_address
+      console.log(pair)
+      return pair as Pool
     }
   }
   return {} as Pool
 }
-export function usePool(network: string = 'mainnet', address: string) {
+export function usePool(chainId: number, address: string) {
   return useQuery({
-    queryKey: ['pool', { network, address }],
-    queryFn: async () => getPoolQueryFn(network, address),
+    queryKey: ['pool', { chainId, address }],
+    queryFn: async () => getPoolQueryFn(chainId, address),
     keepPreviousData: true,
     enabled: Boolean(address),
   })
