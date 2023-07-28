c19bc5b661580f4940096c82230846ea2f1bbbfa
yash-khatri-minddeft <yash.khatri@minddeft.net>
2023-07-28 15:15:20 +0530

    row prop added

--- apps/aptos/app/pool/[id]/page.tsx
+++ apps/aptos/app/pool/[id]/page.tsx
@@ -37,14 +37,14 @@ const _Pool = () => {
           <div className="flex flex-col gap-9">
             <div className="flex flex-col lg:grid lg:grid-cols-[568px_auto] gap-12">
               <div className="flex flex-col order-1 gap-9">
-                <PoolHeader />
+                <PoolHeader row={pool} />
                 <hr className="my-3 border-t border-gray-900/5 dark:border-slate-200/5" />
-                <PoolComposition />
+                <PoolComposition row={pool} />
               </div>
               <div className="flex flex-col order-2 gap-4">
                 <AppearOnMount>
                   <div className="flex flex-col gap-10">
-                    <PoolPosition />
+                    <PoolPosition row={pool} />
                   </div>
                 </AppearOnMount>
                 <div className="hidden lg:flex">
