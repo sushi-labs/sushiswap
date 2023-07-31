bdb871a9d8f7928460a2ff4bc09a137fdeec5499
matthewlilley <hello@matthewlilley.com>
2023-07-31 20:10:33 +0100

    chore: move wagmi test into src

--- packages/wagmi/vitest.config.ts
+++ packages/wagmi/vitest.config.ts
@@ -2,7 +2,21 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['**/*.test.ts', 'test/**/*.test.ts'],
-    setupFiles: ['./test/setup.ts'],
+    // benchmark: {
+    //   outputFile: './bench/report.json',
+    //   reporters: process.env.CI ? ['json'] : ['verbose'],
+    // },
+    // coverage: {
+    //   reporter: process.env.CI ? ['lcov'] : ['text', 'json', 'html'],
+    //   exclude: [
+    //     '**/errors/utils.ts',
+    //     '**/dist/**',
+    //     '**/*.test.ts',
+    //     '**/_test/**',
+    //   ],
+    // },
+    environment: 'node',
+    setupFiles: ['./src/_test/setup.ts'],
+    globalSetup: ['./src/_test/globalSetup.ts'],
   },
 })
