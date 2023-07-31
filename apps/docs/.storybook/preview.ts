a95f2f22416da6de8b0a202fa1b8b3fefa7bb7b8
Chilli Chelli <ramin@sushi.com>
2023-07-31 15:05:11 +0100

    fix(packages/wagmi): move common-bases to pinnedTokens hook

--- apps/docs/.storybook/preview.ts
+++ apps/docs/.storybook/preview.ts
@@ -1,23 +1,26 @@
 import '@sushiswap/ui/src/index.css'
 import './style.css'
-
-import { withThemeByClassName } from '@storybook/addon-styling'
 import { Preview } from '@storybook/react'
-
-export const decorators = [
-  withThemeByClassName({
-    themes: {
-      light: 'light',
-      dark: 'dark',
-    },
-    defaultTheme: 'light',
-  }),
-]
 
 export const preview: Preview = {
   parameters: {
     backgrounds: {
       disable: true,
+    },
+  },
+  globalTypes: {
+    theme: {
+      description: 'Global theme for components',
+      defaultValue: 'light',
+      toolbar: {
+        // The label to show for this toolbar item
+        title: 'Theme',
+        icon: 'circlehollow',
+        // Array of plain string values or MenuItem shape (see below)
+        items: ['light', 'dark'],
+        // Change title based on selected value
+        dynamicTitle: true,
+      },
     },
   },
 }
--- config/router/src/index.ts
+++ config/router/src/index.ts
@@ -1,4 +1,3 @@
 export { ADDITIONAL_BASES } from './additional-bases.js'
 export { BASES_TO_CHECK_TRADES_AGAINST } from './bases-to-check-trades-against.js'
-export { COMMON_BASES } from './common-bases.js'
 export { CUSTOM_BASES } from './custom-bases.js'
