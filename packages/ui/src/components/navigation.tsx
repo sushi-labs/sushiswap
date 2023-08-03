df8e7367448be177247d40b3cab6391a9395a9e9
matthewlilley <hello@matthewlilley.com>
2023-08-03 16:47:07 +0100

    fix: missing import

--- packages/ui/src/components/navigation.tsx
+++ packages/ui/src/components/navigation.tsx
@@ -1,4 +1,5 @@
 import { cva, type VariantProps } from 'class-variance-authority'
+import Link from 'next/link'
 import * as React from 'react'
 
 import { classNames, navigationMenuTriggerStyle, SushiIcon } from '../index'
