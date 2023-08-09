459259e552145d35ed4437d3c475d29206d63c0d
Chilli Chelli <ramin@sushi.com>
2023-08-09 01:52:57 +0100

    fix(apps/pool): improve back button

--- apps/evm/app/pool/[id]/positions/[positionId]/page.tsx
+++ apps/evm/app/pool/[id]/positions/[positionId]/page.tsx
@@ -1,17 +1,13 @@
-import { ArrowLeftIcon } from '@heroicons/react/20/solid'
-import { IconButton, LinkInternal } from '@sushiswap/ui'
+import { LinkInternal } from '@sushiswap/ui'
 
 import { PositionView } from '../../../../../ui/pool/PositionView'
 
 export default async function PositionsPage({ params }: { params: { id: string; positionId: string } }) {
   return (
-    <div className="flex flex-col gap-1">
-      <div className="flex items-center gap-4">
-        <LinkInternal href={`/pool/${params.id}`} className="flex items-center gap-1">
-          <IconButton variant="ghost" size="sm" name="go back" icon={ArrowLeftIcon} />
-          <span className="text-sm font-medium">View all positions</span>
-        </LinkInternal>
-      </div>
+    <div className="flex flex-col gap-4">
+      <LinkInternal href={`/pool/${params.id}`} className="text-blue hover:underline text-sm">
+        ← Back
+      </LinkInternal>
       <PositionView params={{ id: `${params.id}%3A${params.positionId}` }} />
     </div>
   )
