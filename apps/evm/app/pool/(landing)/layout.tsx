366fba520a88dcc156be3c895ce5531168fa748a
Chilli Chelli <ramin@sushi.com>
2023-08-09 03:30:55 +0100

    fix(apps/pool): add ids to buttons

--- apps/evm/app/pool/(landing)/layout.tsx
+++ apps/evm/app/pool/(landing)/layout.tsx
@@ -13,22 +13,22 @@ export default function TabsLayout({ children }: { children: React.ReactNode }) 
       <Container maxWidth="7xl" className="px-4">
         <div className="flex flex-wrap items-center gap-2 mb-4">
           <LinkInternal shallow={true} scroll={false} href={`/pool/`}>
-            <PathnameButton pathname={`/pool`} asChild size="sm">
+            <PathnameButton id="all-pools" pathname={`/pool`} asChild size="sm">
               All Pools
             </PathnameButton>
           </LinkInternal>
           <LinkInternal shallow={true} scroll={false} href={`/pool/my-positions`}>
-            <PathnameButton pathname={`/pool/my-positions`} asChild size="sm">
+            <PathnameButton id="my-positions" pathname={`/pool/my-positions`} asChild size="sm">
               My Positions
             </PathnameButton>
           </LinkInternal>
           <LinkInternal shallow={true} scroll={false} href={`/pool/my-rewards`}>
-            <PathnameButton pathname={`/pool/my-rewards`} asChild size="sm">
+            <PathnameButton id="my-rewards" pathname={`/pool/my-rewards`} asChild size="sm">
               My Rewards
             </PathnameButton>
           </LinkInternal>
           <LinkInternal shallow={true} scroll={false} href={`/pool/migrate`}>
-            <PathnameButton pathname={`/pool/migrate`} asChild size="sm">
+            <PathnameButton id="migrate" pathname={`/pool/migrate`} asChild size="sm">
               Migrate
             </PathnameButton>
           </LinkInternal>
