be99d479c80b288c19f133bf61f83c5aed6aa112
Chilli Chelli <ramin@sushi.com>
2023-08-12 04:47:32 +0100

    fix(apps/swap): reuse providers

--- apps/evm/app/swap/(simple)/providers.tsx
+++ apps/evm/app/swap/(simple)/providers.tsx
@@ -1,18 +1,12 @@
 'use client'
 
-import { CheckerProvider } from '@sushiswap/wagmi/future/systems/Checker/Provider'
-import { DeferUntilWalletReady } from 'ui/swap/defer-until-wallet-ready'
 import { DerivedstateSimpleSwapProvider } from 'ui/swap/simple/derivedstate-simple-swap-provider'
 import { SimpleSwapDeferUntilTokensReady } from 'ui/swap/simple/simple-swap-defer-until-tokens-ready'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <DeferUntilWalletReady>
-      <CheckerProvider>
-        <DerivedstateSimpleSwapProvider>
-          <SimpleSwapDeferUntilTokensReady>{children}</SimpleSwapDeferUntilTokensReady>
-        </DerivedstateSimpleSwapProvider>
-      </CheckerProvider>
-    </DeferUntilWalletReady>
+    <DerivedstateSimpleSwapProvider>
+      <SimpleSwapDeferUntilTokensReady>{children}</SimpleSwapDeferUntilTokensReady>
+    </DerivedstateSimpleSwapProvider>
   )
 }
--- apps/evm/app/swap/cross-chain/providers.tsx
+++ apps/evm/app/swap/cross-chain/providers.tsx
@@ -1,18 +1,12 @@
 'use client'
 
-import { CheckerProvider } from '@sushiswap/wagmi/future/systems/Checker/Provider'
 import { CrossChainSwapDeferUntilTokensReady } from 'ui/swap/cross-chain/cross-chain-swap-defer-until-tokens-ready'
 import { DerivedstateCrossChainSwapProvider } from 'ui/swap/cross-chain/derivedstate-cross-chain-swap-provider'
-import { DeferUntilWalletReady } from 'ui/swap/defer-until-wallet-ready'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <DeferUntilWalletReady>
-      <CheckerProvider>
-        <DerivedstateCrossChainSwapProvider>
-          <CrossChainSwapDeferUntilTokensReady>{children}</CrossChainSwapDeferUntilTokensReady>
-        </DerivedstateCrossChainSwapProvider>
-      </CheckerProvider>
-    </DeferUntilWalletReady>
+    <DerivedstateCrossChainSwapProvider>
+      <CrossChainSwapDeferUntilTokensReady>{children}</CrossChainSwapDeferUntilTokensReady>
+    </DerivedstateCrossChainSwapProvider>
   )
 }
