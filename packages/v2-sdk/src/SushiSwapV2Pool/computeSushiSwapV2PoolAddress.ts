1b5b2b73bb308eb23bc8031489d18b826a1e7a26
matthewlilley <hello@matthewlilley.com>
2023-08-17 15:26:16 +0100

    fix: invariant

--- packages/v2-sdk/src/SushiSwapV2Pool/computeSushiSwapV2PoolAddress.ts
+++ packages/v2-sdk/src/SushiSwapV2Pool/computeSushiSwapV2PoolAddress.ts
@@ -3,7 +3,7 @@ import { keccak256, pack } from '@ethersproject/solidity'
 import { Token } from '@sushiswap/currency'
 import invariant from 'tiny-invariant'
 
-import { isSushiSwapV2ChainId, SUSHISWAP_V2_INIT_CODE_HASH } from '../constants'
+import { SUSHISWAP_V2_INIT_CODE_HASH, SushiSwapV2ChainId } from '../constants'
 
 /**
  * Computes a pair address
@@ -26,17 +26,9 @@ export const computeSushiSwapV2PoolAddress = ({
 }): string => {
   const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA] // does safety checks
   invariant(token0.chainId === token1.chainId, 'CHAIN_ID')
-  invariant(
-    initCodeHashManualOverride || (!initCodeHashManualOverride && isSushiSwapV2ChainId(token0.chainId)),
-    'CHAIN_ID'
-  )
-  invariant(
-    initCodeHashManualOverride || (!initCodeHashManualOverride && isSushiSwapV2ChainId(token1.chainId)),
-    'CHAIN_ID'
-  )
   return getCreate2Address(
     factoryAddress,
     keccak256(['bytes'], [pack(['address', 'address'], [token0.address, token1.address])]),
-    initCodeHashManualOverride ?? SUSHISWAP_V2_INIT_CODE_HASH[token0.chainId]
+    initCodeHashManualOverride ?? SUSHISWAP_V2_INIT_CODE_HASH[token0.chainId as SushiSwapV2ChainId]
   )
 }
