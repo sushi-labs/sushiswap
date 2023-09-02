18ae0f9d468ef562ef81195a662229f54960d0c5
matthewlilley <hello@matthewlilley.com>
2023-09-02 07:21:33 +0100

    fix: use 127.0.0.1

--- apps/evm/test/create-erc20.ts
+++ apps/evm/test/create-erc20.ts
@@ -23,11 +23,11 @@ export async function createERC20({
     throw new Error('Unsupported chain')
   }
 
-  const account = privateKeyToAccount('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80') // hardcoded for now, import from @sushiswap/wagmi /_test
+  const account = privateKeyToAccount('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80')
   const client = createTestClient({
     chain,
     mode: 'anvil',
-    transport: http('http://localhost:8545'),
+    transport: http('http://127.0.0.1:8545'),
   })
     .extend(walletActions)
     .extend(publicActions)
