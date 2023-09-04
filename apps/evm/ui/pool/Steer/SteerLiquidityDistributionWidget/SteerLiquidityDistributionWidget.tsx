ab745a5994f0bc22fd8265d5e6b88db4d4b19775
LufyCZ <lufy@sushi.com>
2023-09-04 17:22:38 +0000

    fix(apps/steer): constant refreshing

--- apps/evm/ui/pool/Steer/SteerLiquidityDistributionWidget/SteerLiquidityDistributionWidget.tsx
+++ apps/evm/ui/pool/Steer/SteerLiquidityDistributionWidget/SteerLiquidityDistributionWidget.tsx
@@ -1,12 +1,8 @@
-'use client'
-
 import { Pool } from '@sushiswap/client'
-import { Token } from '@sushiswap/currency'
-import { CardTitle, Chip, classNames, SkeletonBox } from '@sushiswap/ui'
-import { SushiSwapV3ChainId } from '@sushiswap/v3-sdk'
-import { useConcentratedActiveLiquidity } from 'lib/hooks/useConcentratedActiveLiquidity'
-import React, { FC, useMemo } from 'react'
+import { CardTitle } from '@sushiswap/ui'
+import React, { FC } from 'react'
 
+import { SteerLiquidityInRangeChip } from './SteerLiquidityInRangeChip'
 import { SteerTokenDistributionBar } from './SteerTokenDistributionBar'
 
 interface SteerLiquidityDistributionWidgetProps {
@@ -14,43 +10,12 @@ interface SteerLiquidityDistributionWidgetProps {
 }
 
 export const SteerLiquidityDistributionWidget: FC<SteerLiquidityDistributionWidgetProps> = ({ vault }) => {
-  const concentratedActiveLiquidityArgs = useMemo(() => {
-    const token0 = new Token({ ...vault.token0, chainId: vault.chainId })
-    const token1 = new Token({ ...vault.token1, chainId: vault.chainId })
-
-    return {
-      token0,
-      token1,
-      feeAmount: vault.feeTier * 1000000,
-      chainId: vault.chainId as SushiSwapV3ChainId,
-    }
-  }, [vault.chainId, vault.feeTier, vault.token0, vault.token1])
-
-  const { activeTick, isLoading: isActiveTickLoading } = useConcentratedActiveLiquidity(concentratedActiveLiquidityArgs)
-
-  const inRange = useMemo(() => {
-    if (activeTick === undefined) return undefined
-    return vault.lowerTick < activeTick && vault.upperTick > activeTick
-  }, [vault.lowerTick, vault.upperTick, activeTick])
-
   return (
     <>
       <div className="flex justify-between">
         <CardTitle>Liquidity Distribution</CardTitle>
 
-        {isActiveTickLoading && <SkeletonBox className="w-4 h-1 rounded-full" />}
-        {typeof inRange === 'boolean' && (
-          <Chip
-            variant={'outline'}
-            className={classNames(
-              inRange ? 'bg-green/20 text-green hover:bg-green/40' : 'bg-red/20 text-red hover:bg-red/[0.35]',
-              'space-x-1'
-            )}
-          >
-            <div className={classNames(inRange ? 'bg-green' : 'bg-red', 'w-2 h-2 rounded-full')} />
-            {inRange ? 'In' : 'Out of'} Range
-          </Chip>
-        )}
+        <SteerLiquidityInRangeChip vault={vault} />
       </div>
       <SteerTokenDistributionBar vault={vault} />
     </>
