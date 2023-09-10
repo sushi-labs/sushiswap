fd3c991b921cadb0111f400c634fa5c26ff9fdae
tom-teadao <tom@teadao.finance>
2023-09-10 13:55:00 +0200

    fix: expression

--- apps/evm/ui/pool/AddSectionMyPositionStaked.tsx
+++ apps/evm/ui/pool/AddSectionMyPositionStaked.tsx
@@ -10,8 +10,8 @@ export const AddSectionMyPositionStaked: FC = () => {
   if (isLoading && !isError && !balance) {
     return (
       <div className="flex flex-col gap-2">
-        <div className="flex gap-1 justify-between items-center">
-          <p className="text-sm font-semibold  dark:text-slate-50 text-gray-900">My Staked Position</p>
+        <div className="flex items-center justify-between gap-1">
+          <p className="text-sm font-semibold text-gray-900 dark:text-slate-50">My Staked Position</p>
           <div className="h-[16px] w-[40px] animate-pulse bg-slate-600 rounded-full" />
         </div>
         <div className="flex flex-col gap-1.5">
@@ -30,25 +30,25 @@ export const AddSectionMyPositionStaked: FC = () => {
 
   return (
     <div className="flex flex-col gap-2">
-      <div className="flex gap-1 justify-between items-center">
-        <p className="text-sm font-semibold  dark:text-slate-50 text-gray-900">My Staked Position</p>
-        <p className="text-xs font-medium dark:text-slate-400 text-gray-600">{formatUSD(value0 + value1)}</p>
+      <div className="flex items-center justify-between gap-1">
+        <p className="text-sm font-semibold text-gray-900 dark:text-slate-50">My Staked Position</p>
+        <p className="text-xs font-medium text-gray-600 dark:text-slate-400">{formatUSD(value0 + value1)}</p>
       </div>
       <div className="flex flex-col gap-1.5">
         <div className="flex items-center gap-1.5">
           <div className="w-4 h-4">
             {underlying0 && <Currency.Icon currency={underlying0.currency} width={16} height={16} />}
           </div>
-          <p className="text-xs font-medium flex items-center gap-1 dark:text-slate-400 text-gray-600">
-            {underlying0 && underlying0?.toSignificant(3)} {underlying0?.currency.symbol}
+          <p className="flex items-center gap-1 text-xs font-medium text-gray-600 dark:text-slate-400">
+            {underlying0?.toSignificant(3)} {underlying0?.currency.symbol}
           </p>
         </div>
         <div className="flex items-center gap-1.5">
           <div className="w-4 h-4">
             {underlying1 && <Currency.Icon currency={underlying1.currency} width={16} height={16} />}
           </div>
-          <p className="text-xs font-medium flex items-center gap-1 dark:text-slate-400 text-gray-600">
-            {underlying1 && underlying1?.toSignificant(3)} {underlying1?.currency.symbol}
+          <p className="flex items-center gap-1 text-xs font-medium text-gray-600 dark:text-slate-400">
+            {underlying1?.toSignificant(3)} {underlying1?.currency.symbol}
           </p>
         </div>
       </div>
