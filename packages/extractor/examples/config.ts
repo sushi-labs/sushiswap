d35ffb5cca913d445d8846d78b542343f2c4c2c3
matthewlilley <hello@matthewlilley.com>
2023-09-12 17:33:18 +0100

    fix: use same log depth for avalanche

--- packages/extractor/examples/config.ts
+++ packages/extractor/examples/config.ts
@@ -116,7 +116,7 @@ export const EXTRACTOR_CONFIG = {
     factoriesV3: [sushiswapV3Factory(ChainId.AVALANCHE)],
     tickHelperContract: SUSHISWAP_V3_TICK_LENS[ChainId.AVALANCHE],
     cacheDir: './cache',
-    logDepth: 50,
+    logDepth: 100,
     logging: true,
   },
   [ChainId.BOBA]: {
