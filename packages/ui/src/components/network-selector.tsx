0c48b3836b21dcdb5033b9be5279bd0cddf75f48
Ola Stenberg <hello@olastenberg.com>
2023-09-13 09:23:28 +0200

    chore(packages/ui): bump linea order, new emoji

--- packages/ui/src/components/network-selector.tsx
+++ packages/ui/src/components/network-selector.tsx
@@ -14,6 +14,7 @@ const PREFERRED_CHAINID_ORDER: ChainId[] = [
   ChainId.ARBITRUM,
   ChainId.BASE,
   ChainId.POLYGON,
+  ChainId.LINEA,
   ChainId.OPTIMISM,
   ChainId.BSC,
   ChainId.THUNDERCORE,
@@ -60,7 +61,7 @@ const NetworkSelector = <T extends number>({
               >
                 <div className="flex items-center gap-2">
                   <NetworkIcon chainId={el} width={22} height={22} />
-                  {Chain.from(el).name}
+                  {el !== ChainId.LINEA ? Chain.from(el).name : `${Chain.from(el).name} 🆕`}
                 </div>
               </CommandItem>
             ))}
