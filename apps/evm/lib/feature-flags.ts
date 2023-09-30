9938bb13b7d882d00de89e1032f5d1ee75db92af
matthewlilley <hello@matthewlilley.com>
2023-09-30 04:48:07 +0100

    feature: initial feature flags and maintenance mode handling

---
+++ apps/evm/lib/feature-flags.ts
@@ -0,0 +1,14 @@
+import { createClient } from '@vercel/edge-config'
+
+interface FeatureFlags {
+  maintenance: boolean
+  swapApi: boolean
+}
+
+export async function get(key: keyof FeatureFlags) {
+  const edgeConfig = createClient(process.env.EDGE_CONFIG)
+  const featureFlag = await edgeConfig.get(key)
+  return featureFlag
+}
+
+// export const get = async (key: keyof FeatureFlags) => _get<FeatureFlags>(key)
