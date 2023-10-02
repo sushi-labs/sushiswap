4c3c3b3684a85f288f68199415b3d0bbfeb35681
Chilli Chelli <ramin@sushi.com>
2023-10-02 12:06:06 +0100

    fix: suppress hydration warning in body and html as per https://stackoverflow.com/questions/75337953/what-causes-nextjs-warning-extra-attributes-from-the-server-data-new-gr-c-s-c

--- apps/evm/app/layout.tsx
+++ apps/evm/app/layout.tsx
@@ -32,14 +32,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     // <html lang="en" className="[color-scheme:dark]">
-    <html lang="en" className={`${inter.variable} ${roboto_mono.variable} dark`}>
+    <html lang="en" className={`${inter.variable} ${roboto_mono.variable} dark`} suppressHydrationWarning={true}>
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1" />
       <link rel="manifest" href="/site.webmanifest?v=1" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#fa52a0" />
       <link rel="shortcut icon" href="/favicon.ico?v=1" />
-      <body className="h-screen">
+      <body className="h-screen" suppressHydrationWarning={true}>
         <div className="flex flex-col h-full">
           <Providers>
             <SanctionedAddressDialog />
