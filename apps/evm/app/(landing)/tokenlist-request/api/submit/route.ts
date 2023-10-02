1dca8cb4a5386f73d8d3c74d6efbe0232dc642bb
LufyCZ <me@lufy.cz>
2023-10-02 18:17:00 +0000

    feat(apps/tokenlist-request): move from KV to Upstash

--- apps/evm/app/(landing)/tokenlist-request/api/submit/route.ts
+++ apps/evm/app/(landing)/tokenlist-request/api/submit/route.ts
@@ -3,7 +3,7 @@ import { ChainId, ChainKey } from '@sushiswap/chain'
 import { formatUSD } from '@sushiswap/format'
 import { CHAIN_NAME } from '@sushiswap/graph-config'
 import { Ratelimit } from '@upstash/ratelimit'
-import { kv } from '@vercel/kv'
+import { Redis } from '@upstash/redis'
 import stringify from 'fast-json-stable-stringify'
 import { NextRequest, NextResponse } from 'next/server'
 import { Octokit } from 'octokit'
@@ -30,8 +30,16 @@ interface MutationParams extends ApplyForTokenListTokenSchemaType {
 // To allow for development without rate limiting
 let ratelimit: Ratelimit | undefined
 try {
+  if (!process.env.UPSTASH_REDIS_REST_URL) throw new Error('UPSTASH_REDIS_REST_URL undefined')
+  if (!process.env.UPSTASH_REDIS_REST_TOKEN) throw new Error('UPSTASH_REDIS_REST_TOKEN undefined')
+
+  const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  })
+
   ratelimit = new Ratelimit({
-    redis: kv,
+    redis,
     limiter: Ratelimit.slidingWindow(5, '1 h'),
   })
 } catch {
