654e6f2b6a81ba68510d9f70410f6c471814f501
matthewlilley <hello@matthewlilley.com>
2023-10-04 17:12:09 +0100

    chore: use next tsconfig

--- packages/client/tsup.config.ts
+++ packages/client/tsup.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   minify: true,
-  target: 'es2022',
+  // target: 'es2022',
   external: ['react', 'swr'],
   sourcemap: true,
   dts: true,
   format: ['esm'],
-  esbuildOptions(options) {
-    options.banner = {
-      js: '"use client"',
-    }
-  },
+  // esbuildOptions(options) {
+  //   options.banner = {
+  //     js: '"use client"',
+  //   }
+  // },
 })
