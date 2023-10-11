76d0c4189bb06e51c4e3cce09cc4731ae3136e03
0xMasayoshi <0xmasayoshi@protonmail.com>
2023-10-12 01:52:33 +0700

    fix: import

--- packages/wagmi/src/future/components/TokenSecurityView.tsx
+++ packages/wagmi/src/future/components/TokenSecurityView.tsx
@@ -1,28 +1,48 @@
-import { ExclamationTriangleIcon, HandThumbUpIcon } from '@heroicons/react/24/solid'
-import { Token } from '@sushiswap/currency'
-import { TokenSecurity, TokenSecurityLabel, TokenSecurityMessage, TokenSecurityResponse } from '@sushiswap/react-query'
-import { Button, classNames, Explainer, GoPlusLabsIcon, List, SelectIcon } from '@sushiswap/ui'
+import {
+  ExclamationTriangleIcon,
+  HandThumbUpIcon,
+} from '@heroicons/react/24/solid'
+import {
+  TokenSecurity,
+  TokenSecurityLabel,
+  TokenSecurityMessage,
+  TokenSecurityResponse,
+} from '@sushiswap/react-query'
+import {
+  Button,
+  Explainer,
+  GoPlusLabsIcon,
+  List,
+  SelectIcon,
+  classNames,
+} from '@sushiswap/ui'
 import { useMemo, useState } from 'react'
+import { Token } from 'sushi/currency'
 
 const isTokenSecurityIssue = {
   // Contract security
   is_open_source: (value: TokenSecurity['is_open_source']) => value === false,
   is_mintable: (value: TokenSecurity['is_mintable']) => value === true,
-  can_take_back_ownership: (value: TokenSecurity['can_take_back_ownership']) => value === true,
-  owner_change_balance: (value: TokenSecurity['owner_change_balance']) => value === true,
+  can_take_back_ownership: (value: TokenSecurity['can_take_back_ownership']) =>
+    value === true,
+  owner_change_balance: (value: TokenSecurity['owner_change_balance']) =>
+    value === true,
   gas_abuse: (value: TokenSecurity['gas_abuse']) => value === true,
   // Trading security
   buy_tax: (value: TokenSecurity['buy_tax']) => value === true,
   sell_tax: (value: TokenSecurity['sell_tax']) => value === true,
   cannot_buy: (value: TokenSecurity['cannot_buy']) => value === true,
   cannot_sell_all: (value: TokenSecurity['cannot_sell_all']) => value === true,
-  slippage_modifiable: (value: TokenSecurity['slippage_modifiable']) => value === true,
+  slippage_modifiable: (value: TokenSecurity['slippage_modifiable']) =>
+    value === true,
   is_honeypot: (value: TokenSecurity['is_honeypot']) => value === true,
-  transfer_pausable: (value: TokenSecurity['transfer_pausable']) => value === true,
+  transfer_pausable: (value: TokenSecurity['transfer_pausable']) =>
+    value === true,
   is_blacklisted: (value: TokenSecurity['is_blacklisted']) => value === true,
   is_whitelisted: (value: TokenSecurity['is_whitelisted']) => value === true,
   is_anti_whale: (value: TokenSecurity['is_anti_whale']) => value === true,
-  trading_cooldown: (value: TokenSecurity['trading_cooldown']) => value === true,
+  trading_cooldown: (value: TokenSecurity['trading_cooldown']) =>
+    value === true,
   // Info security
   is_true_token: (value: TokenSecurity['is_true_token']) => value === false,
   is_airdrop_scam: (value: TokenSecurity['is_airdrop_scam']) => value === true,
@@ -44,9 +64,13 @@ export const TokenSecurityView = ({
 
     tokenSecurity &&
       Object.entries(tokenSecurity).forEach(([key, value]) => {
-        if (key in isTokenSecurityIssue && isTokenSecurityIssue[key as keyof typeof isTokenSecurityIssue](value))
+        if (
+          key in isTokenSecurityIssue &&
+          isTokenSecurityIssue[key as keyof typeof isTokenSecurityIssue](value)
+        )
           issues.push(key as keyof TokenSecurity)
-        else if (key in isTokenSecurityIssue) nonIssues.push(key as keyof TokenSecurity)
+        else if (key in isTokenSecurityIssue)
+          nonIssues.push(key as keyof TokenSecurity)
       })
 
     return { tokenSecurity, issues, nonIssues }
@@ -69,7 +93,9 @@ export const TokenSecurityView = ({
             <div
               className={classNames(
                 'rounded-full flex items-center px-2 py-1 gap-1',
-                Number(issues?.length) > 0 ? 'bg-yellow/20 text-yellow' : 'bg-green/20 text-green'
+                Number(issues?.length) > 0
+                  ? 'bg-yellow/20 text-yellow'
+                  : 'bg-green/20 text-green',
               )}
             >
               {Number(issues?.length) > 0 ? (
@@ -95,8 +121,18 @@ export const TokenSecurityView = ({
             }
           >
             <div className="flex items-center gap-1">
-              <div>{tokenSecurity?.[key] === undefined ? 'Unknown' : tokenSecurity[key] ? 'Yes' : 'No'}</div>
-              <ExclamationTriangleIcon width={14} height={14} className="fill-yellow" />
+              <div>
+                {tokenSecurity?.[key] === undefined
+                  ? 'Unknown'
+                  : tokenSecurity[key]
+                  ? 'Yes'
+                  : 'No'}
+              </div>
+              <ExclamationTriangleIcon
+                width={14}
+                height={14}
+                className="fill-yellow"
+              />
             </div>
           </List.KeyValue>
         ))}
@@ -114,13 +150,22 @@ export const TokenSecurityView = ({
               >
                 <div className="flex items-center gap-1">
                   <span>{tokenSecurity?.[key] ? 'Yes' : 'No'}</span>
-                  <HandThumbUpIcon width={14} height={14} className="fill-green" />
+                  <HandThumbUpIcon
+                    width={14}
+                    height={14}
+                    className="fill-green"
+                  />
                 </div>
               </List.KeyValue>
             ))
           : null}
         <div className="p-3">
-          <Button size="xs" fullWidth onClick={() => setShowMore(!showMore)} variant="ghost">
+          <Button
+            size="xs"
+            fullWidth
+            onClick={() => setShowMore(!showMore)}
+            variant="ghost"
+          >
             {showMore ? (
               <>
                 <SelectIcon className="rotate-180" />
