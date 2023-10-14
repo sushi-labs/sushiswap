7c0a6f8a03402816a6ca04c322313c0c8130af13
chillichelli <chillichelli@users.noreply.github.com>
2023-10-14 09:07:30 +0000

    chore: format

--- packages/ui/src/components/dialog.tsx
+++ packages/ui/src/components/dialog.tsx
@@ -1,16 +1,37 @@
 'use client'
 
-import {XMarkIcon} from '@heroicons/react/24/solid'
+import { XMarkIcon } from '@heroicons/react/24/solid'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
-import {cva, type VariantProps} from 'class-variance-authority'
+import { cva, type VariantProps } from 'class-variance-authority'
 import * as React from 'react'
-import {createContext, Dispatch, FC, ReactNode, SetStateAction, useCallback, useContext, useMemo, useState} from 'react'
-import {Chain, ChainId} from 'sushi/chain'
+import {
+  createContext,
+  Dispatch,
+  FC,
+  ReactNode,
+  SetStateAction,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+} from 'react'
+import { Chain, ChainId } from 'sushi/chain'
 
-import {Button, classNames, Dots, IconButton, LinkExternal, LinkInternal, Loader,} from '../index'
-import {CheckMarkIcon} from './icons/CheckmarkIcon'
-import {FailedMarkIcon} from './icons/FailedMarkIcon'
-import {ArrowTopRightOnSquareIcon, ArrowUpIcon,} from '@heroicons/react/20/solid'
+import {
+  Button,
+  classNames,
+  Dots,
+  IconButton,
+  LinkExternal,
+  LinkInternal,
+  Loader,
+} from '../index'
+import { CheckMarkIcon } from './icons/CheckmarkIcon'
+import { FailedMarkIcon } from './icons/FailedMarkIcon'
+import {
+  ArrowTopRightOnSquareIcon,
+  ArrowUpIcon,
+} from '@heroicons/react/20/solid'
 
 const dialogVariants = cva(
   'duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
--- packages/wagmi/src/future/components/UserProfile/TransactionsView.tsx
+++ packages/wagmi/src/future/components/UserProfile/TransactionsView.tsx
@@ -1,45 +1,45 @@
-import {ArrowLeftIcon} from "@heroicons/react/20/solid";
-import {Button} from "@sushiswap/ui/components/button";
-import React, {Dispatch, FC, SetStateAction} from "react";
+import { ArrowLeftIcon } from '@heroicons/react/20/solid'
+import { Button } from '@sushiswap/ui/components/button'
+import React, { Dispatch, FC, SetStateAction } from 'react'
 
-import {ProfileView} from "./index";
-import {useClearNotifications} from "@sushiswap/dexie";
-import {IconButton} from "@sushiswap/ui/components/iconbutton";
-import {useTransactions} from "../../hooks";
+import { ProfileView } from './index'
+import { useClearNotifications } from '@sushiswap/dexie'
+import { IconButton } from '@sushiswap/ui/components/iconbutton'
+import { useTransactions } from '../../hooks'
 
 interface TransactionsProps {
-	address: `0x${string}`;
-	setView: Dispatch<SetStateAction<ProfileView>>;
+  address: `0x${string}`
+  setView: Dispatch<SetStateAction<ProfileView>>
 }
 
 export const TransactionsView: FC<TransactionsProps> = ({
-	setView,
-	address,
+  setView,
+  address,
 }) => {
-	const { data: transactions } = useTransactions({ account: address });
-	const clearNotifications = useClearNotifications({ account: address });
+  const { data: transactions } = useTransactions({ account: address })
+  const clearNotifications = useClearNotifications({ account: address })
 
-	return (
-		<>
-			<div className="flex justify-between items-center mb-3">
-				<IconButton
-					size="sm"
-					onClick={() => setView(ProfileView.Default)}
-					icon={ArrowLeftIcon}
-					name="Back"
-				/>
-				<Button
-					onClick={() => clearNotifications()}
-					variant="ghost"
-					size="sm"
-					className="!px-2"
-				>
-					Clear all
-				</Button>
-			</div>
-			{transactions?.map((el) => (
-				<div>{el.timestamp}</div>
-			))}
-		</>
-	);
-};
+  return (
+    <>
+      <div className="flex justify-between items-center mb-3">
+        <IconButton
+          size="sm"
+          onClick={() => setView(ProfileView.Default)}
+          icon={ArrowLeftIcon}
+          name="Back"
+        />
+        <Button
+          onClick={() => clearNotifications()}
+          variant="ghost"
+          size="sm"
+          className="!px-2"
+        >
+          Clear all
+        </Button>
+      </div>
+      {transactions?.map((el) => (
+        <div>{el.timestamp}</div>
+      ))}
+    </>
+  )
+}
