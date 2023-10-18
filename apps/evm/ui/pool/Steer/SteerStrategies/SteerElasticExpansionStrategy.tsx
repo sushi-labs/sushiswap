cd0ef43ccf83116d5823745e162701054ecd2eae
Chilli Chelli <ramin@sushi.com>
2023-10-18 22:48:58 +0100

    fix(apps/pool): add link to steer

--- apps/evm/ui/pool/Steer/SteerStrategies/SteerElasticExpansionStrategy.tsx
+++ apps/evm/ui/pool/Steer/SteerStrategies/SteerElasticExpansionStrategy.tsx
@@ -1,176 +1,177 @@
-'use client'
+"use client";
 
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-  Explainer,
-  Separator,
-  Stat,
-  StatLabel,
-  StatValue,
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from '@sushiswap/ui'
-import { formatPercent, formatUSD } from 'sushi/format'
-import { APRHoverCard } from 'ui/pool/APRHoverCard'
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+	Explainer,
+	LinkExternal,
+	Separator,
+	Stat,
+	StatLabel,
+	StatValue,
+	Tabs,
+	TabsContent,
+	TabsList,
+	TabsTrigger,
+} from "@sushiswap/ui";
+import {formatPercent, formatUSD} from "sushi/format";
+import {APRHoverCard} from "ui/pool/APRHoverCard";
 
-import { SteerStrategyComponent } from '.'
-import { SteerAPRChart } from '../SteerAPRChart'
-import { SteerLiquidityInRangeChip } from '../SteerLiquidityDistributionWidget/SteerLiquidityInRangeChip'
+import {SteerStrategyComponent} from ".";
+import {SteerAPRChart} from "../SteerAPRChart";
+import {SteerLiquidityInRangeChip} from "../SteerLiquidityDistributionWidget/SteerLiquidityInRangeChip";
 import {
-  SteerPositionAdd,
-  SteerPositionAddProvider,
-  SteerPositionDetails,
-  SteerPositionRemove,
-} from '../SteerLiquidityManagement'
-import { SteerStrategyLiquidityDistribution } from '../SteerStrategyLiquidityChart'
-import { SteerStrategyConfig } from '../constants'
+	SteerPositionAdd,
+	SteerPositionAddProvider,
+	SteerPositionDetails,
+	SteerPositionRemove,
+} from "../SteerLiquidityManagement";
+import {SteerStrategyLiquidityDistribution} from "../SteerStrategyLiquidityChart";
+import {SteerStrategyConfig} from "../constants";
 
 export const SteerElasticExpansionStrategy: SteerStrategyComponent = ({
-  vault,
-  generic: { priceExtremes, tokenRatios, adjustment, positions },
+	vault,
+	generic: { priceExtremes, tokenRatios, adjustment, positions },
 }) => {
-  return (
-    <div className="grid md:grid-cols-2 gap-4">
-      <div>
-        <Card>
-          <CardHeader>
-            <CardTitle>Liquidity</CardTitle>
-            <CardDescription>
-              Depending on your range, the supplied tokens for this position
-              will not always be a 50:50 ratio.
-            </CardDescription>
-          </CardHeader>
-          <Tabs className="w-full" defaultValue="add">
-            <CardContent>
-              <TabsList className="!flex">
-                <TabsTrigger
-                  testdata-id="add-tab"
-                  value="add"
-                  className="flex flex-1"
-                >
-                  Add
-                </TabsTrigger>
-                <TabsTrigger
-                  testdata-id="remove-tab"
-                  value="remove"
-                  className="flex flex-1"
-                >
-                  Remove
-                </TabsTrigger>
-                <TabsTrigger
-                  testdata-id="position-tab"
-                  value="position"
-                  className="flex flex-1"
-                >
-                  Position
-                </TabsTrigger>
-              </TabsList>
-            </CardContent>
-            <div className="px-6">
-              <Separator />
-            </div>
-            <TabsContent value="add">
-              <CardHeader>
-                <CardTitle>Add liquidity</CardTitle>
-                <CardDescription>
-                  Provide liquidity to earn fees & rewards.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <SteerPositionAddProvider>
-                  <SteerPositionAdd vault={vault} />
-                </SteerPositionAddProvider>
-              </CardContent>
-            </TabsContent>
-            <TabsContent value="remove">
-              <CardHeader>
-                <CardTitle>Remove liquidity</CardTitle>
-                <CardDescription>
-                  Please enter how much of the position you want to remove.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <SteerPositionRemove vault={vault} />
-              </CardContent>
-            </TabsContent>
-            <TabsContent value="position">
-              <SteerPositionDetails vault={vault} />
-            </TabsContent>
-          </Tabs>
-        </Card>
-      </div>
-      <div>
-        <Card>
-          <CardHeader>
-            <CardTitle>{SteerStrategyConfig[vault.strategy].name}</CardTitle>
-            <CardDescription>
-              {SteerStrategyConfig[vault.strategy].description}
-            </CardDescription>
-          </CardHeader>
-          <Separator />
-          <div className="h-[200px] rounded-xl flex flex-col p-6">
-            <Stat className="mb-2">
-              <StatLabel size="sm">7 day average fee APR</StatLabel>
-            </Stat>
-            <SteerAPRChart vault={vault} />
-          </div>
-          <Separator />
-          <div className="grid grid-cols-2">
-            <Stat className="px-6 py-3">
-              <StatLabel size="sm">Total APR (24h)</StatLabel>
-              <StatValue size="sm">
-                <APRHoverCard pool={vault.pool} smartPoolAPR={vault.apr}>
-                  <span className="underline decoration-dotted underline-offset-2">
-                    {formatPercent(vault.apr + vault.pool.incentiveApr)}
-                  </span>
-                </APRHoverCard>
-              </StatValue>
-            </Stat>
-            <Stat className="px-6 py-3">
-              <StatLabel size="sm">TVL</StatLabel>
-              <StatValue size="sm">{formatUSD(vault.reserveUSD)}</StatValue>
-            </Stat>
-            <Stat className="px-6 py-3">
-              <StatLabel size="sm">
-                {vault.token0.symbol}:{vault.token1.symbol} Ratio (%)
-              </StatLabel>
-              <StatValue size="sm">
-                {formatPercent(tokenRatios.token0)} :{' '}
-                {formatPercent(tokenRatios.token1)}
-              </StatValue>
-            </Stat>
-            <Stat className="px-6 py-3">
-              <StatLabel size="sm" className="flex gap-1">
-                Adjustment frequency
-                <Explainer>
-                  The frequency at which this strategy gets adjusted. This does
-                  not guarantee the strategy gets adjusted, its a minimum
-                  threshold.
-                </Explainer>
-              </StatLabel>
-              <StatValue size="sm">
-                At most every {adjustment.frequency}
-              </StatValue>
-            </Stat>
-            <Stat className="px-6 py-3">
-              <StatLabel size="sm">
-                {adjustment.next.includes('ago') ? 'Last' : 'Next'} Adjustment
-              </StatLabel>
-              <StatValue size="sm">{adjustment.next}</StatValue>
-            </Stat>
-            <Stat className="px-6 py-3">
-              <StatLabel size="sm">Liquidity pool fee</StatLabel>
-              <StatValue size="sm">
-                {formatPercent(vault.pool.swapFee)}
-              </StatValue>
-            </Stat>
-            {/* <Stat className="px-6 py-3">
+	return (
+		<div className="grid md:grid-cols-2 gap-4">
+			<div>
+				<Card>
+					<CardHeader>
+						<CardTitle>Liquidity</CardTitle>
+						<CardDescription>
+							Depending on your range, the supplied tokens for this position
+							will not always be a 50:50 ratio.
+						</CardDescription>
+					</CardHeader>
+					<Tabs className="w-full" defaultValue="add">
+						<CardContent>
+							<TabsList className="!flex">
+								<TabsTrigger
+									testdata-id="add-tab"
+									value="add"
+									className="flex flex-1"
+								>
+									Add
+								</TabsTrigger>
+								<TabsTrigger
+									testdata-id="remove-tab"
+									value="remove"
+									className="flex flex-1"
+								>
+									Remove
+								</TabsTrigger>
+								<TabsTrigger
+									testdata-id="position-tab"
+									value="position"
+									className="flex flex-1"
+								>
+									Position
+								</TabsTrigger>
+							</TabsList>
+						</CardContent>
+						<div className="px-6">
+							<Separator />
+						</div>
+						<TabsContent value="add">
+							<CardHeader>
+								<CardTitle>Add liquidity</CardTitle>
+								<CardDescription>
+									Provide liquidity to earn fees & rewards.
+								</CardDescription>
+							</CardHeader>
+							<CardContent>
+								<SteerPositionAddProvider>
+									<SteerPositionAdd vault={vault} />
+								</SteerPositionAddProvider>
+							</CardContent>
+						</TabsContent>
+						<TabsContent value="remove">
+							<CardHeader>
+								<CardTitle>Remove liquidity</CardTitle>
+								<CardDescription>
+									Please enter how much of the position you want to remove.
+								</CardDescription>
+							</CardHeader>
+							<CardContent>
+								<SteerPositionRemove vault={vault} />
+							</CardContent>
+						</TabsContent>
+						<TabsContent value="position">
+							<SteerPositionDetails vault={vault} />
+						</TabsContent>
+					</Tabs>
+				</Card>
+			</div>
+			<div>
+				<Card>
+					<CardHeader>
+						<CardTitle>{SteerStrategyConfig[vault.strategy].name}</CardTitle>
+						<CardDescription>
+							{SteerStrategyConfig[vault.strategy].description}
+						</CardDescription>
+					</CardHeader>
+					<Separator />
+					<div className="h-[200px] rounded-xl flex flex-col p-6">
+						<Stat className="mb-2">
+							<StatLabel size="sm">7 day average fee APR</StatLabel>
+						</Stat>
+						<SteerAPRChart vault={vault} />
+					</div>
+					<Separator />
+					<div className="grid grid-cols-2">
+						<Stat className="px-6 py-3">
+							<StatLabel size="sm">Total APR (24h)</StatLabel>
+							<StatValue size="sm">
+								<APRHoverCard pool={vault.pool} smartPoolAPR={vault.apr}>
+									<span className="underline decoration-dotted underline-offset-2">
+										{formatPercent(vault.apr + vault.pool.incentiveApr)}
+									</span>
+								</APRHoverCard>
+							</StatValue>
+						</Stat>
+						<Stat className="px-6 py-3">
+							<StatLabel size="sm">TVL</StatLabel>
+							<StatValue size="sm">{formatUSD(vault.reserveUSD)}</StatValue>
+						</Stat>
+						<Stat className="px-6 py-3">
+							<StatLabel size="sm">
+								{vault.token0.symbol}:{vault.token1.symbol} Ratio (%)
+							</StatLabel>
+							<StatValue size="sm">
+								{formatPercent(tokenRatios.token0)} :{" "}
+								{formatPercent(tokenRatios.token1)}
+							</StatValue>
+						</Stat>
+						<Stat className="px-6 py-3">
+							<StatLabel size="sm" className="flex gap-1">
+								Adjustment frequency
+								<Explainer>
+									The frequency at which this strategy gets adjusted. This does
+									not guarantee the strategy gets adjusted, its a minimum
+									threshold.
+								</Explainer>
+							</StatLabel>
+							<StatValue size="sm">
+								At most every {adjustment.frequency}
+							</StatValue>
+						</Stat>
+						<Stat className="px-6 py-3">
+							<StatLabel size="sm">
+								{adjustment.next.includes("ago") ? "Last" : "Next"} Adjustment
+							</StatLabel>
+							<StatValue size="sm">{adjustment.next}</StatValue>
+						</Stat>
+						<Stat className="px-6 py-3">
+							<StatLabel size="sm">Liquidity pool fee</StatLabel>
+							<StatValue size="sm">
+								{formatPercent(vault.pool.swapFee)}
+							</StatValue>
+						</Stat>
+						{/* <Stat className="px-6 py-3">
               <StatLabel size="sm">Time frame</StatLabel>
               <StatValue size="sm">20 days</StatValue>
             </Stat>
@@ -178,40 +179,46 @@ export const SteerElasticExpansionStrategy: SteerStrategyComponent = ({
               <StatLabel size="sm">Deviation</StatLabel>
               <StatValue size="sm">2</StatValue>
             </Stat> */}
-            <Stat className="px-6 py-3">
-              <StatLabel size="sm">Performance fee</StatLabel>
-              <StatValue size="sm">
-                {formatPercent(vault.performanceFee)}
-              </StatValue>
-            </Stat>
-          </div>
-          <Separator />
-          <CardHeader>
-            <div className="flex justify-between">
-              <CardTitle>Liquidity Distribution</CardTitle>
-              <SteerLiquidityInRangeChip vault={vault} />
-            </div>
-          </CardHeader>
-          <div className="px-6">
-            <div className="h-[200px] w-full bg-secondary rounded-xl flex items-center justify-center">
-              <SteerStrategyLiquidityDistribution
-                pool={vault.pool}
-                positions={positions}
-              />
-            </div>
-          </div>
-          <div className="grid grid-cols-2">
-            <Stat className="p-6">
-              <StatLabel size="sm">Minimum price</StatLabel>
-              <StatValue size="sm">{priceExtremes.min} ETH/DAI</StatValue>
-            </Stat>
-            <Stat className="p-6">
-              <StatLabel size="sm">Maximum price</StatLabel>
-              <StatValue size="sm">{priceExtremes.max} ETH/DAI</StatValue>
-            </Stat>
-          </div>
-        </Card>
-      </div>
-    </div>
-  )
-}
+						<Stat className="px-6 py-3">
+							<StatLabel size="sm">Performance fee</StatLabel>
+							<StatValue size="sm">
+								{formatPercent(vault.performanceFee)}
+							</StatValue>
+						</Stat>
+						<Stat className="px-6 py-3">
+							<StatLabel size="sm">Provider</StatLabel>
+							<StatValue size="sm">
+								<LinkExternal href="https://steer.finance/">SteerFinance</LinkExternal>
+							</StatValue>
+						</Stat>
+					</div>
+					<Separator />
+					<CardHeader>
+						<div className="flex justify-between">
+							<CardTitle>Liquidity Distribution</CardTitle>
+							<SteerLiquidityInRangeChip vault={vault} />
+						</div>
+					</CardHeader>
+					<div className="px-6">
+						<div className="h-[200px] w-full bg-secondary rounded-xl flex items-center justify-center">
+							<SteerStrategyLiquidityDistribution
+								pool={vault.pool}
+								positions={positions}
+							/>
+						</div>
+					</div>
+					<div className="grid grid-cols-2">
+						<Stat className="p-6">
+							<StatLabel size="sm">Minimum price</StatLabel>
+							<StatValue size="sm">{priceExtremes.min} ETH/DAI</StatValue>
+						</Stat>
+						<Stat className="p-6">
+							<StatLabel size="sm">Maximum price</StatLabel>
+							<StatValue size="sm">{priceExtremes.max} ETH/DAI</StatValue>
+						</Stat>
+					</div>
+				</Card>
+			</div>
+		</div>
+	);
+};
