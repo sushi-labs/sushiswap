05896963c30475006a40e6075d6e2d54e5a8ee5f
Chilli Chelli <ramin@sushi.com>
2023-10-19 17:31:19 +0100

    fix(apps/pool): EGN-616

--- packages/sushi/src/format/number.ts
+++ packages/sushi/src/format/number.ts
@@ -1,5 +1,17 @@
 import numeral from 'numeral'
 
 export const formatNumber = (value: any) => {
+  const _val = Number(value).toLocaleString("en-US", {
+    useGrouping: false,
+  });
+
+  if (value < 0.0001) {
+    return "<0.01%";
+  }
+
+  if (value > 100000) {
+    return `${Number.parseFloat(_val).toExponential(3)}%`;
+  }
+
   return numeral(value).format('(0.00a)')
 }
--- packages/sushi/src/format/percent.ts
+++ packages/sushi/src/format/percent.ts
@@ -1,5 +1,17 @@
-import numeral from 'numeral'
+import numeral from "numeral";
 
 export const formatPercent = (value: any) => {
-  return numeral(value).format('(0.00%)')
-}
+	const _val = Number(value / 100).toLocaleString("en-US", {
+		useGrouping: false,
+	});
+
+	if (value < 0.0001) {
+		return "<0.01%";
+	}
+
+	if (value > 100000) {
+		return `${Number.parseFloat(_val).toExponential(3)}%`;
+	}
+
+	return numeral(value).format("(0.00%)");
+};
