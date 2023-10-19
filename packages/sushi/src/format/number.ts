d45d8b054b97fd8eb9f1927bd53ac6a36edc6862
chillichelli <chillichelli@users.noreply.github.com>
2023-10-19 16:33:16 +0000

    chore: format

--- packages/sushi/src/format/number.ts
+++ packages/sushi/src/format/number.ts
@@ -1,16 +1,16 @@
 import numeral from 'numeral'
 
 export const formatNumber = (value: any) => {
-  const _val = Number(value).toLocaleString("en-US", {
+  const _val = Number(value).toLocaleString('en-US', {
     useGrouping: false,
-  });
+  })
 
   if (value < 0.0001) {
-    return "<0.01%";
+    return '<0.01%'
   }
 
   if (value > 100000) {
-    return `${Number.parseFloat(_val).toExponential(3)}%`;
+    return `${Number.parseFloat(_val).toExponential(3)}%`
   }
 
   return numeral(value).format('(0.00a)')
