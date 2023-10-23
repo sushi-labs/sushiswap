21334fd1b1b6d07c0f11a536aa273936cc805762
matthewlilley <hello@matthewlilley.com>
2023-10-23 18:46:37 +0100

    fix: use correct vercel config key

--- apps/evm/app/api/config/xswap/route.ts
+++ apps/evm/app/api/config/xswap/route.ts
@@ -11,6 +11,6 @@ export const runtime = 'edge'
 export const revalidate = 60
 
 export async function GET() {
-  const data = await get('swap')
+  const data = await get('xswap')
   return NextResponse.json(schema.safeParse(data))
 }
