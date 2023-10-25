aa45ec4ed7dace4bd363118427c6f8f03afc7add
Chilli Chelli <ramin@sushi.com>
2023-10-25 11:03:32 +0100

    fix(apps/pool): missing container

--- apps/evm/app/pool/[id]/(landing)/positions/create/page.tsx
+++ apps/evm/app/pool/[id]/(landing)/positions/create/page.tsx
@@ -1,30 +1,32 @@
-import {LinkInternal} from '@sushiswap/ui'
-import {isSushiSwapV3ChainId} from '@sushiswap/v3-sdk'
+import {Container, LinkInternal} from "@sushiswap/ui";
+import {isSushiSwapV3ChainId} from "@sushiswap/v3-sdk";
 
-import {getChainIdAddressFromId, unsanitize} from 'sushi/format'
-import {ConcentratedLiquidityProvider} from 'ui/pool/ConcentratedLiquidityProvider'
-import {NewPosition} from 'ui/pool/NewPosition'
+import {getChainIdAddressFromId, unsanitize} from "sushi/format";
+import {ConcentratedLiquidityProvider} from "ui/pool/ConcentratedLiquidityProvider";
+import {NewPosition} from "ui/pool/NewPosition";
 
 export default async function PositionsCreatePage({
-  params,
+	params,
 }: { params: { id: string } }) {
-  const { chainId, address } = getChainIdAddressFromId(unsanitize(params.id))
+	const { chainId, address } = getChainIdAddressFromId(unsanitize(params.id));
 
-  if (!isSushiSwapV3ChainId(chainId)) {
-    throw new Error('This page only supports SushiSwap V3 pools')
-  }
+	if (!isSushiSwapV3ChainId(chainId)) {
+		throw new Error("This page only supports SushiSwap V3 pools");
+	}
 
-  return (
-    <div className="flex flex-col gap-4">
-      <LinkInternal
-        href={`/pool/${params.id}`}
-        className="text-blue hover:underline text-sm"
-      >
-        ← Back
-      </LinkInternal>
-      <ConcentratedLiquidityProvider>
-        <NewPosition address={address} chainId={chainId} />
-      </ConcentratedLiquidityProvider>
-    </div>
-  )
+	return (
+		<Container maxWidth="5xl" className="px-2 sm:px-4">
+			<div className="flex flex-col gap-4">
+				<LinkInternal
+					href={`/pool/${params.id}`}
+					className="text-blue hover:underline text-sm"
+				>
+					← Back
+				</LinkInternal>
+				<ConcentratedLiquidityProvider>
+					<NewPosition address={address} chainId={chainId} />
+				</ConcentratedLiquidityProvider>
+			</div>
+		</Container>
+	);
 }
