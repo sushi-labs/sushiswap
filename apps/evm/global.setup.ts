3892d22750f4d5ecf7ef78db953c243d9846f48c
matthewlilley <hello@matthewlilley.com>
2023-10-26 17:03:55 +0100

    chore: test evm app

--- apps/evm/global.setup.ts
+++ apps/evm/global.setup.ts
@@ -3,8 +3,17 @@ import { type FullConfig } from '@playwright/test'
 
 async function globalSetup(_config: FullConfig) {
   console.log('globalSetup')
+
   // const projectDir = process.cwd()
   // loadEnvConfig(projectDir)
+
+  // const { createAnvil } = await import('@viem/anvil')
+  // const anvil = createAnvil({
+  //   forkUrl: String(process.env.ANVIL_FORK_URL),
+  //   forkBlockNumber: Number(process.env.ANVIL_BLOCK_NUMBER),
+  //   port: Number(process.env.ANVIL_PORT || 8545),
+  // })
+  // await anvil.start()
 }
 
 export default globalSetup
