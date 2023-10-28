d09217130e508052897691135255fc0faf78a475
matthewlilley <hello@matthewlilley.com>
2023-10-28 04:04:34 +0100

    test: get-port override and cleanup

--- apps/evm/test/global.setup.ts
+++ apps/evm/test/global.setup.ts
@@ -3,12 +3,17 @@ import { startProxy } from '@viem/anvil'
 
 export default async function globalSetup(_config: FullConfig) {
   console.log('globalSetup')
-  await startProxy({
+  const shutdown = await startProxy({
+    host: '0.0.0.0',
     port: Number(process.env.ANVIL_PORT || 8545),
     options: {
       forkUrl: String(process.env.ANVIL_FORK_URL),
       forkBlockNumber: Number(process.env.ANVIL_BLOCK_NUMBER),
       startTimeout: 60_000,
     },
+  })
+
+  process.on('exit', async () => {
+    await shutdown()
   })
 }
