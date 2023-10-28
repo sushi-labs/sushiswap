6a6fb69c1da242f5dd8a7e5f2e51a741139ea7c3
matthewlilley <hello@matthewlilley.com>
2023-10-28 01:45:42 +0100

    test: simplify proxy

--- apps/evm/test/global.setup.ts
+++ apps/evm/test/global.setup.ts
@@ -1,26 +1,14 @@
 import { type FullConfig } from '@playwright/test'
-
-// import { startProxy } from '@viem/anvil'
-// import { createERC20 } from './create-erc20'
-import './anvil-proxy'
-import { proxy } from './anvil-proxy'
+import { startProxy } from '@viem/anvil'
 
 export default async function globalSetup(_config: FullConfig) {
   console.log('globalSetup')
-  return proxy
-  // const anvil = createAnvil({
-  //   forkUrl: String(process.env.ANVIL_FORK_URL),
-  //   forkBlockNumber: Number(process.env.ANVIL_BLOCK_NUMBER),
-  //   port: Number(process.env.ANVIL_PORT || 8545),
-  //   startTimeout: 60_000,
-  // })
-  // return anvil.start()
-  // return startProxy({
-  //   port: Number(process.env.ANVIL_PORT || 8545),
-  //   options: {
-  //     forkUrl: String(process.env.ANVIL_FORK_URL),
-  //     forkBlockNumber: Number(process.env.ANVIL_BLOCK_NUMBER),
-  //     startTimeout: 60_000,
-  //   },
-  // })
+  return startProxy({
+    port: Number(process.env.ANVIL_PORT || 8545),
+    options: {
+      forkUrl: String(process.env.ANVIL_FORK_URL),
+      forkBlockNumber: Number(process.env.ANVIL_BLOCK_NUMBER),
+      startTimeout: 60_000,
+    },
+  })
 }
--- apps/evm/test/global.teardown.ts
+++ apps/evm/test/global.teardown.ts
@@ -1,11 +1,8 @@
 import { type FullConfig } from '@playwright/test'
-import { proxy } from './anvil-proxy'
 
 async function globalTeardown(_config: FullConfig) {
   try {
     console.log('globalTeardown')
-    const shutdown = await proxy
-    await shutdown()
   } catch (error) {
     console.error('globalTeardown', error)
   }
