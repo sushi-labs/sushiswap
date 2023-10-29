a361b651322e24b9e36d2516e2c3df4a0c6aec26
matthewlilley <matthewlilley@users.noreply.github.com>
2023-10-29 21:25:48 +0000

    chore: lint

--- packages/ui/src/components/accordion.tsx
+++ packages/ui/src/components/accordion.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import * as AccordionPrimitive from '@radix-ui/react-accordion'
-import * as React from 'react'
 import { ChevronDown } from 'lucide-react'
+import * as React from 'react'
 
 import classNames from 'classnames'
 
@@ -42,8 +42,6 @@ AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName
 
 const AccordionContent = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Content>,
-
-  
   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
 >(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Content
