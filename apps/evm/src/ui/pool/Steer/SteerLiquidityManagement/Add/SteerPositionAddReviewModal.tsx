bd12d558a68c9819eccdcb10175046b865f0808e
chillichelli <chillichelli@users.noreply.github.com>
2023-11-01 11:40:03 +0000

    chore: format

--- apps/evm/src/ui/pool/Steer/SteerLiquidityManagement/Add/SteerPositionAddReviewModal.tsx
+++ apps/evm/src/ui/pool/Steer/SteerLiquidityManagement/Add/SteerPositionAddReviewModal.tsx
@@ -41,7 +41,7 @@ import { SteerStrategyConfig } from '../../constants'
 import { useSteerPositionAddDerivedInfo } from './SteerPositionAddProvider'
 import { useSlippageTolerance } from '@sushiswap/hooks'
 import { useTokenAmountDollarValues } from '../../../../../lib/hooks'
-import {slippageAmount} from "sushi";
+import { slippageAmount } from 'sushi'
 
 interface SteerPositionAddReviewModalProps {
   vault: SteerVault
@@ -154,14 +154,8 @@ export const SteerPositionAddReviewModal: FC<SteerPositionAddReviewModalProps> =
             vault.address as Address,
             parsedAmounts.CURRENCY_A.quotient,
             parsedAmounts.CURRENCY_B.quotient,
-            slippageAmount(
-              parsedAmounts.CURRENCY_A,
-              slippagePercent,
-            )[0],
-              slippageAmount(
-              parsedAmounts.CURRENCY_B,
-              slippagePercent,
-            )[0],
+            slippageAmount(parsedAmounts.CURRENCY_A, slippagePercent)[0],
+            slippageAmount(parsedAmounts.CURRENCY_B, slippagePercent)[0],
             address,
           ],
         }),
