62fa94be495ac85a2abc900d9d94fa905a80e6c5
Ola Stenberg <hello@olastenberg.com>
2023-11-02 19:40:20 +0100

    test(apps/evm): init smart pool test

--- apps/evm/test/erc20.ts
+++ apps/evm/test/erc20.ts
@@ -1,17 +1,8 @@
 import { ChainId } from 'sushi/chain'
 import { erc20Contract } from 'sushi/contract'
 import { Token } from 'sushi/currency'
-import {
-  http,
-  Address,
-  createTestClient,
-  formatUnits,
-  parseGwei,
-  parseUnits,
-  publicActions,
-  walletActions,
-} from 'viem'
-import { privateKeyToAccount, publicKeyToAddress } from 'viem/accounts'
+import { createTestClient, http, publicActions, walletActions } from 'viem'
+import { privateKeyToAccount } from 'viem/accounts'
 import { Chain, arbitrum, mainnet, polygon } from 'viem/chains'
 import { localHttpUrl } from './constants'
 
@@ -79,31 +70,19 @@ export async function createERC20({
   })
 }
 
-export async function prepareERC20Balance({
+export async function impersonateAccount({
   chainId,
+  address,
 }: {
   chainId: number
+  address: `0x${string}`
 }) {
   let chain: Chain
-  let tokenAddress: `0x${string}`
   if (chainId === ChainId.POLYGON) {
     chain = polygon
-    tokenAddress = '0x2791bca1f2de4661ed88a30c99a7a9449aa84174'
-    // }
-    // } else if (chainId === ChainId.ETHEREUM) {
-    //   chain = mainnet
-    // } else if (chainId === ChainId.ARBITRUM) {
-    //   chain = arbitrum
   } else {
     throw new Error('Unsupported chain')
   }
-
-  const account = privateKeyToAccount(
-    '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
-  )
-  const publicAddress = publicKeyToAddress(account.publicKey)
-  const impersonatedAccount = '0x23DefC2Ca207e7FBD84AE43B00048Fb5Cb4DB5B2'
-  const amount = parseUnits('10', 6)
 
   const client = createTestClient({
     chain,
@@ -114,30 +93,69 @@ export async function prepareERC20Balance({
     .extend(publicActions)
 
   await client.impersonateAccount({
-    address: impersonatedAccount,
+    address,
   })
-
-  await client.writeContract({
-    ...erc20Contract,
-    address: tokenAddress as Address,
-    functionName: 'transfer',
-    account: impersonatedAccount,
-    args: [publicAddress, amount],
-  })
-
-  await client.stopImpersonatingAccount({
-    address: impersonatedAccount,
-  })
-  const balance = await client.readContract({
-    abi: erc20Contract.abi,
-    address: tokenAddress as Address,
-    functionName: 'balanceOf',
-    args: [publicAddress],
-  })
-  console.log(
-    `${impersonatedAccount} gave ${publicAddress} ${formatUnits(
-      balance,
-      6,
-    )} USDC`,
-  )
 }
+
+// export async function prepareERC20Balance({
+//   chainId,
+// }: {
+//   chainId: number
+// }) {
+//   let chain: Chain
+//   let tokenAddress: `0x${string}`
+//   if (chainId === ChainId.POLYGON) {
+//     chain = polygon
+//     tokenAddress = '0x2791bca1f2de4661ed88a30c99a7a9449aa84174'
+//     // }
+//     // } else if (chainId === ChainId.ETHEREUM) {
+//     //   chain = mainnet
+//     // } else if (chainId === ChainId.ARBITRUM) {
+//     //   chain = arbitrum
+//   } else {
+//     throw new Error('Unsupported chain')
+//   }
+
+//   const account = privateKeyToAccount(
+//     '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
+//   )
+//   const publicAddress = publicKeyToAddress(account.publicKey)
+//   const impersonatedAccount = '0x23DefC2Ca207e7FBD84AE43B00048Fb5Cb4DB5B2'
+//   const amount = parseUnits('10', 6)
+
+//   const client = createTestClient({
+//     chain,
+//     mode: 'anvil',
+//     transport: http(localHttpUrl, { timeout: 120_000 }),
+//   })
+//     .extend(walletActions)
+//     .extend(publicActions)
+
+//   await client.impersonateAccount({
+//     address: impersonatedAccount,
+//   })
+
+//   await client.writeContract({
+//     ...erc20Contract,
+//     address: tokenAddress as Address,
+//     functionName: 'transfer',
+//     account: impersonatedAccount,
+//     args: [publicAddress, amount],
+//   })
+
+//   await client.stopImpersonatingAccount({
+//     address: impersonatedAccount,
+//   })
+//   const balance = await client.readContract({
+//     abi: erc20Contract.abi,
+//     address: tokenAddress as Address,
+//     functionName: 'balanceOf',
+//     args: [publicAddress],
+//   })
+//   console.log(
+//     `${impersonatedAccount} gave ${publicAddress} ${formatUnits(
+//       balance,
+//       6,
+//     )} USDC`,
+//   )
+// }
--- apps/evm/test/pages/smart.test.ts
+++ apps/evm/test/pages/smart.test.ts
@@ -4,28 +4,34 @@ import {
   test,
 } from 'next/experimental/testmode/playwright'
 import { SupportedChainId } from 'src/config'
-import { prepareERC20Balance } from 'test/erc20'
+import { ChainId } from 'sushi/chain'
+import { impersonateAccount } from 'test/erc20'
 
 if (typeof process.env.NEXT_PUBLIC_CHAIN_ID !== 'string') {
   new Error('NEXT_PUBLIC_CHAIN_ID not set')
 }
+const BASE_URL = 'http://localhost:3000/pool'
 
 const CHAIN_ID = Number(
   process.env.NEXT_PUBLIC_CHAIN_ID as string,
 ) as SupportedChainId
 
-
 test.beforeAll(async () => {
+  test.skip(CHAIN_ID !== ChainId.POLYGON)
+  const address = '0x23DefC2Ca207e7FBD84AE43B00048Fb5Cb4DB5B2'
   try {
-    await prepareERC20Balance({ chainId: CHAIN_ID })
+    await impersonateAccount({
+      chainId: CHAIN_ID,
+      address,
+    })
   } catch (error) {
-    console.error(
-      'Couldn\'t prepare ERC20 balance',
-      error,
-    )
+    console.error(`Couldn't impersonate account, ${address}`, error)
   }
 })
 
 test('Create and remove position', async ({ page }) => {
   // TODO:
+  await page.goto(BASE_URL)
+  await page.locator('[testdata-id=my-positions-button]').click()
+
 })
