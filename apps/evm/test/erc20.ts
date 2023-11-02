28f219f7b54cb5674e81abce343d99d4456a92c3
sushi-actions <sushi-actions@users.noreply.github.com>
2023-11-02 20:12:07 +0000

    chore: lint

--- apps/evm/test/erc20.ts
+++ apps/evm/test/erc20.ts
@@ -1,7 +1,7 @@
 import { ChainId } from 'sushi/chain'
 import { erc20Contract } from 'sushi/contract'
 import { Token } from 'sushi/currency'
-import { createTestClient, http, publicActions, walletActions } from 'viem'
+import { http, createTestClient, publicActions, walletActions } from 'viem'
 import { privateKeyToAccount } from 'viem/accounts'
 import { Chain, arbitrum, mainnet, polygon } from 'viem/chains'
 import { localHttpUrl } from './constants'
