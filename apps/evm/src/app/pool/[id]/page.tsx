97e44076d6b63436848f4b98fd83ba1c48bec965
LufyCZ <me@lufy.cz>
2023-11-08 00:33:48 +0000

    refactor(apps/pool): extract the PoolPageV2 component

--- apps/evm/src/app/pool/[id]/page.tsx
+++ apps/evm/src/app/pool/[id]/page.tsx
@@ -1,23 +1,8 @@
-import { Separator } from '@sushiswap/ui'
 import { notFound } from 'next/navigation'
-import { ManageV2LiquidityCard } from 'src/ui/pool/ManageV2LiquidityCard'
-import { PoolTransactionsV2 } from 'src/ui/pool/PoolTransactionsV2'
-import { ChainId } from 'sushi/chain'
 
 import { Pool } from '@sushiswap/client'
-import {
-  PoolPositionProvider,
-  PoolPositionRewardsProvider,
-  PoolPositionStakedProvider,
-  UnknownTokenAlert,
-} from '../../../ui/pool'
-import { PoolChartV2 } from '../../../ui/pool/PoolChartV2'
-import { PoolComposition } from '../../../ui/pool/PoolComposition'
-import { PoolMyRewards } from '../../../ui/pool/PoolMyRewards'
+import { PoolPageV2 } from 'src/ui/pool/PoolPageV2'
 import { PoolPageV3 } from '../../../ui/pool/PoolPageV3'
-import { PoolPosition } from '../../../ui/pool/PoolPosition'
-import { PoolRewards } from '../../../ui/pool/PoolRewards'
-import { PoolStats } from '../../../ui/pool/PoolStats'
 
 export default async function PoolPage({ params }: { params: { id: string } }) {
   const [chainId, address] = params.id.split(
@@ -38,46 +23,5 @@ export default async function PoolPage({ params }: { params: { id: string } }) {
     return <PoolPageV3 pool={pool} />
   }
 
-  return (
-    <>
-      <UnknownTokenAlert pool={pool} />
-      <div className="flex flex-col gap-6">
-        <div className="grid grid-cols-1 md:grid-cols-[auto_400px] gap-6">
-          <div>
-            <ManageV2LiquidityCard pool={pool} />
-          </div>
-          <div className="flex flex-col gap-6">
-            <PoolPositionProvider pool={pool}>
-              <PoolPositionStakedProvider pool={pool}>
-                <PoolPositionRewardsProvider pool={pool}>
-                  <PoolPosition pool={pool} />
-                  <PoolMyRewards pool={pool} />
-                </PoolPositionRewardsProvider>
-              </PoolPositionStakedProvider>
-            </PoolPositionProvider>
-          </div>
-        </div>
-        <div className="py-4">
-          <Separator />
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-[auto_400px] gap-6">
-          <div>
-            <PoolChartV2
-              address={pool.address}
-              chainId={pool.chainId as ChainId}
-            />
-          </div>
-          <div className="flex flex-col gap-6">
-            <PoolComposition pool={pool} />
-            <PoolStats pool={pool} />
-            <PoolRewards pool={pool} />
-          </div>
-        </div>
-        <div className="py-4">
-          <Separator />
-        </div>
-        <PoolTransactionsV2 pool={pool} poolId={pool.address} />
-      </div>
-    </>
-  )
+  return <PoolPageV2 pool={pool} />
 }
