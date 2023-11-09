6688d799f6c682363ec4c2ac18ebf3bacd7c4fef
Chilli Chelli <ramin@sushi.com>
2023-11-09 14:37:30 +0000

    fix(apps/pool): add liquidity in card

--- apps/aptos/components/TradeInput.tsx
+++ apps/aptos/components/TradeInput.tsx
@@ -9,7 +9,6 @@ import React from 'react'
 import { Token } from 'utils/tokenType'
 import { BalancePanel } from './BalancePanel'
 import { Icon } from './Icon'
-import { Modal } from './Modal/Modal'
 import { PricePanel } from './PricePanel'
 import TokenListDialog from './TokenListDialog'
 interface PropType {
@@ -20,7 +19,7 @@ interface PropType {
   value: string
   setAmount?: (value: string) => void
   disabled?: boolean
-  setToken: (token: Token) => void
+  setToken?: (token: Token) => void
   balance: number | undefined
   error?: string
   isLoadingPrice: boolean
@@ -30,6 +29,7 @@ interface PropType {
   className?: string
   fetching?: boolean
 }
+
 export default function TradeInput({
   id,
   type,
@@ -107,23 +107,44 @@ export default function TradeInput({
           />
         </div>
 
-        <TokenListDialog
-          id={id}
-          selected={token}
-          alteredSelected={alteredSelected}
-          handleChangeToken={setToken}
-          handleSwap={handleSwap}
-        >
-          <Button
-            size="lg"
-            data-state={isLoadingPrice ? 'inactive' : 'active'}
-            variant={token ? 'secondary' : 'default'}
-            id={`${id}-token-selector`}
-            type="button"
-            testdata-id="swap-from-button"
+        {setToken ? (
+          <TokenListDialog
+            id={id}
+            selected={token}
+            alteredSelected={alteredSelected}
+            handleChangeToken={setToken}
+            handleSwap={handleSwap}
+          >
+            <Button
+              size="lg"
+              data-state={isLoadingPrice ? 'inactive' : 'active'}
+              variant={token ? 'secondary' : 'default'}
+              id={`${id}-token-selector`}
+              type="button"
+              testdata-id="swap-from-button"
+              className={classNames(
+                token ? 'pl-2 pr-3 text-xl' : '',
+                '!rounded-full data-[state=inactive]:hidden data-[state=active]:flex',
+              )}
+            >
+              {token ? (
+                <>
+                  <span className="w-[28px] h-[28px] mr-0.5">
+                    <Icon currency={token} height={28} width={28} />
+                  </span>
+                  {token.symbol}
+                  <SelectIcon />
+                </>
+              ) : (
+                'Select'
+              )}
+            </Button>
+          </TokenListDialog>
+        ) : (
+          <div
+            id={`${id}-button`}
             className={classNames(
-              token ? 'pl-2 pr-3 text-xl' : '',
-              '!rounded-full data-[state=inactive]:hidden data-[state=active]:flex',
+              'flex items-center gap-1 text-xl py-2 pl-2 pr-2 rounded-full font-medium whitespace-nowrap',
             )}
           >
             {token ? (
@@ -132,13 +153,14 @@ export default function TradeInput({
                   <Icon currency={token} height={28} width={28} />
                 </span>
                 {token.symbol}
-                <SelectIcon />
               </>
             ) : (
-              'Select'
+              <span className="text-gray-400 dark:text-slate-500">
+                No token selected
+              </span>
             )}
-          </Button>
-        </TokenListDialog>
+          </div>
+        )}
       </div>
       <div className="flex flex-row items-center justify-between h-[36px]">
         <PricePanel isLoading={isLoadingPrice} error={error} />
