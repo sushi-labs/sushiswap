579225fd52343c4070d5e23709a8e1322cffd861
OlaStenberg <OlaStenberg@users.noreply.github.com>
2023-11-20 10:24:01 +0000

    chore: lint

--- apis/tokens/api/v1/[chainId]/index.ts
+++ apis/tokens/api/v1/[chainId]/index.ts
@@ -11,7 +11,10 @@ const schema = z.object({
 })
 
 const handler = async (request: VercelRequest, response: VercelResponse) => {
-  response.setHeader('Cache-Control', 's-maxage=1800, stale-while-revalidate=3600')
+  response.setHeader(
+    'Cache-Control',
+    's-maxage=1800, stale-while-revalidate=3600',
+  )
   const { chainId } = schema.parse(request.query)
 
   const tokenList = await fetchTokensFromLists(chainId)
