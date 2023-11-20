ff7086f15083d96a75c85b66220fdc09a17805df
Ola Stenberg <hello@olastenberg.com>
2023-11-20 11:19:48 +0100

    feat(apis/tokens): update v1, sushi list take precedence, remove duplicates

--- apis/tokens/lib/api/v1.ts
+++ apis/tokens/lib/api/v1.ts
@@ -1,6 +1,6 @@
-import { isPromiseFulfilled } from 'sushi'
 import {
   DEFAULT_LIST_OF_LISTS,
+  SUSHI_DEFAULT_TOKEN_LIST,
   type TokenInfo,
   type TokenList,
 } from 'sushi/token-list'
@@ -10,31 +10,50 @@ import { redis } from '../redis.js'
 const REDIS_KEY_PREFIX = 'token-list-v2-'
 
 export async function fetchTokensFromLists(chainId?: number) {
-  const redisResult = await Promise.allSettled(
-    DEFAULT_LIST_OF_LISTS.map((url) =>
-      redis.get<TokenList | null>(`${REDIS_KEY_PREFIX}-${url}`.toLowerCase()),
-    ),
+  const redisResult = await Promise.all(
+    DEFAULT_LIST_OF_LISTS.map(async (url) => {
+      const value = await redis.get<TokenList | null>(
+        `${REDIS_KEY_PREFIX}-${url}`.toLowerCase(),
+      )
+      return [url, value] as [string, TokenList | null]
+    }),
   )
-  const tokenLists = redisResult
-    .filter(isPromiseFulfilled)
-    .map((r) => r.value)
-    .filter((v) => v !== null)
 
-  // TODO: this filtering below should be extracted to it's own function, e.g. sushis token lists should take precedence over others,
-  // e.g. partners might have renamed their tokens and those changes might only exist in our token list.
-  return chainId
-    ? tokenLists.flatMap((tokenList) =>
-        tokenList.tokens
-          .filter((t) => t.chainId === chainId)
-          .map((t) => ({
-            ...(t as TokenInfo),
-            address: getAddress(t.address), // Token addresses are sometimes lowercase from token lists
-          })),
-      )
-    : tokenLists.flatMap((tokenList) =>
-        tokenList.tokens.map((t) => ({
-          ...(t as TokenInfo),
-          address: getAddress(t.address), // Token addresses are sometimes lowercase from token lists
-        })),
-      )
+  const tokenLists = new Map<string, TokenList>(
+    redisResult
+      .filter(([_, v]) => v !== null)
+      .map(([url, v]) => [url, v as TokenList]),
+  )
+
+  return merge(tokenLists, chainId)
+}
+
+function merge(tokenLists: Map<string, TokenList>, chainId?: number) {
+  const tokens = new Map<string, TokenInfo>()
+
+  // Set sushi default tokens first, they will take precedence
+  tokenLists.get(SUSHI_DEFAULT_TOKEN_LIST)?.tokens.forEach((token) => {
+    if (chainId === undefined || token.chainId === chainId) {
+      tokens.set(token.address.toLowerCase(), {
+        ...(token as TokenInfo),
+        address: getAddress(token.address), // Token addresses are sometimes lowercase from token lists
+      })
+    }
+  })
+
+  tokenLists.forEach((tokenList, url) => {
+    if (url === SUSHI_DEFAULT_TOKEN_LIST) return // skip because we already ran this
+    tokenList.tokens.forEach((token) => {
+      if (
+        (chainId === undefined || token.chainId === chainId) &&
+        !tokens.has(token.address.toLowerCase())
+      ) {
+        tokens.set(token.address.toLowerCase(), {
+          ...(token as TokenInfo),
+          address: getAddress(token.address), // Token addresses are sometimes lowercase from token lists
+        })
+      }
+    })
+  })
+  return Array.from(tokens.values())
 }
