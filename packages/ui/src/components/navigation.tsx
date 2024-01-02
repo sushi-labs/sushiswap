70b1feac97188cc918033426d9716836bf0e58b9
0xMasayoshi <0xMasayoshi@protonmail.com>
2024-01-02 15:21:54 +0700

    feat: add Sushi Bar to  navigation menu

--- packages/ui/src/components/navigation.tsx
+++ packages/ui/src/components/navigation.tsx
@@ -31,6 +31,11 @@ const EXPLORE_NAVIGATION_LINKS: {
     description: 'Earn fees by providing liquidity.',
   },
   {
+    title: 'Sushi Bar',
+    href: '/bar',
+    description: 'Earn protocol fees by staking SUSHI.',
+  },
+  {
     title: 'Pay',
     href: '/furo',
     description: 'Automate salaries and vesting schedules.',
@@ -215,6 +220,23 @@ const Navigation: React.FC<NavProps> = ({
                 className={navigationMenuTriggerStyle()}
               >
                 Pools
+              </NavigationMenuLink>
+            )}
+          </NavigationMenuItem>
+          <NavigationMenuItem className="hidden md:block">
+            {legacyBehavior ? (
+              <NavigationMenuLink
+                asChild
+                className={navigationMenuTriggerStyle()}
+              >
+                <a href="/bar">Sushi Bar</a>
+              </NavigationMenuLink>
+            ) : (
+              <NavigationMenuLink
+                href="/bar"
+                className={navigationMenuTriggerStyle()}
+              >
+                Sushi Bar
               </NavigationMenuLink>
             )}
           </NavigationMenuItem>
