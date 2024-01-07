63ee6574a903f0b576cc4c303cc7afe982c79cb7
LufyCZ <me@lufy.cz>
2024-01-07 00:14:58 +0000

    fix(apps/bonds): button color

--- apps/evm/src/ui/bonds/bonds-widget.tsx
+++ apps/evm/src/ui/bonds/bonds-widget.tsx
@@ -19,7 +19,12 @@ import {
   CardHeader,
   CardTitle,
 } from '@sushiswap/ui'
-import { Checker, Web3Input, useBondMarketDetails } from '@sushiswap/wagmi'
+import {
+  Checker,
+  Web3Input,
+  useBondMarketDetails,
+  useNetwork,
+} from '@sushiswap/wagmi'
 import { CheckerProvider } from '@sushiswap/wagmi/systems/Checker/Provider'
 import format from 'date-fns/format'
 import formatDistance from 'date-fns/formatDistance'
@@ -33,6 +38,8 @@ export const BondsWidget = ({ bond: staleBond }: { bond: Bond }) => {
   const [quoteToken, payoutToken] = useMemo(() => {
     return [new Token(staleBond.quoteToken), new Token(staleBond.payoutToken)]
   }, [staleBond.quoteToken, staleBond.payoutToken])
+
+  const { chain } = useNetwork()
 
   const { maxAmountAccepted, currentCapacity, marketPrice, discount } =
     useBondMarketDetails({
@@ -158,62 +165,57 @@ export const BondsWidget = ({ bond: staleBond }: { bond: Bond }) => {
   const buttonText = isNegativeDiscount ? 'Bond Anyways' : 'Bond'
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Bond</CardTitle>
-        <CardDescription>
-          Acquire vested tokens at a discount using the widget below.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="flex flex-col gap-4">
-          <Web3Input.Currency
-            type="INPUT"
-            className="border border-accent px-3 py-1.5 !rounded-xl"
-            loading={false}
-            value={quoteInputField}
-            onChange={onQuoteInput}
-            currency={quoteToken}
-            chainId={staleBond.chainId}
-          />
-          <div className="flex items-center justify-center mt-[-24px] mb-[-24px] z-10">
-            <div className="p-1 bg-white dark:bg-slate-900 border border-accent rounded-full">
-              <ArrowDownIcon
-                width={16}
-                height={16}
-                className="text-muted-foreground"
-              />
+    <CheckerProvider>
+      <Card>
+        <CardHeader>
+          <CardTitle>Bond</CardTitle>
+          <CardDescription>
+            Acquire vested tokens at a discount using the widget below.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-4">
+            <Web3Input.Currency
+              type="INPUT"
+              className="border border-accent px-3 py-1.5 !rounded-xl"
+              loading={false}
+              value={quoteInputField}
+              onChange={onQuoteInput}
+              currency={quoteToken}
+              chainId={staleBond.chainId}
+            />
+            <div className="flex items-center justify-center mt-[-24px] mb-[-24px] z-10">
+              <div className="p-1 bg-white dark:bg-slate-900 border border-accent rounded-full">
+                <ArrowDownIcon
+                  width={16}
+                  height={16}
+                  className="text-muted-foreground"
+                />
+              </div>
             </div>
+            <Web3Input.Currency
+              type="OUTPUT"
+              className="border border-accent px-3 py-1.5 !rounded-xl"
+              value={payoutInputField}
+              onChange={onPayoutInput}
+              currency={payoutToken}
+              chainId={staleBond.chainId}
+            />
           </div>
-          <Web3Input.Currency
-            type="OUTPUT"
-            className="border border-accent px-3 py-1.5 !rounded-xl"
-            value={payoutInputField}
-            onChange={onPayoutInput}
-            currency={payoutToken}
-            chainId={staleBond.chainId}
-          />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <div className="flex flex-col">
-          {isMounted ? (
-            <CheckerProvider>
-              <Checker.Connect variant={buttonVariant}>
-                <Checker.Network
-                  chainId={staleBond.chainId}
-                  variant={buttonVariant}
-                >
+        </CardContent>
+        <CardFooter>
+          <div className="flex flex-col">
+            {isMounted ? (
+              <Checker.Connect>
+                <Checker.Network chainId={staleBond.chainId}>
                   <Checker.Amounts
                     chainId={staleBond.chainId}
                     amounts={[quoteAmount]}
-                    variant={buttonVariant}
                   >
                     <Checker.ApproveERC20
                       id="approve-erc20"
                       amount={quoteAmount}
                       contract={staleBond.tellerAddress}
-                      variant={buttonVariant}
                     >
                       <Checker.Success tag={APPROVE_TAG_BONDS}>
                         <Checker.Guard
@@ -246,85 +248,87 @@ export const BondsWidget = ({ bond: staleBond }: { bond: Bond }) => {
                   </Checker.Amounts>
                 </Checker.Network>
               </Checker.Connect>
-            </CheckerProvider>
-          ) : (
-            <Button size="xl" fullWidth disabled>
-              Enter Amount
-            </Button>
-          )}
-          <Collapsible
-            open={quoteAmount.greaterThan(0)}
-            className="gap-4 flex flex-col"
-          >
-            <div className="mt-4 flex items-start px-4 py-3 rounded-xl bg-red/20">
-              <input
-                id="expert-checkbox"
-                type="checkbox"
-                checked={noDiscountConfirmed}
-                onChange={(e) => setNoDiscountConfirmed(e.target.checked)}
-                className="cursor-pointer mr-1 w-5 h-5 mt-0.5 text-red-500 !ring-red-600 bg-white border-red rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 scale-75"
-              />
-              <label
-                htmlFor="expert-checkbox"
-                className="text-sm ml-2 font-medium text-red-500"
-              >
-                I understand this market is currently priced at a premium, it is
-                cheaper to buy on the market.
-              </label>
-            </div>
+            ) : (
+              <Button size="xl" fullWidth disabled>
+                Enter Amount
+              </Button>
+            )}
+            <Collapsible
+              open={
+                quoteAmount.greaterThan(0) && chain?.id === staleBond.chainId
+              }
+              className="gap-4 flex flex-col"
+            >
+              <div className="mt-4 flex items-start px-4 py-3 rounded-xl bg-red/20">
+                <input
+                  id="expert-checkbox"
+                  type="checkbox"
+                  checked={noDiscountConfirmed}
+                  onChange={(e) => setNoDiscountConfirmed(e.target.checked)}
+                  className="cursor-pointer mr-1 w-5 h-5 mt-0.5 text-red-500 !ring-red-600 bg-white border-red rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 scale-75"
+                />
+                <label
+                  htmlFor="expert-checkbox"
+                  className="text-sm ml-2 font-medium text-red-500"
+                >
+                  I understand this market is currently priced at a premium, it
+                  is cheaper to buy on the market.
+                </label>
+              </div>
 
-            <div className=" w-full px-2 flex flex-col gap-2">
-              <div className="flex justify-between items-center gap-2">
-                <span className="text-sm text-gray-700 dark:text-slate-400">
-                  Discount
-                </span>
-                {discount !== undefined ? (
-                  <span
-                    className={classNames(
-                      isNegativeDiscount ? 'text-red' : 'text-green',
-                      'text-sm font-medium text-right',
-                    )}
-                  >
-                    {formatPercent(discount)}
+              <div className=" w-full px-2 flex flex-col gap-2">
+                <div className="flex justify-between items-center gap-2">
+                  <span className="text-sm text-gray-700 dark:text-slate-400">
+                    Discount
                   </span>
-                ) : (
-                  <SkeletonText fontSize="sm" />
-                )}
+                  {discount !== undefined ? (
+                    <span
+                      className={classNames(
+                        isNegativeDiscount ? 'text-red' : 'text-green',
+                        'text-sm font-medium text-right',
+                      )}
+                    >
+                      {formatPercent(discount)}
+                    </span>
+                  ) : (
+                    <SkeletonText fontSize="sm" />
+                  )}
+                </div>
+                <div className="flex justify-between items-center gap-2">
+                  <span className="text-sm text-gray-700 dark:text-slate-400">
+                    Cliff
+                  </span>
+                  <span className="text-sm font-medium text-gray-700 dark:text-slate-400 text-right">
+                    {isMounted &&
+                      formatDistance(
+                        getCliffTimestamp({
+                          vestingLength: staleBond.vesting,
+                          vestingType: staleBond.vestingType,
+                        }) * 1000,
+                        Date.now(),
+                      )}
+                  </span>
+                </div>
+                <div className="flex justify-between items-center gap-2">
+                  <span className="text-sm text-gray-700 dark:text-slate-400">
+                    Claimable Date
+                  </span>
+                  <span className="text-sm font-medium text-gray-700 dark:text-slate-400 text-right">
+                    {isMounted &&
+                      format(
+                        getCliffTimestamp({
+                          vestingLength: staleBond.vesting,
+                          vestingType: staleBond.vestingType,
+                        }) * 1000,
+                        'MMM dd, yyyy HH:mm',
+                      )}
+                  </span>
+                </div>
               </div>
-              <div className="flex justify-between items-center gap-2">
-                <span className="text-sm text-gray-700 dark:text-slate-400">
-                  Cliff
-                </span>
-                <span className="text-sm font-medium text-gray-700 dark:text-slate-400 text-right">
-                  {isMounted &&
-                    formatDistance(
-                      getCliffTimestamp({
-                        vestingLength: staleBond.vesting,
-                        vestingType: staleBond.vestingType,
-                      }) * 1000,
-                      Date.now(),
-                    )}
-                </span>
-              </div>
-              <div className="flex justify-between items-center gap-2">
-                <span className="text-sm text-gray-700 dark:text-slate-400">
-                  Claimable Date
-                </span>
-                <span className="text-sm font-medium text-gray-700 dark:text-slate-400 text-right">
-                  {isMounted &&
-                    format(
-                      getCliffTimestamp({
-                        vestingLength: staleBond.vesting,
-                        vestingType: staleBond.vestingType,
-                      }) * 1000,
-                      'MMM dd, yyyy HH:mm',
-                    )}
-                </span>
-              </div>
-            </div>
-          </Collapsible>
-        </div>
-      </CardFooter>
-    </Card>
+            </Collapsible>
+          </div>
+        </CardFooter>
+      </Card>
+    </CheckerProvider>
   )
 }
