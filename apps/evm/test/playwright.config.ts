c34f14eb20b908664a991a9dff56cc38b1c4d59f
Matthew Lilley <hello@matthewlilley.com>
2024-01-20 19:51:13 +0000

    chore: ignores

--- apps/evm/test/playwright.config.ts
+++ apps/evm/test/playwright.config.ts
@@ -20,7 +20,10 @@ const baseURL = `http://localhost:${PORT}`
 const config: PlaywrightTestConfig = {
   // quiet: true,
   // testMatch: 'pool.test.ts',
-  testIgnore: 'cross-chain-swap.test.ts',
+  testIgnore: [
+    // 'swap.test.test',
+    'cross-chain.test.ts'
+  ],
   /* Maximum time one test can run for. Defaults to 30s. */
   timeout: 180_000,
   expect: {
