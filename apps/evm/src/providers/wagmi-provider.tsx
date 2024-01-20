c1c4f3aa462b83aa1be74ee6fbad6c912ec4af0c
Matthew Lilley <hello@matthewlilley.com>
2024-01-20 23:47:06 +0000

    savepoint

--- apps/evm/src/providers/wagmi-provider.tsx
+++ apps/evm/src/providers/wagmi-provider.tsx
@@ -7,12 +7,8 @@ import { type FC, type ReactNode, useMemo } from 'react'
 
 const isTest = process.env.NEXT_PUBLIC_APP_ENV === 'test'
 
-// const config = !isTest ? createProductionConfig() : createTestConfig()
+const config = !isTest ? createProductionConfig() : createTestConfig()
 
 export const WagmiConfig: FC<{ children: ReactNode }> = ({ children }) => {
-  const config = useMemo(
-    () => (!isTest ? createProductionConfig() : createTestConfig()),
-    [],
-  )
   return <_WagmiConfig config={config}>{children}</_WagmiConfig>
 }
