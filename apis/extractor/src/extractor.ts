dd0a95a5328d740778a802dcf986596c586e32eb
Matthew Lilley <hello@matthewlilley.com>
2024-02-18 03:12:51 +0000

    chore: disable preload

--- apis/extractor/src/extractor.ts
+++ apis/extractor/src/extractor.ts
@@ -17,35 +17,34 @@ const extractor = new Extractor({
   },
 })
 
-const start = Date.now()
+// const start = Date.now()
+// fetch('https://token-list.sushi.com')
+//   .then((res) => res.json() as Promise<TokenList>)
+//   .then((tokenList) => {
+//     const additional = tokenList.tokens
+//       .filter((token) => token.chainId === CHAIN_ID)
+//       .map(
+//         (token) =>
+//           new Token({
+//             chainId: token.chainId,
+//             address: token.address,
+//             decimals: token.decimals,
+//             symbol: token.symbol,
+//             name: token.name,
+//           }),
+//       )
 
-fetch('https://token-list.sushi.com')
-  .then((res) => res.json() as Promise<TokenList>)
-  .then((tokenList) => {
-    const additional = tokenList.tokens
-      .filter((token) => token.chainId === CHAIN_ID)
-      .map(
-        (token) =>
-          new Token({
-            chainId: token.chainId,
-            address: token.address,
-            decimals: token.decimals,
-            symbol: token.symbol,
-            name: token.name,
-          }),
-      )
+//     return extractor.start(BASES_TO_CHECK_TRADES_AGAINST[CHAIN_ID], additional)
+//   })
+//   .then(() => {
+//     const time = Date.now() - start
+//     console.log('Preload complete, took', time * 1000, 'seconds')
+//   })
+//   .catch((e) => {
+//     console.log('Error fetching tokens to preload')
+//     throw e
+//   })
 
-    return extractor.start(BASES_TO_CHECK_TRADES_AGAINST[CHAIN_ID], additional)
-  })
-  .then(() => {
-    const time = Date.now() - start
-    console.log('Preload complete, took', time * 1000, 'seconds')
-  })
-  .catch((e) => {
-    console.log('Error fetching tokens to preload')
-    throw e
-  })
-
-// extractor.start(BASES_TO_CHECK_TRADES_AGAINST[CHAIN_ID])
+extractor.start(BASES_TO_CHECK_TRADES_AGAINST[CHAIN_ID])
 
 export default extractor
