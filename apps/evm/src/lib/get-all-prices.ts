359237b5ea51f2a1ce8a595f91f4857187d77a53
matthewlilley <matthewlilley@users.noreply.github.com>
2024-02-23 18:16:37 +0000

    chore: format

--- apps/evm/src/lib/get-all-prices.ts
+++ apps/evm/src/lib/get-all-prices.ts
@@ -1,23 +1,26 @@
-import { isPromiseFulfilled } from "sushi"
-import { EXTRACTOR_SUPPORTED_CHAIN_IDS, ExtractorSupportedChainId } from "sushi/config"
+import { isPromiseFulfilled } from 'sushi'
+import {
+  EXTRACTOR_SUPPORTED_CHAIN_IDS,
+  ExtractorSupportedChainId,
+} from 'sushi/config'
 
 export async function getAllPrices() {
-    const results = await Promise.allSettled(
-        EXTRACTOR_SUPPORTED_CHAIN_IDS.map((chainId) =>
-          fetch(`https://api.sushi.com/price/v1/${chainId}`).then((res) =>
-            res.json(),
-          ),
-        ),
+  const results = await Promise.allSettled(
+    EXTRACTOR_SUPPORTED_CHAIN_IDS.map((chainId) =>
+      fetch(`https://api.sushi.com/price/v1/${chainId}`).then((res) =>
+        res.json(),
+      ),
+    ),
+  )
+  return results.reduce(
+    (previousValue, currentValue, i) => {
+      previousValue[EXTRACTOR_SUPPORTED_CHAIN_IDS[i]] = isPromiseFulfilled(
+        currentValue,
       )
-      return results.reduce(
-        (previousValue, currentValue, i) => {
-          previousValue[EXTRACTOR_SUPPORTED_CHAIN_IDS[i]] = isPromiseFulfilled(
-            currentValue,
-          )
-            ? currentValue.value
-            : {}
-          return previousValue
-        },
-        {} as Record<ExtractorSupportedChainId, Record<string, number>>,
-      )
+        ? currentValue.value
+        : {}
+      return previousValue
+    },
+    {} as Record<ExtractorSupportedChainId, Record<string, number>>,
+  )
 }
