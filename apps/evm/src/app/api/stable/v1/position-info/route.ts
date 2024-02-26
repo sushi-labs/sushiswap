5ebabff260fe4da667d4b2a3e4d698d35b442b80
LufyCZ <me@lufy.cz>
2024-02-26 15:37:22 +0000

    feat(pwn): error handling

--- apps/evm/src/app/api/stable/v1/position-info/route.ts
+++ apps/evm/src/app/api/stable/v1/position-info/route.ts
@@ -49,97 +49,103 @@ export async function GET(request: NextRequest) {
 
   const args = result.data
 
-  const position = await getPosition({
-    chainId: args.chainId,
-    tokenId: args.positionId,
-  })
-
-  const [token0, token1] = await Promise.all([
-    getToken(args.chainId, position.token0),
-    getToken(args.chainId, position.token1),
-  ])
-
-  const [{ pool, poolAddress }, prices] = await Promise.all([
-    getPool({
+  try {
+    const position = await getPosition({
       chainId: args.chainId,
-      token0: new Token({ chainId: args.chainId, ...token0 }),
-      token1: new Token({ chainId: args.chainId, ...token1 }),
-      feeAmount: position.fee as SushiSwapV3FeeAmount,
-    }),
-    getPrices({ chainId: args.chainId }),
-  ])
+      tokenId: args.positionId,
+    })
 
-  const { amount0, amount1 } = new Position({
-    pool,
-    liquidity: position.liquidity.toString(),
-    tickLower: position.tickLower,
-    tickUpper: position.tickUpper,
-  })
+    const [token0, token1] = await Promise.all([
+      getToken(args.chainId, position.token0),
+      getToken(args.chainId, position.token1),
+    ])
 
-  const token0PriceUSD = prices[getAddress(token0.address)] || 0
-  const token1PriceUSD = prices[getAddress(token1.address)] || 0
+    const [{ pool, poolAddress }, prices] = await Promise.all([
+      getPool({
+        chainId: args.chainId,
+        token0: new Token({ chainId: args.chainId, ...token0 }),
+        token1: new Token({ chainId: args.chainId, ...token1 }),
+        feeAmount: position.fee as SushiSwapV3FeeAmount,
+      }),
+      getPrices({ chainId: args.chainId }),
+    ])
 
-  const amount0USD = Number(amount0.toFixed(token0.decimals)) * token0PriceUSD
-  const amount1USD = Number(amount1.toFixed(token1.decimals)) * token1PriceUSD
-
-  const positionValueUSD = amount0USD + amount1USD
-
-  const fees0USD =
-    (Number(position.fees.amount0) / 10 ** token0.decimals) * token0PriceUSD
-  const fees1USD =
-    (Number(position.fees.amount1) / 10 ** token1.decimals) * token1PriceUSD
-
-  const unclaimedFeesValueUSD = fees0USD + fees1USD
-
-  const totalValueUSD = positionValueUSD + unclaimedFeesValueUSD
-
-  const data = {
-    poolAddress,
-    owner: position.owner,
-    token0: {
-      id: token0.id,
-      chainId: args.chainId,
-      address: token0.address,
-      symbol: token0.symbol,
-      decimals: token0.decimals,
-      priceUSD: token0PriceUSD,
-    },
-    token1: {
-      id: token1.id,
-      chainId: args.chainId,
-      address: token1.address,
-      symbol: token1.symbol,
-      decimals: token1.decimals,
-      priceUSD: token1PriceUSD,
-    },
-    poolFeeAmount: formatPercent(position.fee / 1000000),
-    position: {
-      amount0: String(amount0.quotient),
-      amount0Formatted: Number(amount0.quotient) / 10 ** token0.decimals,
-      amount0USD: amount0USD,
-      amount1: String(amount1.quotient),
-      amount1Formatted: Number(amount1.quotient) / 10 ** token1.decimals,
-      amount1USD: amount1USD,
-    },
-    fees: {
-      amount0: String(position.fees.amount0),
-      amount0Formatted: Number(position.fees.amount0) / 10 ** token0.decimals,
-      amount0USD: fees0USD,
-      amount1: String(position.fees.amount1),
-      amount1Formatted: Number(position.fees.amount1) / 10 ** token1.decimals,
-      amount1USD: fees1USD,
-    },
-    positionValueUSD,
-    unclaimedFeesValueUSD,
-    totalValueUSD,
-    range: {
+    const { amount0, amount1 } = new Position({
+      pool,
+      liquidity: position.liquidity.toString(),
       tickLower: position.tickLower,
       tickUpper: position.tickUpper,
-    },
-    inRange:
-      pool.tickCurrent >= position.tickLower &&
-      pool.tickCurrent <= position.tickUpper,
-  }
+    })
 
-  return NextResponse.json(data, { headers: CORS })
+    const token0PriceUSD = prices[getAddress(token0.address)] || 0
+    const token1PriceUSD = prices[getAddress(token1.address)] || 0
+
+    const amount0USD = Number(amount0.toFixed(token0.decimals)) * token0PriceUSD
+    const amount1USD = Number(amount1.toFixed(token1.decimals)) * token1PriceUSD
+
+    const positionValueUSD = amount0USD + amount1USD
+
+    const fees0USD =
+      (Number(position.fees.amount0) / 10 ** token0.decimals) * token0PriceUSD
+    const fees1USD =
+      (Number(position.fees.amount1) / 10 ** token1.decimals) * token1PriceUSD
+
+    const unclaimedFeesValueUSD = fees0USD + fees1USD
+
+    const totalValueUSD = positionValueUSD + unclaimedFeesValueUSD
+
+    const data = {
+      poolAddress,
+      owner: position.owner,
+      token0: {
+        id: token0.id,
+        chainId: args.chainId,
+        address: token0.address,
+        symbol: token0.symbol,
+        decimals: token0.decimals,
+        priceUSD: token0PriceUSD,
+      },
+      token1: {
+        id: token1.id,
+        chainId: args.chainId,
+        address: token1.address,
+        symbol: token1.symbol,
+        decimals: token1.decimals,
+        priceUSD: token1PriceUSD,
+      },
+      poolFeeAmount: formatPercent(position.fee / 1000000),
+      position: {
+        amount0: String(amount0.quotient),
+        amount0Formatted: Number(amount0.quotient) / 10 ** token0.decimals,
+        amount0USD: amount0USD,
+        amount1: String(amount1.quotient),
+        amount1Formatted: Number(amount1.quotient) / 10 ** token1.decimals,
+        amount1USD: amount1USD,
+      },
+      fees: {
+        amount0: String(position.fees.amount0),
+        amount0Formatted: Number(position.fees.amount0) / 10 ** token0.decimals,
+        amount0USD: fees0USD,
+        amount1: String(position.fees.amount1),
+        amount1Formatted: Number(position.fees.amount1) / 10 ** token1.decimals,
+        amount1USD: fees1USD,
+      },
+      positionValueUSD,
+      unclaimedFeesValueUSD,
+      totalValueUSD,
+      range: {
+        tickLower: position.tickLower,
+        tickUpper: position.tickUpper,
+      },
+      inRange:
+        pool.tickCurrent >= position.tickLower &&
+        pool.tickCurrent <= position.tickUpper,
+    }
+
+    return NextResponse.json(data, { headers: CORS })
+  } catch (e) {
+    if (e instanceof Error) {
+      return NextResponse.json({ error: e.message }, { status: 500 })
+    }
+  }
 }
