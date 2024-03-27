7b044d32f7a2b4a4060f26c4b718f16ac0f02dac
Ilya Lyalin <bretnwood@gmail.com>
2024-03-27 12:58:18 +0300

    feat: +prices statistics

--- apis/router/src/handlers/price/index.ts
+++ apis/router/src/handlers/price/index.ts
@@ -2,7 +2,11 @@ import { Request, Response } from 'express'
 import { CHAIN_ID, ROUTER_CONFIG } from '../../config.js'
 import { extractorClient } from '../../index.js'
 import { priceUpdateInterval, prices } from '../../prices.js'
+import { PricesRequestStatistics } from './PricesRequestStatistics.js'
 import { Currency, allPricesSchema, singleAddressSchema } from './schema.js'
+
+const priceStatistics = new PricesRequestStatistics(60_000) // update log once per min
+priceStatistics.start()
 
 export const pricesHandler = (req: Request, res: Response) => {
   const { currency } = allPricesSchema.parse(req.params)
@@ -12,6 +16,7 @@ export const pricesHandler = (req: Request, res: Response) => {
       currency === Currency.USD ? extractorClient?.getPrices() ?? {} : {},
     )
   else res.json(prices[currency])
+  priceStatistics.addAllPricesRequest()
 }
 
 export const priceByAddressHandler = (req: Request, res: Response) => {
@@ -21,16 +26,26 @@ export const priceByAddressHandler = (req: Request, res: Response) => {
   })
   res.setHeader('Cache-Control', `maxage=${priceUpdateInterval}`)
   if (ROUTER_CONFIG[CHAIN_ID]?.['experimantalPriceIncrementalMode'] === true) {
-    if (currency === Currency.USD) res.json(extractorClient?.getPrice(address))
-    else res.json()
+    if (currency === Currency.USD) {
+      const price = extractorClient?.getPrice(address)
+      if (price !== undefined) {
+        res.json(price)
+        priceStatistics.addKnownPriceRequest()
+      } else {
+        res.json()
+        priceStatistics.addUnKnownPriceRequest()
+      }
+    } else res.json()
   } else {
     if (
       prices[currency] === undefined ||
       prices[currency][address] === undefined
     ) {
       res.json()
+      priceStatistics.addUnKnownPriceRequest()
       return
     }
     res.json(prices[currency][address])
+    priceStatistics.addKnownPriceRequest()
   }
 }
