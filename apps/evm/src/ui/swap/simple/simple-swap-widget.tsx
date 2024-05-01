59b49bd7e07d9046c7ac25ce183bc3883df7e581
0xMasayoshi <0xMasayoshi@protonmail.com>
2024-05-01 23:12:54 +0700

    feat: skale bridge banner

--- apps/evm/src/ui/swap/simple/simple-swap-widget.tsx
+++ apps/evm/src/ui/swap/simple/simple-swap-widget.tsx
@@ -3,6 +3,7 @@
 import { CrossChainBanner } from '../cross-chain-banner'
 import { SwapModeButtons } from '../swap-mode-buttons'
 import { SimpleSwapAdditionalButton } from './simple-swap-additional-button'
+import { SimpleSwapBridgeBanner } from './simple-swap-bridge-banner'
 import { SimpleSwapHeader } from './simple-swap-header'
 import { SimpleSwapSettingsOverlay } from './simple-swap-settings-overlay'
 import { SimpleSwapSwitchTokensButton } from './simple-swap-switch-tokens-button'
@@ -16,6 +17,7 @@ import { SwapMaintenanceMessage } from './swap-maintenance-message'
 export const SimpleSwapWidget = () => {
   return (
     <div className="flex flex-col gap-4">
+      <SimpleSwapBridgeBanner />
       <SimpleSwapHeader />
       <div className="flex items-center justify-between">
         <SwapModeButtons />
