8f5558bf54f82d51454765d445b5b905ae95bb82
0xMasayoshi <0xMasayoshi@protonmail.com>
2024-06-08 01:03:18 +0700

    chore: remove 'Claim sFuel' button

--- apps/evm/src/ui/swap/simple/simple-swap-widget.tsx
+++ apps/evm/src/ui/swap/simple/simple-swap-widget.tsx
@@ -2,7 +2,6 @@
 
 import { CrossChainBanner } from '../cross-chain-banner'
 import { SwapModeButtons } from '../swap-mode-buttons'
-import { SimpleSwapAdditionalButton } from './simple-swap-additional-button'
 import { SimpleSwapBridgeBanner } from './simple-swap-bridge-banner'
 import { SimpleSwapHeader } from './simple-swap-header'
 import { SimpleSwapSettingsOverlay } from './simple-swap-settings-overlay'
@@ -30,7 +29,6 @@ export const SimpleSwapWidget = () => {
       <div className="flex flex-col">
         <SimpleSwapToken1Input />
         <SimpleSwapTradeButton />
-        <SimpleSwapAdditionalButton />
       </div>
       <SimpleSwapTradeStats />
       <SimpleSwapTokenNotFoundDialog />
