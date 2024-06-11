e6dea278cb92ff8f9ac0265e38a3f0f59f7380a0
0xMasayoshi <0xMasayoshi@protonmail.com>
2024-06-11 16:01:17 +0700

    chore: remove Ruby from SKALE config

--- apis/extractor/src/config.ts
+++ apis/extractor/src/config.ts
@@ -785,16 +785,7 @@ export const EXTRACTOR_CONFIG: Record<
   },
   [ChainId.SKALE_EUROPA]: {
     client: createPublicClient(extractorClientConfig(ChainId.SKALE_EUROPA)),
-    factoriesV2: [
-      sushiswapV2Factory(ChainId.SKALE_EUROPA),
-      {
-        address: '0x71f7BbbB33550fa5d70CA3F7eeAD87529f2DC3C8' as Address,
-        provider: LiquidityProviders.Ruby,
-        fee: 0.003,
-        initCodeHash:
-          '0xba9f7d123cf1f1b0f57891be300d90939d1a591af80a90cfb7e904a821927963',
-      },
-    ],
+    factoriesV2: [sushiswapV2Factory(ChainId.SKALE_EUROPA)],
     factoriesV3: [sushiswapV3Factory(ChainId.SKALE_EUROPA)],
     tickHelperContractV3: SUSHISWAP_V3_TICK_LENS[
       ChainId.SKALE_EUROPA
