17187981c477df081223a50f46ff6470deb8d843
rouzwelt <rouzweltt@gmail.com>
2024-06-26 04:19:25 +0000

    init

--- protocols/route-processor/test/DataFetcher.test.ts
+++ protocols/route-processor/test/DataFetcher.test.ts
@@ -11,7 +11,7 @@ import {
   USDT,
   WNATIVE,
 } from 'sushi/currency'
-import { DataFetcher, LiquidityProviders } from 'sushi/router'
+import { DataFetcher, LiquidityProviders, Router } from 'sushi/router'
 
 async function testDF(
   _chainName: string,
@@ -69,6 +69,40 @@ function reportMissingDexes(reports: Record<string, number>[]): {
   else return { hasMissingDex: false, missingDexNames }
 }
 
+// tries to find a route for a token pair from current fetched pools
+function findRoute(
+  dataFetcher: DataFetcher,
+  fromToken: Type,
+  toToken: Type,
+  chainId: ChainId,
+): boolean {
+  try {
+    // find the best route map
+    const pcMap = dataFetcher.getCurrentPoolCodeMap(fromToken, toToken)
+    const route = Router.findBestRoute(
+      pcMap,
+      chainId,
+      fromToken,
+      BigInt(`1${'0'.repeat(fromToken.decimals)}`),
+      toToken,
+      30e9,
+    )
+    // call rp4 route data encoder to build route data
+    Router.routeProcessor4Params(
+      pcMap,
+      route,
+      fromToken,
+      toToken,
+      `0x${'1'.repeat(40)}`,
+      `0x${'2'.repeat(40)}`,
+    )
+    return route.status !== 'NoWay'
+  } catch {
+    // no route found
+    return false
+  }
+}
+
 // exclude testnets and unsupported chains (chains with no dex/pool)
 const excludedChains = [
   ...TESTNET_CHAIN_IDS,
@@ -93,6 +127,7 @@ async function runTest() {
         dataFetcher.startDataFetching()
         console.log(chName)
         const allFoundPools = []
+        const foundRouteReports = []
 
         // a pool with this pair is available in most dexes and chains, but some may not have this, so
         // for those, other pairs are tried if there happened to be a missing dex from previous results
@@ -107,11 +142,22 @@ async function runTest() {
           ),
         )
 
+        // check that route is found and rp4 encoder encoded a route data correctly
+        // this is repeated for each pair throughout the test
+        foundRouteReports.push(
+          findRoute(
+            dataFetcher,
+            WNATIVE[chainId],
+            USDC[chainId as keyof typeof USDC],
+            chainId,
+          ),
+        )
+
         // only for Dfyn and JetSwap on fantom chain
         if (
           chainId === ChainId.FANTOM &&
           reportMissingDexes(allFoundPools).hasMissingDex
-        )
+        ) {
           allFoundPools.push(
             await testDF(
               chName,
@@ -122,12 +168,21 @@ async function runTest() {
               'DAI',
             ),
           )
+          foundRouteReports.push(
+            findRoute(
+              dataFetcher,
+              WNATIVE[chainId],
+              DAI[chainId as keyof typeof DAI],
+              chainId,
+            ),
+          )
+        }
 
         // only for Blast chain
         if (
           chainId === ChainId.BLAST &&
           reportMissingDexes(allFoundPools).hasMissingDex
-        )
+        ) {
           allFoundPools.push(
             await testDF(
               chName,
@@ -138,51 +193,80 @@ async function runTest() {
               'USDB',
             ),
           )
+          foundRouteReports.push(
+            findRoute(
+              dataFetcher,
+              WNATIVE[chainId],
+              USDB[chainId as keyof typeof USDB],
+              chainId,
+            ),
+          )
+        }
 
         // only for Moonbeam chain
         if (
           chainId === ChainId.MOONBEAM &&
           reportMissingDexes(allFoundPools).hasMissingDex
-        )
+        ) {
+          const token = new Token({
+            chainId: ChainId.MOONBEAM,
+            address: '0xA649325Aa7C5093d12D6F98EB4378deAe68CE23F',
+            decimals: 18,
+            symbol: 'BUSD',
+          })
           allFoundPools.push(
             await testDF(
               chName,
               dataFetcher,
-              new Token({
-                chainId: ChainId.MOONBEAM,
-                address: '0xA649325Aa7C5093d12D6F98EB4378deAe68CE23F',
-                decimals: 18,
-                symbol: 'BUSD',
-              }),
+              token,
               USDC[chainId as keyof typeof USDC],
               'USDC',
               'BUSD',
             ),
           )
+          foundRouteReports.push(
+            findRoute(
+              dataFetcher,
+              token,
+              USDC[chainId as keyof typeof USDC],
+              chainId,
+            ),
+          )
+        }
 
         // only for Elk dex on Moonriver since it only has 1 pool deployed which is with following pair
         if (
           chainId === ChainId.MOONRIVER &&
           reportMissingDexes(allFoundPools).hasMissingDex
-        )
+        ) {
+          const token = new Token({
+            chainId: ChainId.MOONRIVER,
+            address: '0xE1C110E1B1b4A1deD0cAf3E42BfBdbB7b5d7cE1C',
+            decimals: 18,
+            symbol: 'ELK',
+          })
           allFoundPools.push(
             await testDF(
               chName,
               dataFetcher,
               DAI[chainId as keyof typeof DAI],
-              new Token({
-                chainId: ChainId.MOONRIVER,
-                address: '0xE1C110E1B1b4A1deD0cAf3E42BfBdbB7b5d7cE1C',
-                decimals: 18,
-                symbol: 'ELK',
-              }),
+              token,
               'DAI',
               'ELK',
             ),
           )
+          foundRouteReports.push(
+            findRoute(
+              dataFetcher,
+              DAI[chainId as keyof typeof DAI],
+              token,
+              chainId,
+            ),
+          )
+        }
 
         // shared pairs for all chains and dexes
-        if (reportMissingDexes(allFoundPools).hasMissingDex)
+        if (reportMissingDexes(allFoundPools).hasMissingDex) {
           allFoundPools.push(
             await testDF(
               chName,
@@ -193,8 +277,17 @@ async function runTest() {
               'USDT',
             ),
           )
+          foundRouteReports.push(
+            findRoute(
+              dataFetcher,
+              WNATIVE[chainId],
+              USDT[chainId as keyof typeof USDT],
+              chainId,
+            ),
+          )
+        }
 
-        if (reportMissingDexes(allFoundPools).hasMissingDex)
+        if (reportMissingDexes(allFoundPools).hasMissingDex) {
           allFoundPools.push(
             await testDF(
               chName,
@@ -205,8 +298,17 @@ async function runTest() {
               'FRAX',
             ),
           )
+          foundRouteReports.push(
+            findRoute(
+              dataFetcher,
+              SUSHI[chainId as keyof typeof SUSHI],
+              FRAX[chainId as keyof typeof FRAX],
+              chainId,
+            ),
+          )
+        }
 
-        if (reportMissingDexes(allFoundPools).hasMissingDex)
+        if (reportMissingDexes(allFoundPools).hasMissingDex) {
           allFoundPools.push(
             await testDF(
               chName,
@@ -217,9 +319,22 @@ async function runTest() {
               'USDT',
             ),
           )
+          foundRouteReports.push(
+            findRoute(
+              dataFetcher,
+              SUSHI[chainId as keyof typeof SUSHI],
+              USDT[chainId as keyof typeof USDT],
+              chainId,
+            ),
+          )
+        }
 
         dataFetcher.stopDataFetching()
 
+        // should have found route
+        assert.ok(foundRouteReports.some((v) => v))
+
+        // should not have any missing dex
         const { hasMissingDex, missingDexNames } =
           reportMissingDexes(allFoundPools)
         if (hasMissingDex) {
