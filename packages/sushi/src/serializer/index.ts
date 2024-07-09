4f89838b5e2e12df07786d718b3ea240be88864e
Matthew Lilley <hello@matthewlilley.com>
2024-07-09 22:35:21 +0100

    fix: move snapshot utils to rp test utils

--- packages/sushi/src/serializer/index.ts
+++ packages/sushi/src/serializer/index.ts
@@ -1,8 +1,4 @@
-import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url'
 import serializer from 'serialijse'
-import { ChainId } from 'sushi/chain'
 import { Native } from '../currency/index.js'
 import {
   BentoBridgePoolCode,
@@ -147,47 +143,6 @@ export function serializePoolCodesJSON(pools: PoolCode[]): object {
 
 export function deserializePoolCodesJSON(data: string): PoolCode[] {
   const poolCodes = serializer.deserialize(data) as PoolCode[]
-  restoreAfterSerialization(poolCodes)
-  return poolCodes
-}
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-// default dir for pools snapshots
-const snapshotDirDefault = path.resolve(__dirname, '../pool-snapshots/')
-
-export async function savePoolSnapshot(
-  poolCodes: PoolCode[],
-  chainId: ChainId,
-  blockNumber: number | undefined,
-  directory?: string,
-) {
-  // pools preparation for serialization
-  makeSerializable(poolCodes)
-  const data = serializer.serialize(poolCodes)
-  const obj = JSON.parse(data)
-  const humanReadableStr = JSON.stringify(obj, undefined, '  ')
-  restoreAfterSerialization(poolCodes)
-
-  directory = directory || snapshotDirDefault
-  if (!existsSync(directory)) mkdirSync(directory)
-  writeFileSync(
-    path.resolve(directory, `${chainId}-${blockNumber}`),
-    humanReadableStr,
-  )
-}
-
-export async function loadPoolSnapshot(
-  chainId: ChainId,
-  blockNumber: number | undefined,
-  directory?: string,
-) {
-  directory = directory || snapshotDirDefault
-  const fileName = path.resolve(directory, `${chainId}-${blockNumber}`)
-  if (!existsSync(fileName)) return undefined
-  const str = readFileSync(fileName, 'utf8')
-  const poolCodes = serializer.deserialize(str) as PoolCode[]
   restoreAfterSerialization(poolCodes)
   return poolCodes
 }
