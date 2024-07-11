e2ee12cdb1ab491fb8291078e0bebc3b8e22f72c
Matthew Lilley <hello@matthewlilley.com>
2024-07-11 03:02:49 +0100

    Revert "try tracking with vercel custom events"

--- packages/telemetry/src/sendAnalyticsEvent.ts
+++ packages/telemetry/src/sendAnalyticsEvent.ts
@@ -1,14 +1,7 @@
-import { track } from '@vercel/analytics'
-
 export function sendAnalyticsEvent(
   eventName: string,
   eventProperties?: Record<string, unknown>,
 ) {
-  if (typeof window.gtag !== 'undefined') {
+  typeof window.gtag !== 'undefined' &&
     window.gtag('event', eventName, eventProperties)
-  }
-  track(
-    eventName,
-    eventProperties as Record<string, string | number | boolean | null>,
-  )
 }
