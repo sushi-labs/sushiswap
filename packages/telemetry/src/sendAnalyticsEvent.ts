69f8cdde2fb2187a173c36fd9c319fbc578ea00d
0xMasayoshi <0xMasayoshi@protonmail.com>
2024-07-18 00:06:08 +0700

    feat: use amplitude for telemetry logging

--- packages/telemetry/src/sendAnalyticsEvent.ts
+++ packages/telemetry/src/sendAnalyticsEvent.ts
@@ -1,7 +1,12 @@
+import { init, track } from '@amplitude/analytics-browser'
+
+typeof window !== 'undefined' && init('1a4621bcd5f74de90bb1cd5253e28f06')
+
 export function sendAnalyticsEvent(
   eventName: string,
   eventProperties?: Record<string, unknown>,
 ) {
-  typeof window.gtag !== 'undefined' &&
-    window.gtag('event', eventName, eventProperties)
+  const origin = window.location.origin
+
+  track(eventName, { ...eventProperties, origin })
 }
