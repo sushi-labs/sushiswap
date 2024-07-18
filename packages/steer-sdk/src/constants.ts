b0dafd6f4b4ae0b5771f62beecc74bcfcc6a6285
OlaStenberg <hello@olastenberg.com>
2024-07-18 15:59:11 +0200

    feat(packages/steer-sdk): update steer subgraph urls, disable celo

--- packages/steer-sdk/src/constants.ts
+++ packages/steer-sdk/src/constants.ts
@@ -11,7 +11,7 @@ export const STEER_SUPPORTED_CHAIN_IDS = [
   ChainId.BASE,
   ChainId.AVALANCHE,
   ChainId.POLYGON_ZKEVM,
-  ChainId.CELO,
+  // ChainId.CELO, // No V3 Celo deployment yet
   ChainId.KAVA,
   ChainId.LINEA,
   ChainId.SCROLL,
@@ -37,7 +37,7 @@ export const STEER_PERIPHERY_ADDRESS: Record<SteerChainId, `0x${string}`> = {
   [ChainId.BASE]: '0x16BA7102271dC83Fff2f709691c2B601DAD7668e',
   [ChainId.AVALANCHE]: '0x5D8249e3F5f702e1Fd720167b40424fc2daDCd1e',
   [ChainId.POLYGON_ZKEVM]: '0xcA19bEc25A41443F35EeAE03411Dce87D8c0Edc4',
-  [ChainId.CELO]: '0xdca3251Ebe8f85458E8d95813bCb816460e4bef1',
+  // [ChainId.CELO]: '0xdca3251Ebe8f85458E8d95813bCb816460e4bef1',
   [ChainId.KAVA]: '0xf90107890B640387ec3b0474F0c61674AEbCb510',
   [ChainId.LINEA]: '0x0C5c5BEB833fD382b04e039f151942DC3D9A60ce',
   [ChainId.SCROLL]: '0xD90c8970708FfdFC403bdb56636621e3E9CCe921',
@@ -61,15 +61,14 @@ export const STEER_SUBGRAPH_URL: Record<SteerChainId, string> = {
   [ChainId.AVALANCHE]: `${DECENTRALIZED_HOST_BY_SUBGRAPH_ID}/GZotTj3rQJ8ZqVyodtK8TcnKcUxMgeF7mCJHGPYbu8dA`,
   [ChainId.POLYGON_ZKEVM]:
     'subgraph.steer.finance/zkevm/subgraphs/name/steerprotocol/steer-zkevm',
-  [ChainId.CELO]: `${DECENTRALIZED_HOST_BY_SUBGRAPH_ID}/BPaFHyfVrhv3pdjGodpQcWggAg1Bcrvc9SFc2t2BXeho`,
+  // [ChainId.CELO]: `${DECENTRALIZED_HOST_BY_SUBGRAPH_ID}/BPaFHyfVrhv3pdjGodpQcWggAg1Bcrvc9SFc2t2BXeho`,
   [ChainId.KAVA]:
     'subgraph.steer.finance/kava/subgraphs/name/steerprotocol/steer-kava-evm',
   [ChainId.LINEA]:
-    'subgraph.steer.finance/linea/subgraphs/name/steerprotocol/steer-linea',
+  'https://api.goldsky.com/api/public/project_clohj3ta78ok12nzs5m8yag0b/subgraphs/steer-protocol-linea/1.1.2/gn',
   [ChainId.SCROLL]:
     'subgraph.steer.finance/scroll/subgraphs/name/steerprotocol/steer-scroll',
-  [ChainId.FANTOM]:
-    'api.thegraph.com/subgraphs/name/rakeshbhatt10/steer-protocol-fantom-test',
+  [ChainId.FANTOM]: `${DECENTRALIZED_HOST_BY_SUBGRAPH_ID}/9uyX2WDuaxmcYh11ehUhU68M9uSCp5FXVQV2w4LqbpbV`,
   [ChainId.BLAST]:
     'api.goldsky.com/api/public/project_clohj3ta78ok12nzs5m8yag0b/subgraphs/steer-protocol-blast/1.1.1/gn',
   // [ChainId.MANTA]: 'subgraph.steer.finance/manta/subgraphs/name/steerprotocol/steer-manta'
