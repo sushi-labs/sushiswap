5199677b6a4d1d7a95d7a3f6bad114d5afb1628c
0xMasayoshi <0xMasayoshi@protonmail.com>
2024-08-03 20:45:46 +0700

    fix: network icon classnames

--- packages/ui/src/icons/network/circle/EthereumCircle.tsx
+++ packages/ui/src/icons/network/circle/EthereumCircle.tsx
@@ -2,12 +2,13 @@ import * as React from 'react'
 
 import { EthereumNaked } from '../naked/EthereumNaked'
 
+import classNames from 'classnames'
 import { IconComponent } from '../../../types'
 
 export const EthereumCircle: IconComponent = (props) => (
   <EthereumNaked
     {...props}
-    className="text-white"
+    className={classNames(props.className, 'text-white')}
     circle={<rect rx={64} width={128} height={128} fill="#627EEA" />}
   />
 )
