231e7b08f5c8ee4a9fa2af0de7536384b1359951
0xMasayoshi <0xMasayoshi@protonmail.com>
2024-08-08 21:55:29 +0700

    feat: explore/smart-pools

--- apps/web/src/app/(evm)/[chainId]/explore/layout.tsx
+++ apps/web/src/app/(evm)/[chainId]/explore/layout.tsx
@@ -1,5 +1,7 @@
-import { Container } from '@sushiswap/ui'
+import { Container, LinkInternal } from '@sushiswap/ui'
 import { GlobalStatsCharts } from 'src/ui/explore/global-stats-charts'
+import { PathnameButton } from 'src/ui/pathname-button'
+import { PoolsFiltersProvider } from 'src/ui/pool'
 import { ChainId } from 'sushi/chain'
 import { Hero } from './hero'
 
@@ -19,9 +21,41 @@ export default async function ExploreLayout({
           <GlobalStatsCharts chainId={+params.chainId as ChainId} />
         </div>
       </Container>
+      <Container maxWidth="7xl" className="px-4">
+        <div className="flex flex-wrap items-center gap-2 mb-4">
+          <LinkInternal
+            shallow={true}
+            scroll={false}
+            href={`/${params.chainId}/explore/pools`}
+          >
+            <PathnameButton
+              id="all-pools"
+              pathname={`/${params.chainId}/explore/pools`}
+              asChild
+              size="sm"
+            >
+              All Pools
+            </PathnameButton>
+          </LinkInternal>
+          <LinkInternal
+            shallow={true}
+            scroll={false}
+            href={`/${params.chainId}/explore/smart-pools`}
+          >
+            <PathnameButton
+              id="smart-pools"
+              pathname={`/${params.chainId}/explore/smart-pools`}
+              asChild
+              size="sm"
+            >
+              Smart Pools
+            </PathnameButton>
+          </LinkInternal>
+        </div>
+      </Container>
       <section className="flex flex-col flex-1">
         <div className="bg-gray-50 dark:bg-white/[0.02] border-t border-accent pt-4 pb-20 h-full">
-          {children}
+          <PoolsFiltersProvider>{children}</PoolsFiltersProvider>
         </div>
       </section>
     </>
--- apps/web/src/app/(evm)/[chainId]/explore/smart-pools/page.tsx
+++ apps/web/src/app/(evm)/[chainId]/explore/smart-pools/page.tsx
@@ -1,9 +1,8 @@
-import { getTopPools } from '@sushiswap/graph-client/data-api'
+import { getSmartPools } from '@sushiswap/graph-client/data-api'
 import { Container } from '@sushiswap/ui'
 import { unstable_cache } from 'next/cache'
 import React from 'react'
-import { PoolsTable } from 'src/ui/pool/PoolsTable'
-import { TableFiltersSmartPoolsOnly } from 'src/ui/pool/TableFilterSmartPoolsOnly'
+import { SmartPoolsTable } from 'src/ui/pool/SmartPoolsTable'
 import { TableFiltersFarmsOnly } from 'src/ui/pool/TableFiltersFarmsOnly'
 import { TableFiltersNetwork } from 'src/ui/pool/TableFiltersNetwork'
 import { TableFiltersPoolType } from 'src/ui/pool/TableFiltersPoolType'
@@ -11,14 +10,14 @@ import { TableFiltersResetButton } from 'src/ui/pool/TableFiltersResetButton'
 import { TableFiltersSearchToken } from 'src/ui/pool/TableFiltersSearchToken'
 import { ChainId } from 'sushi/chain'
 
-export default async function PoolsPage({
+export default async function SmartPoolsPage({
   params,
 }: {
   params: { chainId: string }
 }) {
-  const pools = await unstable_cache(
-    async () => getTopPools({ chainId: +params.chainId }),
-    ['pools', params.chainId],
+  const smartPools = await unstable_cache(
+    async () => getSmartPools({ chainId: +params.chainId }),
+    ['smart-pools', params.chainId],
     {
       revalidate: 60 * 3,
     },
@@ -31,10 +30,9 @@ export default async function PoolsPage({
         <TableFiltersPoolType />
         <TableFiltersNetwork chainId={+params.chainId as ChainId} />
         <TableFiltersFarmsOnly />
-        <TableFiltersSmartPoolsOnly />
         <TableFiltersResetButton />
       </div>
-      <PoolsTable pools={pools} />
+      <SmartPoolsTable smartPools={smartPools} />
     </Container>
   )
 }
