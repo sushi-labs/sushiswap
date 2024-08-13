6e2921d970bcbab5df447f31c11a48662afce0b1
matthewlilley <matthewlilley@users.noreply.github.com>
2024-08-13 16:12:20 +0000

    chore: format

--- apps/web/src/app/(evm)/swap/dca/page.tsx
+++ apps/web/src/app/(evm)/swap/dca/page.tsx
@@ -2,20 +2,21 @@ import { Container } from '@sushiswap/ui'
 import { Providers } from '../(simple)/providers'
 import dynamic from 'next/dynamic'
 
+const TWAPPanel = dynamic(
+  () => import('src/ui/swap/twap/twap').then((it) => it.TWAPPanel),
+  { ssr: false },
+)
 
-const TWAPPanel = dynamic(() => import('src/ui/swap/twap/twap').then(it => it.TWAPPanel), {ssr: false})
-  
 export const metadata = {
   title: 'SushiSwap',
 }
 
 export default function SwapTwapPage() {
-    return (
-      <Providers>
-        <Container maxWidth="lg" className="px-4">
-          <TWAPPanel />
-        </Container>
-      </Providers>
-    )
-  }
-  
+  return (
+    <Providers>
+      <Container maxWidth="lg" className="px-4">
+        <TWAPPanel />
+      </Container>
+    </Providers>
+  )
+}
