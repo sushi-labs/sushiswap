09e2adb34df5baa674b55250136069d991631a29
denis-orbs <denis@orbs.com>
2024-08-14 17:42:52 +0300

    dTWAP and dLIMIT by Orbs

--- apps/web/src/ui/swap/swap-mode-buttons.tsx
+++ apps/web/src/ui/swap/swap-mode-buttons.tsx
@@ -22,6 +22,16 @@ export const SwapModeButtons = () => {
           Swap
         </PathnameButton>
       </Link>
+      <Link href="/swap/limit">
+      <PathnameButton pathname="/swap/limit" size="sm">
+        Limit
+      </PathnameButton>
+    </Link>
+    <Link href="/swap/dca">
+      <PathnameButton pathname="/swap/dca" size="sm">
+        DCA
+      </PathnameButton>
+    </Link>
       <HoverCard>
         <Link href="/swap/cross-chain">
           <PathnameButton pathname="/swap/cross-chain" size="sm">
