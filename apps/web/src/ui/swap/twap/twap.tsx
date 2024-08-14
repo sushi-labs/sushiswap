9ed0c17daff0ce4a7ef7d59bfa999b060824dced
Matthew Lilley <hello@matthewlilley.com>
2024-08-14 13:01:47 +0100

    chore: lint

--- apps/web/src/ui/swap/twap/twap.tsx
+++ apps/web/src/ui/swap/twap/twap.tsx
@@ -128,7 +128,7 @@ const getTokenLogo = (currency: Currency) => {
     return `https://cdn.sushi.com/image/upload/${params.join(
       ',',
     )}/d_unknown.png/${src}`
-  } catch (error) {
+  } catch (_error) {
     return ''
   }
 }
@@ -231,7 +231,7 @@ function Provider({ isLimit }: { isLimit?: boolean }) {
     if (state.swapAmountString !== '1') {
       mutate.setSwapAmount('1')
     }
-  }, [state.swapAmountString])
+  }, [state.swapAmountString, mutate])
 
   return (
     <div className="flex flex-col gap-4">
