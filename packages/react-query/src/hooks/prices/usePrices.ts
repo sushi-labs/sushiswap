5861c3eb89f54b04408e705c61dff820b6fa51b9
LufyCZ <me@lufy.cz>
2024-08-20 14:38:03 +0000

    feat: move all prices to lowercase

--- packages/react-query/src/hooks/prices/usePrices.ts
+++ packages/react-query/src/hooks/prices/usePrices.ts
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
 import ms from 'ms'
+import { LowercaseMap } from 'sushi'
 import { API_BASE_URL } from 'sushi/config'
 import { withoutScientificNotation } from 'sushi/format'
 import { Fraction } from 'sushi/math'
-import { Address, getAddress, isAddress, parseUnits } from 'viem'
+import { Address, isAddress, parseUnits } from 'viem'
 
 interface UsePrices {
   chainId: number | undefined
@@ -18,13 +19,13 @@ export const usePrices = ({ chainId, enabled = true }: UsePrices) => {
         `${API_BASE_URL}/price/v1/${chainId}`,
       ).then((response) => response.json())
 
-      const priceMap = new Map<Address, Fraction>()
+      const priceMap = new LowercaseMap<Address, Fraction>()
 
       Object.entries(data).forEach(([address, _price]) => {
         const price = withoutScientificNotation(_price.toFixed(18))
         if (isAddress(address) && typeof price !== 'undefined') {
           priceMap.set(
-            getAddress(address),
+            address,
             new Fraction(
               parseUnits(price, 18).toString(),
               parseUnits('1', 18).toString(),
