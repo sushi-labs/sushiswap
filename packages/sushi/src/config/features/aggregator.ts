a72c1044513a87d57708c12e2d1d5e63ab8da00b
0xMasayoshi <0xMasayoshi@protonmail.com>
2024-09-03 04:21:28 +0800

    chore: hide navbar items on unsupported chains

--- packages/sushi/src/config/features/aggregator.ts
+++ packages/sushi/src/config/features/aggregator.ts
@@ -5,3 +5,10 @@ export const AGGREGATOR_ONLY_CHAIN_IDS = [
   ChainId.MANTLE,
   ChainId.ZKSYNC_ERA,
 ] as const
+
+export type AggregatorOnlyChainId = (typeof AGGREGATOR_ONLY_CHAIN_IDS)[number]
+
+export const isAggregatorOnlyChainId = (
+  chainId: ChainId,
+): chainId is AggregatorOnlyChainId =>
+  AGGREGATOR_ONLY_CHAIN_IDS.includes(chainId as AggregatorOnlyChainId)
