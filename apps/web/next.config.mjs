85be6e8d07d40f760af439f32049dfd0cee5337c
solidityteam <tyler@solidity.io>
2024-09-03 12:44:25 -0400

    chore: fix linting

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,126 +1,131 @@
-import { withSentryConfig } from '@sentry/nextjs'
-import defaultNextConfig from '@sushiswap/nextjs-config'
-import { withAxiom } from 'next-axiom'
+import { withSentryConfig } from "@sentry/nextjs";
+import defaultNextConfig from "@sushiswap/nextjs-config";
+import { withAxiom } from "next-axiom";
 
-import withBundleAnalyzer from '@next/bundle-analyzer'
+import withBundleAnalyzer from "@next/bundle-analyzer";
 const bundleAnalyzer = withBundleAnalyzer({
-  enabled: false && process.env.NODE_ENV !== 'development',
-})
+	enabled: false && process.env.NODE_ENV !== "development",
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = bundleAnalyzer({
-  ...defaultNextConfig,
-  experimental: {
-    ...defaultNextConfig.experimental,
-    testProxy: process.env.NEXT_PUBLIC_APP_ENV === 'test',
-  },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'test.sushi.com',
-          },
-        ],
-        destination: 'https://sushi.com/test/:path*',
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'aptos.sushi.com',
-          },
-        ],
-        destination: 'https://sushi.com/aptos/:path*',
-        permanent: true,
-      },
-      {
-        source: '/aptos',
-        permanent: true,
-        destination: '/aptos/swap',
-      },
-      {
-        source: '/',
-        permanent: true,
-        destination: '/swap',
-      },
-      {
-        source: '/discord{/}?',
-        permanent: true,
-        destination: 'https://discord.gg/ej78AWjy6Y',
-      },
-      {
-        source: '/github{/}?',
-        permanent: true,
-        destination: 'https://github.com/sushiswap',
-      },
-      {
-        source: '/twitter{/}?',
-        permanent: true,
-        destination: 'https://twitter.com/sushiswap',
-      },
-      {
-        source: '/instagram{/}?',
-        permanent: true,
-        destination: 'https://instagram.com/instasushiswap',
-      },
-      {
-        source: '/medium{/}?',
-        permanent: true,
-        destination: 'https://medium.com/sushiswap-org',
-      },
-      {
-        source: '/skale/swap',
-        permanent: true,
-        destination: '/swap?chainId=2046399126',
-      },
-      {
-        source: '/swap/cross-chain:path*',
-        permanent: true,
-        destination: '/cross-chain-swap:path*',
-      },
-    ]
-  },
-  async rewrites() {
-    return []
-  },
-})
+	...defaultNextConfig,
+	experimental: {
+		...defaultNextConfig.experimental,
+		testProxy: process.env.NEXT_PUBLIC_APP_ENV === "test",
+	},
+	async redirects() {
+		return [
+			{
+				source: "/:path*",
+				has: [
+					{
+						type: "host",
+						value: "test.sushi.com",
+					},
+				],
+				destination: "https://sushi.com/test/:path*",
+				permanent: true,
+			},
+			{
+				source: "/:path*",
+				has: [
+					{
+						type: "host",
+						value: "aptos.sushi.com",
+					},
+				],
+				destination: "https://sushi.com/aptos/:path*",
+				permanent: true,
+			},
+			{
+				source: "/aptos",
+				permanent: true,
+				destination: "/aptos/swap",
+			},
+			{
+				source: "/tron",
+				permanent: true,
+				destination: "/tron/swap",
+			},
+			{
+				source: "/",
+				permanent: true,
+				destination: "/swap",
+			},
+			{
+				source: "/discord{/}?",
+				permanent: true,
+				destination: "https://discord.gg/ej78AWjy6Y",
+			},
+			{
+				source: "/github{/}?",
+				permanent: true,
+				destination: "https://github.com/sushiswap",
+			},
+			{
+				source: "/twitter{/}?",
+				permanent: true,
+				destination: "https://twitter.com/sushiswap",
+			},
+			{
+				source: "/instagram{/}?",
+				permanent: true,
+				destination: "https://instagram.com/instasushiswap",
+			},
+			{
+				source: "/medium{/}?",
+				permanent: true,
+				destination: "https://medium.com/sushiswap-org",
+			},
+			{
+				source: "/skale/swap",
+				permanent: true,
+				destination: "/swap?chainId=2046399126",
+			},
+			{
+				source: "/swap/cross-chain:path*",
+				permanent: true,
+				destination: "/cross-chain-swap:path*",
+			},
+		];
+	},
+	async rewrites() {
+		return [];
+	},
+});
 
 export default withSentryConfig(withAxiom(nextConfig), {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
+	// For all available options, see:
+	// https://github.com/getsentry/sentry-webpack-plugin#options
 
-  org: 'sushi-j9',
-  project: 'evm',
+	org: "sushi-j9",
+	project: "evm",
 
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
+	// Only print logs for uploading source maps in CI
+	silent: !process.env.CI,
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+	// For all available options, see:
+	// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
+	// Upload a larger set of source maps for prettier stack traces (increases build time)
+	widenClientFileUpload: true,
 
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: '/monitoring',
+	// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+	// This can increase your server load as well as your hosting bill.
+	// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+	// side errors will fail.
+	tunnelRoute: "/monitoring",
 
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
+	// Hides source maps from generated client bundles
+	hideSourceMaps: true,
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
+	// Automatically tree-shake Sentry logger statements to reduce bundle size
+	disableLogger: true,
 
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-})
+	// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+	// See the following for more information:
+	// https://docs.sentry.io/product/crons/
+	// https://vercel.com/docs/cron-jobs
+	automaticVercelMonitors: true,
+});
--- apps/web/src/app/(non-evm)/tron/pool/page.tsx
+++ apps/web/src/app/(non-evm)/tron/pool/page.tsx
@@ -1,78 +1,59 @@
-'use client'
-import { ChevronRightIcon } from '@heroicons/react/24/outline'
-import {
-  Button,
-  Container,
-  LinkExternal,
-  LinkInternal,
-  typographyVariants,
-} from '@sushiswap/ui'
-import { DiscordIcon } from '@sushiswap/ui/icons/DiscordIcon'
-import { PoolsView } from '~tron/_common/ui/Pools/PoolsView'
+"use client";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import { Button, Container, LinkExternal, LinkInternal, typographyVariants } from "@sushiswap/ui";
+import { DiscordIcon } from "@sushiswap/ui/icons/DiscordIcon";
+import { PoolsView } from "~tron/_common/ui/Pools/PoolsView";
 
 export default function Pool() {
-  return (
-    <>
-      <Container maxWidth="7xl" className="px-4 pt-20 pb-10">
-        <section className="flex flex-col justify-between gap-12 lg:flex-row lg:items-start mb-12">
-          <div className="flex flex-col items-center flex-grow gap-6 lg:items-start">
-            <div className="flex flex-col">
-              <h1 className={typographyVariants({ variant: 'h1' })}>
-                Put your funds to work <br />
-                by providing liquidity.
-              </h1>
-              <p
-                className={typographyVariants({
-                  variant: 'lead',
-                  className: 'max-w-[500px]',
-                })}
-              >
-                When you add liquidity to a pool, you can receive a share of its
-                trading volume and potentially snag extra rewards when there are
-                incentives involved!
-              </p>
-            </div>
-            <div className="flex flex-col sm:flex-row w-full sm:w-[unset] gap-4">
-              <div className="flex items-center w-full">
-                <Button asChild size="lg">
-                  <LinkInternal href="/pool/add">
-                    I want to create a position
-                  </LinkInternal>
-                </Button>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col items-center gap-4 lg:items-end">
-            <div className="flex flex-col items-center gap-1 lg:items-end">
-              <span className="font-semibold lg:text-sm">
-                Looking for a partnership with Sushi?
-              </span>
-              <Button
-                className="flex-1 w-full sm:flex-0 sm:w-[unset]"
-                variant="link"
-                size="sm"
-                asChild
-              >
-                <LinkExternal href="https://rbieu62gj0f.typeform.com/to/KkrPkOFe">
-                  <div className="flex items-center">
-                    Join Onsen <ChevronRightIcon width={16} height={16} />
-                  </div>
-                </LinkExternal>
-              </Button>
-            </div>
-            <div className="flex flex-col items-center gap-1 lg:items-end">
-              <span className="font-semibold lg:text-sm">Need Help?</span>
-              <Button icon={DiscordIcon} variant="link" size="sm" asChild>
-                <LinkExternal href="https://discord.gg/NVPXN4e">
-                  Join our discord
-                </LinkExternal>
-              </Button>
-            </div>
-          </div>
-        </section>
-      </Container>
+	return (
+		<>
+			<Container maxWidth="7xl" className="px-4 pt-20 pb-10">
+				<section className="flex flex-col justify-between gap-12 lg:flex-row lg:items-start mb-12">
+					<div className="flex flex-col items-center flex-grow gap-6 lg:items-start">
+						<div className="flex flex-col">
+							<h1 className={typographyVariants({ variant: "h1" })}>
+								Put your funds to work <br />
+								by providing liquidity.
+							</h1>
+							<p
+								className={typographyVariants({
+									variant: "lead",
+									className: "max-w-[500px]",
+								})}>
+								When you add liquidity to a pool, you can receive a share of its trading volume and
+								potentially snag extra rewards when there are incentives involved!
+							</p>
+						</div>
+						<div className="flex flex-col sm:flex-row w-full sm:w-[unset] gap-4">
+							<div className="flex items-center w-full">
+								<Button asChild size="lg">
+									<LinkInternal href="/tron/pool/add">I want to create a position</LinkInternal>
+								</Button>
+							</div>
+						</div>
+					</div>
+					<div className="flex flex-col items-center gap-4 lg:items-end">
+						<div className="flex flex-col items-center gap-1 lg:items-end">
+							<span className="font-semibold lg:text-sm">Looking for a partnership with Sushi?</span>
+							<Button className="flex-1 w-full sm:flex-0 sm:w-[unset]" variant="link" size="sm" asChild>
+								<LinkExternal href="https://rbieu62gj0f.typeform.com/to/KkrPkOFe">
+									<div className="flex items-center">
+										Join Onsen <ChevronRightIcon width={16} height={16} />
+									</div>
+								</LinkExternal>
+							</Button>
+						</div>
+						<div className="flex flex-col items-center gap-1 lg:items-end">
+							<span className="font-semibold lg:text-sm">Need Help?</span>
+							<Button icon={DiscordIcon} variant="link" size="sm" asChild>
+								<LinkExternal href="https://discord.gg/NVPXN4e">Join our discord</LinkExternal>
+							</Button>
+						</div>
+					</div>
+				</section>
+			</Container>
 
-      <PoolsView />
-    </>
-  )
+			<PoolsView />
+		</>
+	);
 }
