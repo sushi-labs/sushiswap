a18cbde5f0ba907e362cfeee3dbe9b85acc0fd3a
LufyCZ <me@lufy.cz>
2024-09-04 09:40:31 +0000

    chore: disable sentry tracing, walletconnect logging patch

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -7,6 +7,8 @@ import * as Sentry from '@sentry/nextjs'
 Sentry.init({
   dsn: 'https://d15baeafe615a5dc057445e683b2c66c@o960777.ingest.us.sentry.io/4507504234659840',
 
+  enabled: process.env.NEXT_PUBLIC_APP_ENV !== 'test',
+
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
@@ -27,5 +29,4 @@ Sentry.init({
       blockAllMedia: false,
     }),
   ],
-  enabled: process.env.NEXT_PUBLIC_APP_ENV !== 'test',
 })
