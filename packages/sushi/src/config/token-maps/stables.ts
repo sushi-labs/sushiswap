abf14549f70e1d850c5e9171706604c1f5f28358
LufyCZ <me@lufy.cz>
2024-09-11 09:35:43 +0000

    feat(pkgs/sushi): add curtis

--- packages/sushi/src/config/token-maps/stables.ts
+++ packages/sushi/src/config/token-maps/stables.ts
@@ -237,4 +237,5 @@ export const STABLES = {
   [ChainId.AVALANCHE_TESTNET]: [],
   [ChainId.SEPOLIA]: [],
   [ChainId.GÖRLI]: [],
+  [ChainId.CURTIS]: [],
 } as const satisfies Record<ChainId, Token[]>
