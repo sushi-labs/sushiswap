b2698b7a629c5b5bb551717276f641a6fba34418
solidityteam <tyler@solidity.io>
2024-09-12 13:14:26 -0400

    chore: new router and factory contracts

--- apps/web/src/app/(non-evm)/tron/_common/constants/contracts.ts
+++ apps/web/src/app/(non-evm)/tron/_common/constants/contracts.ts
@@ -1,11 +1,5 @@
-import { IS_TESTNET } from './is-testnet'
+import { IS_TESTNET } from "./is-testnet";
 
-export const FACTORY_CONTRACT = IS_TESTNET
-  ? ''
-  : 'TKWJdrQkqHisa1X8HUdHEfREvTzw4pMAaY' //sun.io factory contract address
-export const ROUTER_CONTRACT = IS_TESTNET
-  ? ''
-  : 'TKzxdSv2FZKQrEqkKVgp5DcwEXBEKMg2Ax' //sun.io router contract address
-export const MULTICALL_CONTRACT = IS_TESTNET
-  ? ''
-  : 'TGXuuKAb4bnrn137u39EKbYzKNXvdCes98'
+export const FACTORY_CONTRACT = IS_TESTNET ? "" : "TPA5vJu579Ub5BRXETiEYZUs1SmGh7cDia"; //sun.io factory contract address
+export const ROUTER_CONTRACT = IS_TESTNET ? "" : "TG61TbGhkx757ATfceRbnSHD3kHzQ7tk97"; //sun.io router contract address
+export const MULTICALL_CONTRACT = IS_TESTNET ? "" : "TGXuuKAb4bnrn137u39EKbYzKNXvdCes98";
