bdd40bd48a4d6e9bdb4c1f123b5415785e999bdc
Matthew Lilley <hello@matthewlilley.com>
2024-09-13 19:37:55 +0100

    chore: removal of client side trades

--- apps/web/src/types/global.d.ts
+++ apps/web/src/types/global.d.ts
@@ -1,10 +1,6 @@
 import type { Address } from 'viem'
 
 export declare global {
-  interface Window {
-    useSwapApi?: boolean
-  }
-
   interface String {
     toLowerCase<T extends string>(this: T): T extends Address ? Address : string
   }
