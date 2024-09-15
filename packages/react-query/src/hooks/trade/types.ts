0cf4a6f61965f550370363bec43905a600024b90
matthewlilley <matthewlilley@users.noreply.github.com>
2024-09-15 18:23:26 +0000

    chore: lint

--- packages/react-query/src/hooks/trade/types.ts
+++ packages/react-query/src/hooks/trade/types.ts
@@ -1,17 +1,15 @@
-import { ChainId } from 'sushi/chain'
-import { Amount, Price, type Type } from 'sushi/currency'
-import { Percent } from 'sushi/math'
-import type { Address, WriteContractParameters } from 'viem'
-import z from 'zod'
-import { RouterLiquiditySource } from 'sushi/router'
 import {
   routeProcessor2Abi_processRoute,
   routeProcessor2Abi_transferValueAndprocessRoute,
 } from 'sushi/abi'
+import { ChainId } from 'sushi/chain'
+import { Amount, Price, type Type } from 'sushi/currency'
+import { Percent } from 'sushi/math'
+import { RouterLiquiditySource } from 'sushi/router'
+import type { Address, WriteContractParameters } from 'viem'
+import z from 'zod'
 import { legValidator, tradeValidator01 } from './validator01'
 import { tradeValidator02 } from './validator02'
-
-
 
 export interface UseTradeParams {
   chainId: ChainId
