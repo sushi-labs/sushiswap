6a727abf3f3d6f1535738352b837327997cdcebe
LufyCZ <me@lufy.cz>
2024-10-01 08:09:58 +0000

    feat(apps/web): add strapi banner to swap

---
+++ apps/web/src/ui/swap/strapi-banner/strapi-banner.tsx
@@ -0,0 +1,29 @@
+import { getBanners } from '@sushiswap/graph-client/strapi'
+import ms from 'ms'
+import { unstable_cache } from 'next/cache'
+import { cookies } from 'next/headers'
+import { StrapiBannerContent } from './strapi-banner-content'
+
+export async function StrapiBanner() {
+  let banners
+
+  try {
+    banners = await unstable_cache(() => getBanners(), ['banners'], {
+      revalidate: ms('1h'),
+    })()
+  } catch {}
+
+  if (!banners) return <></>
+
+  // Only supporting one active banner at a time for now
+  const activeBanner = banners.find((banner) => banner.isActive)
+
+  if (!activeBanner) return <></>
+
+  return (
+    <StrapiBannerContent
+      banner={activeBanner}
+      cookie={cookies().get('hidden-banner-ids')}
+    />
+  )
+}
