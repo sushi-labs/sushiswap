9988d3995ae15920db35c8335bc3f987a84ee242
solidityteam <tyler@solidity.io>
2024-10-03 19:37:11 -0400

    chore: loading placeholder

--- apps/web/src/app/(non-evm)/tron/_common/ui/Pools/PositionsTable/PositionAprCell.tsx
+++ apps/web/src/app/(non-evm)/tron/_common/ui/Pools/PositionsTable/PositionAprCell.tsx
@@ -5,23 +5,33 @@ import { FEE_PERCENTAGE } from "~tron/_common/constants/fee-percentage";
 import { useLPUsdValue } from "~tron/_common/lib/hooks/useLPUsdValue";
 import { IPositionRowData } from "./PositionsTable";
 import { useMemo } from "react";
+import { SkeletonText } from "@sushiswap/ui";
 
 export const PositionAprCell = ({ data }: { data: IPositionRowData }) => {
 	const { pairAddress, token0, token1, reserve0, reserve1 } = data;
-	const { data: ownership } = usePoolOwnership({ pairAddress });
+	const { data: ownership, isLoading: isLoadingOwnership } = usePoolOwnership({ pairAddress });
 	const ownedLp = Number(ownership?.ownership) ?? 0;
 	const ownedLpTokens = Number(ownership?.ownedSupply) ?? 0;
-	const { data: dayVolumeUSD } = useDayVolumeUSD({ pairAddress });
-	const { data: totalLPUsdValue } = useLPUsdValue({ token0, token1, reserve0, reserve1 });
+	const { data: dayVolumeUSD, isLoading: isLoadingDVol } = useDayVolumeUSD({ pairAddress });
+	const { data: totalLPUsdValue, isLoading: isLoadingLPV } = useLPUsdValue({
+		token0,
+		token1,
+		reserve0,
+		reserve1,
+	});
 
 	const apr = useMemo(() => {
 		const totalDailyFees = Number(FEE_PERCENTAGE) * Number(dayVolumeUSD ?? 0);
 		const annualizedFees = totalDailyFees * 365;
 		const userAnnualEarnings = annualizedFees * ownedLp;
 		const userLiquidityValue = ownedLpTokens * (totalLPUsdValue ?? 0);
-		const _apr = (userAnnualEarnings / userLiquidityValue) * 100;
-		return _apr;
+		const _apr = userAnnualEarnings / userLiquidityValue;
+		return Number.isNaN(_apr) ? 0 : _apr;
 	}, [dayVolumeUSD, totalLPUsdValue, ownedLp, ownedLpTokens]);
+
+	if (isLoadingLPV || isLoadingDVol || isLoadingOwnership) {
+		return <SkeletonText fontSize="lg" />;
+	}
 
 	return (
 		<div className="flex items-center gap-1">
--- apps/web/src/app/(non-evm)/tron/_common/ui/Pools/PositionsTable/PositionValueCell.tsx
+++ apps/web/src/app/(non-evm)/tron/_common/ui/Pools/PositionsTable/PositionValueCell.tsx
@@ -6,10 +6,10 @@ import { useLPUsdValue } from "~tron/_common/lib/hooks/useLPUsdValue";
 
 export const PositionValueCell = ({ data }: { data: IPositionRowData }) => {
 	const { token0, token1, reserve0, reserve1, pairAddress } = data;
-	const { data: ownership } = usePoolOwnership({ pairAddress });
+	const { data: ownership, isLoading: _isLoading } = usePoolOwnership({ pairAddress });
 	const { data: totalLPUsdValue, isLoading } = useLPUsdValue({ token0, token1, reserve0, reserve1 });
 
-	if (isLoading) {
+	if (isLoading || _isLoading) {
 		return <SkeletonText fontSize="lg" />;
 	}
 
@@ -19,7 +19,7 @@ export const PositionValueCell = ({ data }: { data: IPositionRowData }) => {
 		<div className="flex items-center gap-1">
 			<div className="flex flex-col gap-0.5">
 				<span className="flex items-center gap-1 text-sm font-medium text-gray-900 dark:text-slate-50">
-					{formatUSD(poolTvl)}
+					{formatUSD(Number.isNaN(poolTvl) ? 0 : poolTvl)}
 				</span>
 			</div>
 		</div>
