b65e1f2037415cd124eeb736466374a5f79c8661
0xMasayoshi <0xMasayoshi@protonmail.com>
2024-10-03 22:14:39 +0800

    fix: /swap spacing on aptos & tron

--- apps/web/src/app/(non-evm)/tron/swap/page.tsx
+++ apps/web/src/app/(non-evm)/tron/swap/page.tsx
@@ -17,8 +17,10 @@ export default function SwapSimplePage() {
   return (
     <Container maxWidth="lg" className="px-4">
       <div className="flex flex-col gap-4">
-        <div className="flex flex-col items-start gap-2">
+        <div />
+        <div className="flex flex-col items-start gap-2 mb-4 sm:mt-10 mt-2">
           <h1 className={typographyVariants({ variant: 'h1' })}>Trade</h1>
+          <div className="h-5" />
         </div>
         <div className="flex items-center justify-between">
           <SwitchSwapType />
