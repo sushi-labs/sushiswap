13b26cb77421b185d8911aa937d6eaa033020816
solidityteam <tyler@solidity.io>
2024-10-09 14:26:22 -0400

    chore: approve button height - tron

--- apps/web/src/app/(non-evm)/tron/_common/ui/Shared/ApproveToken.tsx
+++ apps/web/src/app/(non-evm)/tron/_common/ui/Shared/ApproveToken.tsx
@@ -1,198 +1,175 @@
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { createFailedToast, createInfoToast, createSuccessToast } from "@sushiswap/notifications";
 import {
-  createFailedToast,
-  createInfoToast,
-  createSuccessToast,
-} from '@sushiswap/notifications'
-import {
-  Button,
-  ButtonProps,
-  Command,
-  CommandGroup,
-  CommandItem,
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@sushiswap/ui'
-import { useWallet } from '@tronweb3/tronwallet-adapter-react-hooks'
-import { useState } from 'react'
-import { MAX_UINT256 } from '~tron/_common/constants/max-uint256'
-import { useTronWeb } from '~tron/_common/lib/hooks/useTronWeb'
-import { parseUnits, toBigNumber } from '~tron/_common/lib/utils/formatters'
-import {
-  getTransactionInfo,
-  parseTxnError,
-} from '~tron/_common/lib/utils/helpers'
-import { getTronscanTxnLink } from '~tron/_common/lib/utils/tronscan-helpers'
-import { IToken } from '~tron/_common/types/token-type'
+	Button,
+	ButtonProps,
+	Command,
+	CommandGroup,
+	CommandItem,
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@sushiswap/ui";
+import { useWallet } from "@tronweb3/tronwallet-adapter-react-hooks";
+import { useState } from "react";
+import { MAX_UINT256 } from "~tron/_common/constants/max-uint256";
+import { useTronWeb } from "~tron/_common/lib/hooks/useTronWeb";
+import { parseUnits, toBigNumber } from "~tron/_common/lib/utils/formatters";
+import { getTransactionInfo, parseTxnError } from "~tron/_common/lib/utils/helpers";
+import { getTronscanTxnLink } from "~tron/_common/lib/utils/tronscan-helpers";
+import { IToken } from "~tron/_common/types/token-type";
 
 export const ApproveToken = ({
-  tokenToApprove,
-  amount,
-  spenderAddress,
-  onSuccess,
-  buttonProps,
+	tokenToApprove,
+	amount,
+	spenderAddress,
+	onSuccess,
+	buttonProps,
 }: {
-  tokenToApprove: IToken
-  amount: string
-  spenderAddress: string
-  onSuccess: () => Promise<void>
-  buttonProps?: ButtonProps
+	tokenToApprove: IToken;
+	amount: string;
+	spenderAddress: string;
+	onSuccess: () => Promise<void>;
+	buttonProps?: ButtonProps;
 }) => {
-  const [isApproving, setIsApproving] = useState<boolean>(false)
-  const { address, signTransaction } = useWallet()
-  const { tronWeb } = useTronWeb()
+	const [isApproving, setIsApproving] = useState<boolean>(false);
+	const { address, signTransaction } = useWallet();
+	const { tronWeb } = useTronWeb();
 
-  const approveToken = async (type: 'one-time' | 'unlimited') => {
-    try {
-      setIsApproving(true)
-      const parsedAmount = parseUnits(amount, tokenToApprove.decimals)
-      const approvalAmount = type === 'one-time' ? parsedAmount : MAX_UINT256
-      const { transaction } =
-        await tronWeb.transactionBuilder.triggerSmartContract(
-          tokenToApprove.address, //contract address
-          'approve(address,uint256)', //function name
-          {}, // options
-          [
-            { type: 'address', value: spenderAddress },
-            { type: 'uint256', value: approvalAmount },
-          ], //parameters
-          address, //issuerAddress
-        )
-      const signedTransation = await signTransaction(transaction)
-      const _result = await tronWeb.trx.sendRawTransaction(signedTransation)
-      if (!_result.result && 'code' in _result) {
-        throw new Error(parseTxnError(_result.code))
-      }
-      const txId = _result?.txid
-      createInfoToast({
-        summary:
-          type === 'one-time'
-            ? 'Approving One Time...'
-            : 'Approval Unlimited Amount...',
-        type: 'approval',
-        account: address as string,
-        chainId: 1,
-        groupTimestamp: Date.now(),
-        timestamp: Date.now(),
-        txHash: txId,
-        href: getTronscanTxnLink(txId),
-      })
-      const transactionInfo = await getTransactionInfo(tronWeb, txId)
-      if (transactionInfo?.receipt?.result !== 'SUCCESS') {
-        throw new Error('Approval failed')
-      }
-      //create success toast
-      createSuccessToast({
-        summary: 'Approval successful',
-        txHash: txId,
-        type: 'swap',
-        account: address as string,
-        chainId: 1,
-        groupTimestamp: Date.now(),
-        timestamp: Date.now(),
-        href: getTronscanTxnLink(txId),
-      })
-      await onSuccess()
-      setIsApproving(false)
-    } catch (error) {
-      const errorMessage =
-        typeof error === 'string'
-          ? error
-          : (error as Error)?.message ??
-            'An error occurred while setting approval'
-      //create error toast
-      createFailedToast({
-        summary: errorMessage,
-        type: 'swap',
-        account: address as string,
-        chainId: 1,
-        groupTimestamp: Date.now(),
-        timestamp: Date.now(),
-      })
-      console.log(error)
-      setIsApproving(false)
-    }
-  }
+	const approveToken = async (type: "one-time" | "unlimited") => {
+		try {
+			setIsApproving(true);
+			const parsedAmount = parseUnits(amount, tokenToApprove.decimals);
+			const approvalAmount = type === "one-time" ? parsedAmount : MAX_UINT256;
+			const { transaction } = await tronWeb.transactionBuilder.triggerSmartContract(
+				tokenToApprove.address, //contract address
+				"approve(address,uint256)", //function name
+				{}, // options
+				[
+					{ type: "address", value: spenderAddress },
+					{ type: "uint256", value: approvalAmount },
+				], //parameters
+				address //issuerAddress
+			);
+			const signedTransation = await signTransaction(transaction);
+			const _result = await tronWeb.trx.sendRawTransaction(signedTransation);
+			if (!_result.result && "code" in _result) {
+				throw new Error(parseTxnError(_result.code));
+			}
+			const txId = _result?.txid;
+			createInfoToast({
+				summary: type === "one-time" ? "Approving One Time..." : "Approval Unlimited Amount...",
+				type: "approval",
+				account: address as string,
+				chainId: 1,
+				groupTimestamp: Date.now(),
+				timestamp: Date.now(),
+				txHash: txId,
+				href: getTronscanTxnLink(txId),
+			});
+			const transactionInfo = await getTransactionInfo(tronWeb, txId);
+			if (transactionInfo?.receipt?.result !== "SUCCESS") {
+				throw new Error("Approval failed");
+			}
+			//create success toast
+			createSuccessToast({
+				summary: "Approval successful",
+				txHash: txId,
+				type: "swap",
+				account: address as string,
+				chainId: 1,
+				groupTimestamp: Date.now(),
+				timestamp: Date.now(),
+				href: getTronscanTxnLink(txId),
+			});
+			await onSuccess();
+			setIsApproving(false);
+		} catch (error) {
+			const errorMessage =
+				typeof error === "string"
+					? error
+					: (error as Error)?.message ?? "An error occurred while setting approval";
+			//create error toast
+			createFailedToast({
+				summary: errorMessage,
+				type: "swap",
+				account: address as string,
+				chainId: 1,
+				groupTimestamp: Date.now(),
+				timestamp: Date.now(),
+			});
+			console.log(error);
+			setIsApproving(false);
+		}
+	};
 
-  return (
-    <Button
-      asChild
-      disabled={isApproving}
-      loading={isApproving}
-      role="combobox"
-      className="w-full relative"
-      onClick={async (e) => {
-        e.stopPropagation()
-        await approveToken('one-time')
-      }}
-      onKeyDown={async (e) => {
-        e.stopPropagation()
-        await approveToken('one-time')
-      }}
-      {...buttonProps}
-    >
-      <span>{isApproving ? 'Approving' : 'Approve'}</span>
-      <Popover>
-        <PopoverTrigger
-          asChild
-          className="w-fit absolute top-0 bottom-1 right-0"
-          disabled={isApproving}
-          onClick={(e) => e.stopPropagation()}
-          onKeyDown={(e) => e.stopPropagation()}
-        >
-          <Button
-            asChild
-            size="xs"
-            variant="ghost"
-            name="Select"
-            className="h-full"
-          >
-            <ChevronDownIcon className="h-4 w-4" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="!p-0 !overflow-x-hidden !overflow-y-scroll scroll">
-          <Command>
-            <CommandGroup>
-              <CommandItem className="cursor-pointer">
-                <div
-                  onClick={async () => {
-                    await approveToken('one-time')
-                  }}
-                  onKeyDown={async () => {
-                    await approveToken('one-time')
-                  }}
-                  className="flex flex-col"
-                >
-                  <p className="font-bold">Approve one-time only</p>
-                  <p>
-                    You&apos;ll give your approval to spend{' '}
-                    {toBigNumber(amount).toString(10)} {tokenToApprove.symbol}{' '}
-                    on your behalf
-                  </p>
-                </div>
-              </CommandItem>
-              <CommandItem className="cursor-pointer">
-                <div
-                  onClick={async () => {
-                    await approveToken('unlimited')
-                  }}
-                  onKeyDown={async () => {
-                    await approveToken('unlimited')
-                  }}
-                  className="flex flex-col"
-                >
-                  <p className="font-bold">Approve unlimited amount</p>
-                  <p>
-                    You won&apos;t need to approve again next time you want to
-                    spend {tokenToApprove.symbol}.
-                  </p>
-                </div>
-              </CommandItem>
-            </CommandGroup>
-          </Command>
-        </PopoverContent>
-      </Popover>
-    </Button>
-  )
-}
+	return (
+		<Button
+			asChild
+			disabled={isApproving}
+			loading={isApproving}
+			role="combobox"
+			className="w-full relative"
+			onClick={async (e) => {
+				e.stopPropagation();
+				await approveToken("one-time");
+			}}
+			onKeyDown={async (e) => {
+				e.stopPropagation();
+				await approveToken("one-time");
+			}}
+			size="xl"
+			{...buttonProps}>
+			<span>{isApproving ? "Approving" : "Approve"}</span>
+			<Popover>
+				<PopoverTrigger
+					asChild
+					className="w-fit absolute top-0 bottom-1 right-0"
+					disabled={isApproving}
+					onClick={(e) => e.stopPropagation()}
+					onKeyDown={(e) => e.stopPropagation()}>
+					<Button asChild size="xs" variant="ghost" name="Select" className="h-full">
+						<ChevronDownIcon className="h-4 w-4" />
+					</Button>
+				</PopoverTrigger>
+				<PopoverContent className="!p-0 !overflow-x-hidden !overflow-y-scroll scroll">
+					<Command>
+						<CommandGroup>
+							<CommandItem className="cursor-pointer">
+								<div
+									onClick={async () => {
+										await approveToken("one-time");
+									}}
+									onKeyDown={async () => {
+										await approveToken("one-time");
+									}}
+									className="flex flex-col">
+									<p className="font-bold">Approve one-time only</p>
+									<p>
+										You&apos;ll give your approval to spend {toBigNumber(amount).toString(10)}{" "}
+										{tokenToApprove.symbol} on your behalf
+									</p>
+								</div>
+							</CommandItem>
+							<CommandItem className="cursor-pointer">
+								<div
+									onClick={async () => {
+										await approveToken("unlimited");
+									}}
+									onKeyDown={async () => {
+										await approveToken("unlimited");
+									}}
+									className="flex flex-col">
+									<p className="font-bold">Approve unlimited amount</p>
+									<p>
+										You won&apos;t need to approve again next time you want to spend {tokenToApprove.symbol}.
+									</p>
+								</div>
+							</CommandItem>
+						</CommandGroup>
+					</Command>
+				</PopoverContent>
+			</Popover>
+		</Button>
+	);
+};
--- apps/web/src/app/(non-evm)/tron/_common/ui/Swap/ReviewSwapDialogTrigger.tsx
+++ apps/web/src/app/(non-evm)/tron/_common/ui/Swap/ReviewSwapDialogTrigger.tsx
@@ -1,156 +1,135 @@
-import { Button, DialogTrigger } from '@sushiswap/ui'
-import { useWallet } from '@tronweb3/tronwallet-adapter-react-hooks'
-import { useMemo, useState } from 'react'
-import { ROUTER_CONTRACT } from '~tron/_common/constants/contracts'
-import { useAllowance } from '~tron/_common/lib/hooks/useAllowance'
-import { useTokenBalance } from '~tron/_common/lib/hooks/useTokenBalance'
-import { formatUnitsForInput } from '~tron/_common/lib/utils/formatters'
-import { getIfWrapOrUnwrap } from '~tron/_common/lib/utils/helpers'
-import { warningSeverity } from '~tron/_common/lib/utils/warning-severity'
-import { useSwapState } from '~tron/swap/swap-provider'
-import { ApproveToken } from '../Shared/ApproveToken'
+import { Button, DialogTrigger } from "@sushiswap/ui";
+import { useWallet } from "@tronweb3/tronwallet-adapter-react-hooks";
+import { useMemo, useState } from "react";
+import { ROUTER_CONTRACT } from "~tron/_common/constants/contracts";
+import { useAllowance } from "~tron/_common/lib/hooks/useAllowance";
+import { useTokenBalance } from "~tron/_common/lib/hooks/useTokenBalance";
+import { formatUnitsForInput } from "~tron/_common/lib/utils/formatters";
+import { getIfWrapOrUnwrap } from "~tron/_common/lib/utils/helpers";
+import { warningSeverity } from "~tron/_common/lib/utils/warning-severity";
+import { useSwapState } from "~tron/swap/swap-provider";
+import { ApproveToken } from "../Shared/ApproveToken";
 
 export const ReviewSwapDialogTrigger = () => {
-  const [isChecked, setIsChecked] = useState<boolean>(false)
-  const {
-    token0,
-    token1,
-    amountIn,
-    isTxnPending,
-    priceImpactPercentage,
-    route,
-  } = useSwapState()
-  const { address } = useWallet()
-  const { data: allowanceAmount, refetch } = useAllowance({
-    tokenAddress: token0?.address as string,
-    ownerAddress: address as string,
-    spenderAddress: ROUTER_CONTRACT,
-  })
-  const { data: tokenBalance, isLoading } = useTokenBalance({
-    accountAddress: address,
-    tokenAddress: token0.address,
-  })
+	const [isChecked, setIsChecked] = useState<boolean>(false);
+	const { token0, token1, amountIn, isTxnPending, priceImpactPercentage, route } = useSwapState();
+	const { address } = useWallet();
+	const { data: allowanceAmount, refetch } = useAllowance({
+		tokenAddress: token0?.address as string,
+		ownerAddress: address as string,
+		spenderAddress: ROUTER_CONTRACT,
+	});
+	const { data: tokenBalance, isLoading } = useTokenBalance({
+		accountAddress: address,
+		tokenAddress: token0.address,
+	});
 
-  const refreshAllowance = async () => {
-    await refetch()
-  }
+	const refreshAllowance = async () => {
+		await refetch();
+	};
 
-  const hasInsufficientBalance = useMemo(() => {
-    if (isLoading) return true
-    return (
-      Number(formatUnitsForInput(tokenBalance ?? '0', token0.decimals)) <
-      Number(amountIn)
-    )
-  }, [tokenBalance, token0, amountIn, isLoading])
+	const hasInsufficientBalance = useMemo(() => {
+		if (isLoading) return true;
+		return Number(formatUnitsForInput(tokenBalance ?? "0", token0.decimals)) < Number(amountIn);
+	}, [tokenBalance, token0, amountIn, isLoading]);
 
-  const swapType = useMemo(() => {
-    return getIfWrapOrUnwrap(token0, token1)
-  }, [token0, token1])
+	const swapType = useMemo(() => {
+		return getIfWrapOrUnwrap(token0, token1);
+	}, [token0, token1]);
 
-  const noRoutes = swapType === 'swap' && route?.length === 0
+	const noRoutes = swapType === "swap" && route?.length === 0;
 
-  const allowanceFormatted = formatUnitsForInput(
-    allowanceAmount ?? '0',
-    token0?.decimals,
-  )
+	const allowanceFormatted = formatUnitsForInput(allowanceAmount ?? "0", token0?.decimals);
 
-  const insufficientLiquidity = priceImpactPercentage >= 100
+	const insufficientLiquidity = priceImpactPercentage >= 100;
 
-  const buttonText = useMemo(() => {
-    if (isTxnPending) {
-      return 'Swapping'
-    }
-    if (!amountIn || amountIn === '0') {
-      return 'Enter Amount'
-    }
-    if (hasInsufficientBalance) {
-      return 'Insufficient Balance'
-    }
-    if (swapType === 'unwrap') {
-      return 'Unwrap'
-    }
-    if (swapType === 'wrap') {
-      return 'Wrap'
-    }
-    if (noRoutes) {
-      return 'No Routes Found'
-    }
-    if (insufficientLiquidity) {
-      return 'Insufficient Liquidity'
-    }
-    if (
-      allowanceAmount &&
-      Number(amountIn) > Number(allowanceFormatted) &&
-      swapType === 'swap'
-    ) {
-      return 'Approve'
-    }
-    return 'Review Swap'
-  }, [
-    amountIn,
-    allowanceAmount,
-    hasInsufficientBalance,
-    noRoutes,
-    allowanceFormatted,
-    swapType,
-    insufficientLiquidity,
-    isTxnPending,
-  ])
+	const buttonText = useMemo(() => {
+		if (isTxnPending) {
+			return "Swapping";
+		}
+		if (!amountIn || amountIn === "0") {
+			return "Enter Amount";
+		}
+		if (hasInsufficientBalance) {
+			return "Insufficient Balance";
+		}
+		if (swapType === "unwrap") {
+			return "Unwrap";
+		}
+		if (swapType === "wrap") {
+			return "Wrap";
+		}
+		if (noRoutes) {
+			return "No Routes Found";
+		}
+		if (insufficientLiquidity) {
+			return "Insufficient Liquidity";
+		}
+		if (allowanceAmount && Number(amountIn) > Number(allowanceFormatted) && swapType === "swap") {
+			return "Approve";
+		}
+		return "Review Swap";
+	}, [
+		amountIn,
+		allowanceAmount,
+		hasInsufficientBalance,
+		noRoutes,
+		allowanceFormatted,
+		swapType,
+		insufficientLiquidity,
+		isTxnPending,
+	]);
 
-  const userConfirmationNeeded = useMemo(() => {
-    if (
-      warningSeverity(priceImpactPercentage ?? 0) > 3 &&
-      (buttonText === 'Review Swap' || buttonText === 'Approve')
-    ) {
-      return true
-    }
-    return false
-  }, [priceImpactPercentage, buttonText])
+	const userConfirmationNeeded = useMemo(() => {
+		if (
+			warningSeverity(priceImpactPercentage ?? 0) > 3 &&
+			(buttonText === "Review Swap" || buttonText === "Approve")
+		) {
+			return true;
+		}
+		return false;
+	}, [priceImpactPercentage, buttonText]);
 
-  return (
-    <>
-      {buttonText === 'Approve' ? (
-        <ApproveToken
-          tokenToApprove={token0}
-          amount={amountIn}
-          spenderAddress={ROUTER_CONTRACT}
-          onSuccess={refreshAllowance}
-          buttonProps={{ size: 'xl', fullWidth: true }}
-        />
-      ) : (
-        <DialogTrigger
-          disabled={
-            insufficientLiquidity ||
-            (userConfirmationNeeded && !isChecked) ||
-            !amountIn ||
-            hasInsufficientBalance ||
-            noRoutes
-          }
-          asChild
-        >
-          <Button size="xl" fullWidth>
-            {buttonText}
-          </Button>
-        </DialogTrigger>
-      )}
-      {userConfirmationNeeded && !isChecked ? (
-        <div className="flex items-start px-4 py-3 mt-4 rounded-xl bg-red/20">
-          <input
-            id="expert-checkbox"
-            type="checkbox"
-            checked={isChecked}
-            onChange={(e) => setIsChecked(e.target.checked)}
-            className="cursor-pointer mr-1 w-5 h-5 mt-0.5 text-red-600 !ring-red-600 bg-white border-red rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2"
-          />
-          <label
-            htmlFor="expert-checkbox"
-            className="ml-2 font-medium text-red-600"
-          >
-            Price impact is too high. You will lose a big portion of your funds
-            in this trade. Please tick the box if you would like to continue.
-          </label>
-        </div>
-      ) : null}
-    </>
-  )
-}
+	return (
+		<>
+			{buttonText === "Approve" ? (
+				<ApproveToken
+					tokenToApprove={token0}
+					amount={amountIn}
+					spenderAddress={ROUTER_CONTRACT}
+					onSuccess={refreshAllowance}
+					buttonProps={{ size: "xl", fullWidth: true }}
+				/>
+			) : (
+				<DialogTrigger
+					disabled={
+						insufficientLiquidity ||
+						(userConfirmationNeeded && !isChecked) ||
+						!amountIn ||
+						hasInsufficientBalance ||
+						noRoutes
+					}
+					asChild>
+					<Button size="xl" fullWidth>
+						{buttonText}
+					</Button>
+				</DialogTrigger>
+			)}
+			{userConfirmationNeeded && !isChecked ? (
+				<div className="flex items-start px-4 py-3 mt-4 rounded-xl bg-red/20">
+					<input
+						id="expert-checkbox"
+						type="checkbox"
+						checked={isChecked}
+						onChange={(e) => setIsChecked(e.target.checked)}
+						className="cursor-pointer mr-1 w-5 h-5 mt-0.5 text-red-600 !ring-red-600 bg-white border-red rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2"
+					/>
+					<label htmlFor="expert-checkbox" className="ml-2 font-medium text-red-600">
+						Price impact is too high. You will lose a big portion of your funds in this trade. Please tick the
+						box if you would like to continue.
+					</label>
+				</div>
+			) : null}
+		</>
+	);
+};
