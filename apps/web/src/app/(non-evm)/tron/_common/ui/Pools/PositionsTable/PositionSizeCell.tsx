10e67a26af642ff744b8dd7047ad6117f3562272
solidityteam <tyler@solidity.io>
2024-10-09 15:29:00 -0400

    fix: columns momentarily show as 0 while loading - tron

--- apps/web/src/app/(non-evm)/tron/_common/ui/Pools/PositionsTable/PositionSizeCell.tsx
+++ apps/web/src/app/(non-evm)/tron/_common/ui/Pools/PositionsTable/PositionSizeCell.tsx
@@ -1,18 +1,23 @@
-import { formatPercent } from 'sushi/format'
-import { usePoolOwnership } from '~tron/_common/lib/hooks/usePoolOwnership'
-import { IPositionRowData } from './PositionsTable'
+import { formatPercent } from "sushi/format";
+import { usePoolOwnership } from "~tron/_common/lib/hooks/usePoolOwnership";
+import { IPositionRowData } from "./PositionsTable";
+import { SkeletonText } from "@sushiswap/ui";
 
 export const PositionSizeCell = ({ data }: { data: IPositionRowData }) => {
-  const { pairAddress } = data
-  const { data: ownership } = usePoolOwnership({ pairAddress })
+	const { pairAddress } = data;
+	const { data: ownership, isLoading } = usePoolOwnership({ pairAddress });
 
-  return (
-    <div className="flex items-center gap-1">
-      <div className="flex flex-col gap-0.5">
-        <span className="flex items-center gap-1 text-sm font-medium text-gray-900 dark:text-slate-50">
-          {formatPercent(ownership?.ownership)}
-        </span>
-      </div>
-    </div>
-  )
-}
+	if (isLoading || ownership === undefined) {
+		return <SkeletonText fontSize="lg" />;
+	}
+
+	return (
+		<div className="flex items-center gap-1">
+			<div className="flex flex-col gap-0.5">
+				<span className="flex items-center gap-1 text-sm font-medium text-gray-900 dark:text-slate-50">
+					{formatPercent(ownership?.ownership)}
+				</span>
+			</div>
+		</div>
+	);
+};
