f9f34e8b914e9abcaf731a5522353ecf00793ae5
solidityteam <tyler@solidity.io>
2024-10-14 14:18:08 -0400

    chore: lint fixes

--- apps/web/src/app/(non-evm)/tron/_common/ui/Swap/AmountIn.tsx
+++ apps/web/src/app/(non-evm)/tron/_common/ui/Swap/AmountIn.tsx
@@ -1,48 +1,56 @@
-import { useEffect } from "react";
-import { useAmountsOut } from "~tron/_common/lib/hooks/useAmountsOut";
-import { formatUnitsForInput, parseUnits } from "~tron/_common/lib/utils/formatters";
-import { useSwapDispatch, useSwapState } from "~tron/swap/swap-provider";
-import { TokenInput } from "../Input/TokenInput";
-import { useDebounce } from "@sushiswap/hooks";
+import { useDebounce } from '@sushiswap/hooks'
+import { useEffect } from 'react'
+import { useAmountsOut } from '~tron/_common/lib/hooks/useAmountsOut'
+import {
+  formatUnitsForInput,
+  parseUnits,
+} from '~tron/_common/lib/utils/formatters'
+import { useSwapDispatch, useSwapState } from '~tron/swap/swap-provider'
+import { TokenInput } from '../Input/TokenInput'
 
 export const AmountIn = () => {
-	const { token0, amountIn, token1 } = useSwapState();
-	const { setToken0, setAmountIn, setAmountOut } = useSwapDispatch();
-	const debouncedAmountIn = useDebounce(amountIn, 500);
+  const { token0, amountIn, token1 } = useSwapState()
+  const { setToken0, setAmountIn, setAmountOut } = useSwapDispatch()
+  const debouncedAmountIn = useDebounce(amountIn, 500)
 
-	const { data: amountsOut } = useAmountsOut({
-		amountIn: parseUnits(debouncedAmountIn, token0.decimals),
-	});
+  const { data: amountsOut } = useAmountsOut({
+    amountIn: parseUnits(debouncedAmountIn, token0.decimals),
+  })
 
-	useEffect(() => {
-		if (amountsOut && amountsOut?.length !== 0) {
-			const _amountOut = formatUnitsForInput(amountsOut[amountsOut?.length - 1], token1.decimals);
-			if (_amountOut === "0") {
-				setAmountOut("");
-			} else {
-				if (amountIn === "0" || amountIn === "") return;
-				setAmountOut(_amountOut);
-			}
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [amountsOut, token1?.decimals, amountIn]);
+  // biome-ignore lint/correctness/useExhaustiveDependencies: Typecheck speedup
+  useEffect(() => {
+    if (amountsOut && amountsOut?.length !== 0) {
+      const _amountOut = formatUnitsForInput(
+        amountsOut[amountsOut?.length - 1],
+        token1.decimals,
+      )
+      if (_amountOut === '0') {
+        setAmountOut('')
+      } else {
+        if (amountIn === '0' || amountIn === '') return
+        setAmountOut(_amountOut)
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [amountsOut, token1?.decimals, amountIn])
 
-	useEffect(() => {
-		if (Number(amountIn) === 0) {
-			setAmountOut("");
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [amountIn]);
+  // biome-ignore lint/correctness/useExhaustiveDependencies: Typecheck speedup
+  useEffect(() => {
+    if (Number(amountIn) === 0) {
+      setAmountOut('')
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [amountIn])
 
-	return (
-		<TokenInput
-			className="border border-accent"
-			type="input"
-			amount={amountIn}
-			setAmount={setAmountIn}
-			currency={token0}
-			setToken={setToken0}
-			label="Sell"
-		/>
-	);
-};
+  return (
+    <TokenInput
+      className="border border-accent"
+      type="input"
+      amount={amountIn}
+      setAmount={setAmountIn}
+      currency={token0}
+      setToken={setToken0}
+      label="Sell"
+    />
+  )
+}
