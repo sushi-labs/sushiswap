762ead720fc1ab1ab7a971ddf3e10359f7db4cf1
solidityteam <tyler@solidity.io>
2024-10-14 17:33:34 -0400

    fix: aptos network fee

--- apps/web/src/app/(non-evm)/aptos/swap/lib/use-swap-network-fee.ts
+++ apps/web/src/app/(non-evm)/aptos/swap/lib/use-swap-network-fee.ts
@@ -1,89 +1,73 @@
-import { Aptos, AptosConfig, Ed25519PublicKey } from '@aptos-labs/ts-sdk'
-import { useWallet } from '@aptos-labs/wallet-adapter-react'
-import { useQuery } from '@tanstack/react-query'
-import { networkNameToNetwork } from '~aptos/(common)/config/chains'
-import { useNetwork } from '~aptos/(common)/lib/common/use-network'
-import { useSimpleSwapState } from '../ui/simple/simple-swap-provider/simple-swap-provider'
-import { getSwapPayload } from './get-swap-payload'
+import { Account, Aptos, AptosConfig, Ed25519Account, Ed25519PublicKey } from "@aptos-labs/ts-sdk";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import { useQuery } from "@tanstack/react-query";
+import { networkNameToNetwork } from "~aptos/(common)/config/chains";
+import { useNetwork } from "~aptos/(common)/lib/common/use-network";
+import { useSimpleSwapState } from "../ui/simple/simple-swap-provider/simple-swap-provider";
+import { getSwapPayload } from "./get-swap-payload";
+import { formatNumberWithDecimals } from "~aptos/(common)/lib/common/format-number-with-decimals";
+import { parseUnits } from "viem";
 
 export const useSwapNetworkFee = () => {
-  const {
-    network,
-    contracts: { swap: swapContract },
-  } = useNetwork()
-  const { bestRoutes, token0, slippageAmount, amount } = useSimpleSwapState()
-  const { account } = useWallet()
+	const {
+		network,
+		contracts: { swap: swapContract },
+	} = useNetwork();
+	const { bestRoutes, token0, slippageAmount, amount, token1, outputAmount } = useSimpleSwapState();
+	const { account } = useWallet();
 
-  return useQuery({
-    queryKey: [
-      'useSwapNetworkFee',
-      bestRoutes,
-      token0,
-      account,
-      slippageAmount,
-      amount,
-    ],
-    queryFn: async () => {
-      if (
-        !account ||
-        !amount ||
-        bestRoutes?.length === 0 ||
-        !slippageAmount ||
-        !token0
-      )
-        return 0
-      if (!account?.address) return 0
-      try {
-        const config = new AptosConfig({
-          network: networkNameToNetwork(network),
-        })
-        const aptos = new Aptos(config)
-        const { gas_estimate } = await aptos.getGasPriceEstimation()
+	return useQuery({
+		queryKey: ["useSwapNetworkFee", bestRoutes, token0, account, slippageAmount, amount],
+		queryFn: async () => {
+			if (!account || !amount || bestRoutes?.length === 0 || !slippageAmount || !token0) return 0;
+			if (!account?.address) return 0;
+			try {
+				const config = new AptosConfig({
+					network: networkNameToNetwork(network),
+				});
+				const aptos = new Aptos(config);
+				const { gas_estimate } = await aptos.getGasPriceEstimation();
 
-        const _account = new Ed25519PublicKey(account.address)
+				const _account = new Ed25519PublicKey(account.publicKey?.toString());
 
-        const payload = getSwapPayload(
-          swapContract,
-          parseInt(
-            (parseFloat(String(amount)) *
-              10 ** token0.decimals) as unknown as string,
-          ),
-          bestRoutes,
-          parseInt(String(slippageAmount)),
-        )
+				const minOutput = slippageAmount
+					? formatNumberWithDecimals(slippageAmount, token1 ? token1.decimals : 8)
+					: 0;
 
-        const transaction = await aptos.transaction.build.simple({
-          sender: account.address,
-          data: {
-            //@ts-expect-error
-            function: payload?.data?.function,
-            functionArguments: payload?.data?.functionArguments,
-            typeArguments: payload?.data?.typeArguments,
-          },
-        })
+				const minOut = parseUnits(minOutput?.toString(), token1.decimals).toString();
+				const amountIn = parseUnits(amount, token0.decimals).toString();
 
-        const [userTransactionResponse] =
-          await aptos.transaction.simulate.simple({
-            signerPublicKey: _account,
-            transaction,
-          })
+				const payload = getSwapPayload(swapContract, parseInt(amountIn), bestRoutes, parseInt(minOut));
 
-        const gasUsed = Number(userTransactionResponse?.gas_used) || 14
+				const transaction = await aptos.transaction.build.simple({
+					sender: account.address,
+					data: {
+						//@ts-expect-error
+						function: payload?.data?.function,
+						functionArguments: payload?.data?.functionArguments,
+						typeArguments: payload?.data?.typeArguments,
+					},
+				});
 
-        const _networkFee = gasUsed * gas_estimate
+				const [userTransactionResponse] = await aptos.transaction.simulate.simple({
+					signerPublicKey: _account,
+					transaction: transaction,
+				});
 
-        const networkFee = Number.isNaN(_networkFee)
-          ? 0.000014
-          : _networkFee / 10 ** 8
-        return networkFee
-      } catch (error) {
-        console.log('useSwapNetworkFee error: ', error)
-        return 0
-      }
-    },
-    enabled: !!account && !!amount && bestRoutes?.length > 0,
-    refetchOnMount: false,
-    refetchOnReconnect: false,
-    refetchOnWindowFocus: false,
-  })
-}
+				const gasUsed = Number(userTransactionResponse?.gas_used) || 14;
+
+				const _networkFee = gasUsed * gas_estimate;
+
+				const networkFee = Number.isNaN(_networkFee) ? 0.000014 : _networkFee / 10 ** 8;
+				return networkFee;
+			} catch (error) {
+				console.log("useSwapNetworkFee error: ", error);
+				return 0;
+			}
+		},
+		enabled: !!account && !!amount && bestRoutes?.length > 0,
+		refetchOnMount: false,
+		refetchOnReconnect: false,
+		refetchOnWindowFocus: false,
+	});
+};
