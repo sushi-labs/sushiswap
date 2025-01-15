c088aa23017c6cfdbe113f5d11de2a19b9340f46
LufyCZ <me@lufy.cz>
2025-01-15 22:59:21 +0000

    chore: delete apis/token-list

--- apis/token-list/api/chainlink.ts
+++
@@ -1,8 +0,0 @@
-import CHAINLINK_TOKEN_LIST from '@sushiswap/chainlink-token-list'
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-const handler = (_request: VercelRequest, response: VercelResponse) => {
-  response.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate=600')
-  return response.status(200).json(CHAINLINK_TOKEN_LIST)
-}
-export default handler
--- apis/token-list/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { default as DEFAULT_TOKEN_LIST } from '@sushiswap/default-token-list'
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-const handler = async (_request: VercelRequest, response: VercelResponse) => {
-  response.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate=600')
-  return response.status(200).json(DEFAULT_TOKEN_LIST)
-}
-
-export default handler
