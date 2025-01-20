04e340cecdac0df2dff0e841fe12c82527dcbb58
LufyCZ <lufy@sushi.com>
2025-01-20 17:38:11 +0100

    feat(apps/faq): add mava script (#1797)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -49,26 +49,32 @@ export default function RootLayout({
       className={`${inter.variable} ${roboto_mono.variable} dark`}
       suppressHydrationWarning
     >
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png?v=1"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png?v=1"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png?v=1"
-      />
-      <link rel="manifest" href="/site.webmanifest?v=1" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#fa52a0" />
-      <link rel="shortcut icon" href="/favicon.ico?v=1" />
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png?v=1"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png?v=1"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png?v=1"
+        />
+        <link rel="manifest" href="/site.webmanifest?v=1" />
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg?v=1"
+          color="#fa52a0"
+        />
+        <link rel="shortcut icon" href="/favicon.ico?v=1" />
+      </head>
       <body className="min-h-screen flex flex-col">
         <ToastContainer />
         <CookieDialogContainer />
