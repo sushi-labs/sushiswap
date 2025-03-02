1b3ab115421e4ee715017ddeeeb5c025953ced04
Mikita Kalashnikau <mikitaka@playtika.com>
2025-03-02 17:06:15 +0100

    [EGN-892]: Change widgets placement to the right bottom corner

---
+++ apps/web/src/ui/swap/simple/simple-swap-banners.tsx
@@ -0,0 +1,9 @@
+import { StrapiBanner } from "../strapi-banner/strapi-banner";
+import { SimpleSwapBridgeBanner } from "./simple-swap-bridge-banner";
+
+export const SimpleSwapBanners = () => {
+    return <div className='xl:fixed xl:right-8 xl:bottom-8 w-[360]'>
+        <StrapiBanner />
+        <SimpleSwapBridgeBanner/>
+    </div>
+}
--- apps/web/src/ui/swap/simple/simple-swap-widget.tsx
+++ apps/web/src/ui/swap/simple/simple-swap-widget.tsx
@@ -1,6 +1,4 @@
-import { StrapiBanner } from '../strapi-banner/strapi-banner'
 import { SwapModeButtons } from '../swap-mode-buttons'
-import { SimpleSwapBridgeBanner } from './simple-swap-bridge-banner'
 import { SimpleSwapHeader } from './simple-swap-header'
 import { SimpleSwapSettingsOverlay } from './simple-swap-settings-overlay'
 import { SimpleSwapSwitchTokensButton } from './simple-swap-switch-tokens-button'
@@ -10,26 +8,26 @@ import { SimpleSwapToken1Input } from './simple-swap-token1-input'
 import { SimpleSwapTradeButton } from './simple-swap-trade-button'
 import { SimpleSwapTradeStats } from './simple-swap-trade-stats'
 import { SwapMaintenanceMessage } from './swap-maintenance-message'
+import { SimpleSwapBanners } from "./simple-swap-banners";
 
 export const SimpleSwapWidget = () => {
-  return (
-    <div className="flex flex-col gap-4">
-      <SimpleSwapBridgeBanner />
-      <SimpleSwapHeader />
-      <div className="flex items-center justify-between">
-        <SwapModeButtons />
-        <SimpleSwapSettingsOverlay />
-      </div>
-      <SwapMaintenanceMessage />
-      <SimpleSwapToken0Input />
-      <SimpleSwapSwitchTokensButton />
-      <div className="flex flex-col">
-        <SimpleSwapToken1Input />
-        <SimpleSwapTradeButton />
-      </div>
-      <SimpleSwapTradeStats />
-      <StrapiBanner />
-      <SimpleSwapTokenNotFoundDialog />
-    </div>
-  )
+    return (
+        <div className="flex flex-col gap-4">
+            <SimpleSwapBanners/>
+            <SimpleSwapHeader/>
+            <div className="flex items-center justify-between">
+                <SwapModeButtons/>
+                <SimpleSwapSettingsOverlay/>
+            </div>
+            <SwapMaintenanceMessage/>
+            <SimpleSwapToken0Input/>
+            <SimpleSwapSwitchTokensButton/>
+            <div className="flex flex-col">
+                <SimpleSwapToken1Input/>
+                <SimpleSwapTradeButton/>
+            </div>
+            <SimpleSwapTradeStats/>
+            <SimpleSwapTokenNotFoundDialog/>
+        </div>
+    )
 }
