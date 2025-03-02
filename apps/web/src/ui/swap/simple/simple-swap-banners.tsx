012ad88b52cdacb126f28bfddfd67299b56bb78c
mikita-kalashnikau <68056083+mikita-kalashnikau@users.noreply.github.com>
2025-03-02 16:20:38 +0000

    chore: lint

--- apps/web/src/ui/swap/simple/simple-swap-banners.tsx
+++ apps/web/src/ui/swap/simple/simple-swap-banners.tsx
@@ -1,9 +1,11 @@
-import { StrapiBanner } from "../strapi-banner/strapi-banner";
-import { SimpleSwapBridgeBanner } from "./simple-swap-bridge-banner";
+import { StrapiBanner } from '../strapi-banner/strapi-banner'
+import { SimpleSwapBridgeBanner } from './simple-swap-bridge-banner'
 
 export const SimpleSwapBanners = () => {
-    return <div className='xl:fixed xl:right-8 xl:bottom-8 w-[360]'>
-        <StrapiBanner />
-        <SimpleSwapBridgeBanner/>
+  return (
+    <div className="xl:fixed xl:right-8 xl:bottom-8 w-[360]">
+      <StrapiBanner />
+      <SimpleSwapBridgeBanner />
     </div>
+  )
 }
--- apps/web/src/ui/swap/simple/simple-swap-widget.tsx
+++ apps/web/src/ui/swap/simple/simple-swap-widget.tsx
@@ -1,4 +1,5 @@
 import { SwapModeButtons } from '../swap-mode-buttons'
+import { SimpleSwapBanners } from './simple-swap-banners'
 import { SimpleSwapHeader } from './simple-swap-header'
 import { SimpleSwapSettingsOverlay } from './simple-swap-settings-overlay'
 import { SimpleSwapSwitchTokensButton } from './simple-swap-switch-tokens-button'
@@ -8,26 +9,25 @@ import { SimpleSwapToken1Input } from './simple-swap-token1-input'
 import { SimpleSwapTradeButton } from './simple-swap-trade-button'
 import { SimpleSwapTradeStats } from './simple-swap-trade-stats'
 import { SwapMaintenanceMessage } from './swap-maintenance-message'
-import { SimpleSwapBanners } from "./simple-swap-banners";
 
 export const SimpleSwapWidget = () => {
-    return (
-        <div className="flex flex-col gap-4">
-            <SimpleSwapBanners/>
-            <SimpleSwapHeader/>
-            <div className="flex items-center justify-between">
-                <SwapModeButtons/>
-                <SimpleSwapSettingsOverlay/>
-            </div>
-            <SwapMaintenanceMessage/>
-            <SimpleSwapToken0Input/>
-            <SimpleSwapSwitchTokensButton/>
-            <div className="flex flex-col">
-                <SimpleSwapToken1Input/>
-                <SimpleSwapTradeButton/>
-            </div>
-            <SimpleSwapTradeStats/>
-            <SimpleSwapTokenNotFoundDialog/>
-        </div>
-    )
+  return (
+    <div className="flex flex-col gap-4">
+      <SimpleSwapBanners />
+      <SimpleSwapHeader />
+      <div className="flex items-center justify-between">
+        <SwapModeButtons />
+        <SimpleSwapSettingsOverlay />
+      </div>
+      <SwapMaintenanceMessage />
+      <SimpleSwapToken0Input />
+      <SimpleSwapSwitchTokensButton />
+      <div className="flex flex-col">
+        <SimpleSwapToken1Input />
+        <SimpleSwapTradeButton />
+      </div>
+      <SimpleSwapTradeStats />
+      <SimpleSwapTokenNotFoundDialog />
+    </div>
+  )
 }
