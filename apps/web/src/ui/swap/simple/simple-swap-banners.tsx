5676f881066362291ff088e5be2dcb2e77e008e3
Mikita <mikita.kalasnikau@gmail.com>
2025-03-04 10:56:24 +0100

    [EGN-892]: Change widgets placement to the right bottom corner

--- apps/web/src/ui/swap/simple/simple-swap-banners.tsx
+++ apps/web/src/ui/swap/simple/simple-swap-banners.tsx
@@ -2,7 +2,7 @@ import { StrapiBanner } from "../strapi-banner/strapi-banner";
 import { SimpleSwapBridgeBanner } from "./simple-swap-bridge-banner";
 
 export const SimpleSwapBanners = () => {
-    return <div className='xl:fixed xl:right-8 xl:bottom-8 w-[360]'>
+    return <div className='xl:fixed xl:right-8 xl:bottom-8 w-[360] max-w-[360]'>
         <StrapiBanner />
         <SimpleSwapBridgeBanner/>
     </div>
