e417e3490a3e1c7cf81e42cf5005bf8a86bf30b9
0xMasayoshi <0xMasayoshi@protonmail.com>
2025-03-07 17:10:02 +0700

    chore: separate client & server logic from strapi banner

---
+++ apps/web/src/ui/swap/strapi-banner/strapi-banner-provider.tsx
@@ -0,0 +1,27 @@
+import { getBanners } from '@sushiswap/graph-client/strapi'
+import { unstable_cache } from 'next/cache'
+import { cookies } from 'next/headers'
+import type { FC, ReactNode } from 'react'
+import { StrapiBannerContextProvider } from './strapi-banner-context-provider'
+
+export const StrapiBannerProvider: FC<{ children: ReactNode }> = async ({
+  children,
+}) => {
+  let banners
+
+  try {
+    banners = await unstable_cache(() => getBanners(), ['banners'], {
+      revalidate: 3600,
+    })()
+  } catch {}
+
+  // Only supporting one active banner at a time for now
+  const activeBanner = banners?.find((banner) => banner.isActive)
+  const cookie = (await cookies()).get('hidden-banner-ids')
+
+  return (
+    <StrapiBannerContextProvider banner={activeBanner} cookie={cookie}>
+      {children}
+    </StrapiBannerContextProvider>
+  )
+}
