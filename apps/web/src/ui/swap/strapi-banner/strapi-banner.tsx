d475893223bb047c17a70543981d55415474eb50
0xMasayoshi <0xMasayoshi@protonmail.com>
2025-03-11 13:24:27 +0700

    fix: build

--- apps/web/src/ui/swap/strapi-banner/strapi-banner.tsx
+++ apps/web/src/ui/swap/strapi-banner/strapi-banner.tsx
@@ -3,10 +3,16 @@
 import { StrapiBannerContent } from './strapi-banner-content'
 import { useStrapiBanner } from './strapi-banner-context-provider'
 
-export function StrapiBanner() {
+export function StrapiBanner({ className }: { className?: string }) {
   const { banner, cookie } = useStrapiBanner()
 
   if (!banner) return <></>
 
-  return <StrapiBannerContent banner={banner} cookie={cookie} />
+  return (
+    <StrapiBannerContent
+      banner={banner}
+      cookie={cookie}
+      className={className}
+    />
+  )
 }
