8d32ecc87f6d21d320754d9ac743036f731e44a7
LufyCZ <me@lufy.cz>
2025-03-13 23:42:23 +0000

    sapla doapla d

--- apps/web/src/app/portal/_common/lib/auth-env.ts
+++ apps/web/src/app/portal/_common/lib/auth-env.ts
@@ -8,4 +8,10 @@ export const authEnv = z
     ZITADEL_CLIENT_SECRET: z.string(),
     ZITADEL_SA_TOKEN: z.string(),
   })
-  .parse(process.env)
+  .parse({
+    AUTH_SESSION_SECRET: process.env.AUTH_SESSION_SECRET,
+    ZITADEL_ISSUER: process.env.ZITADEL_ISSUER,
+    ZITADEL_CLIENT_ID: process.env.ZITADEL_CLIENT_ID,
+    ZITADEL_CLIENT_SECRET: process.env.ZITADEL_CLIENT_SECRET,
+    ZITADEL_SA_TOKEN: process.env.ZITADEL_SA_TOKEN,
+  })
