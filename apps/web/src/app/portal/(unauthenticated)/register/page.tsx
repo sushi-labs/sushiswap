b301413a132e199443045642a975ca0d74bcf3e0
LufyCZ <me@lufy.cz>
2025-03-18 14:07:45 +0000

    save

--- apps/web/src/app/portal/(unauthenticated)/register/page.tsx
+++ apps/web/src/app/portal/(unauthenticated)/register/page.tsx
@@ -1,4 +1,5 @@
 import { Separator } from '@sushiswap/ui'
+import Link from 'next/link'
 import { OAuthButton } from '../../_common/ui/oauth/oauth-button'
 import { OAuthProvider } from '../../_common/ui/oauth/oauth-config'
 import { RegisterForm } from './_common/ui/register-form'
@@ -9,6 +10,16 @@ export default function Page() {
       <h1 className="text-4xl font-bold">Register</h1>
       <span className="flex flex-col space-y-8">
         <RegisterForm />
+        <div className="flex space-x-1 w-full justify-center text-sm">
+          <span>{`Already have an account?`}</span>
+          <Link
+            href="/portal/login"
+            prefetch={true}
+            className="text-blue font-medium hover:text-blue-600"
+          >
+            <span>{`Sign In`}</span>
+          </Link>
+        </div>
         <Separator />
         <div className="flex flex-col space-y-4">
           <OAuthButton
