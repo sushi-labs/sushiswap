797be80357ca2efb8282d4d20b9e795953db0af8
solidityteam <tyler@solidity.io>
2025-04-24 15:01:17 -0400

    feat: add no wallets found handling

--- apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/WalletConnector/WalletListView.tsx
+++ apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/WalletConnector/WalletListView.tsx
@@ -1,30 +1,41 @@
+import { useKadenaWallet } from '@kadena/wallet-adapter-react'
 import { List, SelectIcon } from '@sushiswap/ui'
 import Image from 'next/image'
 import { useKadena } from '~kadena/kadena-wallet-provider'
 
 export const WalletListView = () => {
   const { adapters, handleConnect } = useKadena()
+  const { adapters: adapters2, client } = useKadenaWallet()
+
+  console.log({
+    adapters2,
+    client,
+  })
 
   return (
     <List className="flex flex-col gap-1 !p-0">
       <List.Control className="bg-gray-100 ">
-        {adapters.map((adapter) => (
-          <List.MenuItem
-            icon={() => (
-              <Image
-                src={adapter.imageURI}
-                alt={adapter.name}
-                width={12}
-                height={12}
-              />
-            )}
-            className="flex items-center justify-start w-full px-0 text-left"
-            key={adapter.name}
-            title={adapter.name}
-            onClick={() => handleConnect(adapter.name)}
-            hoverIcon={() => <SelectIcon className="-rotate-90" />}
-          />
-        ))}
+        {client.adapters.length === 0 ? (
+          <div className="p-3 text-sm">No wallets found</div>
+        ) : (
+          adapters.map((adapter) => (
+            <List.MenuItem
+              icon={() => (
+                <Image
+                  src={adapter.imageURI}
+                  alt={adapter.name}
+                  width={12}
+                  height={12}
+                />
+              )}
+              className="flex items-center justify-start w-full px-0 text-left"
+              key={adapter.name}
+              title={adapter.name}
+              onClick={() => handleConnect(adapter.name)}
+              hoverIcon={() => <SelectIcon className="-rotate-90" />}
+            />
+          ))
+        )}
       </List.Control>
     </List>
   )
