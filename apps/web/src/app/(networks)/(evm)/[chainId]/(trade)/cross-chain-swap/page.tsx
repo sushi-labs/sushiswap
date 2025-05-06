02f1804d2b96d486b9879a7a876fad743c77034d
Yurii Rohovtsov <yurii.rohovtsov@gmail.com>
2025-05-06 15:29:47 +0100

    fix: nit

--- apps/web/src/app/(networks)/(evm)/[chainId]/(trade)/cross-chain-swap/page.tsx
+++ apps/web/src/app/(networks)/(evm)/[chainId]/(trade)/cross-chain-swap/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Container, classNames } from '@sushiswap/ui'
+import { Container } from '@sushiswap/ui'
 import { CrossChainSwapRouteSelector } from 'src/ui/swap/cross-chain/cross-chain-swap-route-selector'
 import { CrossChainSwapWidget } from 'src/ui/swap/cross-chain/cross-chain-swap-widget'
 import { useCrossChainTradeRoutes } from 'src/ui/swap/cross-chain/derivedstate-cross-chain-swap-provider'
@@ -10,7 +10,7 @@ export default function CrossChainSwapPage() {
   const showRouteSelector = isLoading || isFetched
 
   return (
-    <div className={classNames('flex justify-center flex-wrap gap-y-4 h-full')}>
+    <div className="flex justify-center flex-wrap gap-y-4 h-full">
       <Container maxWidth="lg" className="px-4 !mx-[unset]">
         <CrossChainSwapWidget />
       </Container>
