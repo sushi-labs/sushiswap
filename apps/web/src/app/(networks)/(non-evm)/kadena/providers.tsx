b6461688fc79e8b686cb7788576d3092e8a8c87a
solidityteam <tyler@solidity.io>
2025-05-14 14:55:03 -0400

    chore: update kadena WC pkg and use test proj ID

--- apps/web/src/app/(networks)/(non-evm)/kadena/providers.tsx
+++ apps/web/src/app/(networks)/(non-evm)/kadena/providers.tsx
@@ -1,18 +1,23 @@
-'use client'
+"use client";
 
-import { eckoAdapter } from '@kadena/wallet-adapter-ecko'
-import { createWalletConnectAdapter } from '@kadena/wallet-adapter-walletconnect'
+import { eckoAdapter } from "@kadena/wallet-adapter-ecko";
+import { createWalletConnectAdapter } from "@kadena/wallet-adapter-walletconnect";
+import { KadenaWalletProvider as KadenaWalletProviderReact } from "@kadena/wallet-adapter-react";
+import { KadenaWalletProvider } from "./kadena-wallet-provider";
 
-import { KadenaWalletProvider as KadenaWalletProviderReact } from '@kadena/wallet-adapter-react'
-import { KadenaWalletProvider } from './kadena-wallet-provider'
+//@dev will remove later, for testing purposes only
+const TEST_ID = "5329e5621bb8e903c0de8ad458cc8934";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const walletConnectAdapter = createWalletConnectAdapter({
-    projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
-  })
-  return (
-    <KadenaWalletProviderReact adapters={[eckoAdapter(), walletConnectAdapter]}>
-      <KadenaWalletProvider>{children}</KadenaWalletProvider>
-    </KadenaWalletProviderReact>
-  )
+	return (
+		<KadenaWalletProviderReact
+			adapters={[
+				eckoAdapter(),
+				createWalletConnectAdapter({
+					projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ?? TEST_ID,
+				}),
+			]}>
+			<KadenaWalletProvider>{children}</KadenaWalletProvider>
+		</KadenaWalletProviderReact>
+	);
 }
