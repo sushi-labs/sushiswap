1c3fb599251cf2205b6fbaba83ff3b3eab8b6122
0xMasayoshi <0xMasayoshi@protonmail.com>
2025-05-15 23:10:31 +0700

    feat: add WNativeChecker

--- apps/web/src/ui/swap/twap/twap-trade-button.tsx
+++ apps/web/src/ui/swap/twap/twap-trade-button.tsx
@@ -1,15 +1,58 @@
 'use client'
 
-import { DialogTrigger } from '@sushiswap/ui'
+import { type ButtonProps, DialogTrigger, classNames } from '@sushiswap/ui'
 import { Button } from '@sushiswap/ui'
-import React from 'react'
+import React, { type FC } from 'react'
+import { APPROVE_TAG_SWAP } from 'src/lib/constants'
+import { TwapSDK } from 'src/lib/swap/twap/TwapSDK'
+import { useWrapNative } from 'src/lib/wagmi/hooks/wnative/useWrapNative'
 import { Checker } from 'src/lib/wagmi/systems/Checker'
-import { useTwapTrade } from './derivedstate-twap-provider'
+import type { Amount, Type } from 'sushi/currency'
+import type { Address } from 'viem'
+import { useDerivedStateTwap, useTwapTrade } from './derivedstate-twap-provider'
 import { TwapTradeReviewDialog } from './twap-trade-review-dialog'
 import { useIsTwapMaintenance } from './use-is-twap-maintenance'
 
+type WrapNativeCheckerProps = ButtonProps & {
+  amount: Amount<Type> | undefined
+}
+
+const WrapNativeChecker: FC<WrapNativeCheckerProps> = ({
+  id,
+  amount,
+  children,
+  className,
+  fullWidth = true,
+  size = 'xl',
+  enabled = true,
+  ...props
+}) => {
+  const { write, isPending, isSuccess } = useWrapNative({ amount, enabled })
+
+  return amount?.currency?.isNative && !isSuccess ? (
+    <Button
+      disabled={!write}
+      className={classNames(className, 'group relative')}
+      loading={isPending}
+      onClick={() => write?.()}
+      fullWidth={fullWidth}
+      size={size}
+      testId={id}
+      {...props}
+    >
+      Wrap {amount?.currency.symbol}
+    </Button>
+  ) : (
+    <>{children}</>
+  )
+}
+
 export const TwapTradeButton = () => {
   const { data: maintenance } = useIsTwapMaintenance()
+
+  const {
+    state: { swapAmount, chainId },
+  } = useDerivedStateTwap()
 
   const trade = useTwapTrade()
 
@@ -19,11 +62,34 @@ export const TwapTradeButton = () => {
         guardWhen={maintenance}
         guardText="Maintenance in progress"
       >
-        <DialogTrigger asChild>
-          <Button size="xl" disabled={!trade} fullWidth testId="swap">
-            Place order
-          </Button>
-        </DialogTrigger>
+        <Checker.Connect>
+          <Checker.Network chainId={chainId}>
+            <Checker.Amounts chainId={chainId} amount={swapAmount}>
+              <WrapNativeChecker amount={swapAmount}>
+                <Checker.ApproveERC20
+                  id="approve-erc20"
+                  amount={swapAmount?.wrapped}
+                  contract={
+                    TwapSDK.onNetwork(chainId).config.twapAddress as Address
+                  }
+                >
+                  <Checker.Success tag={APPROVE_TAG_SWAP}>
+                    <DialogTrigger asChild>
+                      <Button
+                        size="xl"
+                        disabled={!trade}
+                        fullWidth
+                        testId="swap"
+                      >
+                        Place order
+                      </Button>
+                    </DialogTrigger>
+                  </Checker.Success>
+                </Checker.ApproveERC20>
+              </WrapNativeChecker>
+            </Checker.Amounts>
+          </Checker.Network>
+        </Checker.Connect>
       </Checker.Guard>
     </TwapTradeReviewDialog>
   )
