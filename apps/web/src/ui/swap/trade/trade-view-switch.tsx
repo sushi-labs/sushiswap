8e1aabb5e9565c3042f43b8360e9353c71efddb8
solidityteam <tyler@solidity.io>
2025-05-21 12:29:17 -0400

    fix: trade view banner flicker

--- apps/web/src/ui/swap/trade/trade-view-switch.tsx
+++ apps/web/src/ui/swap/trade/trade-view-switch.tsx
@@ -1,5 +1,5 @@
 import { XIcon } from '@heroicons/react-v1/solid'
-import { useLocalStorage } from '@sushiswap/hooks'
+import { useIsMounted, useLocalStorage } from '@sushiswap/hooks'
 import { Button, Switch } from '@sushiswap/ui'
 import { useDerivedStateSimpleTrade } from './derivedstate-simple-trade-provider'
 
@@ -8,6 +8,7 @@ export const TradeViewSwitch = () => {
     mutate: { setTradeView },
     state: { tradeView },
   } = useDerivedStateSimpleTrade()
+  const isMounted = useIsMounted()
   const [hasClosedBanner, closeBanner] = useLocalStorage(
     'has-closed-trade-view-banner',
     false,
@@ -17,7 +18,7 @@ export const TradeViewSwitch = () => {
     closeBanner(true)
   }
 
-  if (hasClosedBanner) return null
+  if (hasClosedBanner || !isMounted) return null
 
   return (
     <div className="w-full flex justify-center p-3 bg-gradient-to-r relative from-blue/10 to-skyblue/10 animate-slide">
