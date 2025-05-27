724049a5ab5c364f9d80bcd2efd75463ce654be8
solidityteam <tyler@solidity.io>
2025-05-27 18:11:56 -0400

    fix: mobile buy sell bridge btn placement

--- apps/web/src/ui/swap/trade/search/search-item.tsx
+++ apps/web/src/ui/swap/trade/search/search-item.tsx
@@ -1,150 +1,129 @@
-import { ArrowUpIcon, StarIcon } from '@heroicons/react-v1/solid'
-import {
-  Badge,
-  Button,
-  Collapsible,
-  Currency,
-  LinkExternal,
-  classNames,
-} from '@sushiswap/ui'
-import { NetworkIcon } from '@sushiswap/ui/icons/NetworkIcon'
-import { useState } from 'react'
-import { shortenAddress } from 'sushi'
-import { Token } from 'sushi/currency'
-import { formatNumber, formatUSD } from 'sushi/format'
+import { ArrowUpIcon, StarIcon } from "@heroicons/react-v1/solid";
+import { Badge, Button, Collapsible, Currency, LinkExternal, classNames } from "@sushiswap/ui";
+import { NetworkIcon } from "@sushiswap/ui/icons/NetworkIcon";
+import { useState } from "react";
+import { shortenAddress } from "sushi";
+import { Token } from "sushi/currency";
+import { formatNumber, formatUSD } from "sushi/format";
 
 export const SearchItem = () => {
-  const [isHovered, setIsHovered] = useState(false)
-  const [isBridgeViewOpen, setIsBridgeViewOpen] = useState(false)
+	const [isHovered, setIsHovered] = useState(false);
+	const [isBridgeViewOpen, setIsBridgeViewOpen] = useState(false);
 
-  const toggleBridgeView = (value: 'open' | 'close') => {
-    if (value === 'open') {
-      if (isBridgeViewOpen) return
-      setIsBridgeViewOpen(true)
-      setIsHovered(false)
-    }
-    if (value === 'close') {
-      setIsBridgeViewOpen(false)
-    }
-  }
+	const toggleBridgeView = (value: "open" | "close") => {
+		if (value === "open") {
+			if (isBridgeViewOpen) return;
+			setIsBridgeViewOpen(true);
+			setIsHovered(false);
+		}
+		if (value === "close") {
+			setIsBridgeViewOpen(false);
+		}
+	};
 
-  return (
-    <>
-      <div
-        className={classNames(
-          'grid col-span-4 grid-cols-[30px_auto_auto_auto] py-2 pr-2 rounded-lg',
-          isHovered && 'bg-blue-500/10',
-        )}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-        <FavoriteButton />
-        <div className="flex items-center gap-3.5">
-          <Badge
-            className="border border-slate-50 dark:border-slate-900 rounded-full z-[11]"
-            position="bottom-right"
-            badgeContent={<NetworkIcon chainId={1} width={16} height={16} />}
-          >
-            <Currency.Icon
-              disableLink
-              currency={
-                new Token({
-                  address: '0x6b3595068778dd592e39a122f4f5a5cf09c90fe2',
-                  name: 'SushiToken',
-                  symbol: 'SUSHI',
-                  chainId: 1,
-                  decimals: 18,
-                })
-              }
-              width={32}
-              height={32}
-            />
-          </Badge>
-          <div className="flex flex-col items-start">
-            <div className="text-sm font-semibold">SUSHI</div>
-            <LinkExternal
-              href="https://etherscan.io/token/0x6b3595068778dd592e39a122f4f5a5cf09c90fe2"
-              className="text-xs flex items-center gap-0.5 !text-muted-foreground"
-            >
-              {shortenAddress('0x6b3595068778dd592e39a122f4f5a5cf09c90fe2')}
-              <ArrowUpIcon width={10} height={10} className="rotate-45" />
-            </LinkExternal>
-          </div>
-        </div>
-        {isHovered ? (
-          <div className="col-span-2 gap-2 ml-auto items-center flex ">
-            <Button
-              size="xs"
-              className="text-slate-50 !rounded-full bg-green-500 font-semibold hover:bg-green-500 active:bg-green-500/95 focus:bg-green-500"
-            >
-              BUY
-            </Button>
-            <Button
-              size="xs"
-              className="text-slate-50 bg-red-100 !rounded-full font-semibold hover:bg-red-100 active:bg-red-100/95 focus:bg-red-500"
-            >
-              SELL
-            </Button>
-            <Button
-              onClick={() => {
-                toggleBridgeView('open')
-              }}
-              size="xs"
-              className="text-slate-50 !rounded-full bg-blue font-semibold"
-            >
-              Bridge
-            </Button>
-          </div>
-        ) : (
-          <>
-            <div className="mx-auto flex flex-col items-end text-slate-900 dark:text-[#FFF5FA]">
-              <span>{formatUSD(0.87)}</span>
-              <span className="font-medium text-red">-5.5%</span>
-            </div>
+	return (
+		<>
+			<div
+				className={classNames(
+					"grid col-span-4 grid-cols-[30px_auto_auto_auto] py-2 pr-2 rounded-lg",
+					isHovered && "bg-blue-500/10"
+				)}
+				onMouseEnter={() => setIsHovered(true)}
+				onMouseLeave={() => setIsHovered(false)}>
+				<FavoriteButton />
+				<div className="flex items-center gap-3.5">
+					<Badge
+						className="border border-slate-50 dark:border-slate-900 rounded-full z-[11]"
+						position="bottom-right"
+						badgeContent={<NetworkIcon chainId={1} width={16} height={16} />}>
+						<Currency.Icon
+							disableLink
+							currency={
+								new Token({
+									address: "0x6b3595068778dd592e39a122f4f5a5cf09c90fe2",
+									name: "SushiToken",
+									symbol: "SUSHI",
+									chainId: 1,
+									decimals: 18,
+								})
+							}
+							width={32}
+							height={32}
+						/>
+					</Badge>
+					<div className="flex flex-col items-start">
+						<div className="text-sm font-semibold">SUSHI</div>
+						<LinkExternal
+							href="https://etherscan.io/token/0x6b3595068778dd592e39a122f4f5a5cf09c90fe2"
+							className="text-xs flex items-center gap-0.5 !text-muted-foreground">
+							{shortenAddress("0x6b3595068778dd592e39a122f4f5a5cf09c90fe2")}
+							<ArrowUpIcon width={10} height={10} className="rotate-45" />
+						</LinkExternal>
+					</div>
+				</div>
+				{isHovered ? (
+					<div className="col-span-4 md:col-span-2 gap-2 mt-3 md:mt-0 md:ml-auto items-center flex px-8 md:px-0">
+						<Button
+							size="xs"
+							className="text-slate-50 w-full md:w-fit !rounded-full bg-green-500 font-semibold hover:bg-green-500 active:bg-green-500/95 focus:bg-green-500">
+							BUY
+						</Button>
+						<Button
+							size="xs"
+							className="text-slate-50 w-full md:w-fit bg-red-100 !rounded-full font-semibold hover:bg-red-100 active:bg-red-100/95 focus:bg-red-500">
+							SELL
+						</Button>
+						<Button
+							onClick={() => {
+								toggleBridgeView("open");
+							}}
+							size="xs"
+							className="text-slate-50 w-full md:w-fit !rounded-full bg-blue font-semibold">
+							Bridge
+						</Button>
+					</div>
+				) : (
+					<>
+						<div className="mx-auto flex flex-col items-end text-slate-900 dark:text-[#FFF5FA]">
+							<span>{formatUSD(0.87)}</span>
+							<span className="font-medium text-red">-5.5%</span>
+						</div>
 
-            <div className="ml-auto flex flex-col items-end text-slate-900 dark:text-[#FFF5FA]">
-              <span>{formatUSD(16232.5)}</span>
-              <span className="font-medium text-muted-foreground">
-                {formatNumber(82.2)} SUSHI
-              </span>
-            </div>
-          </>
-        )}
-      </div>
-      <div className="col-span-4">
-        <Collapsible open={isBridgeViewOpen}>
-          {/* TODO: make this its own component file */}
-          <div
-            className={classNames(
-              'grid col-span-4 grid-cols-[30px_auto_auto_auto] border border-black/5 dark:border-white/5 py-3 px-4 rounded-lg bg-skyblue/10',
-            )}
-          >
-            <div className="flex w-full col-span-4 flex-col gap-4">
-              <div>TODO: bridge info</div>
-              <div className="flex items-center gap-5">
-                <Button
-                  className="w-1/2"
-                  size="sm"
-                  variant="secondary"
-                  onClick={() => toggleBridgeView('close')}
-                >
-                  Close
-                </Button>
-                <Button
-                  className="w-full"
-                  size="sm"
-                  onClick={() => toggleBridgeView('close')}
-                >
-                  Confirm
-                </Button>
-              </div>
-            </div>
-          </div>
-        </Collapsible>
-      </div>
-    </>
-  )
-}
+						<div className="ml-auto flex flex-col items-end text-slate-900 dark:text-[#FFF5FA]">
+							<span>{formatUSD(16232.5)}</span>
+							<span className="font-medium text-muted-foreground">{formatNumber(82.2)} SUSHI</span>
+						</div>
+					</>
+				)}
+			</div>
+			<div className="col-span-4">
+				<Collapsible open={isBridgeViewOpen}>
+					{/* TODO: make this its own component file */}
+					<div
+						className={classNames(
+							"grid col-span-4 grid-cols-[30px_auto_auto_auto] border border-black/5 dark:border-white/5 py-3 px-4 rounded-lg bg-skyblue/10"
+						)}>
+						<div className="flex w-full col-span-4 flex-col gap-4">
+							<div>TODO: bridge info</div>
+							<div className="flex items-center gap-5">
+								<Button
+									className="w-1/2"
+									size="sm"
+									variant="secondary"
+									onClick={() => toggleBridgeView("close")}>
+									Close
+								</Button>
+								<Button className="w-full" size="sm" onClick={() => toggleBridgeView("close")}>
+									Confirm
+								</Button>
+							</div>
+						</div>
+					</div>
+				</Collapsible>
+			</div>
+		</>
+	);
+};
 
 /* @DEV: FOR LATER WHEN WE HAVE DATA <div
 		className={classNames(
@@ -160,25 +139,20 @@ export const SearchItem = () => {
 	</div> */
 
 const FavoriteButton = () => {
-  const [isFavorited, setIsFavorited] = useState(false)
-  const toggleFavorite = () => {
-    setIsFavorited((prev) => !prev)
-  }
-  return (
-    <button
-      className={classNames(
-        'text-[#0000001F] dark:text-[#FFFFFF1F] p-2 hover:text-black/20 dark:hover:text-white/20 rounded-full flex items-center justify-center',
-        'transition-colors duration-200 ease-in-out',
-      )}
-      onClick={toggleFavorite}
-      aria-label="Toggle Favorite"
-      type="button"
-    >
-      <StarIcon
-        width={16}
-        height={16}
-        className={classNames(isFavorited ? 'text-yellow' : '')}
-      />
-    </button>
-  )
-}
+	const [isFavorited, setIsFavorited] = useState(false);
+	const toggleFavorite = () => {
+		setIsFavorited((prev) => !prev);
+	};
+	return (
+		<button
+			className={classNames(
+				"text-[#0000001F] dark:text-[#FFFFFF1F] p-2 hover:text-black/20 dark:hover:text-white/20 rounded-full flex items-center justify-center",
+				"transition-colors duration-200 ease-in-out"
+			)}
+			onClick={toggleFavorite}
+			aria-label="Toggle Favorite"
+			type="button">
+			<StarIcon width={16} height={16} className={classNames(isFavorited ? "text-yellow" : "")} />
+		</button>
+	);
+};
--- apps/web/src/ui/swap/trade/search/search.tsx
+++ apps/web/src/ui/swap/trade/search/search.tsx
@@ -1,34 +1,34 @@
-import { Button } from '@sushiswap/ui'
-import { Wrapper } from '../wrapper'
-import { AvailableTokens } from './available-tokens'
-import { SearchBar } from './search-bar'
-import { useSearchContext } from './search-provider'
+import { Button } from "@sushiswap/ui";
+import { Wrapper } from "../wrapper";
+import { AvailableTokens } from "./available-tokens";
+import { SearchBar } from "./search-bar";
+import { useSearchContext } from "./search-provider";
 
 export const Search = () => {
-  const {
-    state: { searchValue },
-  } = useSearchContext()
-  return (
-    <Wrapper>
-      <div className="flex flex-col gap-4 md:hidden">
-        <div className="flex justify-between items-center">
-          <div className="flex items-center gap-1">
-            <Button variant="quaternary">Available</Button>
-            <Button variant="ghost">Favourite</Button>
-            <Button variant="ghost">Recent</Button>
-          </div>
+	const {
+		state: { searchValue },
+	} = useSearchContext();
+	return (
+		<Wrapper>
+			<div className="flex flex-col gap-4 md:hidden">
+				<div className="flex justify-between items-center">
+					<div className="flex items-center gap-1">
+						<Button variant="quaternary">Available</Button>
+						<Button variant="ghost">Favourite</Button>
+						<Button variant="ghost">Recent</Button>
+					</div>
 
-          <SearchBar />
-        </div>
-        <div className="overflow-x-auto">
-          <AvailableTokens />
-        </div>
-      </div>
+					<SearchBar />
+				</div>
+				<div className="overflow-x-auto">
+					<AvailableTokens />
+				</div>
+			</div>
 
-      <div className="hidden md:flex flex-col gap-4">
-        <SearchBar />
-        {!searchValue ? null : <AvailableTokens />}
-      </div>
-    </Wrapper>
-  )
-}
+			<div className="hidden md:flex flex-col gap-4">
+				<SearchBar />
+				{searchValue === "" ? <AvailableTokens /> : null}
+			</div>
+		</Wrapper>
+	);
+};
