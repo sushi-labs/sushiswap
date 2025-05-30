4b932f53f1935906e494512e8cfe7221dfdffd06
solidityteam <tyler@solidity.io>
2025-05-30 19:15:29 -0400

    feat: add pool table filters

--- apps/web/src/ui/swap/trade/tab-tables/trade-tabs/trade-table-tabs.tsx
+++ apps/web/src/ui/swap/trade/tab-tables/trade-tabs/trade-table-tabs.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import {
+  Button,
   Tabs,
   TabsContent,
   TabsList,
@@ -11,6 +12,7 @@ import { useState } from 'react'
 import { DCAOrdersTable } from '../dca-orders-table/dca-orders-table'
 import { HistoryTable } from '../history-tables/history-table'
 import { LimitOrdersTable } from '../limit-orders-table/limit-orders-table'
+import { TradeTableFilters } from './trade-table-filters'
 
 export const TABS = [
   {
@@ -36,22 +38,32 @@ export const TradeTableTabs = () => {
       onValueChange={setCurrentTab}
       className="-mx-5 md:mx-0"
     >
-      <TabsList className="!px-5 w-full md:!px-0 md:!pb-0 !pb-6 !justify-start bg-white dark:bg-background border-t-0 border-l-0 border-r-0 rounded-none shadow-none md:rounded-lg md:border-none md:mx-0">
-        {TABS.map((tab) => (
-          <TabsTrigger
-            key={tab.value}
-            value={tab.value}
-            className={classNames(
-              '!shadow-none data-[state=active]:!text-blue  dark:data-[state=active]:!text-skyblue transition-colors !pl-3 !pt-2 border-none',
-              currentTab === tab.value
-                ? '!bg-blue/[8%] hover:!bg-blue/[15%] focus:!bg-blue/[18%] dark:!bg-[#3DB1FF]/[8%] dark:hover:!bg-[#3DB1FF]/[15%] dark:focus:!bg-[#3DB1FF]/[18%]'
-                : 'hover:!bg-blue/[8%] focus:!bg-blue/[18%] dark:hover:!bg-[#3DB1FF]/[8%] dark:focus:!bg-[#3DB1FF]/[18%]',
-            )}
-          >
-            {tab.label}
-          </TabsTrigger>
-        ))}
-      </TabsList>
+      <div className="flex flex-col items-start justify-between xl:items-center xl:flex-row">
+        <TabsList className="!px-5 w-full md:!px-0 md:!pb-0 !pb-6 !justify-start bg-white dark:bg-background border-t-0 border-l-0 border-r-0 rounded-none shadow-none md:rounded-lg md:border-none md:mx-0">
+          {TABS.map((tab) => (
+            <TabsTrigger
+              key={tab.value}
+              asChild
+              value={tab.value}
+              className="bg-transparent"
+            >
+              <Button
+                size="sm"
+                asChild
+                className={classNames(
+                  '!shadow-none data-[state=active]:!text-blue  dark:data-[state=active]:!text-skyblue transition-colors !pl-3 !pt-2 border-none',
+                  currentTab === tab.value
+                    ? '!bg-blue/[8%] hover:!bg-blue/[15%] dark:!bg-[#3DB1FF]/[8%] dark:hover:!bg-[#3DB1FF]/[15%]'
+                    : 'hover:!bg-blue/[8%] dark:hover:!bg-[#3DB1FF]/[8%]',
+                )}
+              >
+                {tab.label}
+              </Button>
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        <TradeTableFilters />
+      </div>
       <TabsContent
         value={TABS[0].value}
         className="px-5 !mt-0 !pt-2 bg-[#F9FAFB] dark:bg-slate-900 md:px-0 pb-[86px] md:pb-0"
