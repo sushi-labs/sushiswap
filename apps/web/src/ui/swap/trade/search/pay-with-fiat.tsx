163b44d5f910d2ca9932f8cf7f7e8c5888255f75
solidityteam <tyler@solidity.io>
2025-06-04 11:21:14 -0400

    fix: icon overflow

--- apps/web/src/ui/swap/trade/search/pay-with-fiat.tsx
+++ apps/web/src/ui/swap/trade/search/pay-with-fiat.tsx
@@ -1,19 +1,15 @@
-import { ChevronDownIcon, CreditCardIcon } from '@heroicons/react-v1/solid'
-import { Button, classNames } from '@sushiswap/ui'
+import { ChevronDownIcon, CreditCardIcon } from "@heroicons/react-v1/solid";
+import { Button, classNames } from "@sushiswap/ui";
 
 export const PayWithFiat = () => {
-  //TODO: make this a menu trigger with options for different fiat payment methods
-  return (
-    <Button
-      variant={'secondary'}
-      type="button"
-      className={classNames('!rounded-full')}
-    >
-      <div className="flex items-center gap-0.5">
-        <CreditCardIcon width={20} height={20} />
-        Pay with Fiat
-        <ChevronDownIcon width={16} height={16} />
-      </div>
-    </Button>
-  )
-}
+	//TODO: make this a menu trigger with options for different fiat payment methods
+	return (
+		<Button variant={"secondary"} type="button" className={classNames("!rounded-full md:overflow-hidden")}>
+			<div className="flex items-center gap-0.5">
+				<CreditCardIcon width={18} height={18} />
+				Pay with Fiat
+				<ChevronDownIcon width={16} height={16} />
+			</div>
+		</Button>
+	);
+};
