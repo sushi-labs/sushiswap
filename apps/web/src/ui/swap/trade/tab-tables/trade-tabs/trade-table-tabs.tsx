74ceb85964bbfb4895afff70e9ea530896036fd1
solidityteam <tyler@solidity.io>
2025-06-04 13:13:56 -0400

    fix: trade tables colors

--- apps/web/src/ui/swap/trade/tab-tables/trade-tabs/trade-table-tabs.tsx
+++ apps/web/src/ui/swap/trade/tab-tables/trade-tabs/trade-table-tabs.tsx
@@ -1,10 +1,13 @@
 'use client'
 
+import { useDarkMode } from '@sushiswap/hooks'
 import { Button, Tabs, TabsContent, TabsList, TabsTrigger } from '@sushiswap/ui'
+import { useTheme } from 'next-themes'
 import { useState } from 'react'
 import { DCAOrdersTable } from '../dca-orders-table/dca-orders-table'
 import { HistoryTable } from '../history-tables/history-table'
 import { LimitOrdersTable } from '../limit-orders-table/limit-orders-table'
+import { TradeTableFilters } from './trade-table-filters'
 
 export const TABS = [
   {
@@ -23,6 +26,9 @@ export const TABS = [
 
 export const TradeTableTabs = () => {
   const [currentTab, setCurrentTab] = useState(TABS[0].value)
+  const { theme } = useTheme()
+
+  console.log('theme', theme)
 
   return (
     <Tabs
@@ -31,13 +37,13 @@ export const TradeTableTabs = () => {
       className="-mx-5 md:mx-0"
     >
       <div className="flex flex-col items-start justify-between xl:items-center xl:flex-row">
-        <div className="w-full p-3 pb-0 bg-white border-b rounded-t-lg md:border-none xl:bg-transparent dark:bg-background md:dark:bg-slate-800 border-accent">
-          <TabsList className="!px-2.5 w-full md:!px-0 gap-2 md:!pb-0 !pb-6 !justify-start bg-white xl:bg-transparent dark:bg-background md:dark:bg-slate-800 !border-none rounded-none shadow-none md:rounded-lg md:border-none md:mx-0 xl:rounded-lg !rounded-b-none">
+        <div className="w-full p-3 bg-white border-b rounded-t-lg xl:px-0 md:border-none xl:!bg-background dark:bg-background md:dark:bg-slate-800 border-accent">
+          <TabsList className="!px-2.5 w-full md:!px-0 gap-2 md:!pb-0 !pb-6 !justify-start bg-white xl:!bg-transparent dark:bg-background md:dark:bg-slate-800 !border-none rounded-none shadow-none md:rounded-lg md:border-none md:mx-0 xl:rounded-lg !rounded-b-none">
             {TABS.map((tab) => (
               <TabsTrigger
                 key={tab.value}
                 value={tab.value}
-                className="!bg-transparent !border-none !shadow-none !px-0"
+                className="!bg-transparent xl:!bg-background !border-none !shadow-none !px-0"
               >
                 <Button
                   key={tab.value}
@@ -45,10 +51,10 @@ export const TradeTableTabs = () => {
                   size="sm"
                   variant={
                     currentTab === tab.value
-                      ? 'quaternary'
-                      : currentTab === tab.value
-                        ? 'quinary'
-                        : 'ghost'
+                      ? theme === 'dark'
+                        ? 'quaternary'
+                        : 'quinary'
+                      : 'ghost'
                   }
                   className="select-none !gap-1"
                 >
@@ -58,22 +64,23 @@ export const TradeTableTabs = () => {
             ))}
           </TabsList>
         </div>
+        <TradeTableFilters />
       </div>
       <TabsContent
         value={TABS[0].value}
-        className="px-5 !mt-0 !pt-2 md:!pt-0 bg-[#F9FAFB] dark:bg-slate-900 md:px-0 pb-[86px] md:bg-white xl:bg-transparent md:pb-0 rounded-b-xl"
+        className="px-5 !mt-0 !pt-2 md:!pt-0 xl:!pt-2 bg-[#F9FAFB] dark:bg-slate-900 md:px-0 pb-[86px] md:bg-white xl:bg-transparent md:pb-0 rounded-b-xl"
       >
         <LimitOrdersTable />
       </TabsContent>
       <TabsContent
         value={TABS[1].value}
-        className="px-5 !mt-0 !pt-2 md:!pt-0 bg-[#F9FAFB] dark:bg-slate-900 md:px-0 pb-[86px] md:bg-white xl:bg-transparent md:pb-0 rounded-b-xl"
+        className="px-5 !mt-0 !pt-2 md:!pt-0 xl:!pt-2 bg-[#F9FAFB] dark:bg-slate-900 md:px-0 pb-[86px] md:bg-white xl:bg-transparent md:pb-0 rounded-b-xl"
       >
         <DCAOrdersTable />
       </TabsContent>
       <TabsContent
         value={TABS[2].value}
-        className="px-5 !mt-0 !pt-2 md:!pt-0 bg-[#F9FAFB] dark:bg-slate-900 md:px-0 pb-[86px] md:bg-white xl:bg-transparent md:pb-0 rounded-b-xl"
+        className="px-5 !mt-0 !pt-2 md:!pt-0 xl:!pt-2 bg-[#F9FAFB] dark:bg-slate-900 md:px-0 pb-[86px] md:bg-white xl:bg-transparent md:pb-0 rounded-b-xl"
       >
         <HistoryTable />
       </TabsContent>
