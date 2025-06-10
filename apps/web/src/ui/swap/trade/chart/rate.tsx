97791382817d44ff1e51e1e7aad4e13d2454a0b1
solidityteam <tyler@solidity.io>
2025-06-10 16:54:49 -0400

    fix: rate coloring and wrapping

--- apps/web/src/ui/swap/trade/chart/rate.tsx
+++ apps/web/src/ui/swap/trade/chart/rate.tsx
@@ -20,20 +20,20 @@ export const Rate: React.FC<RateProps> = ({ token0, token1 }) => {
   const base = flipped ? token1 : token0
   const quote = flipped ? token0 : token1
 
-  const rate = base.usdPrice / quote.usdPrice
+  // const rate = base.usdPrice / quote.usdPrice;
 
   return (
-    <div className="flex items-center space-x-2 text-sm font-medium text-[#0F172A] dark:text-white">
-      <span>
-        1 {base.symbol} (${base.usdPrice.toFixed(2)}) = {rate.toFixed(2)}{' '}
-        {quote.symbol} (${quote.usdPrice.toFixed(2)})
-      </span>
+    <div className="flex items-center flex-wrap text-sm font-medium text-[#0F172A] dark:text-white">
+      <div className="mr-1">
+        1 {base.symbol} = ${base.usdPrice.toFixed(2)}
+      </div>
       <button
         type="button"
         onClick={() => setFlipped(!flipped)}
-        className="transition hover:opacity-75"
+        className="transition hover:opacity-75 text-blue dark:text-skyblue flex items-center gap-1"
       >
-        <ArrowsLeftRightIcon />
+        <ArrowsLeftRightIcon width={18} />
+        Switch To {quote.symbol}
       </button>
     </div>
   )
