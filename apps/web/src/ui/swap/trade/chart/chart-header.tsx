65fd645dec7bcf324e651704532fc39e4e18d785
solidityteam <tyler@solidity.io>
2025-06-11 13:21:26 -0400

    fix: token selection for chart

--- apps/web/src/ui/swap/trade/chart/chart-header.tsx
+++ apps/web/src/ui/swap/trade/chart/chart-header.tsx
@@ -7,25 +7,46 @@ import { useMemo } from 'react'
 import type { Type } from 'sushi/currency'
 
 import { useBreakpoint } from '@sushiswap/hooks'
+import { useSearchParams } from 'next/navigation'
 import { useState } from 'react'
 import { TokenSelectorV2 } from 'src/lib/wagmi/components/token-selector/token-selector-v2'
+import { useTokenWithCache } from 'src/lib/wagmi/hooks/tokens/useTokenWithCache'
 import { USDC } from 'sushi/currency'
+import type { Address } from 'viem'
+import { isAddress } from 'viem/utils'
+import { useChainId } from 'wagmi'
 import { Rate } from './rate'
 
 export const ChartHeader = () => {
   const [token, setToken] = useState<Type>(Native.onChain(43114))
   const [isTokenLoading, _setIsTokenLoading] = useState(false)
   const { isMd: isMdScreen } = useBreakpoint('md')
+  const connectedChainId = useChainId()
+  const searchParams = useSearchParams()
+  const _token0 = searchParams.get('token0')
+  const token0 =
+    _token0 && _token0 === 'NATIVE'
+      ? Native.onChain(connectedChainId).wrapped.address
+      : _token0
+        ? _token0
+        : token.wrapped.address
 
-  const input0 = token
+  const { data: token0FromCache } = useTokenWithCache({
+    chainId: connectedChainId,
+    address: token0 as Address,
+    enabled: isAddress(token0, { strict: false }),
+    keepPreviousData: false,
+  })
+
+  const input0 = token0FromCache ?? Native.onChain(connectedChainId)
   const input1 = USDC[43114]
 
   const selector = useMemo(() => {
     return (
       <TokenSelectorV2
         type="buy"
-        selected={token}
-        chainId={1}
+        selected={input0}
+        chainId={connectedChainId}
         onSelect={(token) => {
           setToken(token)
         }}
@@ -34,15 +55,15 @@ export const ChartHeader = () => {
         <Button
           data-state={isTokenLoading ? 'inactive' : 'active'}
           size="lg"
-          variant={token ? 'secondary' : 'default'}
+          variant={input0 ? 'secondary' : 'default'}
           id={'swap-to'}
           type="button"
           className={classNames(
-            token ? 'pl-2 pr-3' : '',
+            input0 ? 'pl-2 pr-3' : '',
             '!rounded-full !min-h-[40px] !h-[40px] data-[state=inactive]:hidden data-[state=active]:flex bg-slate-200 dark:bg-slate-750',
           )}
         >
-          {token ? (
+          {input0 ? (
             <>
               <div className="w-[23px] h-[23px] mr-0.5">
                 <Badge
@@ -52,7 +73,7 @@ export const ChartHeader = () => {
                     <NetworkIcon
                       type="square"
                       className="rounded-[3px]"
-                      chainId={token.chainId}
+                      chainId={input0.chainId}
                       width={12}
                       height={12}
                     />
@@ -60,13 +81,13 @@ export const ChartHeader = () => {
                 >
                   <Currency.Icon
                     disableLink
-                    currency={token}
+                    currency={input0}
                     width={23}
                     height={23}
                   />
                 </Badge>
               </div>
-              {token.symbol}
+              {input0.symbol}
               <SelectIcon />
             </>
           ) : (
@@ -75,7 +96,7 @@ export const ChartHeader = () => {
         </Button>
       </TokenSelectorV2>
     )
-  }, [token, isTokenLoading, isMdScreen])
+  }, [input0, isTokenLoading, isMdScreen, connectedChainId])
 
   return (
     <div className="flex flex-col items-start justify-between w-full gap-4 lg:items-center md:flex-col lg:flex-row lg:gap-0">
