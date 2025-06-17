39dc320213270707aaf445488889c6d12de895ae
solidityteam <tyler@solidity.io>
2025-06-17 17:20:43 -0400

    chore: rm unused imports

--- apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/WalletConnector/WalletConnector.tsx
+++ apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/WalletConnector/WalletConnector.tsx
@@ -1,64 +1,45 @@
-import { Pact, createClient } from '@kadena/client'
-import {
-  Button,
-  type ButtonProps,
-  Chip,
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@sushiswap/ui'
-import { JazzIcon } from '@sushiswap/ui/icons/JazzIcon'
-import { useEffect, useState } from 'react'
-import { IS_TESTNET } from '~kadena/_common/constants/is-testnet'
-import { truncateText } from '~kadena/_common/lib/utils/formatters'
-import { useKadena } from '~kadena/kadena-wallet-provider'
-import { DefaultView } from './DefaultView'
-import { SettingsView } from './SettingsView'
-import { WalletListView } from './WalletListView'
+import { Button, type ButtonProps, Chip, Popover, PopoverContent, PopoverTrigger } from "@sushiswap/ui";
+import { JazzIcon } from "@sushiswap/ui/icons/JazzIcon";
+import { useState } from "react";
+import { IS_TESTNET } from "~kadena/_common/constants/is-testnet";
+import { truncateText } from "~kadena/_common/lib/utils/formatters";
+import { useKadena } from "~kadena/kadena-wallet-provider";
+import { DefaultView } from "./DefaultView";
+import { SettingsView } from "./SettingsView";
+import { WalletListView } from "./WalletListView";
 
-export type IProfileView = 'default' | 'settings'
+export type IProfileView = "default" | "settings";
 
 export const WalletConnector = (props: ButtonProps) => {
-  const { isConnected, isConnecting, activeAccount } = useKadena()
+	const { isConnected, isConnecting, activeAccount } = useKadena();
 
-  const [view, setView] = useState<IProfileView>('default')
+	const [view, setView] = useState<IProfileView>("default");
 
-  return (
-    <Popover>
-      <PopoverTrigger className="relative w-full">
-        <Button
-          loading={isConnecting}
-          disabled={isConnecting}
-          asChild
-          {...props}
-        >
-          {isConnected && activeAccount?.accountName ? (
-            <>
-              <JazzIcon diameter={20} address={activeAccount?.accountName} />
-              <span className="hidden sm:block">
-                {truncateText(activeAccount?.accountName)}
-              </span>
-            </>
-          ) : (
-            <>{isConnecting ? 'Connecting' : 'Connect'} </>
-          )}
-        </Button>
-        {IS_TESTNET && isConnected ? (
-          <Chip className="!text-white rounded-md h-fit absolute right-0 !px-1 !py-0 text-[8px] -top-1">
-            Testnet
-          </Chip>
-        ) : null}
-      </PopoverTrigger>
+	return (
+		<Popover>
+			<PopoverTrigger className="relative w-full">
+				<Button loading={isConnecting} disabled={isConnecting} asChild {...props}>
+					{isConnected && activeAccount?.accountName ? (
+						<>
+							<JazzIcon diameter={20} address={activeAccount?.accountName} />
+							<span className="hidden sm:block">{truncateText(activeAccount?.accountName)}</span>
+						</>
+					) : (
+						<>{isConnecting ? "Connecting" : "Connect"} </>
+					)}
+				</Button>
+				{IS_TESTNET && isConnected ? (
+					<Chip className="!text-white rounded-md h-fit absolute right-0 !px-1 !py-0 text-[8px] -top-1">
+						Testnet
+					</Chip>
+				) : null}
+			</PopoverTrigger>
 
-      <PopoverContent className="!p-1 !rounded-2xl w-full">
-        {!isConnected ? <WalletListView isFullWidth={props.fullWidth} /> : null}
-        {view === 'default' && isConnected ? (
-          <DefaultView setView={setView} />
-        ) : null}
-        {view === 'settings' && isConnected ? (
-          <SettingsView setView={setView} />
-        ) : null}
-      </PopoverContent>
-    </Popover>
-  )
-}
+			<PopoverContent className="!p-1 !rounded-2xl w-full">
+				{!isConnected ? <WalletListView isFullWidth={props.fullWidth} /> : null}
+				{view === "default" && isConnected ? <DefaultView setView={setView} /> : null}
+				{view === "settings" && isConnected ? <SettingsView setView={setView} /> : null}
+			</PopoverContent>
+		</Popover>
+	);
+};
--- apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/WalletConnector/WalletListView.tsx
+++ apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/WalletConnector/WalletListView.tsx
@@ -1,44 +1,36 @@
-import { List, SelectIcon } from '@sushiswap/ui'
-import Image from 'next/image'
-import { useKadena } from '~kadena/kadena-wallet-provider'
+import { List, SelectIcon } from "@sushiswap/ui";
+import Image from "next/image";
+import { useKadena } from "~kadena/kadena-wallet-provider";
 
-export const WalletListView = ({
-  isFullWidth = false,
-}: {
-  isFullWidth?: boolean
-}) => {
-  const { adapters, handleConnect } = useKadena()
-
-  console.log('adapters', adapters)
-
-  return (
-    <List
-      className={`flex flex-col gap-1 !p-0 ${isFullWidth ? '[width:calc(var(--radix-popover-trigger-width)_-_10px)]' : ''}`}
-    >
-      <List.Control className="bg-gray-100">
-        {adapters.map((adapter) => (
-          <List.MenuItem
-            icon={() => (
-              <Image
-                src={adapter.imageURI}
-                alt={adapter.name}
-                width={25}
-                height={25}
-                className="max-h-[25px]"
-              />
-            )}
-            className="flex items-center justify-start w-full min-w-[180px] text-left"
-            key={adapter.name}
-            title={adapter.name === 'Ecko' ? 'eckoWALLET' : adapter.name}
-            onClick={() => {
-              adapter.detected
-                ? handleConnect(adapter.name)
-                : window.open(adapter.installUrl, '_blank')
-            }}
-            hoverIcon={() => <SelectIcon className="-rotate-90" />}
-          />
-        ))}
-      </List.Control>
-    </List>
-  )
-}
+export const WalletListView = ({ isFullWidth = false }: { isFullWidth?: boolean }) => {
+	const { adapters, handleConnect } = useKadena();
+	return (
+		<List
+			className={`flex flex-col gap-1 !p-0 ${
+				isFullWidth ? "[width:calc(var(--radix-popover-trigger-width)_-_10px)]" : ""
+			}`}>
+			<List.Control className="bg-gray-100">
+				{adapters.map((adapter) => (
+					<List.MenuItem
+						icon={() => (
+							<Image
+								src={adapter.imageURI}
+								alt={adapter.name}
+								width={25}
+								height={25}
+								className="max-h-[25px]"
+							/>
+						)}
+						className="flex items-center justify-start w-full min-w-[180px] text-left"
+						key={adapter.name}
+						title={adapter.name === "Ecko" ? "eckoWALLET" : adapter.name}
+						onClick={() => {
+							adapter.detected ? handleConnect(adapter.name) : window.open(adapter.installUrl, "_blank");
+						}}
+						hoverIcon={() => <SelectIcon className="-rotate-90" />}
+					/>
+				))}
+			</List.Control>
+		</List>
+	);
+};
