e804995684e9a4791e74db6277bad67f6a8fcbea
solidityteam <tyler@solidity.io>
2025-06-18 17:01:55 -0400

    fix: add add liq button to pool by id page

--- apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/Pools/PoolDetails/PoolHeader.tsx
+++ apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/Pools/PoolDetails/PoolHeader.tsx
@@ -5,6 +5,7 @@ import {
   Button,
   Currency,
   LinkExternal,
+  LinkInternal,
   SkeletonCircle,
   SkeletonText,
   typographyVariants,
@@ -59,112 +60,119 @@ export const PoolHeader = ({ poolId }: { poolId: string }) => {
   }, [pool?.token0, pool?.token1, setToken0, setToken1, baseTokens])
 
   return (
-    <div className="flex flex-col gap-6">
-      <div className="flex flex-col gap-4">
-        <button
-          className="text-sm text-blue hover:underline w-fit"
-          type="button"
-          onClick={() => {
-            router.back()
-          }}
-        >
-          ← Back
-        </button>
-        {isLoading ? (
-          <div className="flex items-center w-full gap-3">
-            <div className="flex items-center">
-              <SkeletonCircle radius={40} />
-              <SkeletonCircle radius={40} className="-ml-[13.33px]" />
+    <div className="flex justify-between w-full">
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-4">
+          <button
+            className="text-sm text-blue hover:underline w-fit"
+            type="button"
+            onClick={() => {
+              router.back()
+            }}
+          >
+            ← Back
+          </button>
+          {isLoading ? (
+            <div className="flex items-center w-full gap-3">
+              <div className="flex items-center">
+                <SkeletonCircle radius={40} />
+                <SkeletonCircle radius={40} className="-ml-[13.33px]" />
+              </div>
+              <div className="w-[200px]">
+                <SkeletonText fontSize="3xl" />
+              </div>
             </div>
-            <div className="w-[200px]">
-              <SkeletonText fontSize="3xl" />
-            </div>
-          </div>
-        ) : (
-          <div className="relative flex items-center gap-3 max-w-[100vh]">
-            <Currency.IconList iconWidth={36} iconHeight={36}>
-              <Icon currency={token0} />
-              <Icon currency={token1} />
-            </Currency.IconList>
-            <Button
-              asChild
-              variant="link"
-              className={typographyVariants({
-                variant: 'h1',
-                className:
-                  'sm:!text2-xl sm:!text-4xl !font-bold text-gray-900 dark:text-slate-50 truncate overflow-x-auto',
-              })}
-            >
-              <LinkExternal href={getChainwebAddressLink(poolAddress ?? '')}>
-                {token0?.tokenSymbol}/{token1?.tokenSymbol}
-              </LinkExternal>
-            </Button>
-            {/* <div className="bg-pink/20 text-pink text-sm px-2 py-1 font-semibold rounded-full mt-0.5">
+          ) : (
+            <div className="relative flex items-center gap-3 max-w-[100vh]">
+              <Currency.IconList iconWidth={36} iconHeight={36}>
+                <Icon currency={token0} />
+                <Icon currency={token1} />
+              </Currency.IconList>
+              <Button
+                asChild
+                variant="link"
+                className={typographyVariants({
+                  variant: 'h1',
+                  className:
+                    'sm:!text2-xl sm:!text-4xl !font-bold text-gray-900 dark:text-slate-50 truncate overflow-x-auto',
+                })}
+              >
+                <LinkExternal href={getChainwebAddressLink(poolAddress ?? '')}>
+                  {token0?.tokenSymbol}/{token1?.tokenSymbol}
+                </LinkExternal>
+              </Button>
+              {/* <div className="bg-pink/20 text-pink text-sm px-2 py-1 font-semibold rounded-full mt-0.5">
               V2
             </div> */}
+            </div>
+          )}
+        </div>
+        <div className="flex flex-wrap items-center gap-y-5 gap-x-[32px] text-secondary-foreground mb-8 mt-1.5">
+          <div className="flex items-center gap-1.5">
+            <span className="font-semibold tracking-tighter">Fee</span>
+            0.3%
           </div>
-        )}
+          <div className="flex items-center gap-1.5">
+            <span className="font-semibold tracking-tighter">Network</span>
+            Kadena
+          </div>
+          {isLoading ? (
+            <>
+              <div className="w-48">
+                <SkeletonText />
+              </div>
+              <div className="w-48">
+                <SkeletonText />
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex items-center gap-1.5">
+                <span className="font-semibold tracking-tighter">
+                  {token0?.tokenSymbol}
+                </span>
+                <LinkExternal
+                  target="_blank"
+                  href={getChainwebAddressLink(token0?.tokenAddress ?? '')}
+                >
+                  <Button
+                    asChild
+                    variant="link"
+                    size="sm"
+                    className="!font-medium !text-secondary-foreground"
+                  >
+                    {`${token0?.tokenName}`}
+                    <ArrowTopRightOnSquareIcon className="w-3 h-3" />
+                  </Button>
+                </LinkExternal>
+              </div>
+              <div className="flex items-center gap-1.5">
+                <span className="font-semibold tracking-tighter">
+                  {token1?.tokenSymbol}
+                </span>
+                <LinkExternal
+                  target="_blank"
+                  href={getChainwebAddressLink(token1?.tokenAddress ?? '')}
+                >
+                  <Button
+                    asChild
+                    variant="link"
+                    size="sm"
+                    className="!font-medium !text-secondary-foreground"
+                  >
+                    {`${token1?.tokenName}`}
+                    <ArrowTopRightOnSquareIcon className="w-3 h-3" />
+                  </Button>
+                </LinkExternal>
+              </div>
+            </>
+          )}
+        </div>
       </div>
-      <div className="flex flex-wrap items-center gap-y-5 gap-x-[32px] text-secondary-foreground mb-8 mt-1.5">
-        <div className="flex items-center gap-1.5">
-          <span className="font-semibold tracking-tighter">Fee</span>
-          0.3%
-        </div>
-        <div className="flex items-center gap-1.5">
-          <span className="font-semibold tracking-tighter">Network</span>
-          Kadena
-        </div>
-        {isLoading ? (
-          <>
-            <div className="w-48">
-              <SkeletonText />
-            </div>
-            <div className="w-48">
-              <SkeletonText />
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="flex items-center gap-1.5">
-              <span className="font-semibold tracking-tighter">
-                {token0?.tokenSymbol}
-              </span>
-              <LinkExternal
-                target="_blank"
-                href={getChainwebAddressLink(token0?.tokenAddress ?? '')}
-              >
-                <Button
-                  asChild
-                  variant="link"
-                  size="sm"
-                  className="!font-medium !text-secondary-foreground"
-                >
-                  {`${token0?.tokenName}`}
-                  <ArrowTopRightOnSquareIcon className="w-3 h-3" />
-                </Button>
-              </LinkExternal>
-            </div>
-            <div className="flex items-center gap-1.5">
-              <span className="font-semibold tracking-tighter">
-                {token1?.tokenSymbol}
-              </span>
-              <LinkExternal
-                target="_blank"
-                href={getChainwebAddressLink(token1?.tokenAddress ?? '')}
-              >
-                <Button
-                  asChild
-                  variant="link"
-                  size="sm"
-                  className="!font-medium !text-secondary-foreground"
-                >
-                  {`${token1?.tokenName}`}
-                  <ArrowTopRightOnSquareIcon className="w-3 h-3" />
-                </Button>
-              </LinkExternal>
-            </div>
-          </>
-        )}
+      <div className="flex items-center gap-2">
+        <LinkInternal shallow={true} href={`/kadena/pool/add`}>
+          <Button asChild>Add Liquidity</Button>
+        </LinkInternal>
       </div>
     </div>
   )
