dc5c83ab5e55338ce0d8cc04edd27996a4a2564c
solidityteam <tyler@solidity.io>
2025-06-19 17:22:45 -0400

    fix: decode uri comp

--- apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/Pools/PoolsTable/PoolsTable.tsx
+++ apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/Pools/PoolsTable/PoolsTable.tsx
@@ -1,139 +1,118 @@
-'use client'
+"use client";
 
+import { Card, CardHeader, CardTitle, DataTable, SkeletonText } from "@sushiswap/ui";
+import type { ColumnDef, PaginationState, SortingState, TableState } from "@tanstack/react-table";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { usePoolFilters } from "src/ui/pool";
+import { useAllPools } from "~kadena/_common/lib/hooks/use-all-pools";
+import type { Pool } from "~kadena/_common/types/get-all-pools-type";
 import {
-  Card,
-  CardHeader,
-  CardTitle,
-  DataTable,
-  SkeletonText,
-} from '@sushiswap/ui'
-import type {
-  ColumnDef,
-  PaginationState,
-  SortingState,
-  TableState,
-} from '@tanstack/react-table'
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
-import { usePoolFilters } from 'src/ui/pool'
-import { useAllPools } from '~kadena/_common/lib/hooks/use-all-pools'
-import type { Pool } from '~kadena/_common/types/get-all-pools-type'
-import {
-  APR_COLUMN,
-  FEES_1D_COLUMN,
-  NAME_COLUMN,
-  TRANSACTIONS_1D_COLUMN,
-  TVL_COLUMN,
-  VOLUME_1D_COLUMN,
-} from './PoolColumns'
+	APR_COLUMN,
+	FEES_1D_COLUMN,
+	NAME_COLUMN,
+	TRANSACTIONS_1D_COLUMN,
+	TVL_COLUMN,
+	VOLUME_1D_COLUMN,
+} from "./PoolColumns";
 
 const COLUMNS = [
-  NAME_COLUMN,
-  TVL_COLUMN,
-  VOLUME_1D_COLUMN,
-  FEES_1D_COLUMN,
-  TRANSACTIONS_1D_COLUMN,
-  APR_COLUMN,
-] satisfies ColumnDef<Pool, unknown>[]
+	NAME_COLUMN,
+	TVL_COLUMN,
+	VOLUME_1D_COLUMN,
+	FEES_1D_COLUMN,
+	TRANSACTIONS_1D_COLUMN,
+	APR_COLUMN,
+] satisfies ColumnDef<Pool, unknown>[];
 
 export const PoolsTable = () => {
-  const { tokenSymbols } = usePoolFilters()
+	const { tokenSymbols } = usePoolFilters();
 
-  const [sorting, setSorting] = useState<SortingState>([
-    { id: 'liquidityUSD', desc: true },
-  ])
+	const [sorting, setSorting] = useState<SortingState>([{ id: "liquidityUSD", desc: true }]);
 
-  const [paginationState, setPaginationState] = useState<PaginationState>({
-    pageIndex: 0,
-    pageSize: 10,
-  })
+	const [paginationState, setPaginationState] = useState<PaginationState>({
+		pageIndex: 0,
+		pageSize: 10,
+	});
 
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } =
-    useAllPools({
-      first: 10,
-      orderBy: 'TVL_USD_DESC',
-    })
+	const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } = useAllPools({
+		first: 10,
+		orderBy: "TVL_USD_DESC",
+	});
+	console.log(data);
 
-  const start = paginationState.pageIndex * paginationState.pageSize
-  const end = start + paginationState.pageSize
-  const pageRows = data?.pools.slice(start, end)
+	const start = paginationState.pageIndex * paginationState.pageSize;
+	const end = start + paginationState.pageSize;
+	const pageRows = data?.pools.slice(start, end);
 
-  useEffect(() => {
-    if (
-      data?.pools &&
-      end > data?.pools?.length &&
-      hasNextPage &&
-      !isFetchingNextPage
-    ) {
-      fetchNextPage()
-    }
-  }, [data?.pools, fetchNextPage, end, hasNextPage, isFetchingNextPage])
+	useEffect(() => {
+		if (data?.pools && end > data?.pools?.length && hasNextPage && !isFetchingNextPage) {
+			fetchNextPage();
+		}
+	}, [data?.pools, fetchNextPage, end, hasNextPage, isFetchingNextPage]);
 
-  const rowLink = useCallback((row: Pool) => {
-    return `/kadena/pool/${row.id}`
-  }, [])
+	const rowLink = useCallback((row: Pool) => {
+		return `/kadena/pool/${row.id}`;
+	}, []);
 
-  const filtered = useMemo(() => {
-    if (!data?.pools) return [] as Pool[]
+	const filtered = useMemo(() => {
+		if (!data?.pools) return [] as Pool[];
 
-    return data.pools.filter((pool) => {
-      if (tokenSymbols.length) {
-        if (
-          !tokenSymbols.every((symbol) => {
-            symbol = symbol.toLowerCase()
+		return data.pools.filter((pool) => {
+			if (tokenSymbols.length) {
+				if (
+					!tokenSymbols.every((symbol) => {
+						symbol = symbol.toLowerCase();
 
-            const poolName = `${pool.token0.name}-${pool.token1.name}`
+						const poolName = `${pool.token0.name}-${pool.token1.name}`;
 
-            if (poolName.toLowerCase().includes(symbol)) return true
+						if (poolName.toLowerCase().includes(symbol)) return true;
 
-            return false
-          })
-        ) {
-          return false
-        }
-      }
+						return false;
+					})
+				) {
+					return false;
+				}
+			}
 
-      return true
-    })
-  }, [tokenSymbols, data?.pools])
+			return true;
+		});
+	}, [tokenSymbols, data?.pools]);
 
-  const state: Partial<TableState> = useMemo(() => {
-    return {
-      sorting,
-      pagination: {
-        pageIndex: 0,
-        pageSize: filtered?.length,
-      },
-    }
-  }, [sorting, filtered])
+	const state: Partial<TableState> = useMemo(() => {
+		return {
+			sorting,
+			pagination: {
+				pageIndex: 0,
+				pageSize: filtered?.length,
+			},
+		};
+	}, [sorting, filtered]);
 
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle>
-          {isLoading ? (
-            <div className="!w-28 !h-[18px]">
-              <SkeletonText />
-            </div>
-          ) : (
-            <span>
-              Pools{' '}
-              <span className="text-gray-400 dark:text-slate-500">
-                ({filtered.length})
-              </span>
-            </span>
-          )}
-        </CardTitle>
-      </CardHeader>
-      <DataTable
-        state={state}
-        onSortingChange={setSorting}
-        loading={isLoading}
-        linkFormatter={rowLink}
-        columns={COLUMNS}
-        data={pageRows ?? ([] as unknown as Pool[])}
-        pagination={true}
-        onPaginationChange={setPaginationState}
-      />
-    </Card>
-  )
-}
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>
+					{isLoading ? (
+						<div className="!w-28 !h-[18px]">
+							<SkeletonText />
+						</div>
+					) : (
+						<span>
+							Pools <span className="text-gray-400 dark:text-slate-500">({filtered.length})</span>
+						</span>
+					)}
+				</CardTitle>
+			</CardHeader>
+			<DataTable
+				state={state}
+				onSortingChange={setSorting}
+				loading={isLoading}
+				linkFormatter={rowLink}
+				columns={COLUMNS}
+				data={pageRows ?? ([] as unknown as Pool[])}
+				pagination={true}
+				onPaginationChange={setPaginationState}
+			/>
+		</Card>
+	);
+};
