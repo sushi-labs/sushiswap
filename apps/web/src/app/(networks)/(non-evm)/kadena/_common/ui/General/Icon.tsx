0cc8f5a82a515a5918d937d0f99d80d0f85581b4
solidityteam <tyler@solidity.io>
2025-06-20 15:06:45 -0400

    fix: use correct kda logo

--- apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/General/Icon.tsx
+++ apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/General/Icon.tsx
@@ -1,55 +1,49 @@
-import { cloudinaryLogoFetchLoader } from '@sushiswap/ui'
-import Image from 'next/image'
-import { hashStringToColor } from '~kadena/_common/lib/utils/formatters'
-import type { KadenaToken } from '~kadena/_common/types/token-type'
-import type { TempToken } from '../Pools/PoolPosition/PoolPosition'
+import { cloudinaryLogoFetchLoader } from "@sushiswap/ui";
+import Image from "next/image";
+import { hashStringToColor } from "~kadena/_common/lib/utils/formatters";
+import type { KadenaToken } from "~kadena/_common/types/token-type";
+import type { TempToken } from "../Pools/PoolPosition/PoolPosition";
+import { KadenaCircle } from "@sushiswap/ui/icons/network/circle/KadenaCircle";
 
 type IconProps = {
-  currency: KadenaToken | undefined | TempToken
-  height?: number
-  width?: number
-  fontSize?: number
-}
+	currency: KadenaToken | undefined | TempToken;
+	height?: number;
+	width?: number;
+	fontSize?: number;
+};
 
-export const Icon = ({
-  currency,
-  height = 40,
-  width = 40,
-  fontSize = 12,
-}: IconProps) => {
-  return (
-    <>
-      {currency?.tokenImage ? (
-        <div
-          style={{ width, height }}
-          className="relative flex overflow-hidden rounded-full shrink-0"
-        >
-          <Image
-            loader={cloudinaryLogoFetchLoader}
-            // @TODO: need this to render local images, remove when we have a CDN
-            unoptimized
-            src={currency.tokenImage}
-            alt={currency.tokenSymbol ?? currency.tokenName ?? ''}
-            height={height}
-            width={width}
-            className="w-full h-full aspect-square"
-          />
-        </div>
-      ) : (
-        <div
-          className="flex items-center justify-center text-xs font-bold text-white uppercase rounded-full bg-gradient-to-b from-gray-300 to-gray-200 dark:from-blue-700 dark:to-blue-900"
-          style={{
-            width: `${width}px`,
-            height: `${height}px`,
-            background: hashStringToColor(
-              currency ? `${currency.tokenSymbol} ${currency.tokenName}` : '??',
-            ),
-            fontSize: `${fontSize}px`,
-          }}
-        >
-          {currency?.tokenSymbol?.substring(0, 2) ?? '??'}
-        </div>
-      )}
-    </>
-  )
-}
+export const Icon = ({ currency, height = 40, width = 40, fontSize = 12 }: IconProps) => {
+	if (currency?.tokenAddress === "coin") {
+		return <KadenaCircle height={height} width={width} />;
+	}
+
+	return (
+		<>
+			{currency?.tokenImage ? (
+				<div style={{ width, height }} className="relative flex overflow-hidden rounded-full shrink-0">
+					<Image
+						loader={cloudinaryLogoFetchLoader}
+						// @TODO: need this to render local images, remove when we have a CDN
+						unoptimized
+						src={currency.tokenImage}
+						alt={currency.tokenSymbol ?? currency.tokenName ?? ""}
+						height={height}
+						width={width}
+						className="w-full h-full aspect-square"
+					/>
+				</div>
+			) : (
+				<div
+					className="flex items-center justify-center text-xs font-bold text-white uppercase rounded-full bg-gradient-to-b from-gray-300 to-gray-200 dark:from-blue-700 dark:to-blue-900"
+					style={{
+						width: `${width}px`,
+						height: `${height}px`,
+						background: hashStringToColor(currency ? `${currency.tokenSymbol} ${currency.tokenName}` : "??"),
+						fontSize: `${fontSize}px`,
+					}}>
+					{currency?.tokenSymbol?.substring(0, 2) ?? "??"}
+				</div>
+			)}
+		</>
+	);
+};
