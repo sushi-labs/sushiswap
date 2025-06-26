87f3b83b3e5af1b9308cfa7e2d461219d5e612d7
0xMasayoshi <0xMasayoshi@protonmail.com>
2025-06-27 05:35:57 +0700

    feat: katana bridge banner

--- apps/web/src/ui/swap/simple/simple-swap-banner.tsx
+++ apps/web/src/ui/swap/simple/simple-swap-banner.tsx
@@ -1,9 +1,13 @@
 import { StrapiBanner } from '../strapi-banner/strapi-banner'
-import { SimpleSwapBridgeBanner } from './simple-swap-bridge-banner'
+import {
+  KatanaBridgeBanner,
+  SimpleSwapBridgeBanner,
+} from './simple-swap-bridge-banner'
 
 export const SimpleSwapBanner = () => {
   return (
     <div className="flex flex-col gap-4 xl:fixed xl:right-8 xl:bottom-8">
+      <KatanaBridgeBanner className="h-24 min-w-[360px]" />
       <StrapiBanner className="h-24 min-w-[360px]" />
       <SimpleSwapBridgeBanner className="h-24 min-w-[360px]" />
     </div>
