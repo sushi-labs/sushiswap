ceab039d55b3a6bc2a2da6ae24c94bd8b3632ae6
0xMasayoshi <0xMasayoshi@protonmail.com>
2025-06-27 03:56:41 +0700

    feat: kat bg

--- apps/web/src/app/(networks)/(evm)/[chainId]/(trade)/layout.tsx
+++ apps/web/src/app/(networks)/(evm)/[chainId]/(trade)/layout.tsx
@@ -6,5 +6,14 @@ export default async function TradeLayout(props: {
 }) {
   const { children } = props
 
-  return <Providers>{children}</Providers>
+  return (
+    <Providers>
+      <div
+        className="fixed inset-0 -z-10 bg-no-repeat bg-cover bg-left
+          bg-[linear-gradient(180deg,_#FFF_0%,_rgba(255,255,255,0.80)_8.1%,_rgba(255,255,255,0)_100%),url('/katana-bg-light.jpg')]
+          dark:bg-[linear-gradient(180deg,_#0F172A_0%,_rgba(15,23,42,0.80)_8.1%,_rgba(15,23,42,0)_100%),url('/katana-bg-dark.jpg')]"
+      />
+      {children}
+    </Providers>
+  )
 }
