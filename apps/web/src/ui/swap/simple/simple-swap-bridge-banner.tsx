6adece41447890710711709d8c02e681e49005d9
0xMasayoshi <0xMasayoshi@protonmail.com>
2025-06-27 16:54:39 +0700

    style: kat bridge banner

--- apps/web/src/ui/swap/simple/simple-swap-bridge-banner.tsx
+++ apps/web/src/ui/swap/simple/simple-swap-bridge-banner.tsx
@@ -21,7 +21,9 @@ const BridgeInfo = {
   },
   [ChainId.KATANA]: {
     url: 'https://app.katana.network', // TODO
-    background: <div className="absolute inset-0 -z-10 bg-black/50" />,
+    background: (
+      <div className="absolute inset-0 -z-10 bg-black/80 dark:bg-black/50" />
+    ),
     textColor: 'text-white',
   },
 } as const
