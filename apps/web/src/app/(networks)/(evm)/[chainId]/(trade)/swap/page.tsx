4ffb149359199769cbe4ad0e4f283ab2ec1dd1ca
0xMasayoshi <0xMasayoshi@protonmail.com>
2025-06-28 16:44:53 +0700

    fix: widget width

--- apps/web/src/app/(networks)/(evm)/[chainId]/(trade)/swap/page.tsx
+++ apps/web/src/app/(networks)/(evm)/[chainId]/(trade)/swap/page.tsx
@@ -1,15 +1,22 @@
 import { Container } from '@sushiswap/ui'
 import { use } from 'react'
 import { SimpleSwapWidget } from 'src/ui/swap/simple/simple-swap-widget'
-import type { ChainId } from 'sushi/chain'
+import { ChainId } from 'sushi/chain'
 
 export default function SwapSimplePage(props: {
   params: Promise<{ chainId: string }>
 }) {
   const chainId = +use(props.params).chainId as ChainId
   return (
-    <Container maxWidth="lg" className="px-4">
-      <SimpleSwapWidget chainId={chainId} />
+    <Container
+      maxWidth="lg"
+      className={
+        chainId === ChainId.KATANA
+          ? '!max-w-[33rem] p-6 pt-0 bg-[rgba(255,255,255,0.8)] dark:bg-[rgba(15,23,42,0.8)] rounded-3xl backdrop-blur-2xl'
+          : 'px-4'
+      }
+    >
+      <SimpleSwapWidget />
     </Container>
   )
 }
