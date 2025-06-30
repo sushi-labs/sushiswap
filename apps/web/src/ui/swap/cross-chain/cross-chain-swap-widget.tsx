b85e9d01ffb062f4d68eb591708a3490c250114e
Matthew Lilley <hello@matthewlilley.com>
2025-06-30 16:35:14 +0100

    chore: responsive padding and remove route selector since we want best price always

--- apps/web/src/ui/swap/cross-chain/cross-chain-swap-widget.tsx
+++ apps/web/src/ui/swap/cross-chain/cross-chain-swap-widget.tsx
@@ -11,7 +11,7 @@ import { CrossChainSwapTradeStats } from './cross-chain-swap-trade-stats'
 
 export const CrossChainSwapWidget = () => {
   return (
-    <div className="flex flex-col gap-4 p-6 bg-[rgba(255,255,255,0.8)] dark:bg-[rgba(25,32,49,0.8)] rounded-3xl backdrop-blur-2xl">
+    <div className="flex flex-col gap-4 p-4 md:p-6 bg-[rgba(255,255,255,0.8)] dark:bg-[rgba(25,32,49,0.8)] rounded-3xl backdrop-blur-2xl">
       <div className="flex items-center justify-between">
         <SwapModeButtons />
         <CrossChainSwapSettingsOverlay />
