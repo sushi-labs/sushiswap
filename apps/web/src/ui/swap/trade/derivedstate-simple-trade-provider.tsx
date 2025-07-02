ddcb3f92c336f56729b471a6684303a2339c09ee
solidityteam <tyler@solidity.io>
2025-07-02 19:15:25 -0400

    fix: same chain trade for limit dca

--- apps/web/src/ui/swap/trade/derivedstate-simple-trade-provider.tsx
+++ apps/web/src/ui/swap/trade/derivedstate-simple-trade-provider.tsx
@@ -1,178 +1,134 @@
-'use client'
+"use client";
 
-import { useParams, usePathname, useSearchParams } from 'next/navigation'
+import { useParams, usePathname, useSearchParams } from "next/navigation";
+import { type FC, createContext, useCallback, useContext, useEffect, useMemo, useState } from "react";
+import { type SupportedChainId, isSupportedChainId } from "src/config";
+import { useCreateQuery } from "src/lib/hooks/useCreateQuery";
+import { EvmChainId } from "sushi/chain";
+import { EvmChainKey } from "sushi/chain";
 import {
-  type FC,
-  createContext,
-  useCallback,
-  useContext,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react'
-import { type SupportedChainId, isSupportedChainId } from 'src/config'
-import { useCreateQuery } from 'src/lib/hooks/useCreateQuery'
-import { EvmChainId } from 'sushi/chain'
-import { EvmChainKey } from 'sushi/chain'
-import {
-  TRADE_MODES,
-  TRADE_VIEWS,
-  type TradeMode,
-  type TradeView,
-  isSupportedTradeModeOnChainId,
-} from './config'
-import { TradeModeContext } from './trade-mode-buttons'
+	TRADE_MODES,
+	TRADE_VIEWS,
+	type TradeMode,
+	type TradeView,
+	isSupportedTradeModeOnChainId,
+} from "./config";
+import { TradeModeContext } from "./trade-mode-buttons";
 
 interface State {
-  mutate: {
-    setTradeMode(tradeMode: TradeMode): void
-    setTradeView(tradeView: TradeView): void
-  }
-  state: {
-    tradeView: TradeView
-    tradeMode: TradeMode
-    tradeModeChanged: boolean
-    chainId: EvmChainId
-  }
+	mutate: {
+		setTradeMode(tradeMode: TradeMode): void;
+		setTradeView(tradeView: TradeView): void;
+	};
+	state: {
+		tradeView: TradeView;
+		tradeMode: TradeMode;
+		tradeModeChanged: boolean;
+		chainId: EvmChainId;
+	};
 }
 
-const DerivedStateSimpleTradeContext = createContext<State>({} as State)
+const DerivedStateSimpleTradeContext = createContext<State>({} as State);
 
 interface DerivedStateSimpleTradeProviderProps {
-  children: React.ReactNode
+	children: React.ReactNode;
 }
 
 const resolveViewModeFromPathname = (pathname: string): TradeView => {
-  const view = pathname.split('/')?.[3]
-  return TRADE_VIEWS.includes(view as TradeView)
-    ? (view as TradeView)
-    : 'simple'
-}
-
-// const createUrl = (
-//   chainId: EvmChainId,
-//   view: TradeView,
-//   mode: TradeMode,
-// ): string => {
-//   const newPath = `/${EvmChainKey[chainId]}/${mode}${view !== 'simple' ? `/${view}` : ''}`
-//   return new URL(`${window.location.origin}${newPath}`).toString()
-// }
+	const view = pathname.split("/")?.[3];
+	return TRADE_VIEWS.includes(view as TradeView) ? (view as TradeView) : "simple";
+};
 
 /* Parses the URL and provides the tradeMode and chainId globally.
  * URL example:
  * /swap || /limit || /dca ...
  */
-const DerivedstateSimpleTradeProvider: FC<
-  DerivedStateSimpleTradeProviderProps
-> = ({ children }) => {
-  const pathname = usePathname()
-  const { chainId: _chainId, trade } = useParams()
-  const searchParams = useSearchParams()
-  const chainId0 = searchParams.get('chainId0')
-  const chainId = chainId0
-    ? (Number(chainId0) as EvmChainId)
-    : _chainId
-      ? (+_chainId as SupportedChainId)
-      : EvmChainId.ETHEREUM
-  const [tradeMode, _setTradeMode] = useState<TradeMode>(trade as TradeMode)
-  const [tradeView, _setTradeView] = useState<TradeView>(
-    resolveViewModeFromPathname(pathname),
-  )
-  const [tradeModeChanged, _setTradeModeChanged] = useState<boolean>(false)
+const DerivedstateSimpleTradeProvider: FC<DerivedStateSimpleTradeProviderProps> = ({ children }) => {
+	const pathname = usePathname();
+	const { chainId: _chainId, trade } = useParams();
+	const searchParams = useSearchParams();
+	const chainId0 = searchParams.get("chainId0");
+	const chainId = chainId0
+		? (Number(chainId0) as EvmChainId)
+		: _chainId
+		? (+_chainId as SupportedChainId)
+		: EvmChainId.ETHEREUM;
+	const [tradeMode, _setTradeMode] = useState<TradeMode>(trade as TradeMode);
+	const [tradeView, _setTradeView] = useState<TradeView>(resolveViewModeFromPathname(pathname));
+	const [tradeModeChanged, _setTradeModeChanged] = useState<boolean>(false);
 
-  const { createQuery } = useCreateQuery()
+	const { createQuery } = useCreateQuery();
 
-  const setTradeView = useCallback(
-    (view: TradeView) => {
-      if (view === tradeView) {
-        return
-      }
+	const setTradeView = useCallback(
+		(view: TradeView) => {
+			if (view === tradeView) {
+				return;
+			}
 
-      _setTradeView(view)
-      _setTradeModeChanged(false)
-      // window.history.pushState({}, "", createUrl(chainId, view, tradeMode));
-      createQuery(
-        [],
-        `/${pathname?.split('/')[1]}/${tradeMode}${view !== 'simple' ? `/${view}` : ''}`,
-      )
-    },
-    [tradeView, tradeMode, pathname, createQuery],
-  )
+			_setTradeView(view);
+			_setTradeModeChanged(false);
+			// window.history.pushState({}, "", createUrl(chainId, view, tradeMode));
+			createQuery([], `/${pathname?.split("/")[1]}/${tradeMode}${view !== "simple" ? `/${view}` : ""}`);
+		},
+		[tradeView, tradeMode, pathname, createQuery]
+	);
 
-  const setTradeMode = useCallback(
-    (trade: TradeMode) => {
-      _setTradeMode(trade)
-      _setTradeModeChanged(true)
-      createQuery(
-        [],
-        `/${pathname?.split('/')[1]}/${trade}${tradeView !== 'simple' ? `/${tradeView}` : ''}`,
-      )
-    },
-    [tradeView, pathname, createQuery],
-  )
-  // useEffect(() => {
-  //   const newTradeView = resolveViewModeFromPathname(pathname)
-  //   if (newTradeView !== tradeView) {
-  //     _setTradeView(newTradeView)
-  //     _setTradeModeChanged(false)
-  //     createQuery(
-  //       [],
-  //       `/${pathname?.split('/')[1]}/${tradeMode}${newTradeView !== 'simple' ? `/${newTradeView}` : ''}`,
-  //     )
-  //   }
-  // }, [pathname, tradeView, chainId, tradeMode])
+	const setTradeMode = useCallback(
+		(trade: TradeMode) => {
+			_setTradeMode(trade);
+			_setTradeModeChanged(true);
+			const params = [];
+			if (trade === "limit" || trade === "dca") {
+				params.push({ name: "token1", value: null });
+				params.push({ name: "chainId1", value: null });
+			}
 
-  return (
-    <DerivedStateSimpleTradeContext.Provider
-      value={useMemo(() => {
-        return {
-          mutate: {
-            setTradeMode,
-            setTradeView,
-          },
-          state: {
-            tradeView,
-            tradeMode,
-            tradeModeChanged,
-            chainId,
-          },
-        }
-      }, [
-        tradeMode,
-        tradeView,
-        chainId,
-        setTradeMode,
-        setTradeView,
-        tradeModeChanged,
-      ])}
-    >
-      <TradeModeContext.Provider
-        value={useMemo(
-          () => ({
-            tradeMode,
-            supportedTradeModes: TRADE_MODES.filter((item) =>
-              isSupportedTradeModeOnChainId(item, chainId),
-            ),
-            switchTradeMode: setTradeMode,
-          }),
-          [tradeMode, setTradeMode, chainId],
-        )}
-      >
-        {children}
-      </TradeModeContext.Provider>
-    </DerivedStateSimpleTradeContext.Provider>
-  )
-}
+			createQuery(
+				params,
+				`/${pathname?.split("/")[1]}/${trade}${tradeView !== "simple" ? `/${tradeView}` : ""}`
+			);
+		},
+		[tradeView, pathname, createQuery]
+	);
+
+	return (
+		<DerivedStateSimpleTradeContext.Provider
+			value={useMemo(() => {
+				return {
+					mutate: {
+						setTradeMode,
+						setTradeView,
+					},
+					state: {
+						tradeView,
+						tradeMode,
+						tradeModeChanged,
+						chainId,
+					},
+				};
+			}, [tradeMode, tradeView, chainId, setTradeMode, setTradeView, tradeModeChanged])}>
+			<TradeModeContext.Provider
+				value={useMemo(
+					() => ({
+						tradeMode,
+						supportedTradeModes: TRADE_MODES.filter((item) => isSupportedTradeModeOnChainId(item, chainId)),
+						switchTradeMode: setTradeMode,
+					}),
+					[tradeMode, setTradeMode, chainId]
+				)}>
+				{children}
+			</TradeModeContext.Provider>
+		</DerivedStateSimpleTradeContext.Provider>
+	);
+};
 
 const useDerivedStateSimpleTrade = () => {
-  const context = useContext(DerivedStateSimpleTradeContext)
-  if (!context) {
-    throw new Error(
-      'Hook can only be used inside Simple Trade Derived State Context',
-    )
-  }
+	const context = useContext(DerivedStateSimpleTradeContext);
+	if (!context) {
+		throw new Error("Hook can only be used inside Simple Trade Derived State Context");
+	}
 
-  return context
-}
+	return context;
+};
 
-export { DerivedstateSimpleTradeProvider, useDerivedStateSimpleTrade }
+export { DerivedstateSimpleTradeProvider, useDerivedStateSimpleTrade };
--- apps/web/src/ui/swap/trade/trade-mode-buttons.tsx
+++ apps/web/src/ui/swap/trade/trade-mode-buttons.tsx
@@ -1,90 +1,91 @@
-'use client'
+"use client";
 
-import { Button } from '@sushiswap/ui'
-import { usePathname } from 'next/navigation'
-import { createContext, useContext, useMemo } from 'react'
-import { useTradeMode } from 'src/lib/hooks/useTradeMode'
-import { TRADE_MODES, type TradeMode } from './config'
+import { Button } from "@sushiswap/ui";
+import { usePathname } from "next/navigation";
+import { createContext, useContext, useMemo } from "react";
+import { useTradeMode } from "src/lib/hooks/useTradeMode";
+import { TRADE_MODES, type TradeMode } from "./config";
+import { useCreateQuery } from "../../../lib/hooks/useCreateQuery";
 
 interface ContextState {
-  switchTradeMode(mode: TradeMode): void
-  supportedTradeModes?: TradeMode[]
-  tradeMode: TradeMode
+	switchTradeMode(mode: TradeMode): void;
+	supportedTradeModes?: TradeMode[];
+	tradeMode: TradeMode;
 }
 
 export const TradeModeContext = createContext<ContextState>({
-  switchTradeMode: () => {},
-  tradeMode: 'swap',
-} as ContextState)
+	switchTradeMode: () => {},
+	tradeMode: "swap",
+} as ContextState);
 
 interface TradeModeOption {
-  name: string
-  mode: TradeMode
-  active: boolean
-  onClick?: () => void
-  href?: string
+	name: string;
+	mode: TradeMode;
+	active: boolean;
+	onClick?: () => void;
+	href?: string;
 }
 
 const OPTION_NAMES: Record<TradeMode, string> = {
-  swap: 'Market',
-  limit: 'Limit',
-  dca: 'DCA',
-  //@DEV temp taking out cross chain swap for now to show full UI
-  // "cross-chain-swap": "Cross-Chain",
-  fiat: 'Buy Crypto',
-}
+	swap: "Market",
+	limit: "Limit",
+	dca: "DCA",
+	//@DEV temp taking out cross chain swap for now to show full UI
+	// "cross-chain-swap": "Cross-Chain",
+	fiat: "Buy Crypto",
+};
 
 const useTradeModeOptions = (): TradeModeOption[] => {
-  const context = useContext(TradeModeContext)
-  const modes = context.supportedTradeModes ?? [...TRADE_MODES]
+	const context = useContext(TradeModeContext);
+	const modes = context.supportedTradeModes ?? [...TRADE_MODES];
+	const { createQuery } = useCreateQuery();
 
-  const { tradeMode } = useTradeMode()
-  if (context.tradeMode === 'fiat') {
-    //only return swap and fiat
-    return modes
-      .filter((mode) => mode === 'swap' || mode === 'fiat')
-      .map((item) => ({
-        mode: item,
-        name: item === 'swap' ? 'Swap' : OPTION_NAMES[item],
-        active: item === tradeMode,
-        onClick: () => context.switchTradeMode(item),
-      }))
-  }
+	const { tradeMode } = useTradeMode();
+	if (context.tradeMode === "fiat") {
+		//only return swap and fiat
+		return modes
+			.filter((mode) => mode === "swap" || mode === "fiat")
+			.map((item) => ({
+				mode: item,
+				name: item === "swap" ? "Swap" : OPTION_NAMES[item],
+				active: item === tradeMode,
+				onClick: () => context.switchTradeMode(item),
+			}));
+	}
 
-  return useMemo(() => {
-    return modes.slice(0, modes.length - 1).map((item) => {
-      return {
-        mode: item,
-        name: OPTION_NAMES[item],
-        active: item === tradeMode,
-        onClick: () => context.switchTradeMode(item),
-      }
-    })
-  }, [modes, context, tradeMode])
-}
+	return useMemo(() => {
+		return modes.slice(0, modes.length - 1).map((item) => {
+			return {
+				mode: item,
+				name: OPTION_NAMES[item],
+				active: item === tradeMode,
+				onClick: () => context.switchTradeMode(item),
+			};
+		});
+	}, [modes, context, tradeMode]);
+};
 
 const TradeModeOptionButton = (item: TradeModeOption) => {
-  return (
-    <Button
-      size="sm"
-      variant={item.active ? 'secondary' : 'ghost'}
-      onClick={item.onClick}
-      href={item.href}
-      className={item.active ? '!bg-[#0000001F] dark:!bg-[#FFFFFF1F]' : ''}
-    >
-      {item.name}
-    </Button>
-  )
-}
+	return (
+		<Button
+			size="sm"
+			variant={item.active ? "secondary" : "ghost"}
+			onClick={item.onClick}
+			href={item.href}
+			className={item.active ? "!bg-[#0000001F] dark:!bg-[#FFFFFF1F]" : ""}>
+			{item.name}
+		</Button>
+	);
+};
 
 export const TradeModeButtons = () => {
-  const options = useTradeModeOptions()
+	const options = useTradeModeOptions();
 
-  return (
-    <div className="flex flex-wrap gap-2">
-      {options.map((option, index) => (
-        <TradeModeOptionButton key={index} {...option} />
-      ))}
-    </div>
-  )
-}
+	return (
+		<div className="flex flex-wrap gap-2">
+			{options.map((option, index) => (
+				<TradeModeOptionButton key={index} {...option} />
+			))}
+		</div>
+	);
+};
