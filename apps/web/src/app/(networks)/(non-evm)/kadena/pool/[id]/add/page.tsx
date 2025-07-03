860875c31fd35d39afec79e5990c4d2560fb5ae4
solidityteam <tyler@solidity.io>
2025-07-03 18:52:02 -0400

    linting

--- apps/web/src/app/(networks)/(non-evm)/kadena/pool/[id]/add/page.tsx
+++ apps/web/src/app/(networks)/(non-evm)/kadena/pool/[id]/add/page.tsx
@@ -1,32 +1,34 @@
-"use client";
+'use client'
 
-import { Container } from "@sushiswap/ui";
-import { use, useEffect } from "react";
-import { usePoolById } from "~kadena/_common/lib/hooks/use-pool-by-id";
-import { Manage } from "~kadena/_common/ui/Pools/Manage/Manage";
-import { PoolPosition } from "~kadena/_common/ui/Pools/PoolPosition/PoolPosition";
-import { PoolComposition } from "~kadena/_common/ui/Pools/PoolsV2/PoolComposition";
-import { useRemoveLiqDispatch } from "~kadena/_common/ui/Pools/Remove/pool-remove-provider";
+import { Container } from '@sushiswap/ui'
+import { use, useEffect } from 'react'
+import { usePoolById } from '~kadena/_common/lib/hooks/use-pool-by-id'
+import { Manage } from '~kadena/_common/ui/Pools/Manage/Manage'
+import { PoolPosition } from '~kadena/_common/ui/Pools/PoolPosition/PoolPosition'
+import { PoolComposition } from '~kadena/_common/ui/Pools/PoolsV2/PoolComposition'
+import { useRemoveLiqDispatch } from '~kadena/_common/ui/Pools/Remove/pool-remove-provider'
 
-export default function AddRemoveLiqPage(props: { params: Promise<{ id: string }> }) {
-	const params = use(props.params);
-	const poolId = params?.id;
-	const { data } = usePoolById({
-		poolId,
-		first: 1,
-	});
+export default function AddRemoveLiqPage(props: {
+  params: Promise<{ id: string }>
+}) {
+  const params = use(props.params)
+  const poolId = params?.id
+  const { data } = usePoolById({
+    poolId,
+    first: 1,
+  })
 
-	return (
-		<Container maxWidth="5xl" className="px-2 sm:px-4">
-			<div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-				<div className="flex flex-col gap-6">
-					<Manage />
-					<PoolComposition pool={data} />
-				</div>
-				<div className="flex flex-col gap-6">
-					<PoolPosition />
-				</div>
-			</div>
-		</Container>
-	);
+  return (
+    <Container maxWidth="5xl" className="px-2 sm:px-4">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+        <div className="flex flex-col gap-6">
+          <Manage />
+          <PoolComposition pool={data} />
+        </div>
+        <div className="flex flex-col gap-6">
+          <PoolPosition />
+        </div>
+      </div>
+    </Container>
+  )
 }
