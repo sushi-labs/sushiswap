c30f1cee56d1a84ce9b18738d17f0b698797b533
solidityteam <tyler@solidity.io>
2025-07-22 10:25:46 -0400

    feature: dex-75

--- apps/web/src/app/(networks)/(evm)/[chainId]/explore/pools/layout.tsx
+++ apps/web/src/app/(networks)/(evm)/[chainId]/explore/pools/layout.tsx
@@ -5,6 +5,7 @@ import { notFound } from 'next/navigation'
 import type React from 'react'
 import { POOL_SUPPORTED_NETWORKS } from 'src/config'
 import { GlobalStatsCharts } from 'src/ui/explore/global-stats-charts'
+import { ExploreHeader } from 'src/ui/explore/header'
 import { Statistics } from 'src/ui/explore/statistics'
 import { Trending } from 'src/ui/explore/trending/trending'
 import { PoolsFiltersProvider } from 'src/ui/pool'
@@ -36,9 +37,7 @@ export default async function ExploreLayout(props: {
       <Header chainId={chainId} supportedNetworks={POOL_SUPPORTED_NETWORKS} />
       <main className="flex flex-col flex-1 h-full animate-slide">
         <Container maxWidth="screen-3xl" className="px-4 py-4">
-          <h2 className="pb-4 text-lg font-semibold md:pb-8 md:text-4xl">
-            Explore Pool
-          </h2>
+          <ExploreHeader chainId={chainId} />
           <div className="flex flex-col gap-3 justify-between lg:flex-row">
             <Statistics />
             <Trending />
