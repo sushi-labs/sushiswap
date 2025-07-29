f77d3f0e0ceb7406f929740e51447849932ac779
Aaron Iker <aaroni@fun.xyz>
2025-07-29 16:42:36 +0200

    add provider and theming

--- apps/web/src/app/(networks)/layout.tsx
+++ apps/web/src/app/(networks)/layout.tsx
@@ -1,19 +1,24 @@
 import { BaseProviders, OnramperProvider } from '@sushiswap/ui'
 import { headers } from 'next/headers'
+import { FunkitConnectProvider } from 'src/providers/funkit-connect/provider'
 import { QueryClientProvider } from 'src/providers/query-client-provider'
 import { WagmiProvider } from 'src/providers/wagmi-provider'
 import { NewAppVersionDialog } from '../_common/app-version/new-app-version-dialog'
 
 export default async function Layout({
   children,
-}: { children: React.ReactNode }) {
+}: {
+  children: React.ReactNode
+}) {
   const cookie = (await headers()).get('cookie')
   return (
     <BaseProviders>
       <OnramperProvider>
         <QueryClientProvider>
           <NewAppVersionDialog />
-          <WagmiProvider cookie={cookie}>{children}</WagmiProvider>
+          <WagmiProvider cookie={cookie}>
+            <FunkitConnectProvider>{children}</FunkitConnectProvider>
+          </WagmiProvider>
         </QueryClientProvider>
       </OnramperProvider>
     </BaseProviders>
