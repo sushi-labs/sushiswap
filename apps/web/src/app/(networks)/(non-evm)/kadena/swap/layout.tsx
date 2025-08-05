6b5c43ef3c4bc86bce3bbfd8d3f2ac555fa95b60
solidityteam <tyler@solidity.io>
2025-08-05 14:26:04 -0400

    chore: header breakdown update

--- apps/web/src/app/(networks)/(non-evm)/kadena/swap/layout.tsx
+++ apps/web/src/app/(networks)/(non-evm)/kadena/swap/layout.tsx
@@ -13,7 +13,7 @@ export default function SwapLayout({
   return (
     <Providers>
       <Header />
-      <main className="lg:p-4 mt-16 mb-[86px]">{children}</main>
+      <main className="lg:p-4 mt-20 mb-[86px]">{children}</main>
     </Providers>
   )
 }
