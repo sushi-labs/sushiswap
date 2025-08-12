167e7d0148c3b4a44fec7396044534cc3a44ba15
solidityteam <tyler@solidity.io>
2025-08-12 14:57:17 -0400

    chore: rm old header from swap page

--- apps/web/src/app/(networks)/(non-evm)/kadena/swap/page.tsx
+++ apps/web/src/app/(networks)/(non-evm)/kadena/swap/page.tsx
@@ -14,7 +14,6 @@ import { useSimulateSwap } from '~kadena/_common/lib/hooks/use-simulate-swap'
 import { AmountIn } from '~kadena/_common/ui/Swap/AmountIn'
 import { AmountOut } from '~kadena/_common/ui/Swap/AmountOut'
 import { SimpleSwapBanner } from '~kadena/_common/ui/Swap/Banner/SwapBanner'
-import { Rate } from '~kadena/_common/ui/Swap/Rate'
 import { ReviewSwapDialog } from '~kadena/_common/ui/Swap/ReviewSwapDialog'
 import { SwapStats } from '~kadena/_common/ui/Swap/SwapStats'
 import { SwitchSwapDirection } from '~kadena/_common/ui/Swap/SwitchSwapDirection'
@@ -48,11 +47,6 @@ export default function SwapSimplePage() {
   return (
     <Container maxWidth="lg" className="px-4">
       <div className="flex flex-col gap-4">
-        <div className="flex flex-col gap-2 items-start mt-2 mb-4 sm:mt-10">
-          <h1 className={typographyVariants({ variant: 'h1' })}>Trade</h1>
-          <Rate />
-        </div>
-
         <div className="flex flex-col gap-4 p-4 md:p-6 bg-[rgba(255,255,255,0.8)] dark:bg-[rgba(25,32,49,0.8)] rounded-3xl backdrop-blur-2xl">
           <div className="flex justify-between items-center">
             <SwapModeButtons />
