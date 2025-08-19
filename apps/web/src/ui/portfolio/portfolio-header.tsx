5c7ac0d4cec435c0df684413f82ff6a4f8e13132
solidityteam <tyler@solidity.io>
2025-08-19 16:59:09 -0400

    feat: portfolio header mobile layout

--- apps/web/src/ui/portfolio/portfolio-header.tsx
+++ apps/web/src/ui/portfolio/portfolio-header.tsx
@@ -2,8 +2,10 @@ import { NetworkMenu } from '../swap/trade/favorite-recent/network-menu'
 
 export function PortfolioHeader() {
   return (
-    <div className="flex gap-3 items-center">
-      <span className="text-[32px] font-semibold">Portfolio Overview</span>
+    <div className="flex gap-3 justify-between items-center md:justify-start">
+      <span className="md:text-[32px] text-lg font-semibold">
+        Portfolio Overview
+      </span>
       <NetworkMenu
         triggerVariant="networks"
         className="!min-h-[36px] !h-[36px]"
