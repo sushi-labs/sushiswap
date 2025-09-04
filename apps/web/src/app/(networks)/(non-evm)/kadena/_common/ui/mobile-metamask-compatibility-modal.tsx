a00147407d2646895237fdbd0dd99501f2eace61
solidityteam <tyler@solidity.io>
2025-09-04 11:07:55 -0400

    chore: linting

--- apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/mobile-metamask-compatibility-modal.tsx
+++ apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/mobile-metamask-compatibility-modal.tsx
@@ -1,37 +1,45 @@
-"use client";
+'use client'
 
-import { useIsMounted, useIsSmScreen, useLocalStorage } from "@sushiswap/hooks";
-import { Card, Dialog, DialogContent, DialogHeader, DialogTitle } from "@sushiswap/ui";
+import { useIsMounted, useIsSmScreen, useLocalStorage } from '@sushiswap/hooks'
+import {
+  Card,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from '@sushiswap/ui'
 
 export const MobileMetamaskCompatibilityModal = () => {
-	const isMounted = useIsMounted();
-	const isSmallScreen = useIsSmScreen();
-	const [hasClosedBanner, closeBanner] = useLocalStorage(
-		"has-closed-kadena-mobile-compatibility-modal",
-		false
-	);
+  const isMounted = useIsMounted()
+  const isSmallScreen = useIsSmScreen()
+  const [hasClosedBanner, closeBanner] = useLocalStorage(
+    'has-closed-kadena-mobile-compatibility-modal',
+    false,
+  )
 
-	const handleCloseBanner = () => {
-		closeBanner(true);
-	};
+  const handleCloseBanner = () => {
+    closeBanner(true)
+  }
 
-	if (hasClosedBanner || !isMounted || !isSmallScreen) return null;
+  if (hasClosedBanner || !isMounted || !isSmallScreen) return null
 
-	return (
-		<Dialog open={true} onOpenChange={handleCloseBanner}>
-			<DialogContent>
-				<DialogHeader className="py-2.5">
-					<DialogTitle className="text-xl !text-left">Mobile Wallet Limitation</DialogTitle>
-				</DialogHeader>
-				<Card>
-					<div className="flex flex-col gap-4 p-4">
-						<span>
-							MetaMask is not supported on mobile when using Kadena. Use a desktop browser or another wallet
-							on mobile.
-						</span>
-					</div>
-				</Card>
-			</DialogContent>
-		</Dialog>
-	);
-};
+  return (
+    <Dialog open={true} onOpenChange={handleCloseBanner}>
+      <DialogContent>
+        <DialogHeader className="py-2.5">
+          <DialogTitle className="text-xl !text-left">
+            Mobile Wallet Limitation
+          </DialogTitle>
+        </DialogHeader>
+        <Card>
+          <div className="flex flex-col gap-4 p-4">
+            <span>
+              MetaMask is not supported on mobile when using Kadena. Use a
+              desktop browser or another wallet on mobile.
+            </span>
+          </div>
+        </Card>
+      </DialogContent>
+    </Dialog>
+  )
+}
