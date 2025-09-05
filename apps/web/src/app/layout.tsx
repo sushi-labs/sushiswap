20014e0321ed14da3dd1ac2db9dbb92ca5d1c360
0xMasayoshi <0xMasayoshi@protonmail.com>
2025-09-05 19:37:02 +0700

    feat: create UtilityButtons component

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,26 +1,11 @@
 import '@sushiswap/ui/index.css'
 
-import {
-  ArrowTopRightOnSquareIcon,
-  QuestionMarkCircleIcon,
-} from '@heroicons/react/20/solid'
 import { ToastContainer } from '@sushiswap/notifications'
-import {
-  IconButton,
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-  Separator,
-} from '@sushiswap/ui'
-import { BrowserCookieIcon } from '@sushiswap/ui/icons/BrowserCookieIcon'
-import { DiscordIcon } from '@sushiswap/ui/icons/DiscordIcon'
-import { GithubIcon } from '@sushiswap/ui/icons/GithubIcon'
-import { TwitterIcon } from '@sushiswap/ui/icons/TwitterIcon'
 import type { Metadata } from 'next'
 import { Inter, Roboto_Mono } from 'next/font/google'
 import type React from 'react'
-import { CookieDialogContainer } from './_common/cookies/cookie-dialog-container'
 import { Trackers } from './trackers'
+import { UtilityButtons } from './utility-buttons'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -80,60 +65,7 @@ export default function RootLayout({
       </head>
       <body className="min-h-screen flex flex-col">
         <ToastContainer />
-        <div className="fixed bottom-5 right-8 flex">
-          <CookieDialogContainer>
-            <IconButton
-              size="sm"
-              variant="ghost"
-              icon={BrowserCookieIcon}
-              name={'cookies'}
-              className="text-muted-foreground"
-            />
-          </CookieDialogContainer>
-          <Popover>
-            <PopoverTrigger>
-              <IconButton
-                size="sm"
-                variant="ghost"
-                icon={QuestionMarkCircleIcon}
-                name={'help'}
-                className="text-muted-foreground"
-              />
-            </PopoverTrigger>
-            <PopoverContent className="!p-0">
-              <div className="px-5 py-4">Support</div>
-              <Separator />
-              <div className="px-5 py-4 flex flex-col gap-3">
-                <a
-                  href="/academy"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="text-sm flex items-center gap-2"
-                >
-                  Sushi Academy
-                  <ArrowTopRightOnSquareIcon className="w-4 h-4" />
-                </a>
-                <Separator />
-                <a
-                  href="/faq"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="text-sm flex items-center gap-2"
-                >
-                  FAQ
-                  <ArrowTopRightOnSquareIcon className="w-4 h-4" />
-                </a>
-                <Separator />
-                <span>Socials</span>
-                <div className="flex gap-5">
-                  <GithubIcon width={18} height={18} />
-                  <DiscordIcon width={18} height={18} />
-                  <TwitterIcon width={18} height={18} />
-                </div>
-              </div>
-            </PopoverContent>
-          </Popover>
-        </div>
+        <UtilityButtons />
         {children}
         <Trackers />
       </body>
