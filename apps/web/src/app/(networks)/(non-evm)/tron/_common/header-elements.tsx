3c0ba2f8781e2039e8ca5d9bc17e5ae5352bde58
solidityteam <tyler@solidity.io>
2025-09-09 13:02:09 -0400

    chore: add back in href to tron header element

--- apps/web/src/app/(networks)/(non-evm)/tron/_common/header-elements.tsx
+++ apps/web/src/app/(networks)/(non-evm)/tron/_common/header-elements.tsx
@@ -20,6 +20,7 @@ export const headerElements: NavigationElement[] = [
   {
     show: 'desktop',
     type: NavigationElementType.Custom,
+    href: '/swap',
     item: (
       <NavigationMenuItem className={NavigationElementType.Custom}>
         <NavigationMenuTrigger>Trade</NavigationMenuTrigger>
