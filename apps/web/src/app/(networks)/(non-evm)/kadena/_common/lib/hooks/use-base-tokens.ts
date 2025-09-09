2c08dd89f60f61a90cffe5f4f7a74f4ae335f29e
solidityteam <tyler@solidity.io>
2025-09-09 13:16:22 -0400

    chore: add network in queryKey to avoid collisions, and rm unneeded comments

--- apps/web/src/app/(networks)/(non-evm)/kadena/_common/lib/hooks/use-base-tokens.ts
+++ apps/web/src/app/(networks)/(non-evm)/kadena/_common/lib/hooks/use-base-tokens.ts
@@ -8,28 +8,6 @@ const TOKEN_LIST_URL =
 const TOKEN_IMAGE_BASE_URL =
   'https://raw.githubusercontent.com/Mercatus-Kadena/kadena_tokens/refs/heads/main/'
 
-// type KadenaTokensApiResponse = {
-// 	success: boolean;
-// 	data: KadenaToken[];
-// };
-
-// export const useBaseTokens = () => {
-// 	return useQuery({
-// 		queryKey: ["base-tokens"],
-// 		queryFn: async (): Promise<KadenaToken[]> => {
-// 			const response = await fetch("/kadena/api/tokens");
-// 			const data = (await response.json()) as KadenaTokensApiResponse;
-// 			if (!data.success) {
-// 				throw new Error("Failed to fetch Kadena tokens");
-// 			}
-// 			return data.data;
-// 		},
-// 		staleTime: 1000 * 60 * 60 * 24,
-// 		gcTime: 30 * 60 * 1000,
-// 		refetchOnMount: false,
-// 		refetchOnWindowFocus: false,
-// 	});
-// };
 type RawTokenType = Record<
   string,
   {
@@ -60,7 +38,7 @@ type RawListReturnType = {
 
 export const useBaseTokens = () => {
   return useQuery({
-    queryKey: ['base-tokens'],
+    queryKey: ['base-tokens-kadena'],
     queryFn: async (): Promise<KadenaToken[]> => {
       const response = await fetch(TOKEN_LIST_URL)
       const data = await response.text()
