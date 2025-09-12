337b36e82c674c7a071c61945460683f01c35279
solidityteam <tyler@solidity.io>
2025-09-12 15:50:50 -0400

    chore: useCallback on select for my pos hook

--- apps/web/src/app/(networks)/(non-evm)/kadena/_common/lib/hooks/use-my-positions.ts
+++ apps/web/src/app/(networks)/(non-evm)/kadena/_common/lib/hooks/use-my-positions.ts
@@ -1,5 +1,6 @@
-import { useInfiniteQuery } from '@tanstack/react-query'
+import { type InfiniteData, useInfiniteQuery } from '@tanstack/react-query'
 import ms from 'ms'
+import { useCallback } from 'react'
 import type { WalletPosition } from '~kadena/_common/types/get-positions'
 import { useKadena } from '~kadena/kadena-wallet-provider'
 
@@ -19,20 +20,33 @@ export const useMyPositions = (pageSize = 50) => {
   const { activeAccount } = useKadena()
   const walletAddress = activeAccount?.accountName
 
+  const select = useCallback(
+    (
+      data: InfiniteData<
+        {
+          positions: WalletPosition[]
+          pageInfo: {
+            endCursor: string
+            hasNextPage: boolean
+          }
+          totalCount: number
+        },
+        string | null
+      >,
+    ) => {
+      const flat = data.pages.flatMap((p) => p.positions)
+      return { ...data, positions: flat }
+    },
+    [],
+  )
+
   return useInfiniteQuery({
     queryKey: ['kadena-wallet-positions', walletAddress],
     queryFn: async ({ pageParam = null }) => {
       const url = new URL('/kadena/api/positions', window.location.origin)
       url.searchParams.set('walletAddress', walletAddress!)
       url.searchParams.set('first', String(pageSize))
-      // console.log(
-      // 	"Fetching wallet positions for:",
-      // 	walletAddress,
-      // 	"with page size:",
-      // 	pageSize,
-      // 	"and pageParam:",
-      // 	pageParam
-      // );
+
       if (pageParam) url.searchParams.set('after', pageParam)
 
       const res = await fetch(url.toString())
@@ -52,10 +66,7 @@ export const useMyPositions = (pageSize = 50) => {
       return nextParam
     },
 
-    select: (data) => {
-      const flat = data.pages.flatMap((p) => p.positions)
-      return { ...data, positions: flat }
-    },
+    select,
     initialPageParam: null,
     staleTime: ms('10s'),
     enabled: !!walletAddress,
