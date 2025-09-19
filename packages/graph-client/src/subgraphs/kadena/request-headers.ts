63f44cb65bdbfa1e2ac0a1e1c1ab9c0029f708af
solidityteam <tyler@solidity.io>
2025-09-19 11:39:40 -0400

    chore: add back test key until env setup

--- packages/graph-client/src/subgraphs/kadena/request-headers.ts
+++ packages/graph-client/src/subgraphs/kadena/request-headers.ts
@@ -1,3 +1,5 @@
 export const KADENA_INDEXER_REQUEST_HEADERS = {
-  'X-API-KEY': `${process.env['KADENA_INDEXER_API_KEY']}`,
+  // "X-API-KEY": `${process.env["KADENA_INDEXER_API_KEY"]}`,
+  //@dev: test key until env is setup
+  'X-API-KEY': `nEIUObKGsL93Oj9zXwzP81xe3pK4GEiC7huYy5Fp`,
 }
