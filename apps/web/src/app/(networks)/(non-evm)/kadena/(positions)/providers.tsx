1c353d70b6ab89e296fd63aa27a31b4305c6eb32
solidityteam <tyler@solidity.io>
2025-09-23 15:35:41 -0400

    chore: clean up provider

--- apps/web/src/app/(networks)/(non-evm)/kadena/(positions)/providers.tsx
+++ apps/web/src/app/(networks)/(non-evm)/kadena/(positions)/providers.tsx
@@ -1,12 +1,7 @@
 'use client'
 
-import { Suspense } from 'react'
 import { PoolsFiltersProvider } from 'src/app/(networks)/_ui/pools-filters-provider'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <Suspense>
-      <PoolsFiltersProvider>{children}</PoolsFiltersProvider>
-    </Suspense>
-  )
+  return <PoolsFiltersProvider>{children}</PoolsFiltersProvider>
 }
