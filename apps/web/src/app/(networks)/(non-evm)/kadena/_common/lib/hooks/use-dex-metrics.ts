1ff0d7341d0b9ae339bb172396d538d5828f6ff6
solidityteam <tyler@solidity.io>
2025-09-26 12:52:17 -0400

    chore: move dex mertics call to route

--- apps/web/src/app/(networks)/(non-evm)/kadena/_common/lib/hooks/use-dex-metrics.ts
+++ apps/web/src/app/(networks)/(non-evm)/kadena/_common/lib/hooks/use-dex-metrics.ts
@@ -1,17 +1,41 @@
-import { getDexMetrics } from '@sushiswap/graph-client/kadena'
 import { useQuery } from '@tanstack/react-query'
 import ms from 'ms'
-import { KADENA_CONTRACT } from '~kadena/_common/constants/contracts'
+import { z } from 'zod'
+
+const statsResponseSchema = z.object({
+  volumeHistory: z.array(
+    z.object({
+      value: z.number(),
+      timestamp: z.string(),
+    }),
+  ),
+  tvlHistory: z.array(
+    z.object({
+      value: z.number(),
+      timestamp: z.string(),
+    }),
+  ),
+  totalVolumeUsd: z.number(),
+  currentTvlUsd: z.number(),
+  totalPools: z.number(),
+})
 
 export const useDexMetrics = () => {
   return useQuery({
     queryKey: ['kadena-dex-metrics'],
     queryFn: async () => {
-      const data = await getDexMetrics({
-        protocolAddress: KADENA_CONTRACT,
-      })
+      const url = new URL('/kadena/api/dex-metrics', window.location.origin)
 
+      const res = await fetch(url.toString())
+      const data = await res.json()
       if (!data) throw new Error('Failed to fetch DEX metrics')
+
+      const parsed = statsResponseSchema.safeParse(data)
+
+      if (!parsed.success) {
+        throw new Error('Failed to parse dex metrics response')
+      }
+
       return data
     },
     staleTime: ms('10s'),
---
+++ apps/web/src/app/(networks)/(non-evm)/kadena/api/dex-metrics/route.ts
@@ -0,0 +1,16 @@
+import { getDexMetrics } from '@sushiswap/graph-client/kadena'
+import { KADENA_CONTRACT } from '~kadena/_common/constants/contracts'
+
+export const revalidate = 600
+
+export async function GET() {
+  const data = await getDexMetrics({
+    protocolAddress: KADENA_CONTRACT,
+  })
+
+  return Response.json(data, {
+    headers: {
+      'Cache-Control': 'max-age=60, stale-while-revalidate=600',
+    },
+  })
+}
