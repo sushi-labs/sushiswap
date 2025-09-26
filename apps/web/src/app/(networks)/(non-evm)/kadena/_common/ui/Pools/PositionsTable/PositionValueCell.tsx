808bcfb2c2b3a7e1a2b207142cf6c23c94800816
solidityteam <tyler@solidity.io>
2025-09-26 14:02:55 -0400

    chore: use Amount and Fraction primitives

--- apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/Pools/PositionsTable/PositionValueCell.tsx
+++ apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/Pools/PositionsTable/PositionValueCell.tsx
@@ -1,25 +1,37 @@
 import type { WalletPosition } from '@sushiswap/graph-client/kadena'
-
 import { useMemo } from 'react'
-import { formatUSD } from 'sushi'
+import { Amount, Fraction, formatUSD } from 'sushi'
 import type { KvmTokenAddress } from 'sushi/kvm'
+import { KVM_PAIR_TOKEN } from '~kadena/_common/constants/pair'
+import { useLpBalance } from '~kadena/_common/lib/hooks/pools/use-lp-balance'
 import { usePoolFromTokens } from '~kadena/_common/lib/hooks/pools/use-pool-from-tokens'
+import { useKadena } from '~kadena/kadena-wallet-provider'
 
 export const PositionValueCell = ({ data }: { data: WalletPosition }) => {
+  const { activeAccount } = useKadena()
+  const address = activeAccount?.accountName || ''
   const { data: poolData } = usePoolFromTokens({
     token0: data.pair.token0.address as KvmTokenAddress,
     token1: data.pair.token1.address as KvmTokenAddress,
   })
-
-  const totalSupply = poolData?.poolData?.totalSupplyLp ?? 0
+  const { data: lpData } = useLpBalance({
+    account: address,
+    token0Address: data.pair.token0.address as KvmTokenAddress,
+    token1Address: data.pair.token1.address as KvmTokenAddress,
+  })
 
   const tvl = useMemo(() => {
-    const numerator = Number(data.liquidity) * Number(data.pair.tvlUsd)
-    const denominator = Number(totalSupply)
-
-    if (denominator === 0) return 0
-    return numerator / denominator
-  }, [data.liquidity, data.pair.tvlUsd, totalSupply])
+    const numerator = lpData?.balance.mulHuman(Number(data.pair.tvlUsd)).amount
+    const totalSupply = Amount.fromHuman(
+      KVM_PAIR_TOKEN,
+      poolData?.poolData?.totalSupplyLp ?? 0,
+    ).amount
+    if (!numerator) return '0'
+    if (!totalSupply) return '0'
+    return new Fraction({ numerator, denominator: totalSupply })?.toSignificant(
+      4,
+    )
+  }, [lpData, data.pair.tvlUsd, poolData])
 
   return (
     <div className="flex items-center gap-1">
