7576f81c166a22e9af9ccdb35472ec34d4f76357
solidityteam <tyler@solidity.io>
2025-10-06 15:15:41 -0400

    chore: rm unrelated code

--- apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/swap-mode-buttons.tsx
+++ apps/web/src/app/(networks)/(non-evm)/kadena/_common/ui/swap-mode-buttons.tsx
@@ -1,20 +1,10 @@
 'use client'
 
-import {
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from '@sushiswap/ui'
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@sushiswap/ui'
 import { ShuffleIcon } from '@sushiswap/ui/icons/ShuffleIcon'
 import Link from 'next/link'
-import { useParams } from 'next/navigation'
 import type { ReactNode } from 'react'
 import { PathnameButton } from 'src/app/_ui/pathname-button'
-import { isXSwapSupportedChainId } from 'src/config'
 
 const SwapModeButton = ({
   isSupported,
@@ -53,9 +43,6 @@ const SwapModeButton = ({
 }
 
 export const SwapModeButtons = () => {
-  const { chainId: _chainId } = useParams()
-  const chainId = +_chainId!
-
   return (
     <div className="flex flex-wrap gap-1 md:gap-2">
       <SwapModeButton isSupported={true} path={`/kadena/swap`}>
@@ -67,41 +54,17 @@ export const SwapModeButtons = () => {
       <SwapModeButton isSupported={false} path={`/kadena/dca`}>
         DCA
       </SwapModeButton>
-      <HoverCard>
-        <SwapModeButton isSupported={true} path={`/kadena/cross-chain-swap`}>
-          <HoverCardTrigger asChild>
-            <span className="flex gap-2 items-center text-transparent bg-clip-text bg-gradient-to-r saturate-200 from-blue to-pink">
-              <ShuffleIcon
-                width={20}
-                height={20}
-                className="hidden text-blue md:block"
-              />
-              Cross-Chain
-            </span>
-          </HoverCardTrigger>
-        </SwapModeButton>
-        {isXSwapSupportedChainId(chainId) ? (
-          <HoverCardContent className="!p-0 max-w-[320px]">
-            <CardHeader>
-              <CardTitle>Cross-Chain Swap</CardTitle>
-              <CardDescription>
-                Swap tokens natively across 15 chains including Ethereum,
-                Arbitrum, Optimism, Polygon, Base and more!
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <a
-                target="_blank"
-                className="text-sm text-blue hover:underline"
-                href="https://www.sushi.com/blog/sushixswap-v2"
-                rel="noreferrer"
-              >
-                Learn more.
-              </a>
-            </CardContent>
-          </HoverCardContent>
-        ) : null}
-      </HoverCard>
+
+      <SwapModeButton isSupported={true} path={`/kadena/cross-chain-swap`}>
+        <span className="flex gap-2 items-center text-transparent bg-clip-text bg-gradient-to-r saturate-200 from-blue to-pink">
+          <ShuffleIcon
+            width={20}
+            height={20}
+            className="hidden text-blue md:block"
+          />
+          Cross-Chain
+        </span>
+      </SwapModeButton>
     </div>
   )
 }
