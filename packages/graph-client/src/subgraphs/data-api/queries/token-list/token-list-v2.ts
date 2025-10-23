2331e00d796e4a37adea150718ee7d8531404718
solidityteam <tyler@solidity.io>
2025-10-23 10:57:42 -0400

    chore: update data api url

--- packages/graph-client/src/subgraphs/data-api/queries/token-list/token-list-v2.ts
+++ packages/graph-client/src/subgraphs/data-api/queries/token-list/token-list-v2.ts
@@ -41,8 +41,7 @@ export async function getTokenListV2(
   options?: RequestOptions,
 ) {
   // const url = `${SUSHI_DATA_API_HOST}/graphql`;
-  // const url = `https://data-api-feature-balance-v2.data-gcp.sushi.com/graphql`
-  const url = `https://data-api-154-merge.data-gcp.sushi.com/graphql`
+  const url = `https://data-api-feat-sushi2.data-gcp.sushi.com/graphql`
 
   const result = await request(
     {
