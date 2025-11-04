3f950624064921365a002015ef9a85e0341ea920
solidityteam <tyler@solidity.io>
2025-11-04 11:11:28 -0500

    fix: ModeSquare, and small amount math

--- packages/ui/src/icons/network/square/ModeSquare.tsx
+++ packages/ui/src/icons/network/square/ModeSquare.tsx
@@ -6,6 +6,6 @@ import { ModeNaked } from '../naked/ModeNaked'
 export const ModeSquare: IconComponent = (props) => (
   <ModeNaked
     {...props}
-    circle={<circle cx="128" cy="128" r="128" fill="#DFFE00" />}
+    circle={<rect width="256" height="256" fill="#DFFE00" />}
   />
 )
