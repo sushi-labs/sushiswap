cfd2a270ab34cf4639ef988b4a79ddd4cb702f28
solidityteam <tyler@solidity.io>
2025-11-05 11:28:12 -0500

    chore: bttc square icon

--- packages/ui/src/icons/network/square/BttcSquare.tsx
+++ packages/ui/src/icons/network/square/BttcSquare.tsx
@@ -4,4 +4,18 @@ import { BttcNaked } from '../naked/BttcNaked'
 
 import type { IconComponent } from '../../../types'
 
-export const BttcSquare: IconComponent = BttcNaked
+export const BttcSquare: IconComponent = (props) => {
+  return (
+    <BttcNaked
+      circle={
+        <rect
+          width="128"
+          height="128"
+          fill="currentColor"
+          className="!text-white dark:!text-gray-700"
+        />
+      }
+      {...props}
+    />
+  )
+}
