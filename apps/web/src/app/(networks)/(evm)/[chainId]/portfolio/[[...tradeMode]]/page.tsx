6d4cbfc5dfe39ed58412d7f966c68c66fa0e9638
solidityteam <tyler@solidity.io>
2025-11-12 12:45:00 -0400

    fix: mobile spacing

--- apps/web/src/app/(networks)/(evm)/[chainId]/portfolio/[[...tradeMode]]/page.tsx
+++ apps/web/src/app/(networks)/(evm)/[chainId]/portfolio/[[...tradeMode]]/page.tsx
@@ -3,7 +3,7 @@ import { PortfolioPage } from '../_ui/portfolio-page'
 
 export default function Portfolio() {
   return (
-    <main className="lg:p-4 md:p-2 pt-9 mb-6 animate-slide dark:bg-background bg-background">
+    <main className="lg:p-4 md:p-2 pt-4 mb-6 animate-slide dark:bg-background bg-background">
       <Container
         maxWidth="screen-2xl"
         className="flex flex-col gap-6 px-4 md:pt-6"
