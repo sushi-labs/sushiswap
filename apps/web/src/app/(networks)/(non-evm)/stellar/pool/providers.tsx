c226c28e085cfa61178c29f432ef2aa2b14c7b9e
xBalbinus <xiangan9@gmail.com>
2025-12-10 11:33:32 -0500

    fix back issue

--- apps/web/src/app/(networks)/(non-evm)/stellar/pool/providers.tsx
+++ apps/web/src/app/(networks)/(non-evm)/stellar/pool/providers.tsx
@@ -1,12 +1,5 @@
 'use client'
 
-import { Suspense } from 'react'
-import { PoolsFiltersProvider } from 'src/ui/pool'
-
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <Suspense>
-      <PoolsFiltersProvider>{children}</PoolsFiltersProvider>
-    </Suspense>
-  )
+  return <>{children}</>
 }
