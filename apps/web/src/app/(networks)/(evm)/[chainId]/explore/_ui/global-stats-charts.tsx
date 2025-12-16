e9bb410c9179221db09e726b9273bd1910fc0f70
Ty-Sir <tyler@sushi.com>
2025-12-16 11:21:17 -0500

    chore: use a hook instead of unstable_cache

--- apps/web/src/app/(networks)/(evm)/[chainId]/explore/_ui/global-stats-charts.tsx
+++ apps/web/src/app/(networks)/(evm)/[chainId]/explore/_ui/global-stats-charts.tsx
@@ -1,9 +1,7 @@
-import {
-  getAnalyticsDayBuckets,
-  isPoolChainId,
-} from '@sushiswap/graph-client/data-api'
-import { unstable_cache } from 'next/cache'
+'use client'
+import { isPoolChainId } from '@sushiswap/graph-client/data-api'
 import { type FC, Suspense } from 'react'
+import { useAnalyticsDayBuckets } from 'src/lib/hooks/react-query'
 import { type EvmChainId, isBladeChainId } from 'sushi/evm'
 import { GlobalStatsLoading } from './global-stats-loading'
 import { TVLChart } from './tvl-chart'
@@ -17,32 +15,19 @@ export const GlobalStatsCharts: FC<{ chainId: EvmChainId }> = ({ chainId }) => {
   )
 }
 
-const _GlobalStatsCharts: FC<{ chainId: EvmChainId }> = async ({ chainId }) => {
-  const dayBuckets = await unstable_cache(
-    async () =>
-      isPoolChainId(chainId)
-        ? getAnalyticsDayBuckets({
-            chainId,
-          })
-        : {
-            v2: [],
-            v3: [],
-            blade: [],
-          },
-    ['dayBuckets', `${chainId}`],
-    {
-      revalidate: 60 * 15,
-    },
-  )()
-
-  const isLoading =
-    !dayBuckets.v2.length && !dayBuckets.v3.length && !dayBuckets.blade?.length
+const _GlobalStatsCharts: FC<{ chainId: EvmChainId }> = ({ chainId }) => {
+  const { data: dayBuckets, isLoading } = useAnalyticsDayBuckets({
+    chainId,
+    enabled: isPoolChainId(chainId),
+  })
 
   const showBlade = isBladeChainId(chainId)
 
-  return isLoading ? (
-    <GlobalStatsLoading chainId={chainId} />
-  ) : (
+  if (isLoading || !dayBuckets) {
+    return <GlobalStatsLoading chainId={chainId} />
+  }
+
+  return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-20 gap-y-10">
       <TVLChart chainId={chainId} data={dayBuckets} showBlade={showBlade} />
       <VolumeChart chainId={chainId} data={dayBuckets} showBlade={showBlade} />
