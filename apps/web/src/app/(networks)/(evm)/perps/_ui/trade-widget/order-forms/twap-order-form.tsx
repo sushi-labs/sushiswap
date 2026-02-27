ad31edbf19fd5d2d16bfbea33f14b1b2846618c3
Ty-Sir <tyler@sushi.com>
2026-02-27 17:30:21 -0500

    feat: spot twap orders

--- apps/web/src/app/(networks)/(evm)/perps/_ui/trade-widget/order-forms/twap-order-form.tsx
+++ apps/web/src/app/(networks)/(evm)/perps/_ui/trade-widget/order-forms/twap-order-form.tsx
@@ -1,15 +1,19 @@
+import { useAssetState } from '../asset-state-provider'
 import { OrderSizeInput } from './_common/order-size-input'
 import { Randomize } from './_common/randomize'
 import { ReduceOnly } from './_common/reduce-only'
 import { TwapRunningTimeInput } from './_common/twap-running-time-input'
 
 export const TwapOrderForm = () => {
+  const {
+    state: { asset },
+  } = useAssetState()
   return (
     <div className="flex flex-col gap-2">
       <OrderSizeInput />
       <TwapRunningTimeInput />
       <Randomize />
-      <ReduceOnly />
+      {asset?.marketType === 'perp' ? <ReduceOnly /> : null}
     </div>
   )
 }
