ffaf9d4d432994633b8cd94d8979e837a9708bb4
0xMasayoshi <0xMasayoshi@protonmail.com>
2026-03-03 10:32:01 +0700

    feat: add smart pools to /explore/pools

--- apps/web/src/app/(networks)/(evm)/[chainId]/explore/pools/page.tsx
+++ apps/web/src/app/(networks)/(evm)/[chainId]/explore/pools/page.tsx
@@ -1,10 +1,14 @@
-import { isPoolChainId } from '@sushiswap/graph-client/data-api'
+import {
+  isPoolChainId,
+  isSmartPoolChainId,
+} from '@sushiswap/graph-client/data-api'
 import { Container } from '@sushiswap/ui'
 import { notFound } from 'next/navigation'
 import React from 'react'
 import { TableFiltersNetwork } from 'src/app/(networks)/_ui/table-filters-network'
 import { TableFiltersSearchToken } from 'src/app/(networks)/_ui/table-filters-search-token'
 import { POOL_SUPPORTED_NETWORKS } from 'src/config'
+import { SteerSmartPoolsTable } from 'src/lib/steer/components/steer-smart-pools-table'
 import { PoolsTable } from '~evm/[chainId]/_ui/pools-table'
 import { TableFiltersFarmsOnly } from '~evm/[chainId]/_ui/table-filters-farms-only'
 import { TableFiltersPoolType } from '~evm/[chainId]/_ui/table-filters-pool-type'
@@ -23,6 +27,11 @@ export default async function PoolsPage(props: {
 
   return (
     <Container maxWidth="7xl" className="px-4">
+      {isSmartPoolChainId(chainId) ? (
+        <div className="mb-4">
+          <SteerSmartPoolsTable chainId={chainId} />
+        </div>
+      ) : null}
       <div className="flex flex-wrap gap-3 mb-4">
         <TableFiltersSearchToken />
         <TableFiltersPoolType />
