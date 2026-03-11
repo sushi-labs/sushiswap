f038c5d2504aa2d7b27991190c715da1ba86c175
Ty-Sir <tyler@sushi.com>
2026-03-11 17:48:50 -0400

    chore: fade in

--- apps/web/src/app/(networks)/(evm)/perps/_ui/landing/perps-landing-page.tsx
+++ apps/web/src/app/(networks)/(evm)/perps/_ui/landing/perps-landing-page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { motion } from 'framer-motion'
 import { Ellipses } from './ellipses'
 import { LargeSushiIcon } from './large-sushi-icon'
 import { SushiBrandLogo } from './sushi-brand-logo'
@@ -7,7 +9,12 @@ import { Title } from './title'
 export const PerpsLandingPage = () => {
   return (
     <main className="overflow-hidden">
-      <div className="my-20 px-4 md:px-6 md:my-32 flex items-center flex-col gap-3 md:gap-6">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0.2 }}
+        className="my-20 px-4 md:px-6 md:my-32 flex items-center flex-col gap-3 md:gap-6"
+      >
         <SushiBrandLogo className="w-32 md:w-44 h-12 text-[#2C2C2C] dark:text-white" />
         <Title />
         <p className="max-w-sm md:max-w-xl lg:max-w-[645px] text-center font-medium text-base md:text-lg lg:text-xl">
@@ -17,7 +24,7 @@ export const PerpsLandingPage = () => {
         <div className="mt-2 w-full">
           <TelegramForm />
         </div>
-      </div>
+      </motion.div>
       <LargeSushiIcon className="fixed z-[-1] -right-24 -bottom-20 md:-bottom-10 w-[80vw] md:w-[60vw] h-[80vh]" />
       <Ellipses />
     </main>
