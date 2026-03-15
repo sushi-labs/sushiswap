d5e0fe3514969c7932201b581018fed8c16fdd43
Ty-Sir <tyler@sushi.com>
2026-03-14 20:54:53 -0400

    chore: requested copy update

--- apps/web/src/app/(networks)/(evm)/perps/_ui/landing/title.tsx
+++ apps/web/src/app/(networks)/(evm)/perps/_ui/landing/title.tsx
@@ -1,7 +1,7 @@
 export const Title = () => {
   return (
-    <h1 className="text-center max-w-[1100px] text-5xl md:text-7xl lg:text-8xl font-lufga">
-      Perpetual futures, built on Sushi.
+    <h1 className="text-center max-w-[400px] md:max-w-[600px] lg:max-w-[710px] text-5xl md:text-7xl lg:text-8xl font-lufga">
+      Perps on Sushi Coming Soon
     </h1>
   )
 }
