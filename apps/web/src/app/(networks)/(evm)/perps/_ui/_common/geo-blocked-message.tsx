6167815c78d1decf27e01454a416f194f1b5ac9b
Ty-Sir <tyler@sushi.com>
2026-03-16 18:06:34 -0400

    chore: text color on geo block banner

--- apps/web/src/app/(networks)/(evm)/perps/_ui/_common/geo-blocked-message.tsx
+++ apps/web/src/app/(networks)/(evm)/perps/_ui/_common/geo-blocked-message.tsx
@@ -15,14 +15,17 @@ export const GeoBlockedMessage = ({
       data-blocked={isGeoBlocked || !data?.ipAllowed ? 'true' : 'false'}
       className="hidden data-[blocked=true]:block data-[blocked=true]:animate-slide"
     >
-      <Message variant="destructive" className="!p-3 !text-xs !rounded-md">
+      <Message
+        variant="destructive"
+        className="!p-3 !text-xs !rounded-md !text-red-200 border border-red/50 mx-1"
+      >
         You are accessing our products and services from a restricted
         jurisdiction. We do not allow access from certain jurisdictions
         including locations subject to sanctions restrictions and other
         jurisdictions where our services are ineligible for use. For more
         information, see our{' '}
         <LinkExternal href="/legal/terms-of-service">
-          <span className="!text-red underline">Terms of Use</span>
+          <span className="!text-red-200 underline">Terms of Use</span>
         </LinkExternal>
         . If you think this is an error, try refreshing the page or opening a
         support ticket.
