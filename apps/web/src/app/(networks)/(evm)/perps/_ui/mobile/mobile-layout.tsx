a3f1ac7141196f03f8d7c80706c80d5b2b3e6e07
Ty-Sir <tyler@sushi.com>
2026-03-28 20:46:09 -0400

    test removal of activity comp

--- apps/web/src/app/(networks)/(evm)/perps/_ui/mobile/mobile-layout.tsx
+++ apps/web/src/app/(networks)/(evm)/perps/_ui/mobile/mobile-layout.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { useLocalStorage } from '@sushiswap/hooks'
-import { Activity } from 'react'
 import { AssetSelectorAndStats } from '../_common'
 import { AccountManagement } from '../account-management'
 import { TradeTables } from '../trade-tables'
@@ -18,21 +17,13 @@ export const MobileLayout = () => {
   return (
     <div className="pb-[52px] w-full h-full min-h-[calc(100vh-96px)]">
       <div className="flex flex-col gap-1 pt-2">
-        <Activity mode={view === 'account' ? 'hidden' : 'visible'}>
-          <AssetSelectorAndStats />
-        </Activity>
-        <Activity mode={view === 'markets' ? 'visible' : 'hidden'}>
-          <TabbedView />
-        </Activity>
-        <Activity mode={view === 'trade' ? 'visible' : 'hidden'}>
-          <TradeWidget className="min-h-[380px]" />
-        </Activity>
-        <Activity mode={view === 'account' ? 'visible' : 'hidden'}>
+        {view !== 'account' ? <AssetSelectorAndStats /> : null}
+        {view === 'markets' ? <TabbedView /> : null}
+        {view === 'trade' ? <TradeWidget className="min-h-[380px]" /> : null}
+        {view === 'account' ? (
           <AccountManagement className="min-h-[calc(100vh-116px)] rounded-b-none justify-between" />
-        </Activity>
-        <Activity mode={view === 'account' ? 'hidden' : 'visible'}>
-          <TradeTables className="min-h-[300px]" />
-        </Activity>
+        ) : null}
+        {view !== 'account' ? <TradeTables className="min-h-[300px]" /> : null}
       </div>
       <FooterNav view={view} setView={setView} />
     </div>
