92e2adf5f13195dccc141d5522f22cfb5e1c6f2e
Ty-Sir <tyler@sushi.com>
2026-04-13 11:37:48 -0400

    chore: add geo block message in referral page

--- apps/web/src/app/(networks)/(evm)/perps/referrals/page.tsx
+++ apps/web/src/app/(networks)/(evm)/perps/referrals/page.tsx
@@ -1,10 +1,14 @@
 import { Container } from '@sushiswap/ui'
+import { GeoBlockedMessage } from '../_ui/_common'
 import { ReferralsPage } from './_ui/referrals-page'
 
 export default function PerpsReferralsPage() {
   return (
-    <Container maxWidth="7xl" className="px-2 pb-4 pt-6 md:px-4 md:pt-12">
-      <ReferralsPage />
-    </Container>
+    <div className="overflow-x-hidden">
+      <GeoBlockedMessage />
+      <Container maxWidth="7xl" className="px-2 pb-4 pt-6 md:px-4 md:pt-12">
+        <ReferralsPage />
+      </Container>
+    </div>
   )
 }
