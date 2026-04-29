1bd4e023d0fb4a7a638a0057140496f5b792ca83
Ty-Sir <tyler@sushi.com>
2026-04-29 17:57:38 -0400

    chore: rm perps landing

--- apps/web/src/app/(networks)/(evm)/perps/_ui/landing/title.tsx
+++
@@ -1,7 +0,0 @@
-export const Title = () => {
-  return (
-    <h1 className="text-center max-w-[400px] md:max-w-[600px] lg:max-w-[710px] text-5xl md:text-7xl lg:text-8xl font-lufga">
-      Perps on Sushi Coming Soon
-    </h1>
-  )
-}
--- packages/graph-client/src/subgraphs/perps-landing-api/constants.ts
+++
@@ -1,2 +0,0 @@
-export const PERPS_LANDING_API_GRAPHQL_URL =
-  'https://perps-landing-api-production.up.railway.app'
