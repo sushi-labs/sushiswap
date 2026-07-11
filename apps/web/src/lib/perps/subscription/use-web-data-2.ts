882b5912ec5f2de490c2f4324fe141a9152c7c6d
LufyCZ <lufy@sushi.com>
2026-07-11 14:37:24 +0200

    Fix/perps web data 2 polling (#2177)
    * chore: fix webData2 loading
    
    * fix: poll webData2 over HTTP

--- apps/web/src/lib/perps/subscription/use-web-data-2.ts
+++ apps/web/src/lib/perps/subscription/use-web-data-2.ts
@@ -1,64 +1,52 @@
 'use client'
-import {
-  type WebData2Event,
-  webData2,
-} from '@nktkas/hyperliquid/api/subscription'
-import { useQuery, useQueryClient } from '@tanstack/react-query'
-import { useEffect } from 'react'
+import { type WebData2Response, webData2 } from '@nktkas/hyperliquid/api/info'
+import { useQuery } from '@tanstack/react-query'
 import type { EvmAddress } from 'sushi/evm'
-import { hlWebSocketTransport } from '../transports'
+import { hlHttpTransport } from '../transports'
 import { type TokenBalance, isTokenBalance } from '../utils'
 
-type WebData2EventWithTokenBalances = Omit<WebData2Event, 'spotState'> & {
-  spotState?: Omit<NonNullable<WebData2Event['spotState']>, 'balances'> & {
+type WebData2EventWithTokenBalances = Omit<WebData2Response, 'spotState'> & {
+  spotState?: Omit<NonNullable<WebData2Response['spotState']>, 'balances'> & {
     balances: TokenBalance[]
   }
 }
 
+function filterTokenBalances(
+  event: WebData2Response,
+): WebData2EventWithTokenBalances {
+  const { spotState, ...rest } = event
+
+  return {
+    ...rest,
+    ...(spotState
+      ? {
+          spotState: {
+            ...spotState,
+            balances: spotState.balances.filter(isTokenBalance),
+          },
+        }
+      : {}),
+  }
+}
+
 export const useWebData2 = ({ address }: { address?: EvmAddress }) => {
-  const queryClient = useQueryClient()
   const query = useQuery<WebData2EventWithTokenBalances>({
     queryKey: ['useWebData2', address],
-    staleTime: Number.POSITIVE_INFINITY,
-    enabled: false,
-  })
+    queryFn: async () => {
+      if (!address) throw new Error('Address is required')
 
-  useEffect(() => {
-    if (!address) return
-    let unsubscribe: undefined | (() => Promise<void>) = undefined
-    ;(async () => {
-      const sub = await webData2(
-        { transport: hlWebSocketTransport },
+      const event = await webData2(
+        { transport: hlHttpTransport },
         { user: address },
-        (webData2Event) => {
-          queryClient.setQueryData<WebData2EventWithTokenBalances>(
-            ['useWebData2', address],
-            (_prevWebData2Event) => {
-              const { spotState, ...event } = webData2Event
-
-              return {
-                ...event,
-                ...(spotState
-                  ? {
-                      spotState: {
-                        ...spotState,
-                        balances: spotState.balances.filter(isTokenBalance),
-                      },
-                    }
-                  : {}),
-              }
-            },
-          )
-        },
       )
 
-      unsubscribe = sub.unsubscribe
-    })()
-
-    return () => {
-      void unsubscribe?.()
-    }
-  }, [queryClient, address])
+      return filterTokenBalances(event)
+    },
+    staleTime: Number.POSITIVE_INFINITY,
+    refetchInterval: 5_000,
+    refetchIntervalInBackground: true,
+    enabled: Boolean(address),
+  })
 
   const isReady = Boolean(query.data)
 
