12e9adb26136d0d7aecd58aeeeda525234273648
Ty-Sir <tyler@sushi.com>
2026-08-04 10:36:51 -0400

    fix: mainnet/non-mainnet reserve

--- apps/web/src/lib/wagmi/components/web3-input/Currency/native-balance-reserve.ts
+++ apps/web/src/lib/wagmi/components/web3-input/Currency/native-balance-reserve.ts
@@ -1,13 +1,18 @@
-import type { EvmChainId } from 'sushi/evm'
+import { EvmChainId } from 'sushi/evm'
 import { isEvmChainId } from 'sushi/evm'
 import type { SvmChainId } from 'sushi/svm'
+import { parseUnits } from 'viem'
 
-const EVM_NATIVE_RESERVE = 2n * 10n ** 15n // 0.002 native units
+const MAINNET_NATIVE_RESERVE = parseUnits('0.002', 18) // 0.002 native units
+const EVM_NATIVE_RESERVE = parseUnits('0.00004', 18) // 0.00004 native units
 const SOL_NATIVE_RESERVE = 10_000_000n // 0.01 SOL
 
 export function getNativeBalanceReserve(
   chainId: EvmChainId | SvmChainId,
 ): bigint {
+  if (chainId === EvmChainId.ETHEREUM) {
+    return MAINNET_NATIVE_RESERVE
+  }
   return isEvmChainId(chainId) ? EVM_NATIVE_RESERVE : SOL_NATIVE_RESERVE
 }
 
