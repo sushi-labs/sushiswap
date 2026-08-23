3da262769422a354c9cc0bcf9bc1b6910e5220a0
LufyCZ <me@lufy.cz>
2026-08-23 16:39:05 +0000

    fix(web): suspend dynamic token layout

--- apps/web/src/app/(networks)/(evm)/[chainId]/token/[address]/layout.tsx
+++ apps/web/src/app/(networks)/(evm)/[chainId]/token/[address]/layout.tsx
@@ -1,14 +1,25 @@
 import { isPoolChainId } from '@sushiswap/graph-client/data-api'
 import { notFound } from 'next/navigation'
+import { Suspense } from 'react'
 import { POOL_SUPPORTED_NETWORKS } from 'src/config'
 import { type EvmChainId, isEvmChainId } from 'sushi/evm'
 import { Header } from '../../header'
 import { Providers } from './providers'
 
-export default async function PoolLayout(props: {
+interface TokenLayoutProps {
   children: React.ReactNode
   params: Promise<{ chainId: string }>
-}) {
+}
+
+export default function TokenLayout(props: TokenLayoutProps) {
+  return (
+    <Suspense fallback={null}>
+      <TokenLayoutContent {...props} />
+    </Suspense>
+  )
+}
+
+async function TokenLayoutContent(props: TokenLayoutProps) {
   const params = await props.params
 
   const { children } = props
