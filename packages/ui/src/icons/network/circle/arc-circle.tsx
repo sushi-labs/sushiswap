c1b004f166b069cf7e8254b46287f4feca328975
Ty-Sir <tyler@sushi.com>
2026-09-16 11:07:19 -0400

    fix: network icon centering in circle

--- packages/ui/src/icons/network/circle/arc-circle.tsx
+++ packages/ui/src/icons/network/circle/arc-circle.tsx
@@ -5,12 +5,7 @@ export const ArcCircle: IconComponent = (props) => (
   <ArcNaked
     {...props}
     circle={
-      <rect
-        width={128}
-        height={128}
-        rx={64}
-        className="fill-white dark:fill-black"
-      />
+      <rect width={128} height={128} rx={64} className="fill-[#203155]" />
     }
   />
 )
