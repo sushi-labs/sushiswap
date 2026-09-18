cf56237d7845b1464017652f8e636f86d3b0a8d9
Ty-Sir <tyler@sushi.com>
2026-09-18 18:37:08 -0400

    fix: min h for bg color coverage

--- apps/web/src/app/(networks)/(evm)/strategic-reserve/layout.tsx
+++ apps/web/src/app/(networks)/(evm)/strategic-reserve/layout.tsx
@@ -14,7 +14,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Suspense fallback={null}>
       <Header chainId={EvmChainId.ETHEREUM} />
-      <div className="flex flex-col flex-1 overflow-y-auto animate-slide bg-gray-50 dark:bg-white/[0.02]">
+      <div className="flex flex-col flex-1 overflow-y-auto animate-slide min-h-[calc(100vh-56px)] bg-gray-50 dark:bg-white/[0.02]">
         <Container maxWidth="8xl" className="px-4 pt-8 md:pt-16 mb-6 md:mb-12">
           <Hero />
         </Container>
