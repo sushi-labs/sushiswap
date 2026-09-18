660f3d7da7305a4a4b56f52aea8b5e01f6f0d562
Ty-Sir <tyler@sushi.com>
2026-09-18 18:43:34 -0400

    fix: overflow and gap

--- apps/web/src/app/(networks)/(evm)/strategic-reserve/_ui/data-row.tsx
+++ apps/web/src/app/(networks)/(evm)/strategic-reserve/_ui/data-row.tsx
@@ -4,7 +4,7 @@ import { DataItem } from './data-item'
 
 export const DataRow = () => {
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-1 gap-2 md:gap-4 sm:grid-cols-2 lg:grid-cols-3">
       <DataItem
         label={'SUSHI in reserve'}
         value={'18,364.61'}
